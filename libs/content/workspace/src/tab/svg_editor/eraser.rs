d2225d1e25d6865a87f1f22a08468371f29d3462
ad-tra <aew31gcmuy@gmail.com>
2011-02-16 14:00:00 +0100

    remove comment

--- libs/content/workspace/src/tab/svg_editor/eraser.rs
+++ libs/content/workspace/src/tab/svg_editor/eraser.rs
@@ -133,12 +133,8 @@ impl Eraser {
             }
 
             self.draw_eraser_cursor(ui, painter, cursor_pos);
-            // if !cfg!(target_os = "ios") && !cfg!(target_os = "android") {}
 
             if ui.input(|i| i.pointer.primary_down()) {
-                // if cfg!(target_os = "ios") || cfg!(target_os = "android") {
-                //     self.draw_eraser_cursor(ui, painter, cursor_pos);
-                // }
                 return Some(EraseEvent::Start(cursor_pos));
             }
             if ui.input(|i| i.pointer.primary_released()) {
