45ce709bbc16c9aa0a08db76bb6807e39f7be111
Parth <parth@mehrotra.me>
2020-01-09 22:39:23 -0500

    added navigation and key generation

--- lib/encryption_helper.dart
+++ lib/encryption_helper.dart
@@ -5,34 +5,39 @@ import 'dart:typed_data';
 
 import "package:pointycastle/export.dart";
 
-AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> generateKeyPair() {
-  var bitLength = 2048;
+class EncryptionHelper {
 
-  // Create an RSA key generator and initialize it
-  final keyGen = RSAKeyGenerator()
-    ..init(ParametersWithRandom(
-        RSAKeyGeneratorParameters(BigInt.parse('65537'), bitLength, 64),
-        getSecureRandom()));
+  const EncryptionHelper();
 
-  // Use the generator
-  final pair = keyGen.generateKeyPair();
+  AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> generateKeyPair() {
+    var bitLength = 2048;
 
-  // Cast the generated key pair into the RSA key types
-  final myPublic = pair.publicKey as RSAPublicKey;
-  final myPrivate = pair.privateKey as RSAPrivateKey;
+    // Create an RSA key generator and initialize it
+    final keyGen = RSAKeyGenerator()
+      ..init(ParametersWithRandom(
+          RSAKeyGeneratorParameters(BigInt.parse('65537'), bitLength, 64),
+          getSecureRandom()));
 
-  return AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey>(myPublic, myPrivate);
-}
+    // Use the generator
+    final pair = keyGen.generateKeyPair();
 
-SecureRandom getSecureRandom() {
-  final secureRandom = FortunaRandom();
+    // Cast the generated key pair into the RSA key types
+    final myPublic = pair.publicKey as RSAPublicKey;
+    final myPrivate = pair.privateKey as RSAPrivateKey;
 
-  final seedSource = Random.secure();
-  final seeds = <int>[];
-  for (int i = 0; i < 32; i++) {
-    seeds.add(seedSource.nextInt(255));
+    return AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey>(myPublic, myPrivate);
   }
-  secureRandom.seed(KeyParameter(Uint8List.fromList(seeds)));
 
-  return secureRandom;
+  SecureRandom getSecureRandom() {
+    final secureRandom = FortunaRandom();
+
+    final seedSource = Random.secure();
+    final seeds = <int>[];
+    for (int i = 0; i < 32; i++) {
+      seeds.add(seedSource.nextInt(255));
+    }
+    secureRandom.seed(KeyParameter(Uint8List.fromList(seeds)));
+
+    return secureRandom;
+  }
 }
