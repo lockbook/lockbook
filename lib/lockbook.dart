9769bcde78cabde722c1a661ab35a532e3a70950
Parth <parth@mehrotra.me>
2020-01-12 15:37:57 -0500

    conditionally show setup

--- lib/lockbook.dart
+++ lib/lockbook.dart
@@ -1,21 +1,34 @@
+import 'package:client/user_info.dart';
 import 'package:flutter/cupertino.dart';
 
 class Lockbook extends StatelessWidget {
+  final UserInfo _userInfo;
+
+  const Lockbook(this._userInfo);
+
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      home: LockbookHome(),
+      home: LockbookHome(_userInfo),
       theme: CupertinoThemeData(brightness: Brightness.dark),
     );
   }
 }
 
 class LockbookHome extends StatefulWidget {
+  final UserInfo _userInfo;
+
+  const LockbookHome(this._userInfo);
+
   @override
-  State<StatefulWidget> createState() => _LockbookState();
+  State<StatefulWidget> createState() => _LockbookState(_userInfo);
 }
 
 class _LockbookState extends State<LockbookHome> {
+  final UserInfo _userInfo;
+
+  _LockbookState(this._userInfo);
+
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
@@ -23,9 +36,7 @@ class _LockbookState extends State<LockbookHome> {
         home: CupertinoPageScaffold(
           navigationBar: CupertinoNavigationBar(
             backgroundColor: Color(0xff1C1C1E),
-            middle: const Text(
-              'Lockbook',
-            ),
+            middle: Text(_userInfo.username),
             trailing: GestureDetector(
               onTap: () => print("create"),
               child: Icon(
