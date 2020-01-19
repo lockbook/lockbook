9eb5aaf034319707729b6370316752b3298b8c00
Parth <parth@mehrotra.me>
2020-01-18 21:01:39 -0500

    theme editor

---
+++ lib/editor.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quill_delta/quill_delta.dart';
+import 'package:zefyr/zefyr.dart';
+
+import 'main.dart';
+
+class EditorPage extends StatefulWidget {
+  @override
+  EditorPageState createState() => EditorPageState();
+}
+
+class EditorPageState extends State<EditorPage> {
+  /// Allows to control the editor and the document.
+  ZefyrController _controller;
+
+  FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    // Here we must load the document and pass it to Zefyr controller.
+    final document = _loadDocument();
+    _controller = ZefyrController(document);
+    _focusNode = FocusNode();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Note that the editor requires special `ZefyrScaffold` widget to be
+    // one of its parents.
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          decoration: InputDecoration(hintText: 'Name File'),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: ZefyrScaffold(
+        child: ZefyrTheme(
+          child: ZefyrEditor(
+            padding: EdgeInsets.all(16),
+            controller: _controller,
+            focusNode: _focusNode,
+          ), data: zefyrTheme(),
+        ),
+      ),
+    );
+  }
+
+  /// Loads the document to be edited in Zefyr.
+  NotusDocument _loadDocument() {
+    // For simplicity we hardcode a simple document with one line of text
+    // saying "Zefyr Quick Start".
+    // (Note that delta must always end with newline.)
+    final Delta delta = Delta()..insert("Zefyr Quick Start\n");
+    return NotusDocument.fromDelta(delta);
+  }
+}
