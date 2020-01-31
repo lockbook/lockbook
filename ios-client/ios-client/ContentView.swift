ca4dbdef2fdde8d6ca335d6e0e38fd92304d7bbc
Parth <parth@mehrotra.me>
2020-01-30 19:47:31 -0500

    ios client

---
+++ ios-client/ios-client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-client
+//
+//  Created by Parth Mehrotra on 1/30/20.
+//  Copyright © 2020 Lockbook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
