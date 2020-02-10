512250af60bd83daa8bd1948f5b695056fdb8b2c
Parth <parth@mehrotra.me>
2020-02-09 20:23:09 -0500

    welcome screen done

---
+++ ios_client/ios_client/Views/MonokaiButton.swift
@@ -0,0 +1,29 @@
+//
+//  MonokaiButton.swift
+//  ios_client
+//
+//  Created by Parth Mehrotra on 2/9/20.
+//  Copyright © 2020 Lockbook. All rights reserved.
+//
+
+import SwiftUI
+
+struct MonokaiButton: View {
+    var text: String
+    var body: some View {
+        Text(text)
+            .frame(minWidth: 250)
+            .padding(10)
+            .background(Monokai.yellow)
+            .accentColor(Monokai.black)
+            .padding(.bottom, 25)
+            .frame(minWidth: 250)
+
+    }
+}
+
+struct MonokaiButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MonokaiButton(text: "test")
+    }
+}
