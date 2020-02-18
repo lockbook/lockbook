066278139a9309af054cabeddf2ea85346a4be5a
Parth <parth@mehrotra.me>
2020-02-17 20:38:08 -0500

    sorted out navigationlink

---
+++ ios_client/ios_client/Screens/NewLockbookView.swift
@@ -0,0 +1,24 @@
+//
+//  NewLockbookView.swift
+//  ios_client
+//
+//  Created by Parth Mehrotra on 2/9/20.
+//  Copyright © 2020 Lockbook. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewLockbookView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationBarTitle("New Lockbook")
+        
+    }
+}
+
+struct NewLockbookView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewLockbookView()
+    }
+}
