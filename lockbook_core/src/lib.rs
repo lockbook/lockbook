7e2491fa6ca0dea0810454aa210c7569b9ec12f4
Parth <parth@mehrotra.me>
2020-03-05 22:14:36 -0500

    account creation done

--- lockbook_core/src/lib.rs
+++ lockbook_core/src/lib.rs
@@ -1,30 +1,37 @@
+#![feature(try_trait)]
+extern crate base64;
 extern crate reqwest;
-
-mod state;
-mod db_provider;
-mod crypto;
-mod account_repo;
-mod account_service;
 
 use std::ffi::CStr;
 use std::os::raw::{c_char, c_int};
 use std::path::Path;
+
+use crate::account_repo::AccountRepoImpl;
 use crate::account_service::AccountServiceImpl;
 use crate::crypto::RsaCryptoService;
 use crate::db_provider::DbProviderImpl;
-use crate::account_repo::AccountRepoImpl;
+
+mod account_repo;
+mod account_service;
+mod crypto;
+mod db_provider;
+mod error_enum;
+mod state;
+mod account;
 
 static DB_NAME: &str = "lockbook.db3";
 
 type DefaultCrypto = RsaCryptoService;
 type DefaultDbProvider = DbProviderImpl;
+type DefaultAcountRepo = AccountRepoImpl<DefaultDbProvider>;
+type DefaultAcountService = AccountServiceImpl<DefaultCrypto, DefaultAcountRepo>;
 
 #[no_mangle]
 pub unsafe extern "C" fn is_db_present(path_c: *const c_char) -> c_int {
-    let path =
-        CStr::from_ptr(path_c).to_str()
-            .expect("Could not C String -> Rust String")
-            .to_string();
+    let path = CStr::from_ptr(path_c)
+        .to_str()
+        .expect("Could not C String -> Rust String")
+        .to_string();
 
     let db_path = path + "/" + DB_NAME;
 
@@ -37,7 +44,8 @@ pub unsafe extern "C" fn is_db_present(path_c: *const c_char) -> c_int {
 
 #[no_mangle]
 pub unsafe extern "C" fn create_account(c_username: *const c_char) -> c_int {
-    let username = CStr::from_ptr(c_username).to_str()
+    let username = CStr::from_ptr(c_username)
+        .to_str()
         .expect("Could not C String -> Rust String");
 
     println!("username: {}", username);
