309da6aac447e2c72df4bc863ca4f929b1bfff1c
Parth <parth@mehrotra.me>
2020-03-09 22:14:55 -0400

    added tests

--- lockbook_core/src/account.rs
+++ lockbook_core/src/account.rs
@@ -1,57 +1,7 @@
-use std::ops::Try;
-use std::option::NoneError;
-
-use openssl::pkey::Private;
-use openssl::rsa::Rsa;
-
-use crate::error_enum;
-
-#[derive(PartialEq, Debug)]
-pub struct PublicKey {
-    pub n: Vec<u8>,
-    pub e: Vec<u8>,
-}
-
-#[derive(PartialEq, Debug)]
-pub struct PrivateKey {
-    pub d: Vec<u8>,
-    pub p: Vec<u8>,
-    pub q: Vec<u8>,
-    pub dmp1: Vec<u8>,
-    pub dmq1: Vec<u8>,
-    pub iqmp: Vec<u8>,
-}
+use crate::crypto::KeyPair;
 
 #[derive(PartialEq, Debug)]
 pub struct Account {
     pub username: String,
-    pub public_key: PublicKey,
-    pub private_key: PrivateKey,
-}
-
-error_enum! {
-    enum Error {
-        KeyComponentMissing(NoneError)
-    }
-}
-
-impl Account {
-    pub fn new(username: String, keypair: Rsa<Private>) -> Result<Account, Error> {
-        let n = keypair.n().to_vec();
-        let e = keypair.e().to_vec();
-        let d = keypair.d().to_vec();
-        let p = keypair.p().into_result()?.to_vec();
-        let q = keypair.q().into_result()?.to_vec();
-        let dmp1 = keypair.dmp1().into_result()?.to_vec();
-        let dmq1 = keypair.dmq1().into_result()?.to_vec();
-        let iqmp = keypair.iqmp().into_result()?.to_vec();
-
-        Ok(
-            Account {
-                username,
-                public_key: PublicKey { n, e },
-                private_key: PrivateKey { d, p, q, dmp1, dmq1, iqmp },
-            }
-        )
-    }
+    pub keys: KeyPair,
 }
--- lockbook_core/src/account_repo.rs
+++ lockbook_core/src/account_repo.rs
@@ -1,15 +1,14 @@
 extern crate base64;
 
-use std::marker::PhantomData;
 use std::ops::Try;
 use std::option::NoneError;
 
-use base64::{decode, DecodeError, encode};
-use rusqlite::{Connection, params, Row};
+use base64::{decode, encode};
+use rusqlite::{Connection, params};
 
-use crate::account::{Account, PrivateKey, PublicKey};
 use crate::error_enum;
-use crate::state::Config;
+use crate::crypto::{KeyPair, PublicKey, PrivateKey};
+use crate::account::Account;
 
 error_enum! {
     enum Error {
@@ -26,7 +25,6 @@ pub trait AccountRepo {
 
 pub struct AccountRepoImpl;
 
-
 impl AccountRepo for AccountRepoImpl {
     fn insert_account(db: &Connection, account: &Account) -> Result<(), Error> {
         db.execute(
@@ -35,23 +33,22 @@ impl AccountRepo for AccountRepoImpl {
             values (0, ?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9)",
             params![
             &account.username,
-            encode(&account.public_key.n),
-            encode(&account.public_key.e),
-            encode(&account.private_key.d),
-            encode(&account.private_key.p),
-            encode(&account.private_key.q),
-            encode(&account.private_key.dmp1),
-            encode(&account.private_key.dmq1),
-            encode(&account.private_key.iqmp),
+            encode(&account.keys.public_key.n),
+            encode(&account.keys.public_key.e),
+            encode(&account.keys.private_key.d),
+            encode(&account.keys.private_key.p),
+            encode(&account.keys.private_key.q),
+            encode(&account.keys.private_key.dmp1),
+            encode(&account.keys.private_key.dmq1),
+            encode(&account.keys.private_key.iqmp),
             ]).unwrap();
 
         Ok(())
     }
 
     fn get_account(db: &Connection) -> Result<Account, Error> {
-        let mut stmt =
-            db.prepare(
-                "select
+        let mut stmt = db.prepare(
+            "select
                         username,
                         public_n,
                         public_e,
@@ -61,56 +58,56 @@ impl AccountRepo for AccountRepoImpl {
                         private_dmp1,
                         private_dmq1,
                         private_iqmp
-                    from user_info where id = 0"
-            )?;
+                    from user_info where id = 0",
+        )?;
 
         let mut user_iter = stmt.query_map(params![], |row| {
-            Ok(
-                AccountRow { // TODO this step should not be needed, why can't we clone the row and return it here?
-                    username: row.get(0)?,
-                    n: row.get(1)?,
-                    e: row.get(2)?,
-                    d: row.get(3)?,
-                    p: row.get(4)?,
-                    q: row.get(5)?,
-                    dmp1: row.get(6)?,
-                    dmq1: row.get(7)?,
-                    iqmp: row.get(8)?,
-                }
-            )
+            Ok(AccountRow {
+                // TODO this step should not be needed, why can't we clone the row and return it here?
+                username: row.get(0)?,
+                n: row.get(1)?,
+                e: row.get(2)?,
+                d: row.get(3)?,
+                p: row.get(4)?,
+                q: row.get(5)?,
+                dmp1: row.get(6)?,
+                dmq1: row.get(7)?,
+                iqmp: row.get(8)?,
+            })
         })?;
 
         let maybe_row = user_iter.next().into_result()?;
         let row = maybe_row?;
 
+        // TODO should we check for key validity here or let some subsequent operation fail?
         Ok(Account {
             username: row.username,
-            public_key: PublicKey {
-                n: decode(row.n.as_str())?,
-                e: decode(row.e.as_str())?,
-            },
-            private_key: PrivateKey {
-                d: decode(row.d.as_str())?,
-                p: decode(row.p.as_str())?,
-                q: decode(row.q.as_str())?,
-                dmp1: decode(row.dmp1.as_str())?,
-                dmq1: decode(row.dmq1.as_str())?,
-                iqmp: decode(row.iqmp.as_str())?,
+            keys: KeyPair {
+                public_key: PublicKey {
+                    n: decode(row.n.as_str())?,
+                    e: decode(row.e.as_str())?,
+                },
+                private_key: PrivateKey {
+                    d: decode(row.d.as_str())?,
+                    p: decode(row.p.as_str())?,
+                    q: decode(row.q.as_str())?,
+                    dmp1: decode(row.dmp1.as_str())?,
+                    dmq1: decode(row.dmq1.as_str())?,
+                    iqmp: decode(row.iqmp.as_str())?,
+                },
             },
         })
     }
 }
 
-
 #[cfg(test)]
-mod tests {
-    use rusqlite::params;
-
-    use crate::account::{Account, PrivateKey, PublicKey};
+mod unit_tests {
+    use crate::account::Account;
     use crate::account_repo::{AccountRepo, AccountRepoImpl};
     use crate::db_provider::{DbProvider, RamBackedDB};
     use crate::schema::SchemaCreatorImpl;
     use crate::state::Config;
+    use crate::crypto::{KeyPair, PublicKey, PrivateKey};
 
     type DefaultSchema = SchemaCreatorImpl;
     type DefaultDbProvider = RamBackedDB<DefaultSchema>;
@@ -120,18 +117,25 @@ mod tests {
     fn insert_account() {
         let test_account = Account {
             username: "parth".to_string(),
-            public_key: PublicKey { n: vec![1], e: vec![2] },
-            private_key: PrivateKey {
-                d: vec![3],
-                p: vec![4],
-                q: vec![5],
-                dmp1: vec![6],
-                dmq1: vec![7],
-                iqmp: vec![8],
+            keys: KeyPair {
+                public_key: PublicKey {
+                    n: vec![1],
+                    e: vec![2],
+                },
+                private_key: PrivateKey {
+                    d: vec![3],
+                    p: vec![4],
+                    q: vec![5],
+                    dmp1: vec![6],
+                    dmq1: vec![7],
+                    iqmp: vec![8],
+                },
             },
         };
 
-        let config = Config { writeable_path: "ignored".to_string() };
+        let config = Config {
+            writeable_path: "ignored".to_string(),
+        };
         let db = DefaultDbProvider::connect_to_db(config).unwrap();
         DefaultAcountRepo::insert_account(&db, &test_account).unwrap();
 
