3aa149be304d59496506e6df3e363b402c13ffe9
Travis Vanderstad <t.vanderstad@gmail.com>
2020-03-10 22:11:50 -0400

    adds index db creation script

---
+++ index_db/create_db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+	username	TEXT NOT NULL,
+	pub_key_n	TEXT NOT NULL,
+	pub_key_e	TEXT NOT NULL,
+	CONSTRAINT pk_users PRIMARY KEY (username)
+);
+
+CREATE TABLE files (
+	file_id					TEXT NOT NULL,
+	file_name				TEXT NOT NULL,
+	file_path				TEXT NOT NULL,
+	username				TEXT NOT NULL,
+	file_content_version	BIGINT NOT NULL,
+	file_metadata_version	BIGINT NOT NULL,
+	deleted					BOOLEAN,
+	CONSTRAINT pk_files PRIMARY KEY (file_id),
+	CONSTRAINT unique_file_path UNIQUE (file_path),
+	CONSTRAINT fk_files_username FOREIGN KEY (username) REFERENCES users(username)
+);
