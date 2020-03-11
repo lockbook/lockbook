ea6091d8fa974f8c2a752e87d222d097af0e8b22
Travis Vanderstad <t.vanderstad@gmail.com>
2020-03-10 20:52:01 -0400

    lockbook api create_file

--- lockbook_core/src/lockbook_api/mod.rs
+++ lockbook_core/src/lockbook_api/mod.rs
@@ -1,3 +1,5 @@
 pub mod new_account;
+pub mod create_file;
 
-pub use self::new_account::new_account;
+pub use self::new_account::{new_account, NewAccountParams, NewAccountError};
+pub use self::create_file::{create_file, CreateFileParams, CreateFileError};
