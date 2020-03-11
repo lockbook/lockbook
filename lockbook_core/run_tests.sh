53e21118fd35664330429a0ad47fb87185e582ae
Parth <parth@mehrotra.me>
2020-03-10 23:38:19 -0400

    added a lint check

--- lockbook_core/run_tests.sh
+++ lockbook_core/run_tests.sh
@@ -1,1 +1,8 @@
-cargo test -- --nocapture
+if cargo fmt --quiet -- --check ; then
+	cargo test -- --nocapture
+else
+	echo "The following files fail lint checks:"
+	cargo fmt -- --check -l
+	echo "Run cargo fmt" 
+	exit 1
+fi
