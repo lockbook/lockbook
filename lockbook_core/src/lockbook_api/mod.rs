511d01c80819bd6fdb93816cf1e0e74e2c698b35
Travis Vanderstad <t.vanderstad@gmail.com>
2020-03-10 21:18:41 -0400

    lockbook api move_file

--- lockbook_core/src/lockbook_api/mod.rs
+++ lockbook_core/src/lockbook_api/mod.rs
@@ -2,8 +2,10 @@ pub mod new_account;
 pub mod create_file;
 pub mod change_file_content;
 pub mod rename_file;
+pub mod move_file;
 
 pub use self::new_account::{new_account, NewAccountParams, NewAccountError};
 pub use self::create_file::{create_file, CreateFileParams, CreateFileError};
 pub use self::change_file_content::{change_file_content, ChangeFileContentParams, ChangeFileContentError};
 pub use self::rename_file::{rename_file, RenameFileParams, RenameFileError};
+pub use self::move_file::{move_file, MoveFileParams, MoveFileError};
