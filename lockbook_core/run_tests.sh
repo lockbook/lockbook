06e919d0c7d64dc41b62dcd02868033279a1f481
Parth <parth@mehrotra.me>
2020-03-12 19:54:47 -0400

    added crypto tests

--- lockbook_core/run_tests.sh
+++ lockbook_core/run_tests.sh
@@ -1,9 +1,11 @@
+export RUST_BACKTRACE=1;
+
 if cargo fmt --quiet -- --check ; then
 	cargo test -- --nocapture
 else
 	echo "The following files fail lint checks:"
 	cargo fmt -- --check -l
 	echo "Run cargo fmt" 
-	echo "Then re-run this script"
+	cargo test -- --nocapture
 	exit 1
 fi
