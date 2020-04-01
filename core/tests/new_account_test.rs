45f09052516a9f4a420e2832050a6feb25e7d9d1
Travis Vanderstad <t.vanderstad@gmail.com>
2020-03-31 21:58:31 -0400

    adds delete file file_deleted test (failing)

--- core/tests/new_account_test.rs
+++ core/tests/new_account_test.rs
@@ -609,3 +609,53 @@ fn test_delete_file_file_not_found() -> Result<(), TestError> {
         Err(e) => Err(TestError::DeleteFileError(e)),
     }
 }
+
+#[test]
+fn test_delete_file_file_deleted() -> Result<(), TestError> {
+    let username = generate_username();
+    let file_id = generate_file_id();
+
+    new_account(
+        api_loc(),
+        &NewAccountParams {
+            username: username.to_string(),
+            auth: "test_auth".to_string(),
+            pub_key_n: "test_pub_key_n".to_string(),
+            pub_key_e: "test_pub_key_e".to_string(),
+        },
+    )?;
+
+    create_file(
+        api_loc(),
+        &CreateFileParams {
+            username: username.to_string(),
+            auth: "test_auth".to_string(),
+            file_id: file_id.to_string(),
+            file_name: "file_name".to_string(),
+            file_path: "file_path".to_string(),
+            file_content: "file_content".to_string(),
+        },
+    )?;
+
+    delete_file(
+        api_loc(),
+        &DeleteFileParams {
+            username: username.to_string(),
+            auth: "test_auth".to_string(),
+            file_id: file_id.to_string(),
+        },
+    )?;
+
+    match delete_file(
+        api_loc(),
+        &DeleteFileParams {
+            username: username.to_string(),
+            auth: "test_auth".to_string(),
+            file_id: file_id.to_string(),
+        },
+    ) {
+        Err(DeleteFileError::FileDeleted) => Ok(()),
+        Ok(_) => Err(TestError::ErrorExpected),
+        Err(e) => Err(TestError::DeleteFileError(e)),
+    }
+}
