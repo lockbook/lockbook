eae90b7edc13290843f4da904ba5c67a7cc39b41
Smail <SmailBarkouch1@gmail.com>
2020-04-08 13:15:07 -0400

    moved hard coded auth to AuthService

--- core/src/account_api.rs
+++ core/src/account_api.rs
@@ -1,17 +1,21 @@
 extern crate reqwest;
 
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::SystemTime;
 
 use crate::account::Account;
-use crate::account_api::Error::{NetworkError, ServerUnavailable, UsernameTaken};
+use crate::account_api::Error::{NetworkError, ServerUnavailable, UsernameTaken, IncorrectUsername, ExpiredAuth};
 use crate::API_LOC;
 use crate::crypto::*;
+use crate::error_enum;
 
 #[derive(Debug)]
 pub enum Error {
     NetworkError(reqwest::Error),
     UsernameTaken,
+    IncorrectUsername,
     ServerUnavailable(u16),
+    ExpiredAuth,
+    DecryptionFailed(DecryptionError)
 }
 
 pub trait AccountApi {
@@ -19,6 +23,83 @@ pub trait AccountApi {
 }
 
 pub struct AccountApiImpl;
+
+pub trait AuthService {
+    fn verify_auth(
+        pub_key: PublicKey,
+        username: &String,
+        auth: &String,
+    ) -> Result<(), Error>;
+    fn verify_auth_comp(
+        auth_time: u128,
+        auth_username: &String,
+        real_username: &String,
+    ) -> Result<(), Error>;
+    fn generate_auth(
+        keys: &KeyPair,
+        username: &String,
+    ) -> Result<EncryptedValue, EncryptionError>;
+}
+
+struct AuthServiceImpl;
+
+impl AuthService for AuthServiceImpl {
+    fn verify_auth(
+        pub_key: PublicKey,
+        username: &String,
+        auth: &String,
+    ) -> Result<(), Error> {
+        let decrypt_val = RsaCryptoService::decrypt_public(
+            &PublicKey {
+                n: pub_key.n,
+                e: pub_key.e,
+            },
+            &EncryptedValue {
+                garbage: auth.clone(),
+            },
+        )?;
+
+        let decrypt_comp = decrypt_val.secret.split(",");
+
+        match AuthService::verify_auth_comp(decrypt_comp.next().parse::<u128>()?, username, decrypt_comp.next()) {
+            Ok(_) => Ok(()),
+            Err(e) => e
+        }
+    }
+
+    fn verify_auth_comp(
+        auth_time: u128,
+        auth_username: &String,
+        real_username: &String,
+    ) -> Result<(), Error> {
+        let real_time = SystemTime::now()
+            .as_millis();
+        let range = real_time - 50..real_time + 50;
+
+
+
+        if !range.contains(&auth_time) {
+            return Err(ExpiredAuth);
+        }
+        if real_username != auth_username {
+            return Err(IncorrectUsername);
+        }
+        Ok(())
+    }
+
+    fn generate_auth(
+        keys: &KeyPair,
+        username: &String,
+    ) -> Result<EncryptedValue, EncryptionError> {
+        let decrypted = format!("{},{}",
+                                username,
+                                SystemTime::now().as_millis().to_string());
+
+        CryptoService::encrypt_private(
+            keys,
+            &DecryptedValue { secret: decrypted })
+    }
+}
 
 impl From<reqwest::Error> for Error {
     fn from(e: reqwest::Error) -> Self {
@@ -28,14 +109,7 @@ impl From<reqwest::Error> for Error {
 
 impl AccountApi for AccountApiImpl {
     fn new_account(account: &Account) -> Result<(), Error> {
-        let decrypt_val = format!("{}{}{}",
-                                  &account.username,
-                                  ",",
-                                  SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis().to_string());
-
-        let auth = <RsaCryptoService as CryptoService>::encrypt_public( // shouldn't I encrypt private?
-            &account.keys.public_key,
-            &DecryptedValue { secret: decrypt_val }).unwrap().garbage;
+        let auth = AuthService::generate_auth(&account.keys, &account.username)?.garbage;
 
         let params = [
             ("hashed_username", &account.username),
