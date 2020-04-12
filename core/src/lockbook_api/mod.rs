24dee76e08ca1f2d411be2ab34184d6e1d742385
Travis Vanderstad <t.vanderstad@gmail.com>
2020-04-12 14:49:37 -0400

    cargo fmt

--- core/src/lockbook_api/mod.rs
+++ core/src/lockbook_api/mod.rs
@@ -6,10 +6,13 @@ pub mod move_file;
 pub mod new_account;
 pub mod rename_file;
 
-pub use self::change_file_content::{change_file_content, ChangeFileContentError, ChangeFileContentRequest, ChangeFileContentResponse};
+pub use self::change_file_content::{
+    change_file_content, ChangeFileContentError, ChangeFileContentRequest,
+    ChangeFileContentResponse,
+};
 pub use self::create_file::{create_file, CreateFileError, CreateFileRequest, CreateFileResponse};
 pub use self::delete_file::{delete_file, DeleteFileError, DeleteFileRequest, DeleteFileResponse};
-pub use self::get_updates::{get_updates, GetUpdatesError, GetUpdatesRequest, FileMetadata};
+pub use self::get_updates::{get_updates, FileMetadata, GetUpdatesError, GetUpdatesRequest};
 pub use self::move_file::{move_file, MoveFileError, MoveFileRequest, MoveFileResponse};
 pub use self::new_account::{new_account, NewAccountError, NewAccountRequest, NewAccountResponse};
 pub use self::rename_file::{rename_file, RenameFileError, RenameFileRequest, RenameFileResponse};
