f05ddd05e1a0061b14c331f232888456e11d437f
Travis Vanderstad <t.vanderstad@gmail.com>
2020-04-12 12:53:20 -0400

    factors test utils in separate file

--- core/tests/new_account_test.rs
+++ core/tests/new_account_test.rs
@@ -3,94 +3,14 @@ use lockbook_core::lockbook_api;
 use lockbook_core::lockbook_api::{ChangeFileContentError, ChangeFileContentRequest};
 use lockbook_core::lockbook_api::{CreateFileError, CreateFileRequest};
 use lockbook_core::lockbook_api::{DeleteFileError, DeleteFileRequest};
-use lockbook_core::lockbook_api::{FileMetadata, GetUpdatesError, GetUpdatesRequest};
+use lockbook_core::lockbook_api::{FileMetadata, GetUpdatesRequest};
 use lockbook_core::lockbook_api::{MoveFileError, MoveFileRequest};
 use lockbook_core::lockbook_api::{NewAccountError, NewAccountRequest};
 use lockbook_core::lockbook_api::{RenameFileError, RenameFileRequest};
-use std::env;
-use uuid::Uuid;
 
-fn api_loc() -> String {
-    match env::var("LOCKBOOK_API_LOCATION") {
-        Ok(s) => s,
-        Err(e) => panic!(
-            "Could not read environment variable LOCKBOOK_API_LOCATION: {}",
-            e
-        ),
-    }
-}
-
-fn generate_username() -> String {
-    Uuid::new_v4().to_string()
-}
-
-fn generate_file_id() -> String {
-    Uuid::new_v4().to_string()
-}
-
-macro_rules! assert_matches(
-    ($actual:expr, $expected:pat) => {
-        // Only compute actual once
-        let actual_value = $actual;
-        match actual_value {
-            $expected => {},
-            _ => panic!("assertion failed: {:?} did not match expectation", actual_value)
-        }
-    }
-);
-
-#[derive(Debug)]
-enum TestError {
-    NewAccountError(NewAccountError),
-    CreateFileError(CreateFileError),
-    ChangeFileContentError(ChangeFileContentError),
-    RenameFileError(RenameFileError),
-    MoveFileError(MoveFileError),
-    DeleteFileError(DeleteFileError),
-    GetUpdatesError(GetUpdatesError),
-}
-
-impl From<NewAccountError> for TestError {
-    fn from(e: NewAccountError) -> TestError {
-        TestError::NewAccountError(e)
-    }
-}
-
-impl From<CreateFileError> for TestError {
-    fn from(e: CreateFileError) -> TestError {
-        TestError::CreateFileError(e)
-    }
-}
-
-impl From<ChangeFileContentError> for TestError {
-    fn from(e: ChangeFileContentError) -> TestError {
-        TestError::ChangeFileContentError(e)
-    }
-}
-
-impl From<RenameFileError> for TestError {
-    fn from(e: RenameFileError) -> TestError {
-        TestError::RenameFileError(e)
-    }
-}
-
-impl From<MoveFileError> for TestError {
-    fn from(e: MoveFileError) -> TestError {
-        TestError::MoveFileError(e)
-    }
-}
-
-impl From<DeleteFileError> for TestError {
-    fn from(e: DeleteFileError) -> TestError {
-        TestError::DeleteFileError(e)
-    }
-}
-
-impl From<GetUpdatesError> for TestError {
-    fn from(e: GetUpdatesError) -> TestError {
-        TestError::GetUpdatesError(e)
-    }
-}
+#[macro_use]
+mod utils;
+use utils::{api_loc, generate_username, generate_file_id, TestError};
 
 fn new_account() -> Result<(), TestError> {
     lockbook_api::new_account(
