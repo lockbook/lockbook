e6e781e705da502b53c960b1b390759b519e0ca3
Smail <SmailBarkouch1@gmail.com>
2020-04-12 14:39:21 -0400

    made bare functions in lockbook_api folder into their own structs and traits

--- core/src/lockbook_api/create_file.rs
+++ core/src/lockbook_api/create_file.rs
@@ -28,38 +28,49 @@ struct CreateFileResponse {
     current_version: u64,
 }
 
-pub fn create_file(
-    api_location: String,
-    params: &CreateFileRequest,
-) -> Result<u64, CreateFileError> {
-    let client = Client::new();
-    let form_params = [
-        ("username", params.username.as_str()),
-        ("auth", params.auth.as_str()),
-        ("file_id", params.file_id.as_str()),
-        ("file_name", params.file_name.as_str()),
-        ("file_path", params.file_path.as_str()),
-        ("file_content", params.file_content.as_str()),
-    ];
-    let mut response = client
-        .post(format!("{}/create-file", api_location).as_str())
-        .form(&form_params)
-        .send()
-        .map_err(|err| CreateFileError::SendFailed(err))?;
+trait CreateFileClient {
+    fn create_file(
+        api_location: String,
+        params: &CreateFileRequest,
+    ) -> Result<u64, CreateFileError>;
+}
 
-    let response_body = response
-        .json::<CreateFileResponse>()
-        .map_err(|err| CreateFileError::ReceiveFailed(err))?;
+pub struct CreateFileClientImpl;
 
-    match (
-        response.status().as_u16(),
-        response_body.error_code.as_str(),
-    ) {
-        (200..=299, _) => Ok(response_body.current_version),
-        (401, "invalid_auth") => Err(CreateFileError::InvalidAuth),
-        (401, "expired_auth") => Err(CreateFileError::ExpiredAuth),
-        (422, "file_id_taken") => Err(CreateFileError::InvalidAuth),
-        (422, "file_path_taken") => Err(CreateFileError::ExpiredAuth),
-        _ => Err(CreateFileError::Unspecified),
+impl CreateFileClient for CreateFileClientImpl {
+    fn create_file(
+        api_location: String,
+        params: &CreateFileRequest,
+    ) -> Result<u64, CreateFileError> {
+        let client = Client::new();
+        let form_params = [
+            ("username", params.username.as_str()),
+            ("auth", params.auth.as_str()),
+            ("file_id", params.file_id.as_str()),
+            ("file_name", params.file_name.as_str()),
+            ("file_path", params.file_path.as_str()),
+            ("file_content", params.file_content.as_str()),
+        ];
+        let mut response = client
+            .post(format!("{}/create-file", api_location).as_str())
+            .form(&form_params)
+            .send()
+            .map_err(|err| CreateFileError::SendFailed(err))?;
+
+        let response_body = response
+            .json::<CreateFileResponse>()
+            .map_err(|err| CreateFileError::ReceiveFailed(err))?;
+
+        match (
+            response.status().as_u16(),
+            response_body.error_code.as_str(),
+        ) {
+            (200..=299, _) => Ok(response_body.current_version),
+            (401, "invalid_auth") => Err(CreateFileError::InvalidAuth),
+            (401, "expired_auth") => Err(CreateFileError::ExpiredAuth),
+            (422, "file_id_taken") => Err(CreateFileError::InvalidAuth),
+            (422, "file_path_taken") => Err(CreateFileError::ExpiredAuth),
+            _ => Err(CreateFileError::Unspecified),
+        }
     }
 }
--- core/src/lockbook_api/rename_file.rs
+++ core/src/lockbook_api/rename_file.rs
@@ -25,36 +25,47 @@ struct RenameFileResponse {
     error_code: String,
 }
 
-pub fn rename_file(
-    api_location: String,
-    params: &RenameFileRequest,
-) -> Result<(), RenameFileError> {
-    let client = Client::new();
-    let form_params = [
-        ("username", params.username.as_str()),
-        ("auth", params.auth.as_str()),
-        ("file_id", params.file_id.as_str()),
-        ("new_file_name", params.new_file_name.as_str()),
-    ];
-    let mut response = client
-        .put(format!("{}/rename-file", api_location).as_str())
-        .form(&form_params)
-        .send()
-        .map_err(|err| RenameFileError::SendFailed(err))?;
+trait RenameFileClient {
+    fn rename_file(
+        api_location: String,
+        params: &RenameFileRequest,
+    ) -> Result<(), RenameFileError>;
+}
 
-    match (
-        response.status().as_u16(),
-        response
-            .json::<RenameFileResponse>()
-            .map_err(|err| RenameFileError::ReceiveFailed(err))?
-            .error_code
-            .as_str(),
-    ) {
-        (200..=299, _) => Ok(()),
-        (401, "invalid_auth") => Err(RenameFileError::InvalidAuth),
-        (401, "expired_auth") => Err(RenameFileError::ExpiredAuth),
-        (404, "file_not_found") => Err(RenameFileError::FileNotFound),
-        (410, "file_deleted") => Err(RenameFileError::FileDeleted),
-        _ => Err(RenameFileError::Unspecified),
+pub struct RenameFileClientImpl;
+
+impl RenameFileClient for RenameFileClientImpl {
+    fn rename_file(
+        api_location: String,
+        params: &RenameFileRequest,
+    ) -> Result<(), RenameFileError> {
+        let client = Client::new();
+        let form_params = [
+            ("username", params.username.as_str()),
+            ("auth", params.auth.as_str()),
+            ("file_id", params.file_id.as_str()),
+            ("new_file_name", params.new_file_name.as_str()),
+        ];
+        let mut response = client
+            .put(format!("{}/rename-file", api_location).as_str())
+            .form(&form_params)
+            .send()
+            .map_err(|err| RenameFileError::SendFailed(err))?;
+
+        match (
+            response.status().as_u16(),
+            response
+                .json::<RenameFileResponse>()
+                .map_err(|err| RenameFileError::ReceiveFailed(err))?
+                .error_code
+                .as_str(),
+        ) {
+            (200..=299, _) => Ok(()),
+            (401, "invalid_auth") => Err(RenameFileError::InvalidAuth),
+            (401, "expired_auth") => Err(RenameFileError::ExpiredAuth),
+            (404, "file_not_found") => Err(RenameFileError::FileNotFound),
+            (410, "file_deleted") => Err(RenameFileError::FileDeleted),
+            _ => Err(RenameFileError::Unspecified),
+        }
     }
 }
