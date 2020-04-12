c7bfb368e1272bdd24649b613f24c9794ea15756
Smail <SmailBarkouch1@gmail.com>
2020-04-12 14:39:21 -0400

    made bare functions in lockbook_api folder into their own structs and traits

--- core/src/lockbook_api/change_file_content.rs
+++ core/src/lockbook_api/change_file_content.rs
@@ -28,40 +28,51 @@ struct ChangeFileContentResponse {
     current_version: u64,
 }
 
-pub fn change_file_content(
-    api_location: String,
-    params: &ChangeFileContentRequest,
-) -> Result<u64, ChangeFileContentError> {
-    let client = Client::new();
-    let form_params = [
-        ("username", params.username.as_str()),
-        ("auth", params.auth.as_str()),
-        ("file_id", params.file_id.as_str()),
-        ("old_file_version", &params.old_file_version.to_string()),
-        ("new_file_content", params.new_file_content.as_str()),
-    ];
-    let mut response = client
-        .put(format!("{}/change-file-content", api_location).as_str())
-        .form(&form_params)
-        .send()
-        .map_err(|err| ChangeFileContentError::SendFailed(err))?;
+trait FileContentClient {
+    fn change_file_content(
+        api_location: String,
+        params: &ChangeFileContentRequest,
+    ) -> Result<u64, ChangeFileContentError>;
+}
 
-    let response_body = response
-        .json::<ChangeFileContentResponse>()
-        .map_err(|err| ChangeFileContentError::ReceiveFailed(err))?;
+pub struct FileContentClientImpl;
 
-    match (
-        response.status().as_u16(),
-        response_body.error_code.as_str(),
-    ) {
-        (200..=299, _) => Ok(response_body.current_version),
-        (401, "invalid_auth") => Err(ChangeFileContentError::InvalidAuth),
-        (401, "expired_auth") => Err(ChangeFileContentError::ExpiredAuth),
-        (404, "file_not_found") => Err(ChangeFileContentError::FileNotFound),
-        (409, "edit_conflict") => Err(ChangeFileContentError::EditConflict(
-            response_body.current_version,
-        )),
-        (410, "file_deleted") => Err(ChangeFileContentError::FileDeleted),
-        _ => Err(ChangeFileContentError::Unspecified),
+impl FileContentClient for FileContentClientImpl {
+    fn change_file_content(
+        api_location: String,
+        params: &ChangeFileContentRequest,
+    ) -> Result<u64, ChangeFileContentError> {
+        let client = Client::new();
+        let form_params = [
+            ("username", params.username.as_str()),
+            ("auth", params.auth.as_str()),
+            ("file_id", params.file_id.as_str()),
+            ("old_file_version", &params.old_file_version.to_string()),
+            ("new_file_content", params.new_file_content.as_str()),
+        ];
+        let mut response = client
+            .put(format!("{}/change-file-content", api_location).as_str())
+            .form(&form_params)
+            .send()
+            .map_err(|err| ChangeFileContentError::SendFailed(err))?;
+
+        let response_body = response
+            .json::<ChangeFileContentResponse>()
+            .map_err(|err| ChangeFileContentError::ReceiveFailed(err))?;
+
+        match (
+            response.status().as_u16(),
+            response_body.error_code.as_str(),
+        ) {
+            (200..=299, _) => Ok(response_body.current_version),
+            (401, "invalid_auth") => Err(ChangeFileContentError::InvalidAuth),
+            (401, "expired_auth") => Err(ChangeFileContentError::ExpiredAuth),
+            (404, "file_not_found") => Err(ChangeFileContentError::FileNotFound),
+            (409, "edit_conflict") => Err(ChangeFileContentError::EditConflict(
+                response_body.current_version,
+            )),
+            (410, "file_deleted") => Err(ChangeFileContentError::FileDeleted),
+            _ => Err(ChangeFileContentError::Unspecified),
+        }
     }
 }
