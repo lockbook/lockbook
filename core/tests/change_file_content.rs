82bdab099a0c50bf9d5723765c996a02d1a01a18
Smail <SmailBarkouch1@gmail.com>
2020-04-21 19:16:15 -0400

    changes to integration tests

--- core/tests/change_file_content.rs
+++ core/tests/change_file_content.rs
@@ -1,32 +1,40 @@
 extern crate lockbook_core;
+
+use crate::utils::generate_account;
+
 use lockbook_core::client;
 use lockbook_core::client::CreateFileRequest;
 use lockbook_core::client::DeleteFileRequest;
 use lockbook_core::client::NewAccountRequest;
 use lockbook_core::client::{ChangeFileContentError, ChangeFileContentRequest};
 
+
 #[macro_use]
 pub mod utils;
 use utils::{api_loc, generate_file_id, generate_username, TestError};
+use lockbook_core::service::crypto_service::{RsaImpl, PubKeyCryptoService};
+use lockbook_core::service::auth_service::{AuthServiceImpl, AuthService};
+use lockbook_core::service::clock_service::ClockImpl;
 
 fn change_file_content() -> Result<(), TestError> {
-    let username = generate_username();
+    let account = generate_account();
+    let keys = RsaImpl::generate_key().unwrap();
     let file_id = generate_file_id();
 
     client::new_account(
         api_loc(),
         &NewAccountRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
-            public_key: "test_public_key".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
+            public_key: serde_json::to_string(&account.keys.to_public_key()).unwrap(),
         },
     )?;
 
     let old_file_version = client::create_file(
         api_loc(),
         &CreateFileRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
             file_id: file_id.to_string(),
             file_name: "file_name".to_string(),
             file_path: "file_path".to_string(),
@@ -37,8 +45,8 @@ fn change_file_content() -> Result<(), TestError> {
     client::change_file_content(
         api_loc(),
         &ChangeFileContentRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
             file_id: file_id.to_string(),
             old_file_version: old_file_version,
             new_file_content: "new_file_content".to_string(),
@@ -54,22 +62,22 @@ fn test_change_file_content() {
 }
 
 fn change_file_content_file_not_found() -> Result<(), TestError> {
-    let username = generate_username();
+    let account = generate_account();
 
     client::new_account(
         api_loc(),
         &NewAccountRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
-            public_key: "test_public_key".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
+            public_key: serde_json::to_string(&account.keys.to_public_key()).unwrap(),
         },
     )?;
 
     client::change_file_content(
         api_loc(),
         &ChangeFileContentRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
             file_id: generate_file_id(),
             old_file_version: 0,
             new_file_content: "new_file_content".to_string(),
@@ -90,23 +98,23 @@ fn test_change_file_content_file_not_found() {
 }
 
 fn change_file_content_edit_conflict() -> Result<(), TestError> {
-    let username = generate_username();
+    let account = generate_account();
     let file_id = generate_file_id();
 
     client::new_account(
         api_loc(),
         &NewAccountRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
-            public_key: "test_public_key".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
+            public_key: serde_json::to_string(&account.keys.to_public_key()).unwrap(),
         },
     )?;
 
     client::create_file(
         api_loc(),
         &CreateFileRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
             file_id: file_id.to_string(),
             file_name: "file_name".to_string(),
             file_path: "file_path".to_string(),
@@ -117,8 +125,8 @@ fn change_file_content_edit_conflict() -> Result<(), TestError> {
     client::change_file_content(
         api_loc(),
         &ChangeFileContentRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
             file_id: file_id.to_string(),
             old_file_version: 0,
             new_file_content: "new_file_content".to_string(),
@@ -137,23 +145,23 @@ fn test_change_file_content_edit_conflict() {
 }
 
 fn change_file_content_file_deleted() -> Result<(), TestError> {
-    let username = generate_username();
+    let account = generate_account();
     let file_id = generate_file_id();
 
     client::new_account(
         api_loc(),
         &NewAccountRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
-            public_key: "test_public_key".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
+            public_key: serde_json::to_string(&account.keys.to_public_key()).unwrap(),
         },
     )?;
 
     let old_file_version = client::create_file(
         api_loc(),
         &CreateFileRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
             file_id: file_id.to_string(),
             file_name: "file_name".to_string(),
             file_path: "file_path".to_string(),
@@ -164,8 +172,8 @@ fn change_file_content_file_deleted() -> Result<(), TestError> {
     client::delete_file(
         api_loc(),
         &DeleteFileRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
             file_id: file_id.to_string(),
         },
     )?;
@@ -173,8 +181,8 @@ fn change_file_content_file_deleted() -> Result<(), TestError> {
     client::change_file_content(
         api_loc(),
         &ChangeFileContentRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
             file_id: file_id.to_string(),
             old_file_version: old_file_version,
             new_file_content: "new_file_content".to_string(),
--- core/tests/get_updates.rs
+++ core/tests/get_updates.rs
@@ -1,4 +1,7 @@
 extern crate lockbook_core;
+
+use crate::utils::generate_account;
+
 use lockbook_core::client;
 use lockbook_core::client::CreateFileRequest;
 use lockbook_core::client::NewAccountRequest;
@@ -7,22 +10,27 @@ use lockbook_core::client::{FileMetadata, GetUpdatesRequest};
 #[macro_use]
 pub mod utils;
 use utils::{api_loc, generate_file_id, generate_username, TestError};
+use lockbook_core::service::auth_service::{AuthServiceImpl, AuthService};
+use lockbook_core::service::clock_service::ClockImpl;
+use lockbook_core::service::crypto_service::RsaImpl;
 
 fn get_updates(username: String, file_id: String) -> Result<(Vec<FileMetadata>, u64), TestError> {
+    let account = generate_account();
+
     client::new_account(
         api_loc(),
         &NewAccountRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
-            public_key: "test_public_key".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
+            public_key: serde_json::to_string(&account.keys.to_public_key()).unwrap(),
         },
     )?;
 
     let file_version = client::create_file(
         api_loc(),
         &CreateFileRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
             file_id: file_id.to_string(),
             file_name: "file_name".to_string(),
             file_path: "file_path".to_string(),
@@ -33,8 +41,8 @@ fn get_updates(username: String, file_id: String) -> Result<(Vec<FileMetadata>, 
     let updates_metadata = client::get_updates(
         api_loc(),
         &GetUpdatesRequest {
-            username: username.to_string(),
-            auth: "test_auth".to_string(),
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth( &account.keys, &account.username.clone()).unwrap(),
             since_version: 0,
         },
     )?;
@@ -44,10 +52,10 @@ fn get_updates(username: String, file_id: String) -> Result<(Vec<FileMetadata>, 
 
 #[test]
 fn test_get_updates() {
-    let username = generate_username();
+    let account = generate_account();
     let file_id = generate_file_id();
 
-    let updates_metadata_and_file_version = get_updates(username.to_string(), file_id.to_string());
+    let updates_metadata_and_file_version = get_updates(account.username.clone(), file_id.to_string());
     assert_matches!(&updates_metadata_and_file_version, &Ok(_));
     let (updates_metadata, file_version) = updates_metadata_and_file_version.unwrap();
     assert_eq!(
