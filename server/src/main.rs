df25ad59ff4436348faaf27c79726954bfdd5885
Travis Vanderstad <t.vanderstad@gmail.com>
2020-04-21 19:45:57 -0400

    adding hyper + tokio

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,8 @@
 extern crate rocket;
 extern crate base64;
 extern crate lockbook_core;
+extern crate hyper;
+extern crate tokio;
 
 pub mod api;
 pub mod config;
@@ -13,7 +15,7 @@ pub mod index_db;
 use crate::config::{config, ServerState};
 use std::sync::Mutex;
 
-fn main() {
+fn main_rocket() {
     let config = config();
     let index_db_client = match index_db::connect(&config.index_db_config) {
         Ok(client) => client,
@@ -44,4 +46,41 @@ fn main() {
             ],
         )
         .launch();
+}
+
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+
+async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::POST, "/echo") => Ok(Response::new(req.into_body())),
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // let config = config();
+    // let index_db_client = match index_db::connect(&config.index_db_config) {
+    //     Ok(client) => client,
+    //     Err(err) => panic!("{:?}", err),
+    // };
+    // let files_db_client = match files_db::connect(&config.files_db_config) {
+    //     Ok(x) => x,
+    //     Err(err) => panic!("{:?}", err),
+    // };
+    // let server_state = ServerState {
+    //     index_db_client: Mutex::new(index_db_client),
+    //     files_db_client: Mutex::new(files_db_client),
+    // };
+    let addr = ([127, 0, 0, 1], 3000).into();
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(echo)) });
+    let server = Server::bind(&addr).serve(service);
+    println!("Listening on http://{}", addr);
+    server.await?;
+    Ok(())
 }
