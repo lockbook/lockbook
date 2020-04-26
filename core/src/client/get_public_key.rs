b4cd53c7466954330558afc63ed928be6cc838d7
Smail <SmailBarkouch1@gmail.com>
2020-04-26 19:32:13 -0400

    made tests

--- core/src/client/get_public_key.rs
+++ core/src/client/get_public_key.rs
@@ -8,7 +8,7 @@ pub enum GetPublicKeyError {
     UsernameNotFound,
     SendFailed(ReqwestError),
     ParseError(FromUtf8Error),
-    ReceiveFailed
+    ReceiveFailed,
 }
 
 impl From<NoneError> for GetPublicKeyError {
@@ -24,7 +24,7 @@ impl From<FromUtf8Error> for GetPublicKeyError {
 }
 
 pub struct GetPublicKeyRequest {
-    pub username: String
+    pub username: String,
 }
 
 pub fn get_public_key(
@@ -33,21 +33,14 @@ pub fn get_public_key(
 ) -> Result<String, GetPublicKeyError> {
     let client = Client::new();
     let response = client
-        .get(
-            format!(
-                "{}/get-public-key/{}",
-                api_location, params.username
-            )
-                .as_str(),
-        )
+        .get(format!("{}/get-public-key/{}", api_location, params.username).as_str())
         .send()
         .map_err(|err| GetPublicKeyError::SendFailed(err))?;
 
     match response.status().as_u16() {
-        200..=299 => Ok(String::from_utf8(Vec::from(response
-            .headers()
-            .get("public_key")?
-            .as_bytes()))?),
+        200..=299 => Ok(String::from_utf8(Vec::from(
+            response.headers().get("public_key")?.as_bytes(),
+        ))?),
         _ => Err(GetPublicKeyError::UsernameNotFound),
     }
 }
