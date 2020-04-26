e3c50d2e064b1b3e06926d068ee529fe692e4442
Smail <SmailBarkouch1@gmail.com>
2020-04-26 19:32:13 -0400

    made tests

--- core/tests/get_public_key.rs
+++ core/tests/get_public_key.rs
@@ -1,8 +1,43 @@
+extern crate lockbook_core;
 
+use crate::utils::{api_loc, generate_account, TestError};
 
-fn get_public_key() -> Result<String, E>
+use lockbook_core::client;
+use lockbook_core::client::{GetPublicKeyRequest, NewAccountRequest};
+use lockbook_core::service::auth_service::{AuthService, AuthServiceImpl};
+use lockbook_core::service::clock_service::ClockImpl;
+use lockbook_core::service::crypto_service::RsaImpl;
+use rsa::RSAPrivateKey;
+
+pub mod utils;
+
+fn get_public_key(username: String, keys: RSAPrivateKey) -> Result<String, TestError> {
+    client::new_account(
+        api_loc(),
+        &NewAccountRequest {
+            username: username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth(&keys, &username.clone())
+                .unwrap(),
+            public_key: serde_json::to_string(&keys.to_public_key()).unwrap(),
+        },
+    )?;
+
+    let retrieved_key = client::get_public_key(
+        api_loc(),
+        &GetPublicKeyRequest {
+            username: username.clone(),
+        },
+    )?;
+
+    Ok(retrieved_key)
+}
 
 #[test]
 fn test_get_public_key() {
-    assert_eq!()
+    let account = generate_account();
+
+    assert_eq!(
+        get_public_key(account.username.clone(), account.keys.clone()),
+        account.keys.to_public_key()
+    );
 }
