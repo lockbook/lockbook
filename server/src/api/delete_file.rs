1f1168e2c081d6b0087caa9458ed0c8367643960
Smail Barkouch <SmailBarkouch@users.noreply.github.com>
2020-05-09 15:54:55 -0400

    get public key http endpoint (#54)
    * edited Cargo.toml
    
    * changed crypto functions and changed toml version based on edit for crates
    
    * working and more efficent auth check
    
    * moved hard coded auth to AuthService
    
    * fully implemented AuthService, though not neatly, still needs to be tested
    
    * attempted to get rid of DS_Store through gitignore
    
    * engrained authservice into AccountService
    
    * implemented unit test that passes
    
    * implemented unit test that passes
    
    * made test for auth service, checking for a failure due to time
    
    * changed core version for cargo
    
    * removed garbage files from idea via .gitignore
    
    * just added the new dependency to lockbook server
    
    * created auth_service.rs
    
    * added key components of the auth_service in its own function, with optimizations
    
    * included imports and adjusted error_enums
    
    * made the delay time for the auth_verification longer
    
    * moved unit tests into auth_server, have yet to be optimized
    
    * added implementations of some traits as well as tests
    
    * pulled from master and bumped version
    
    * made bare functions in lockbook_api folder into their own structs and traits
    
    * github glitching, test commit
    
    * fixed tests for the new impl classes utilised
    
    * fixed error check using a discrimant, checking variation of error enum
    
    * tests for auth_service that are available due to input created and pass
    
    * included new environment variables and more
    
    * fixed account_service.rs/create_account
    
    * made changes to auth_service errors as well auth_service functions themselves
    
    * seemingly finished auth, now time to fix the tests
    
    * auth service with correct functions, now truly implementing testing
    
    * implemented working tests
    
    * little changes to test
    
    * removed prints and some imports
    
    * implemented auth_service into dependency system and into account_service
    
    * cargo fmt
    
    * Implemented early clock and late clock for tests
    
    * Updated version of toml to update the crate for server
    
    * Optimized imports for server based on the new lockbook core imports
    
    * Little opimizations to lockbook core to reduce dependenies on server, now auth takes the publickey as a stringdue to serde_json
    
    * Optimized tests and trait for changes in verify auth parameters
    
    * Added auth verification to new_account request
    
    * switched to public key
    
    * cargo fmt
    
    * changes to tests as well as server
    
    * added todo
    
    * added println
    
    * println change
    
    * cross.toml
    
    * changes to integration tests
    
    * some changes to auth error time
    
    * changed version
    
    * made some changes to tests
    
    * fixed tests
    
    * cargo fmt
    
    * changes to build
    
    * changes to build
    
    * working get_public_key index_db and api version for server
    
    * added auth check to all http endpoints
    
    * made get_public_key client for core
    
    * made seemingly working public_key_client for core
    
    * made tests
    
    * cargo fmt
    
    * minor changes
    
    * fully working get_public_key http endpoint and indexdb
    
    * second working test
    
    * minor changes
    
    * fully working tests except for non implemented endpoints
    
    * some changes for more functional code
    
    * cargo fmt
    
    * made minor changes
    
    * made optimal types for environment variables
    
    * added get public key api endpoint docs
    
    * changes http returned codes for get public key
    
    * cargo fmt
    
    * optimizations and cargo fmt
    
    * modified integration test for get updates and http endpoint
    
    * little opimization to get_public_key test
    
    * removed all (except 1) warnings
    
    * cargo fmt
    
    Co-authored-by: Parth <parth@mehrotra.me>

--- server/src/api/delete_file.rs
+++ server/src/api/delete_file.rs
@@ -1,8 +1,12 @@
 use crate::api::utils::make_response_generic;
-use crate::config::ServerState;
+use crate::config::{config, ServerState};
 use crate::files_db;
 use crate::index_db;
 use lockbook_core::client::DeleteFileResponse;
+use lockbook_core::service::auth_service::{AuthService, AuthServiceImpl};
+use lockbook_core::service::clock_service::ClockImpl;
+use lockbook_core::service::crypto_service::RsaImpl;
+use rocket::http::Status;
 use rocket::request::Form;
 use rocket::Response;
 use rocket::State;
@@ -18,6 +22,28 @@ pub struct DeleteFile {
 pub fn delete_file(server_state: State<ServerState>, delete_file: Form<DeleteFile>) -> Response {
     let mut locked_index_db_client = server_state.index_db_client.lock().unwrap();
     let locked_files_db_client = server_state.files_db_client.lock().unwrap();
+
+    let public_key =
+        match index_db::get_public_key(&mut locked_index_db_client, &delete_file.username) {
+            Ok(public_key) => public_key,
+            Err(_) => return Response::build().status(Status::NotFound).finalize(),
+        };
+
+    if let Err(e) = AuthServiceImpl::<ClockImpl, RsaImpl>::verify_auth(
+        &delete_file.auth,
+        &public_key,
+        &delete_file.username,
+        config().auth_config.max_auth_delay,
+    ) {
+        println!(
+            "Auth failed for: {}, {}, {}, {:?}",
+            delete_file.username,
+            delete_file.auth,
+            &serde_json::to_string(&public_key).unwrap(),
+            e
+        );
+        return Response::build().status(Status::Unauthorized).finalize();
+    }
 
     let index_db_delete_file_result =
         index_db::delete_file(&mut locked_index_db_client, &delete_file.file_id);
