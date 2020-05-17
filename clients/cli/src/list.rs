969588eafcf5c74ca70d2e0f4b42582cb27e4a50
Parth Mehrotra <parth@mehrotra.me>
2020-05-17 16:17:20 -0400

    impl'd status and sync

--- clients/cli/src/list.rs
+++ clients/cli/src/list.rs
@@ -1,10 +1,7 @@
-use crate::utils::{connect_to_db, get_account};
+use crate::utils::{connect_to_db, get_account, print_last_successful_sync};
 use lockbook_core::repo::file_metadata_repo::FileMetadataRepo;
-use lockbook_core::service::clock_service::Clock;
-use lockbook_core::{DefaultClock, DefaultFileMetadataRepo};
 
-use chrono::Duration;
-use chrono_human_duration::ChronoHumanDuration;
+use lockbook_core::DefaultFileMetadataRepo;
 
 pub fn list() {
     let db = connect_to_db();
@@ -16,16 +13,5 @@ pub fn list() {
         .into_iter()
         .for_each(|metadata| println!("{}", metadata.file_name.trim()));
 
-    let last_updated = DefaultFileMetadataRepo::get_last_updated(&db)
-        .expect("Failed to retrieve content from FileMetadataRepo");
-
-    let duration = if last_updated != 0 {
-        let duration =
-            Duration::milliseconds((DefaultClock::get_time() as u64 - last_updated) as i64);
-        duration.format_human().to_string()
-    } else {
-        "never".to_string()
-    };
-
-    println!("Last updated: {}.", duration);
+    print_last_successful_sync(&db);
 }
