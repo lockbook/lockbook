59c5a0c0062cab80951804acb3810047bc481eeb
Travis Vanderstad <t.vanderstad@gmail.com>
2020-05-17 20:32:41 -0400

    Rewrite of client-server interface (#66)
    * removes api error structs
    
    * reduces code duplication in response creation
    
    * removes dead code
    
    * removes files_db errors that only had one variant
    
    * simplifies index_db::connect
    
    * cargo fmt
    
    * all server endpoints have request/response datatypes that derive all the things
    
    * adding hyper + tokio
    
    * makes data modeling great again
    
    * making the api great again
    
    * finishes create_file api
    
    * fighting the borrow checker
    
    * working...?
    
    * servicifies half the things
    
    * sample correct client (but server is broken)
    
    * sorts out async stuff but still not running
    
    * formatting
    
    * cargo fmt
    
    * I don't even know
    
    * async/await everywhere
    
    * fixes warnings
    
    * fixes clients, tests now passing
    
    * fixing merge conflict pt 2: finished merging core
    
    * fixing merge conflict pt 3: finished merging server
    
    Co-authored-by: Parth <parth@mehrotra.me>

---
+++ server/src/services/change_file_content.rs
@@ -0,0 +1,53 @@
+use crate::files_db;
+use crate::index_db;
+use crate::ServerState;
+use lockbook_core::model::api::{
+    ChangeFileContentError, ChangeFileContentRequest, ChangeFileContentResponse,
+};
+
+pub async fn handle(
+    server_state: &mut ServerState,
+    request: ChangeFileContentRequest,
+) -> Result<ChangeFileContentResponse, ChangeFileContentError> {
+    let update_file_version_result = index_db::update_file_version(
+        &mut server_state.index_db_client,
+        &request.file_id,
+        &(request.old_file_version as i64),
+    )
+    .await;
+    let new_version = match update_file_version_result {
+        Ok(new_version) => new_version,
+        Err(index_db::update_file_version::Error::Uninterpreted(_)) => {
+            println!("Internal server error! {:?}", update_file_version_result);
+            return Err(ChangeFileContentError::InternalError);
+        }
+        Err(index_db::update_file_version::Error::VersionGeneration(_)) => {
+            println!("Internal server error! {:?}", update_file_version_result);
+            return Err(ChangeFileContentError::InternalError);
+        }
+        Err(index_db::update_file_version::Error::FileDoesNotExist) => {
+            return Err(ChangeFileContentError::FileNotFound)
+        }
+        Err(index_db::update_file_version::Error::IncorrectOldVersion(_)) => {
+            return Err(ChangeFileContentError::EditConflict)
+        }
+        Err(index_db::update_file_version::Error::FileDeleted) => {
+            return Err(ChangeFileContentError::FileDeleted)
+        }
+    };
+
+    let create_file_result = files_db::create_file(
+        &server_state.files_db_client,
+        &request.file_id,
+        &request.new_file_content,
+    );
+    match create_file_result {
+        Ok(()) => Ok(ChangeFileContentResponse {
+            current_version: new_version as u64,
+        }),
+        Err(_) => {
+            println!("Internal server error! {:?}", create_file_result);
+            return Err(ChangeFileContentError::InternalError);
+        }
+    }
+}
