2870299c4561f45bc5a7b8bcc2e66cf691ecc850
Travis Vanderstad <t.vanderstad@gmail.com>
2020-05-17 20:32:41 -0400

    Rewrite of client-server interface (#66)
    * removes api error structs
    
    * reduces code duplication in response creation
    
    * removes dead code
    
    * removes files_db errors that only had one variant
    
    * simplifies index_db::connect
    
    * cargo fmt
    
    * all server endpoints have request/response datatypes that derive all the things
    
    * adding hyper + tokio
    
    * makes data modeling great again
    
    * making the api great again
    
    * finishes create_file api
    
    * fighting the borrow checker
    
    * working...?
    
    * servicifies half the things
    
    * sample correct client (but server is broken)
    
    * sorts out async stuff but still not running
    
    * formatting
    
    * cargo fmt
    
    * I don't even know
    
    * async/await everywhere
    
    * fixes warnings
    
    * fixes clients, tests now passing
    
    * fixing merge conflict pt 2: finished merging core
    
    * fixing merge conflict pt 3: finished merging server
    
    Co-authored-by: Parth <parth@mehrotra.me>

--- core/src/client/move_file.rs
+++ core/src/client/move_file.rs
@@ -1,57 +1,30 @@
+use crate::model::api::{MoveFileError, MoveFileRequest, MoveFileResponse};
 use reqwest::blocking::Client;
 use reqwest::Error as ReqwestError;
-use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
-pub enum MoveFileError {
+pub enum Error {
+    Serialize(serde_json::error::Error),
     SendFailed(ReqwestError),
     ReceiveFailed(ReqwestError),
-    InvalidAuth,
-    ExpiredAuth,
-    FileNotFound,
-    FileDeleted,
-    FilePathTaken,
-    Unspecified,
+    Deserialize(serde_json::error::Error),
+    API(MoveFileError),
 }
 
-pub struct MoveFileRequest {
-    pub username: String,
-    pub auth: String,
-    pub file_id: String,
-    pub new_file_path: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
-pub struct MoveFileResponse {
-    pub error_code: String,
-}
-
-pub fn move_file(api_location: String, params: &MoveFileRequest) -> Result<(), MoveFileError> {
+pub fn send(api_location: String, request: &MoveFileRequest) -> Result<MoveFileResponse, Error> {
     let client = Client::new();
-    let form_params = [
-        ("username", params.username.as_str()),
-        ("auth", params.auth.as_str()),
-        ("file_id", params.file_id.as_str()),
-        ("new_file_path", params.new_file_path.as_str()),
-    ];
-    let response = client
+    let serialized_request = serde_json::to_string(&request).map_err(|e| Error::Serialize(e))?;
+    let serialized_response = client
         .put(format!("{}/move-file", api_location).as_str())
-        .form(&form_params)
+        .body(serialized_request)
         .send()
-        .map_err(|err| MoveFileError::SendFailed(err))?;
+        .map_err(|e| Error::SendFailed(e))?
+        .text()
+        .map_err(|e| Error::ReceiveFailed(e))?;
+    let response = serde_json::from_str(&serialized_response).map_err(|e| Error::Deserialize(e))?;
 
-    let status = response.status().clone();
-    let response_body = response
-        .json::<MoveFileResponse>()
-        .map_err(|err| MoveFileError::ReceiveFailed(err))?;
-
-    match (status.as_u16(), response_body.error_code.as_str()) {
-        (200..=299, _) => Ok(()),
-        (401, "invalid_auth") => Err(MoveFileError::InvalidAuth),
-        (401, "expired_auth") => Err(MoveFileError::ExpiredAuth),
-        (404, "file_not_found") => Err(MoveFileError::FileNotFound),
-        (410, "file_deleted") => Err(MoveFileError::FileDeleted),
-        (422, "file_path_taken") => Err(MoveFileError::FilePathTaken),
-        _ => Err(MoveFileError::Unspecified),
+    match response {
+        Ok(r) => Ok(r),
+        Err(e) => Err(Error::API(e)),
     }
 }
---
+++ server/src/services/create_file.rs
@@ -0,0 +1,59 @@
+use crate::files_db;
+use crate::index_db;
+use crate::ServerState;
+use lockbook_core::model::api::{CreateFileError, CreateFileRequest, CreateFileResponse};
+
+pub async fn handle(
+    server_state: &mut ServerState,
+    request: CreateFileRequest,
+) -> Result<CreateFileResponse, CreateFileError> {
+    let get_file_details_result =
+        files_db::get_file_details(&server_state.files_db_client, &request.file_id);
+    match get_file_details_result {
+        Err(files_db::get_file_details::Error::NoSuchFile(())) => {}
+        Err(_) => {
+            println!("Internal server error! {:?}", get_file_details_result);
+            return Err(CreateFileError::InternalError);
+        }
+        Ok(_) => return Err(CreateFileError::FileIdTaken),
+    };
+
+    let index_db_create_file_result = index_db::create_file(
+        &mut server_state.index_db_client,
+        &request.file_id,
+        &request.username,
+        &request.file_name,
+        &request.file_path,
+    )
+    .await;
+    let new_version = match index_db_create_file_result {
+        Ok(version) => version,
+        Err(index_db::create_file::Error::FileIdTaken) => return Err(CreateFileError::FileIdTaken),
+        Err(index_db::create_file::Error::FilePathTaken) => {
+            return Err(CreateFileError::FilePathTaken)
+        }
+        Err(index_db::create_file::Error::Uninterpreted(_)) => {
+            println!("Internal server error! {:?}", index_db_create_file_result);
+            return Err(CreateFileError::InternalError);
+        }
+        Err(index_db::create_file::Error::VersionGeneration(_)) => {
+            println!("Internal server error! {:?}", index_db_create_file_result);
+            return Err(CreateFileError::InternalError);
+        }
+    };
+
+    let files_db_create_file_result = files_db::create_file(
+        &server_state.files_db_client,
+        &request.file_id,
+        &request.file_content,
+    );
+    match files_db_create_file_result {
+        Ok(()) => Ok(CreateFileResponse {
+            current_version: new_version as u64,
+        }),
+        Err(_) => {
+            println!("Internal server error! {:?}", files_db_create_file_result);
+            Err(CreateFileError::InternalError)
+        }
+    }
+}
