09d8dd82ccd101e6ed15f6f7664fdf2d4f68c25c
Travis Vanderstad <t.vanderstad@gmail.com>
2020-05-23 17:35:53 -0400

    cleanup

--- server/src/config.rs
+++ server/src/config.rs
@@ -1,47 +1,49 @@
-pub struct IndexDbConfig {
-    pub user: &'static str,
-    pub pass: &'static str,
-    pub host: &'static str,
+use std::env;
+
+pub struct IndexDbConfig<'a> {
+    pub user: &'a str,
+    pub pass: &'a str,
+    pub host: &'a str,
     pub port: u16,
-    pub db: &'static str,
-    pub cert: &'static str,
+    pub db: &'a str,
+    pub cert: &'a str,
 }
 
-pub struct FilesDbConfig {
-    pub bucket: &'static str,
+pub struct FilesDbConfig<'a> {
+    pub bucket: &'a str,
     pub region: s3::region::Region,
-    pub access_key: &'static str,
-    pub secret_key: &'static str,
+    pub access_key: &'a str,
+    pub secret_key: &'a str,
 }
 
 pub struct AuthConfig {
     pub max_auth_delay: u128,
 }
 
-pub struct Config {
-    pub index_db_config: IndexDbConfig,
-    pub files_db_config: FilesDbConfig,
+pub struct Config<'a> {
+    pub index_db_config: IndexDbConfig<'a>,
+    pub files_db_config: FilesDbConfig<'a>,
     pub auth_config: AuthConfig,
 }
 
-pub fn config() -> Config {
+pub fn config<'a>() -> Config<'a> {
     Config {
         index_db_config: IndexDbConfig {
-            user: env!("INDEX_DB_CONFIG_USER"),
-            pass: env!("INDEX_DB_CONFIG_PASS"),
-            host: env!("INDEX_DB_CONFIG_HOST"),
-            port: env!("INDEX_DB_CONFIG_PORT").parse().unwrap(),
-            db: env!("INDEX_DB_CONFIG_DB"),
-            cert: env!("INDEX_DB_CONFIG_CERT"),
+            user: &env::var("INDEX_DB_CONFIG_USER").unwrap(),
+            pass: &env::var("INDEX_DB_CONFIG_PASS").unwrap(),
+            host: &env::var("INDEX_DB_CONFIG_HOST").unwrap(),
+            port: env::var("INDEX_DB_CONFIG_PORT").unwrap().parse().unwrap(),
+            db: &env::var("INDEX_DB_CONFIG_DB").unwrap(),
+            cert: &env::var("INDEX_DB_CONFIG_CERT").unwrap(),
         },
         files_db_config: FilesDbConfig {
-            bucket: env!("FILES_DB_CONFIG_BUCKET"),
-            region: env!("FILES_DB_CONFIG_REGION").parse().unwrap(),
-            access_key: env!("FILES_DB_CONFIG_ACCESS_KEY"),
-            secret_key: env!("FILES_DB_CONFIG_SECRET_KEY"),
+            bucket: &env::var("FILES_DB_CONFIG_BUCKET").unwrap(),
+            region: env::var("FILES_DB_CONFIG_REGION").unwrap().parse().unwrap(),
+            access_key: &env::var("FILES_DB_CONFIG_ACCESS_KEY").unwrap(),
+            secret_key: &env::var("FILES_DB_CONFIG_SECRET_KEY").unwrap(),
         },
         auth_config: AuthConfig {
-            max_auth_delay: env!("MAX_AUTH_DELAY").parse().unwrap(),
+            max_auth_delay: env::var("MAX_AUTH_DELAY").unwrap().parse().unwrap(),
         },
     }
 }
