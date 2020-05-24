8f6741bb1a6b5d5b48e8b8699cff322bdace21d0
Parth Mehrotra <parth@mehrotra.me>
2020-05-23 20:37:57 -0400

    fixed

--- clients/cli/src/sync.rs
+++ clients/cli/src/sync.rs
@@ -2,8 +2,9 @@ use crate::utils::{connect_to_db, get_account};
 
 use lockbook_core::model::work_unit::get_verb;
 
+use lockbook_core::repo::file_metadata_repo::FileMetadataRepo;
 use lockbook_core::service::sync_service::SyncService;
-use lockbook_core::DefaultSyncService;
+use lockbook_core::{DefaultFileMetadataRepo, DefaultSyncService};
 
 pub fn sync() {
     let db = connect_to_db();
@@ -24,6 +25,9 @@ pub fn sync() {
         work_calculated = DefaultSyncService::calculate_work(&db)
             .expect("Failed to calculate work required to sync");
     }
+
+    DefaultFileMetadataRepo::set_last_updated(&db, work_calculated.most_recent_update_from_server)
+        .expect("Failed to save last updated");
 
     println!("Sync complete.");
 }
