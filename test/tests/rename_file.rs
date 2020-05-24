a1b1cbded4f09a85abf8f99bd8c9e72434a22284
Travis Vanderstad <t.vanderstad@gmail.com>
2020-05-24 02:09:26 -0400

    working integration tests with make test_test (against qa dbs)

--- test/tests/rename_file.rs
+++ test/tests/rename_file.rs
@@ -1,51 +1,18 @@
-extern crate lockbook_core;
-
 use lockbook_core::client;
-use lockbook_core::client::create_file;
-use lockbook_core::model::api::CreateFileError;
+use lockbook_core::client::rename_file;
 use lockbook_core::model::api::CreateFileRequest;
+use lockbook_core::model::api::DeleteFileRequest;
 use lockbook_core::model::api::NewAccountRequest;
+use lockbook_core::model::api::{RenameFileError, RenameFileRequest};
 
 #[macro_use]
-mod utils;
+pub mod utils;
 use lockbook_core::service::auth_service::{AuthService, AuthServiceImpl};
 use lockbook_core::service::clock_service::ClockImpl;
 use lockbook_core::service::crypto_service::RsaImpl;
 use utils::{api_loc, generate_account, generate_file_id, TestError};
 
-fn create_file() -> Result<(), TestError> {
-    let account = generate_account();
-
-    client::new_account::send(
-        api_loc(),
-        &NewAccountRequest {
-            username: account.username.clone(),
-            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth(&account).unwrap(),
-            public_key: serde_json::to_string(&account.keys.to_public_key()).unwrap(),
-        },
-    )?;
-
-    client::create_file::send(
-        api_loc(),
-        &CreateFileRequest {
-            username: account.username.clone(),
-            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth(&account).unwrap(),
-            file_id: generate_file_id(),
-            file_name: "file_name".to_string(),
-            file_path: "file_path".to_string(),
-            file_content: "file_content".to_string(),
-        },
-    )?;
-
-    Ok(())
-}
-
-#[test]
-fn test_create_file() {
-    assert_matches!(create_file(), Ok(_));
-}
-
-fn create_file_duplicate_file_id() -> Result<(), TestError> {
+fn rename_file() -> Result<(), TestError> {
     let account = generate_account();
     let file_id = generate_file_id();
 
@@ -70,15 +37,13 @@ fn create_file_duplicate_file_id() -> Result<(), TestError> {
         },
     )?;
 
-    client::create_file::send(
+    client::rename_file::send(
         api_loc(),
-        &CreateFileRequest {
+        &RenameFileRequest {
             username: account.username.clone(),
             auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth(&account).unwrap(),
             file_id: file_id.to_string(),
-            file_name: "file_name".to_string(),
-            file_path: "file_path_2".to_string(),
-            file_content: "file_content".to_string(),
+            new_file_name: "new_file_name".to_string(),
         },
     )?;
 
@@ -86,17 +51,48 @@ fn create_file_duplicate_file_id() -> Result<(), TestError> {
 }
 
 #[test]
-fn test_create_file_duplicate_file_id() {
+fn test_rename_file() {
+    assert_matches!(rename_file(), Ok(_));
+}
+
+fn rename_file_file_not_found() -> Result<(), TestError> {
+    let account = generate_account();
+
+    client::new_account::send(
+        api_loc(),
+        &NewAccountRequest {
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth(&account).unwrap(),
+            public_key: serde_json::to_string(&account.keys.to_public_key()).unwrap(),
+        },
+    )?;
+
+    client::rename_file::send(
+        api_loc(),
+        &RenameFileRequest {
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth(&account).unwrap(),
+            file_id: generate_file_id(),
+            new_file_name: "new_file_name".to_string(),
+        },
+    )?;
+
+    Ok(())
+}
+
+#[test]
+fn test_rename_file_file_not_found() {
     assert_matches!(
-        create_file_duplicate_file_id(),
-        Err(TestError::CreateFileError(create_file::Error::API(
-            CreateFileError::FileIdTaken
+        rename_file_file_not_found(),
+        Err(TestError::RenameFileError(rename_file::Error::API(
+            RenameFileError::FileNotFound
         )))
     );
 }
 
-fn create_file_duplicate_file_path() -> Result<(), TestError> {
+fn rename_file_file_deleted() -> Result<(), TestError> {
     let account = generate_account();
+    let file_id = generate_file_id();
 
     client::new_account::send(
         api_loc(),
@@ -112,22 +108,29 @@ fn create_file_duplicate_file_path() -> Result<(), TestError> {
         &CreateFileRequest {
             username: account.username.clone(),
             auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth(&account).unwrap(),
-            file_id: generate_file_id(),
+            file_id: file_id.to_string(),
             file_name: "file_name".to_string(),
             file_path: "file_path".to_string(),
             file_content: "file_content".to_string(),
         },
     )?;
 
-    client::create_file::send(
+    client::delete_file::send(
         api_loc(),
-        &CreateFileRequest {
+        &DeleteFileRequest {
             username: account.username.clone(),
             auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth(&account).unwrap(),
-            file_id: generate_file_id(),
-            file_name: "file_name".to_string(),
-            file_path: "file_path".to_string(),
-            file_content: "file_content".to_string(),
+            file_id: file_id.to_string(),
+        },
+    )?;
+
+    client::rename_file::send(
+        api_loc(),
+        &RenameFileRequest {
+            username: account.username.clone(),
+            auth: AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth(&account).unwrap(),
+            file_id: file_id.to_string(),
+            new_file_name: "new_file_name".to_string(),
         },
     )?;
 
@@ -135,11 +138,11 @@ fn create_file_duplicate_file_path() -> Result<(), TestError> {
 }
 
 #[test]
-fn test_create_file_duplicate_file_path() {
+fn test_rename_file_file_deleted() {
     assert_matches!(
-        create_file_duplicate_file_path(),
-        Err(TestError::CreateFileError(create_file::Error::API(
-            CreateFileError::FilePathTaken
+        rename_file_file_deleted(),
+        Err(TestError::RenameFileError(rename_file::Error::API(
+            RenameFileError::FileDeleted
         )))
     );
 }
--- test/tests/utils/mod.rs
+++ test/tests/utils/mod.rs
@@ -1,5 +1,3 @@
-extern crate lockbook_core;
-
 use std::env;
 
 use uuid::Uuid;
