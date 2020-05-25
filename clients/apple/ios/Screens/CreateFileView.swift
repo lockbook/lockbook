c526cfbec980ad159e07f893ee42fd78f1ed96b7
Raayan Pillai <raayan@raayanpillai.com>
2020-05-25 16:26:17 -0400

    renaming things and moving stuff around

---
+++ clients/apple/ios/Screens/CreateFileView.swift
@@ -0,0 +1,45 @@
+//
+//  NewLockbookView.swift
+//  ios_client
+//
+//  Created by Parth Mehrotra on 2/9/20.
+//  Copyright © 2020 Lockbook. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateFileView: View {
+    @State private var fileName: String = ""
+    @State private var showingAlert = false
+    @EnvironmentObject var coordinator: Coordinator
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+    var body: some View {
+        VStack {
+            TextField("name", text: $fileName)
+                .autocapitalization(.none)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 50)
+                
+            MonokaiButton(text: "Create File")
+                .onTapGesture {
+                    if self.coordinator.createFile(name: self.fileName) {
+                        self.coordinator.sync()
+                        self.presentationMode.wrappedValue.dismiss()
+                    } else {
+                        self.showingAlert = true
+                    }
+                }
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Failed to create file!"))
+        }
+    }
+}
+
+struct CreateFileView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateFileView()
+    }
+}
---
+++ clients/apple/ios/Screens/ListView.swift
@@ -0,0 +1,90 @@
+//
+//  ListView.swift
+//  ios_client
+//
+//  Created by Raayan Pillai on 4/11/20.
+//  Copyright © 2020 Lockbook. All rights reserved.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    @EnvironmentObject var coordinator: Coordinator
+    
+    var body: some View {
+        GeometryReader{ geometry in
+            NavigationView {
+                PullDownView(width: geometry.size.width, height: geometry.size.height)
+                    .navigationBarTitle("\(self.coordinator.username)'s Files")
+                    .navigationBarItems(
+                        leading: NavigationLink(destination: DebugView()) {
+                            Image(systemName: "circle.grid.hex")
+                        },
+                        trailing: NavigationLink(destination: CreateFileView()) {
+                            Image(systemName: "plus")
+                        }
+                    )
+            }
+        }
+    }
+}
+
+struct SwiftUIList: View {
+    @EnvironmentObject var coordinator: Coordinator
+
+    var body: some View {
+        List {
+            ForEach(coordinator.files){ file in
+                FileRow(metadata: file)
+            }
+            .onDelete { offset in
+                let meta = self.coordinator.files.remove(at: offset.first!)
+                print("Deleting", meta)
+            }
+        }
+    }
+}
+
+struct PullDownView : UIViewRepresentable {
+    @EnvironmentObject var coordinator: Coordinator
+
+    var width : CGFloat
+    var height : CGFloat
+    
+    func makeCoordinator() -> SVCoordinator {
+        SVCoordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UIScrollView {
+        let control = UIScrollView()
+        control.refreshControl = UIRefreshControl()
+        control.refreshControl?.addTarget(context.coordinator, action: #selector(SVCoordinator.handleRefreshControl), for: .valueChanged)
+        let childView = UIHostingController(rootView: SwiftUIList().environmentObject(self.coordinator))
+        childView.view.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        
+        control.addSubview(childView.view)
+        return control
+    }
+    
+    func updateUIView(_ uiView: UIScrollView, context: Context) {
+    }
+    
+    class SVCoordinator: NSObject {
+        var control: PullDownView
+        
+        init(_ control: PullDownView) {
+            self.control = control
+        }
+        @objc func handleRefreshControl(sender: UIRefreshControl) {
+            self.control.coordinator.sync()
+            sender.endRefreshing()
+        }
+    }
+}
+
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView().environmentObject(Coordinator())
+    }
+}
