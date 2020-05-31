cdab1deefc0df05683ffa813c24610248c6ae115
Travis Vanderstad <t.vanderstad@gmail.com>
2020-05-31 14:02:40 -0400

    transactionify me, capn

--- server/src/services/create_file.rs
+++ server/src/services/create_file.rs
@@ -7,6 +7,14 @@ pub async fn handle(
     server_state: &mut ServerState,
     request: CreateFileRequest,
 ) -> Result<CreateFileResponse, CreateFileError> {
+    let transaction = match server_state.index_db_client.transaction().await {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Internal server error! Cannot begin transaction: {:?}", e);
+            return Err(CreateFileError::InternalError);
+        }
+    };
+
     let get_file_details_result =
         files_db::get_file_details(&server_state.files_db_client, &request.file_id).await;
     match get_file_details_result {
@@ -19,7 +27,7 @@ pub async fn handle(
     };
 
     let index_db_create_file_result = index_db::create_file(
-        &mut server_state.index_db_client,
+        &transaction,
         &request.file_id,
         &request.username,
         &request.file_name,
@@ -48,12 +56,20 @@ pub async fn handle(
         &request.file_content,
     )
     .await;
-    match files_db_create_file_result {
+    let result = match files_db_create_file_result {
         Ok(()) => Ok(CreateFileResponse {
             current_version: new_version as u64,
         }),
         Err(_) => {
             println!("Internal server error! {:?}", files_db_create_file_result);
+            Err(CreateFileError::InternalError)
+        }
+    };
+
+    match transaction.commit().await {
+        Ok(_) => result,
+        Err(e) => {
+            println!("Internal server error! Cannot commit transaction: {:?}", e);
             Err(CreateFileError::InternalError)
         }
     }
--- server/src/services/new_account.rs
+++ server/src/services/new_account.rs
@@ -6,17 +6,29 @@ pub async fn handle(
     server_state: &mut ServerState,
     request: NewAccountRequest,
 ) -> Result<NewAccountResponse, NewAccountError> {
-    let new_account_result = index_db::new_account(
-        &mut server_state.index_db_client,
-        &request.username,
-        &request.public_key,
-    )
-    .await;
-    match new_account_result {
+    let transaction = match server_state.index_db_client.transaction().await {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Internal server error! Cannot begin transaction: {:?}", e);
+            return Err(NewAccountError::InternalError);
+        }
+    };
+
+    let new_account_result =
+        index_db::new_account(&transaction, &request.username, &request.public_key).await;
+    let result = match new_account_result {
         Ok(()) => Ok(NewAccountResponse {}),
         Err(index_db::new_account::Error::UsernameTaken) => Err(NewAccountError::UsernameTaken),
         Err(index_db::new_account::Error::Uninterpreted(_)) => {
             println!("Internal server error! {:?}", new_account_result);
+            Err(NewAccountError::InternalError)
+        }
+    };
+
+    match transaction.commit().await {
+        Ok(_) => result,
+        Err(e) => {
+            println!("Internal server error! Cannot commit transaction: {:?}", e);
             Err(NewAccountError::InternalError)
         }
     }
