19e409d0acef4757965c6cfee095a28107b60ab6
Travis Vanderstad <t.vanderstad@gmail.com>
2020-05-31 14:02:40 -0400

    transactionify me, capn

--- server/src/services/get_public_key.rs
+++ server/src/services/get_public_key.rs
@@ -6,13 +6,28 @@ pub async fn handle(
     server_state: &mut ServerState,
     request: GetPublicKeyRequest,
 ) -> Result<GetPublicKeyResponse, GetPublicKeyError> {
-    let get_public_key_result =
-        index_db::get_public_key(&mut server_state.index_db_client, &request.username).await;
-    match get_public_key_result {
+    let transaction = match server_state.index_db_client.transaction().await {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Internal server error! Cannot begin transaction: {:?}", e);
+            return Err(GetPublicKeyError::InternalError);
+        }
+    };
+
+    let get_public_key_result = index_db::get_public_key(&transaction, &request.username).await;
+    let result = match get_public_key_result {
         Ok(key) => Ok(GetPublicKeyResponse { key: key }),
         Err(index_db::get_public_key::Error::Postgres(_)) => Err(GetPublicKeyError::UserNotFound),
         Err(index_db::get_public_key::Error::SerializationError(_)) => {
             println!("Internal server error! {:?}", get_public_key_result);
+            Err(GetPublicKeyError::InternalError)
+        }
+    };
+
+    match transaction.commit().await {
+        Ok(_) => result,
+        Err(e) => {
+            println!("Internal server error! Cannot commit transaction: {:?}", e);
             Err(GetPublicKeyError::InternalError)
         }
     }
