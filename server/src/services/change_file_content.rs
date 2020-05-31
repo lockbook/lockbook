23b3a52befc68e04dffc38b0604801e5209e5fc7
Travis Vanderstad <t.vanderstad@gmail.com>
2020-05-31 14:02:40 -0400

    transactionify me, capn

--- server/src/services/change_file_content.rs
+++ server/src/services/change_file_content.rs
@@ -9,8 +9,16 @@ pub async fn handle(
     server_state: &mut ServerState,
     request: ChangeFileContentRequest,
 ) -> Result<ChangeFileContentResponse, ChangeFileContentError> {
+    let transaction = match server_state.index_db_client.transaction().await {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Internal server error! Cannot begin transaction: {:?}", e);
+            return Err(ChangeFileContentError::InternalError);
+        }
+    };
+
     let update_file_version_result = index_db::update_file_version(
-        &mut server_state.index_db_client,
+        &transaction,
         &request.file_id,
         request.old_file_version as i64,
     )
@@ -42,12 +50,20 @@ pub async fn handle(
         &request.new_file_content,
     )
     .await;
-    match create_file_result {
+    let result = match create_file_result {
         Ok(()) => Ok(ChangeFileContentResponse {
             current_version: new_version as u64,
         }),
         Err(_) => {
             println!("Internal server error! {:?}", create_file_result);
+            Err(ChangeFileContentError::InternalError)
+        }
+    };
+
+    match transaction.commit().await {
+        Ok(_) => result,
+        Err(e) => {
+            println!("Internal server error! Cannot commit transaction: {:?}", e);
             Err(ChangeFileContentError::InternalError)
         }
     }
