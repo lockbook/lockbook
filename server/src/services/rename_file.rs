e7f66a63d9d54012d51b657ee87d3f61a444fa84
Travis Vanderstad <t.vanderstad@gmail.com>
2020-05-31 14:02:40 -0400

    transactionify me, capn

--- server/src/services/rename_file.rs
+++ server/src/services/rename_file.rs
@@ -6,13 +6,17 @@ pub async fn handle(
     server_state: &mut ServerState,
     request: RenameFileRequest,
 ) -> Result<RenameFileResponse, RenameFileError> {
-    let rename_file_result = index_db::rename_file(
-        &mut server_state.index_db_client,
-        &request.file_id,
-        &request.new_file_name,
-    )
-    .await;
-    match rename_file_result {
+    let transaction = match server_state.index_db_client.transaction().await {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Internal server error! Cannot begin transaction: {:?}", e);
+            return Err(RenameFileError::InternalError);
+        }
+    };
+
+    let rename_file_result =
+        index_db::rename_file(&transaction, &request.file_id, &request.new_file_name).await;
+    let result = match rename_file_result {
         Ok(_) => Ok(RenameFileResponse {}),
         Err(index_db::rename_file::Error::FileDoesNotExist) => Err(RenameFileError::FileNotFound),
         Err(index_db::rename_file::Error::FileDeleted) => Err(RenameFileError::FileDeleted),
@@ -22,6 +26,14 @@ pub async fn handle(
         }
         Err(index_db::rename_file::Error::VersionGeneration(_)) => {
             println!("Internal server error! {:?}", rename_file_result);
+            Err(RenameFileError::InternalError)
+        }
+    };
+
+    match transaction.commit().await {
+        Ok(_) => result,
+        Err(e) => {
+            println!("Internal server error! Cannot commit transaction: {:?}", e);
             Err(RenameFileError::InternalError)
         }
     }
