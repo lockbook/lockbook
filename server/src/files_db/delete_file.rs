b8aa0ed43c13c7e7374a432750a7f2ee755e401c
Travis Vanderstad <t.vanderstad@gmail.com>
2020-06-01 23:46:50 -0400

    server puts files in s3 with content versions

--- server/src/files_db/delete_file.rs
+++ server/src/files_db/delete_file.rs
@@ -4,8 +4,12 @@ use s3::bucket::Bucket as S3Client;
 pub async fn delete_file(
     client: &S3Client,
     file_id: &str,
+    content_version: u64,
 ) -> Result<(), categorized_s3_error::Error> {
-    match client.delete_object(&format!("/{}", file_id)).await? {
+    match client
+        .delete_object(&format!("/{}:{}", file_id, content_version))
+        .await?
+    {
         (_, 204) => Ok(()),
         (body, _) => Err(categorized_s3_error::Error::from(body)),
     }
