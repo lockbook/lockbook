39493188c4dc6c519d599afa5de16108c515ec59
Smail <SmailBarkouch1@gmail.com>
2020-06-02 22:31:59 -0400

    cargo fmt

--- server/src/services/new_account.rs
+++ server/src/services/new_account.rs
@@ -1,16 +1,17 @@
 use crate::index_db;
 use crate::ServerState;
-use rsa::RSAPublicKey;
-use lockbook_core::service::crypto_service::{SignedValue, RsaImpl, PubKeyCryptoService};
 use lockbook_core::model::api::{NewAccountError, NewAccountRequest, NewAccountResponse};
+use lockbook_core::service::crypto_service::{PubKeyCryptoService, RsaImpl, SignedValue};
+use rsa::RSAPublicKey;
 
 pub async fn handle(
     server_state: &mut ServerState,
     request: NewAccountRequest,
 ) -> Result<NewAccountResponse, NewAccountError> {
-
-    let public_key = serde_json::from_str::<RSAPublicKey>(&request.public_key).map_err(|_| NewAccountError::InvalidPublicKey)?;
-    let auth = serde_json::from_str::<SignedValue>(&request.auth).map_err(|_| NewAccountError::InvalidAuth)?;
+    let public_key = serde_json::from_str::<RSAPublicKey>(&request.public_key)
+        .map_err(|_| NewAccountError::InvalidPublicKey)?;
+    let auth = serde_json::from_str::<SignedValue>(&request.auth)
+        .map_err(|_| NewAccountError::InvalidAuth)?;
     RsaImpl::verify(&public_key, &auth).map_err(|_| NewAccountError::InvalidPublicKey)?;
 
     let new_account_result = index_db::new_account(
