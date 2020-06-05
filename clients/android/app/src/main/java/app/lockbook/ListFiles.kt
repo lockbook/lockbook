b9e0d0c77a42306831de423f4414d1d6be32dff1
Parth <parth@mehrotra.me>
2020-06-05 14:00:32 -0400

    conditionally call welcome or list files

---
+++ clients/android/app/src/main/java/app/lockbook/ListFiles.kt
@@ -0,0 +1,12 @@
+package app.lockbook
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListFiles : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_files)
+    }
+}
