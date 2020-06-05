c0968ec1962b86674c615c05084f91eb76836f1c
Parth <parth@mehrotra.me>
2020-06-04 22:31:16 -0400

    color pallete set

--- clients/android/app/src/main/java/app/lockbook/MainActivity.kt
+++ clients/android/app/src/main/java/app/lockbook/MainActivity.kt
@@ -14,11 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
