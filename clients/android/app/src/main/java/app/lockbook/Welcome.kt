0844263871eabe438fa929534ab41618af751063
Parth <parth@mehrotra.me>
2020-06-05 18:18:21 -0400

    skelatal navigation for new account and import account

--- clients/android/app/src/main/java/app/lockbook/Welcome.kt
+++ clients/android/app/src/main/java/app/lockbook/Welcome.kt
@@ -1,7 +1,9 @@
 package app.lockbook
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Welcome : AppCompatActivity() {
 
@@ -9,6 +11,12 @@ class Welcome : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        new_lockbook.setOnClickListener {
+            startActivity(Intent(applicationContext, NewAccount::class.java))
+        }
 
+        import_lockbook.setOnClickListener {
+            startActivity(Intent(applicationContext, ImportAccount::class.java))
+        }
     }
 }
