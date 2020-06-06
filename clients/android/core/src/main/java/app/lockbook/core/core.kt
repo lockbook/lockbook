d5027e44da3cf6d22b88aeebdbadb3546ce34713
Parth <parth@mehrotra.me>
2020-06-05 21:42:50 -0400

    creating new accounts successfully

--- clients/android/core/src/main/java/app/lockbook/core/core.kt
+++ clients/android/core/src/main/java/app/lockbook/core/core.kt
@@ -1,7 +1,12 @@
 package app.lockbook.core
 
+external fun initLogger()
 external fun isDbPresent(path: String): Boolean
+external fun createAccount(path: String, username: String): Byte
+
+
 
 fun loadLockbookCore() {
     System.loadLibrary("lockbook_core")
+    initLogger()
 }
