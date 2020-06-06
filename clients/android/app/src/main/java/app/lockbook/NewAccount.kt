d94ab49b9d16588e0be16644d40f5712608108c2
Parth <parth@mehrotra.me>
2020-06-05 21:42:50 -0400

    creating new accounts successfully

--- clients/android/app/src/main/java/app/lockbook/NewAccount.kt
+++ clients/android/app/src/main/java/app/lockbook/NewAccount.kt
@@ -2,6 +2,8 @@ package app.lockbook
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import app.lockbook.core.createAccount
+import kotlinx.android.synthetic.main.new_account.*
 
 class NewAccount : AppCompatActivity() {
 
@@ -9,6 +11,8 @@ class NewAccount : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_account)
 
-
+        create_lockbook.setOnClickListener {
+            println(createAccount(filesDir.absolutePath, username.text.toString()))
+        }
     }
 }
