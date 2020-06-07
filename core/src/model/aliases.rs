ec2e3d77727fa1b12fed3d5907cd9e09f94a097a
Travis Vanderstad <t.vanderstad@gmail.com>
2020-06-07 17:59:36 -0400

    aliases

---
+++ core/src/model/aliases.rs
@@ -0,0 +1,6 @@
+pub type Username = String;
+pub type FileName = String;
+pub type FileId = String;
+pub type Version = u64;
+pub type EncryptedDocumentContent = String;
+pub type Signature = String;
