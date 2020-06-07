43803b1f034582bfb9a6b192319bf0da07ea0b4c
Parth <parth@mehrotra.me>
2020-06-07 18:30:27 -0400

    better account_string, qr generation, tooltip for importing large strings

--- core/src/service/account_service.rs
+++ core/src/service/account_service.rs
@@ -23,15 +23,23 @@ error_enum! {
 
 error_enum! {
     enum AccountImportError {
-        AccountStringCorrupted(serde_json::error::Error),
+        AccountStringCorrupted(base64::DecodeError),
+        AccountStringFailedToDeserialize(bincode::Error),
         PersistenceError(account_repo::Error),
         InvalidPrivateKey(rsa::errors::Error),
+    }
+}
+
+error_enum! {
+    enum AccountExportError {
+        KeyRetrievalError(account_repo::Error),
     }
 }
 
 pub trait AccountService {
     fn create_account(db: &Db, username: &String) -> Result<Account, AccountCreationError>;
     fn import_account(db: &Db, account_string: &String) -> Result<Account, AccountImportError>;
+    fn export_account(db: &Db) -> Result<String, AccountExportError>;
 }
 
 pub struct AccountServiceImpl<
@@ -70,16 +78,24 @@ impl<Crypto: PubKeyCryptoService, AccountDb: AccountRepo, ApiClient: Client, Aut
         ApiClient::new_account(username, auth, public_key)?;
         info!("Account creation success!");
 
-        debug!("{}", serde_json::to_string(&account).unwrap());
+        debug!("{}", serde_json::to_string(&account)?);
         Ok(account)
     }
 
     fn import_account(db: &Db, account_string: &String) -> Result<Account, AccountImportError> {
-        let account = serde_json::from_str::<Account>(account_string.as_str())?;
+        let decoded = base64::decode(&account_string)?;
+        let account: Account = bincode::deserialize(&decoded[..])?;
+
         account.keys.validate()?;
         AccountDb::insert_account(db, &account)?;
         info!("Account imported successfully");
         Ok(account)
+    }
+
+    fn export_account(db: &Db) -> Result<String, AccountExportError> {
+        let account = &AccountDb::get_account(&db)?;
+        let encoded: Vec<u8> = bincode::serialize(&account).unwrap();
+        Ok(base64::encode(&encoded))
     }
 }
 
@@ -88,7 +104,7 @@ mod unit_tests {
     use crate::client::ClientImpl;
     use crate::model::account::Account;
     use crate::model::state::Config;
-    use crate::repo::account_repo::AccountRepoImpl;
+    use crate::repo::account_repo::{AccountRepo, AccountRepoImpl};
     use crate::repo::db_provider::{DbProvider, TempBackedDB};
     use crate::service::account_service::AccountImportError;
     use crate::service::account_service::{AccountService, AccountServiceImpl};
@@ -126,14 +142,31 @@ mod unit_tests {
         };
 
         let db = DefaultDbProvider::connect_to_db(&config).unwrap();
+        DefaultAccountDb::insert_account(&db, &account).unwrap();
+
         let result = discriminant(
-            &DefaultAccountService::import_account(&db, &serde_json::to_string(&account).unwrap())
-                .unwrap_err(),
+            &DefaultAccountService::import_account(
+                &db,
+                &DefaultAccountService::export_account(&db).unwrap(),
+            )
+            .unwrap_err(),
         );
         let err = discriminant(&AccountImportError::InvalidPrivateKey(
             rsa::errors::Error::InvalidModulus,
         ));
 
         assert_eq!(result, err)
+    }
+
+    #[test]
+    fn test_import_export_opposites() {
+        let account_string = "BgAAAAAAAABwYXJ0aDRAAAAAAAAAAJnSeo+j1kZ6zi/Sfw/k6h8adzTImXng3ZXqvSKOUyYatb1Xm7Kh3AFPNSkTytGC/3ajran8/WhUnImJobEg0MGQoXdLiuwxtMs45RhuSDlBPPwW+Dw8EUt3ElEkgMkXXsZzcIfOSuTxTh+pmJWJJO5v4tyTu0jhXP7WJ9yK44EzQUpWVwTLb4t81wuUU5tJ/f4ybr/UrRmjXSLqKybUdjRQseF4l+aH8Ony3yC93UhlNlZtInoJIZCa+xuoJQsPHM+lzdZcHi3GhAw3t8BSnP5oW/j+mnRbb/h67RRqb+C+7b+x4ixrliCO0ekEhC/W0VhymZQh0YYMb7X/Vm6nSLoBAAAAAAAAAAEAAQBAAAAAAAAAAI1X0y8br/ltxnEYZxfO/6TLorOKEJd5H/0XeDXDiMjSvSPOCzuCbhSGWQVPdU9iegHdCHOrqA21pcSfJ5c2+0I38HRpWYZeQk2ochDTqqe23WJ27kt5CgrK6gXG5MeROCSEMSiJwcelhkdVYf5bSsdqGi681T4416lravO07oSTggy/dw/+w/BcYWXEjN07ujYgt4zOkYBQ4C1t3bVRAjEnx6EkF4UOHxlcbIbdfD/Txmm9AAhIz9MxQLq25U57bK5hoK6orOxxUMIZnpqvy9TH2+AZD2l9HjylVN2wC6gXLfIrPk0NUroxXVRcYuPhkCkvoWtq5bdW++1j5bRxAF4CAAAAAAAAACAAAAAAAAAAhx6QHKVxtuz2yNfzPOb5fJWZmuRWyDFzyrOQXFK7Q3o30iDtP+6AaQuRFX/75N6PDFJfjE/kHobsLd+yhNkDg19EkFM4dceKoR9WylGb3S2QmD9J7ew63EnPMs+mHqBqv1bsgh8+eTwo8teqA0oFSMz0OzwGRz0xn5jzmwZxKcwgAAAAAAAAAN+t+ahUxaKA8d5UDLjzjnxheC/QuneQAJVYDxExP+/9uchnBt1rxYiqBHWgaFiIHgAyfkaak4oFNZ+Cnf/Gb0qjHWGiF/f8/63rmv54XmfbpMifUNYnUSBSbEGU8KNRw1BZpofmadY6KfDV/aoyBUSX7yU9rPT9hbkpjR5oIpXp".to_string();
+        let config = Config {
+            writeable_path: "ignored".to_string(),
+        };
+
+        let db = DefaultDbProvider::connect_to_db(&config).unwrap();
+        DefaultAccountService::import_account(&db, &account_string).unwrap();
+        assert_eq!(DefaultAccountService::export_account(&db).unwrap(), account_string);
     }
 }
