d1c1ad00b927dca7f334e4ff2d1ef686e4a4a2d7
Parth <parth@mehrotra.me>
2020-06-08 19:54:48 -0400

    remove old scripts that aren't needed anymore

--- server/run_tests.sh
+++
@@ -1,9 +0,0 @@
-if cargo fmt --quiet -- --check ; then
-	cargo test -- --nocapture
-else
-	echo "The following files fail lint checks:"
-	cargo fmt -- --check -l
-	echo "Run cargo fmt" 
-	echo "Then re-run this script"
-	exit 1
-fi
