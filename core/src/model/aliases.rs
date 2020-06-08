83359f1d555661743cb2e333ce980e108725ef01
Travis Vanderstad <t.vanderstad@gmail.com>
2020-06-07 20:34:42 -0400

    api model for folders vs documents

--- core/src/model/aliases.rs
+++ core/src/model/aliases.rs
@@ -1,6 +1,8 @@
 pub type Username = String;
-pub type FileName = String;
-pub type FileId = String;
+pub type DocumentName = String;
+pub type DocumentId = String;
+pub type FolderName = String;
+pub type FolderId = String;
 pub type Version = u64;
 pub type EncryptedDocumentContent = String;
 pub type Signature = String;
