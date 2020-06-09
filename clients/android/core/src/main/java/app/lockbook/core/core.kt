a1f419334caf704e2abbcecdd36413081918c2e7
Parth <parth@mehrotra.me>
2020-06-08 21:35:20 -0400

    account string being imported

--- clients/android/core/src/main/java/app/lockbook/core/core.kt
+++ clients/android/core/src/main/java/app/lockbook/core/core.kt
@@ -3,8 +3,7 @@ package app.lockbook.core
 external fun initLogger()
 external fun isDbPresent(path: String): Boolean
 external fun createAccount(path: String, username: String): Int
-
-
+external fun importAccount(path: String, accountString: String): Int
 
 fun loadLockbookCore() {
     System.loadLibrary("lockbook_core")
