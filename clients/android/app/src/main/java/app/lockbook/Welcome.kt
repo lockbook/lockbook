52952067c4b88875991b35c4074bac72be406e9f
Smail <SmailBarkouch1@gmail.com>
2020-06-30 11:27:54 -0400

    added fully working databinding and moved functionality into xml

--- clients/android/app/src/main/java/app/lockbook/Welcome.kt
+++ clients/android/app/src/main/java/app/lockbook/Welcome.kt
@@ -2,24 +2,28 @@ package app.lockbook
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import app.lockbook.databinding.ActivityMainBinding
 
 class Welcome : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.welcomeActivity = this
     }
 
     fun launchNewAccount() {
         startActivity(Intent(applicationContext, NewAccount::class.java))
+        Log.i("info", "LAUNCHED NEW ACCOUNT")
     }
 
     fun launchImportAccount() {
         startActivity(Intent(applicationContext, ImportAccount::class.java))
+        Log.i("info", "LAUNCHED IMPORT ACCOUNT")
+
     }
 
 }
