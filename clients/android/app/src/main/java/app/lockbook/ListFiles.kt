72928f4a015da3450653673dc16704fd363e7dc9
Smail <SmailBarkouch1@gmail.com>
2020-06-30 14:49:33 -0400

    lockbook will now not let you enter the welcome screen the first time logging in, as well as providing an intutive way to exit the application in listfiles

--- clients/android/app/src/main/java/app/lockbook/ListFiles.kt
+++ clients/android/app/src/main/java/app/lockbook/ListFiles.kt
@@ -1,12 +1,30 @@
 package app.lockbook
 
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.system.exitProcess
 
 class ListFiles : AppCompatActivity() {
+
+    private var exit = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_files)
+    }
+
+    override fun onBackPressed() {
+        if(exit) {
+            finish()
+            exitProcess(1) // may be changed
+        } else {
+            Toast.makeText(this, "Press back again to exit.", Toast.LENGTH_SHORT).show()
+            exit = true
+            Handler().postDelayed({
+                exit = false
+            }, 3000)
+        }
     }
 }
