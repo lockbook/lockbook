75b9abd269486fff2d83f385f8c7233cfe0b2ed2
Parth <parth@mehrotra.me>
2020-07-01 17:42:08 -0400

    added whoami

---
+++ clients/cli/src/whoami.rs
@@ -0,0 +1,17 @@
+use lockbook_core::repo::account_repo::{Error, AccountRepo};
+
+use lockbook_core::DefaultAccountRepo;
+
+use crate::utils::connect_to_db;
+
+pub fn whoami() {
+    let db = connect_to_db();
+
+    match DefaultAccountRepo::get_account(&db) {
+        Ok(account) => println!("{}", account.username),
+        Err(err) => match err {
+            Error::SledError(_) | Error::SerdeError(_) => eprintln!("Sled error: {:?}", err),
+            Error::AccountMissing(_) => eprintln!("No account found, run init, import or help."),
+        }
+    }
+}
