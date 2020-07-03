39509b3e7355fa9a5731a5789b19b1401bb3fd30
Smail <SmailBarkouch1@gmail.com>
2020-07-03 11:49:03 -0400

    added more rust extern functions for kotlin

--- clients/android/core/src/main/java/app/lockbook/core/core.kt
+++ clients/android/core/src/main/java/app/lockbook/core/core.kt
@@ -4,6 +4,9 @@ external fun initLogger()
 external fun isDbPresent(path: String): Boolean
 external fun createAccount(path: String, username: String): Int
 external fun importAccount(path: String, accountString: String): Int
+external fun getRoot(path: String): String
+external fun getChildren(path: String, parentUuid: String): String
+external fun getFile(path: String, fileUuid: String): String
 
 fun loadLockbookCore() {
     System.loadLibrary("lockbook_core")
