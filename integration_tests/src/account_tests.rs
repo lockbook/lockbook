1fd22a11665b3c6c363483d14e7e373620ef63c4
Parth <parth@mehrotra.me>
2020-07-03 19:18:22 -0400

    hardcode the hostname for now

--- integration_tests/src/account_tests.rs
+++ integration_tests/src/account_tests.rs
@@ -9,4 +9,12 @@ mod account_tests {
         let db = test_db();
         DefaultAccountService::create_account(&db, &random_username()).unwrap();
     }
+
+    #[test]
+    fn username_taken_test() {
+        let db = test_db();
+        let username = &random_username();
+        DefaultAccountService::create_account(&db, username).unwrap();
+        assert_eq!(DefaultAccountService::create_account(&db, username).unwrap_err(), )
+    }
 }
