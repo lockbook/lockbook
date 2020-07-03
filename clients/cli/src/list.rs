3b9fdf9c0324e8f84f9b1344c0ffa719249cbcd1
Parth <parth@mehrotra.me>
2020-07-03 12:35:56 -0400

    added filters to listing by paths

--- clients/cli/src/list.rs
+++ clients/cli/src/list.rs
@@ -1,14 +1,14 @@
-use lockbook_core::repo::file_metadata_repo::FileMetadataRepo;
+use lockbook_core::repo::file_metadata_repo::{FileMetadataRepo, Filter};
 use lockbook_core::DefaultFileMetadataRepo;
 
 use crate::utils::{connect_to_db, get_account, print_last_successful_sync};
 
-pub fn list() {
+pub fn list(file_filter: Option<Filter>) {
     let db = connect_to_db();
 
     get_account(&db);
 
-    DefaultFileMetadataRepo::get_all_paths(&db)
+    DefaultFileMetadataRepo::get_all_paths(&db, file_filter)
         .expect("Failed to retrieve content from FileMetadataRepo")
         .into_iter()
         .for_each(|path| println!("{}", path));
