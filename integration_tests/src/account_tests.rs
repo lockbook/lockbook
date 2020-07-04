b26445d77874a250f59a084b0370f72a89703a91
Parth <parth@mehrotra.me>
2020-07-03 20:14:34 -0400

    test1 done

--- integration_tests/src/account_tests.rs
+++ integration_tests/src/account_tests.rs
@@ -1,8 +1,12 @@
 #[cfg(test)]
 mod account_tests {
-    use crate::{random_username, test_db};
-    use lockbook_core::service::account_service::AccountService;
+    use lockbook_core::client::Error;
     use lockbook_core::DefaultAccountService;
+    use lockbook_core::model::api::NewAccountError::UsernameTaken;
+    use lockbook_core::service::account_service::{AccountCreationError, AccountService};
+
+    use crate::{random_username, test_db};
+    use lockbook_core::model::api::NewAccountError;
 
     #[test]
     fn create_account_successfully() {
@@ -15,6 +19,47 @@ mod account_tests {
         let db = test_db();
         let username = &random_username();
         DefaultAccountService::create_account(&db, username).unwrap();
-        assert_eq!(DefaultAccountService::create_account(&db, username).unwrap_err(), )
+
+        match DefaultAccountService::create_account(&db, username).unwrap_err() {
+            AccountCreationError::ApiError(api_err) => match api_err {
+                Error::Api(api_api_err) => {
+                    match api_api_err {
+                        UsernameTaken => {
+                            return; // Test passed
+                        },
+                        _ => {}
+                    }
+                },
+                _ => {}
+            },
+            _ => {}
+        };
+
+        panic!("This username should have been taken.")
+    }
+
+
+    #[test]
+    fn invalid_username_test() {
+        let db = test_db();
+        let username = &random_username();
+        DefaultAccountService::create_account(&db, username).unwrap();
+        match DefaultAccountService::create_account(&db, username).unwrap_err() {
+            AccountCreationError::ApiError(api_err) => match api_err {
+                Error::Api(api_api_err) => {
+                    match api_api_err {
+                        UsernameTaken => {},
+                        NewAccountError::InternalError => {},
+                        NewAccountError::InvalidAuth => {},
+                        NewAccountError::ExpiredAuth => {},
+                        NewAccountError::InvalidPublicKey => {},
+                        NewAccountError::InvalidUsername => {},
+                        NewAccountError::FileIdTaken => {},
+                    }
+                },
+                _ => {}
+            },
+            _ => {}
+        }
     }
 }
