24e9131adb4cc5a6b93fe10c9c10b6d2a0273514
Parth <parth@mehrotra.me>
2020-07-03 21:24:27 -0400

    fixed username algo

--- server/src/file_service.rs
+++ server/src/file_service.rs
@@ -8,7 +8,7 @@ pub fn username_is_valid(username: &str) -> bool {
     username
         .to_lowercase()
         .chars()
-        .all(|c| (c > 'a' && c < 'z') || (c > '0' && c < '9'))
+        .all(|c| (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
 }
 
 pub async fn change_document_content(
