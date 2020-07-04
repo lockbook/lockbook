0f5171bcba485ab7d2cb7e67940842afe988b1be
Parth <parth@mehrotra.me>
2020-07-04 12:52:37 -0400

    added some sync tests

--- integration_tests/src/sync_service_tests.rs
+++ integration_tests/src/sync_service_tests.rs
@@ -1,10 +1,11 @@
 #[cfg(test)]
 mod sync_tests {
     use crate::{random_username, test_db};
+    use lockbook_core::model::work_unit::WorkUnit;
     use lockbook_core::service::account_service::AccountService;
     use lockbook_core::service::file_service::FileService;
-    use lockbook_core::{DefaultAccountService, DefaultFileService, DefaultSyncService};
     use lockbook_core::service::sync_service::SyncService;
+    use lockbook_core::{DefaultAccountService, DefaultFileService, DefaultSyncService};
 
     #[test]
     fn new_file_sync() {
@@ -18,6 +19,24 @@ mod sync_tests {
 
         let work_units = DefaultSyncService::calculate_work(&db).unwrap();
 
-        assert_eq!(work_units.most_recent_update_from_server, 0);
+        assert!(work_units.work_units.clone().into_iter().any(|wu| match wu {
+            WorkUnit::PushNewDocument(doc) => doc == file,
+            _ => false,
+        }));
+
+        assert!(work_units.work_units.clone().into_iter().any(|wu| match wu {
+             WorkUnit::PushNewFolder(folder) => folder.name == "a",
+            _ => false,
+        }));
+
+        assert!(work_units.work_units.clone().into_iter().any(|wu| match wu {
+             WorkUnit::PushNewFolder(folder) => folder.name == "b",
+            _ => false,
+        }));
+
+        assert!(work_units.work_units.clone().into_iter().any(|wu| match wu {
+             WorkUnit::PushNewFolder(folder) => folder.name == "c",
+            _ => false,
+        }));
     }
 }
