f25b89604e5db38078a5c544ad7bce10e0a3a978
Parth <parth@mehrotra.me>
2020-07-05 22:59:20 -0400

    cargo fmt

--- integration_tests/src/sync_service_tests.rs
+++ integration_tests/src/sync_service_tests.rs
@@ -3,21 +3,26 @@ mod sync_tests {
     use crate::{random_username, test_db};
     use lockbook_core::service::account_service::AccountService;
     use lockbook_core::service::file_service::FileService;
-    use lockbook_core::{DefaultAccountService, DefaultFileService, DefaultSyncService};
     use lockbook_core::service::sync_service::SyncService;
+    use lockbook_core::{DefaultAccountService, DefaultFileService, DefaultSyncService};
 
     #[test]
     fn new_file_sync() {
         let db = test_db();
         let account = DefaultAccountService::create_account(&db, &random_username()).unwrap();
 
-        assert_eq!(DefaultSyncService::calculate_work(&db).unwrap().work_units.len(), 0);
+        assert_eq!(
+            DefaultSyncService::calculate_work(&db)
+                .unwrap()
+                .work_units
+                .len(),
+            0
+        );
 
         let file = DefaultFileService::create_at_path(
             &db,
             format!("{}/a/b/c/test", account.username).as_str(),
         )
         .unwrap();
-
     }
 }
