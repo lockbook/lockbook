79bf2caf28bb4489d9f6b8b6984a3a6be044865b
Smail <SmailBarkouch1@gmail.com>
2020-07-06 12:10:48 -0400

    some changes

--- clients/android/core/src/main/java/app/lockbook/core/core.kt
+++ clients/android/core/src/main/java/app/lockbook/core/core.kt
@@ -6,7 +6,7 @@ external fun createAccount(path: String, username: String): Int
 external fun importAccount(path: String, accountString: String): Int
 external suspend fun getRoot(path: String): String
 external suspend fun getChildren(path: String, parentUuid: String): String
-external suspend fun getFile(path: String, fileUuid: String): String
+external suspend fun getFileMetadata(path: String, fileUuid: String): String
 
 fun loadLockbookCore() {
     System.loadLibrary("lockbook_core")
