25fbf79b328187fda4fa5b576cd8cf3ee1c06ccb
Raayan Pillai <raayan@raayanpillai.com>
2020-07-06 13:33:24 -0400

    fixing bad unwraps and setting up some ios tests

--- core/src/model/work_unit.rs
+++ core/src/model/work_unit.rs
@@ -1,10 +1,33 @@
 use crate::model::file_metadata::FileMetadata;
 
-use serde::Serialize;
+use serde::ser::SerializeStructVariant;
+use serde::{Serialize, Serializer};
 
-#[derive(Serialize, Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum WorkUnit {
     LocalChange { metadata: FileMetadata },
 
     ServerChange { metadata: FileMetadata },
+}
+
+impl Serialize for WorkUnit {
+    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
+    where
+        S: Serializer,
+    {
+        match *self {
+            WorkUnit::LocalChange { ref metadata } => {
+                let mut sv =
+                    serializer.serialize_struct_variant("work_unit", 0, "local_change", 1)?;
+                sv.serialize_field("metadata", metadata)?;
+                sv.end()
+            }
+            WorkUnit::ServerChange { ref metadata } => {
+                let mut sv =
+                    serializer.serialize_struct_variant("work_unit", 1, "server_change", 1)?;
+                sv.serialize_field("metadata", metadata)?;
+                sv.end()
+            }
+        }
+    }
 }
