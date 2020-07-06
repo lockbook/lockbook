0237414ed0b561a5dd618cc8693958f3462c18a1
Parth <parth@mehrotra.me>
2020-07-05 20:44:34 -0400

    new sync builds

--- core/src/model/work_unit.rs
+++ core/src/model/work_unit.rs
@@ -1,15 +1,11 @@
 use crate::model::file_metadata::FileMetadata;
 use crate::model::local_changes::LocalChange;
 use serde::Serialize;
+use std::cmp::Ordering;
 
 #[derive(Serialize, Debug, Clone, PartialEq)]
 pub enum WorkUnit {
-    LocalChange {
-        metadata: FileMetadata,
-        change_description: LocalChange,
-    },
+    LocalChange { metadata: FileMetadata },
 
-    ServerChange {
-        metadata: FileMetadata,
-    },
+    ServerChange { metadata: FileMetadata },
 }
