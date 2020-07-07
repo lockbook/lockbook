4aadc8990d4138d8a96bff2212c8f22189bf8734
Raayan Pillai <raayan@raayanpillai.com>
2020-07-06 23:51:54 -0400

    things are looking good now

--- clients/apple/iosTests/iosTests.swift
+++ clients/apple/iosTests/iosTests.swift
@@ -10,33 +10,87 @@ import XCTest
 @testable import Lockbook
 
 class CoreApiTests: XCTestCase {
-//    let core = CoreApi(documentsDirectory: FileManager.default.temporaryDirectory.absoluteString)
-    let core = CoreApi(documentsDirectory: "/Users/raayanpillai/.lockbook")
+    static let tempDir = NSTemporaryDirectory().appending(UUID.init().uuidString)
+    static let core = CoreApi(documentsDirectory: CoreApiTests.tempDir)
+//    let core = CoreApi(documentsDirectory: "/Users/raayanpillai/.lockbook")
     
     override class func setUp() {
         // Start logger
         init_logger()
     }
     
-    func testCalculateWork() {
-        let result = core.calculateWork()
+    override func setUp() {
+        print(CoreApiTests.self.core.documentsDirectory)
+    }
+    
+    func test01ImportAccount() {
+        let bundle = Bundle(for: type(of: self))
+        guard let url = bundle.url(forResource: "accountString", withExtension: "txt"), let data = try? String(contentsOf: url) else {
+            return XCTFail("Could not load Account String")
+        }
+        
+        let result = CoreApiTests.core.importAccount(accountString: data.trimmingCharacters(in: .whitespacesAndNewlines))
         
         switch result {
-            case .success(let workUnits):
-                XCTAssertEqual(workUnits.count, 0)
-            case .failure(let error):
-                XCTFail(error.message)
+        case .success(let account):
+            XCTAssertEqual(account.username, "raayan")
+        case .failure(let error):
+            XCTFail(error.message)
         }
     }
     
-    func testListFiles() {
-        let result = core.listFiles(dirId: core.getRoot())
+    func test02Sync() {
+        let result = CoreApiTests.core.synchronize()
         
         switch result {
+        case .success(let b):
+            XCTAssert(b)
+        case .failure(let error):
+            XCTFail(error.message)
+        }
+    }
+    
+    func test03ListFiles() {
+        do {
+            let root = try CoreApiTests.core.getRoot().get()
+            let result = CoreApiTests.core.listFiles(dirId: root.id)
+            
+            switch result {
             case .success(let files):
                 XCTAssertEqual(files.count, 2)
             case .failure(let error):
                 XCTFail(error.message)
+            }
+        } catch let error as CoreError {
+            XCTFail(error.message)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func test04CreateFile() {
+        guard let root = try? CoreApiTests.core.getRoot().get() else {
+            return XCTFail("Couldn't get root!")
+        }
+        
+        let result = CoreApiTests.core.createFile(name: "test.md", dirId: root.id)
+        
+        switch result {
+        case .success(let meta):
+            XCTAssertEqual(meta.name, "test.md")
+        case .failure(let error):
+            XCTFail(error.message)
+        }
+    }
+    
+    func test05CalculateWork() {
+        let result = CoreApiTests.core.calculateWork()
+        
+        switch result {
+        case .success(let workUnits):
+            XCTAssertEqual(workUnits.count, 1)
+        case .failure(let error):
+            XCTFail(error.message)
         }
     }
 }
@@ -45,7 +99,7 @@ class UtilTests: XCTestCase {
     func testWorkUnitDecoding() {
         let bundle = Bundle(for: type(of: self))
         guard let url = bundle.url(forResource: "workUnits", withExtension: "json"), let data = try? String(contentsOf: url) else {
-            return
+            return XCTFail("Could not load JSON")
         }
         
         if let workUnits: [WorkUnit] = (try? deserialize(jsonStr: data).get()) {
