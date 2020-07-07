9b5cb3c7256eac22cea0273e287345b30a0e2c72
Parth <parth@mehrotra.me>
2020-07-06 21:56:22 -0400

    added more integration tests

--- integration_tests/src/sync_service_tests.rs
+++ integration_tests/src/sync_service_tests.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod sync_tests {
     use crate::{random_username, test_db};
+    use lockbook_core::model::crypto::DecryptedValue;
+    use lockbook_core::model::work_unit::WorkUnit;
     use lockbook_core::repo::file_metadata_repo::FileMetadataRepo;
     use lockbook_core::service::account_service::AccountService;
     use lockbook_core::service::file_service::FileService;
@@ -58,5 +60,67 @@ mod sync_tests {
             DefaultFileMetadataRepo::get_all(&db).unwrap(),
             DefaultFileMetadataRepo::get_all(&db2).unwrap()
         );
+
+        assert_eq!(
+            DefaultSyncService::calculate_work(&db2)
+                .unwrap()
+                .work_units
+                .len(),
+            0
+        );
+    }
+
+    #[test]
+    fn test_edit_document_sync() {
+        let db = test_db();
+        let account = DefaultAccountService::create_account(&db, &random_username()).unwrap();
+
+        assert_eq!(
+            DefaultSyncService::calculate_work(&db)
+                .unwrap()
+                .work_units
+                .len(),
+            0
+        );
+
+        let file = DefaultFileService::create_at_path(
+            &db,
+            format!("{}/a/b/c/test", account.username).as_str(),
+        )
+        .unwrap();
+
+        assert!(DefaultSyncService::sync(&db).is_ok());
+
+        let db2 = test_db();
+        DefaultAccountService::import_account(
+            &db2,
+            &DefaultAccountService::export_account(&db).unwrap(),
+        )
+        .unwrap();
+
+        DefaultSyncService::sync(&db2).unwrap();
+
+        DefaultFileService::write_document(&db, file.id, &DecryptedValue::from("")).unwrap();
+
+        assert_eq!(
+            DefaultSyncService::calculate_work(&db)
+                .unwrap()
+                .work_units
+                .len(),
+            1
+        );
+
+        match DefaultSyncService::calculate_work(&db)
+            .unwrap()
+            .work_units
+            .get(0)
+            .unwrap()
+            .clone()
+        {
+            WorkUnit::LocalChange { metadata } => assert_eq!(metadata.name, file.name),
+            WorkUnit::ServerChange { .. } => {
+                panic!("This should have been a local change with no server changes!")
+            }
+        };
     }
 }
--- server/src/file_service.rs
+++ server/src/file_service.rs
@@ -2,7 +2,6 @@ use crate::files_db;
 use crate::index_db;
 use crate::ServerState;
 use lockbook_core::model::api::*;
-use lockbook_core::model::crypto::SignedValue;
 use lockbook_core::model::file_metadata::FileType;
 
 pub fn username_is_valid(username: &str) -> bool {
