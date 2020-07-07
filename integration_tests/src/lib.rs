61e26c479c293c34d7d5090d6e7a52a79ad815d1
Travis Vanderstad <t.vanderstad@gmail.com>
2020-07-07 19:49:49 -0400

    adds reate document tests

--- integration_tests/src/lib.rs
+++ integration_tests/src/lib.rs
@@ -1,18 +1,17 @@
-use lockbook_core::model::state::Config;
-use lockbook_core::model::crypto::*;
 use lockbook_core::model::account::Account;
 use lockbook_core::model::crypto::SignedValue;
+use lockbook_core::model::crypto::*;
+use lockbook_core::model::state::Config;
 use lockbook_core::repo::db_provider::{DbProvider, TempBackedDB};
 use lockbook_core::service::auth_service::{AuthService, AuthServiceImpl};
 use lockbook_core::service::clock_service::ClockImpl;
+use lockbook_core::service::crypto_service::{
+    AesImpl, PubKeyCryptoService, RsaImpl, SymmetricCryptoService,
+};
 use lockbook_core::Db;
+use rsa::RSAPublicKey;
 use std::env;
 use uuid::Uuid;
-use lockbook_core::service::crypto_service::{
-    PubKeyCryptoService,
-    SymmetricCryptoService, RsaImpl, AesImpl
-};
-use rsa::{RSAPublicKey};
 
 #[cfg(test)]
 #[macro_use]
@@ -29,8 +28,9 @@ mod macros {
     );
 }
 mod account_service_tests;
-mod sync_service_tests;
+mod create_document_tests;
 mod new_account_tests;
+mod sync_service_tests;
 
 pub fn test_db() -> Db {
     let config = Config {
@@ -47,8 +47,20 @@ pub fn random_username() -> String {
         .collect()
 }
 
+pub fn random_filename() -> String {
+    Uuid::new_v4()
+        .to_string()
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .collect()
+}
+
 pub fn api_loc() -> String {
-    format!("http://{}:{}", env::var("SERVER_HOST").unwrap(), env::var("SERVER_PORT").unwrap())
+    format!(
+        "http://{}:{}",
+        env::var("SERVER_HOST").unwrap(),
+        env::var("SERVER_PORT").unwrap()
+    )
 }
 
 pub fn generate_account() -> Account {
@@ -62,10 +74,32 @@ pub fn sign(account: &Account) -> SignedValue {
     AuthServiceImpl::<ClockImpl, RsaImpl>::generate_auth(&account).unwrap()
 }
 
-pub fn aes(encrypting_key: &AesKey, encrypted_key: &AesKey) -> EncryptedValueWithNonce {
-    AesImpl::encrypt(&encrypting_key, &DecryptedValue { secret: encrypted_key.key.clone() }).unwrap()
+pub fn aes_key(encrypting_key: &AesKey, encrypted_key: &AesKey) -> EncryptedValueWithNonce {
+    AesImpl::encrypt(
+        &encrypting_key,
+        &DecryptedValue {
+            secret: encrypted_key.key.clone(),
+        },
+    )
+    .unwrap()
 }
 
-pub fn rsa(encrypting_key: &RSAPublicKey, encrypted_key: &AesKey) -> EncryptedValue {
-    RsaImpl::encrypt(encrypting_key, &DecryptedValue { secret: encrypted_key.key.clone() }).unwrap()
+pub fn aes_str(encrypting_key: &AesKey, encrypted_str: &str) -> EncryptedValueWithNonce {
+    AesImpl::encrypt(
+        &encrypting_key,
+        &DecryptedValue {
+            secret: String::from(encrypted_str),
+        },
+    )
+    .unwrap()
+}
+
+pub fn rsa_key(encrypting_key: &RSAPublicKey, encrypted_key: &AesKey) -> EncryptedValue {
+    RsaImpl::encrypt(
+        encrypting_key,
+        &DecryptedValue {
+            secret: encrypted_key.key.clone(),
+        },
+    )
+    .unwrap()
 }
