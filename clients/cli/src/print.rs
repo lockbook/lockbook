6043aba4ef318d3ad7cf8fda3e082388cd627e61
Parth <parth@mehrotra.me>
2020-07-10 18:07:05 -0400

    make cli take arguments instead of reading from stdin

--- clients/cli/src/print.rs
+++ clients/cli/src/print.rs
@@ -7,19 +7,8 @@ use lockbook_core::{DefaultFileMetadataRepo, DefaultFileService};
 
 use crate::utils::{connect_to_db, get_account};
 
-pub fn print() {
+pub fn print(file_name: &str) {
     get_account(&connect_to_db());
-
-    if atty::is(atty::Stream::Stdin) {
-        print!("Enter a filepath: ");
-    }
-
-    io::stdout().flush().unwrap();
-    let mut file_name = String::new();
-    io::stdin()
-        .read_line(&mut file_name)
-        .expect("Failed to read from stdin");
-    file_name.retain(|c| !c.is_whitespace());
 
     let file_metadata = DefaultFileMetadataRepo::get_by_path(&connect_to_db(), &file_name)
         .expect("Could not search files ")
