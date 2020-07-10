4553b9b5c2811c4e938aa1bf439170cf8055822f
Smail <SmailBarkouch1@gmail.com>
2020-07-10 11:40:09 -0400

    lots of additions, crashes though

---
+++ clients/android/app/src/main/java/app/lockbook/loggedin/newfilefolder/NewFileFolderActivity.kt
@@ -0,0 +1,55 @@
+package app.lockbook.loggedin.newfilefolder
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import app.lockbook.R
+import app.lockbook.core.createFileFolder
+import app.lockbook.databinding.ActivityImportAccountBinding
+import app.lockbook.databinding.ActivityNewFileFolderBinding
+import app.lockbook.utils.FileType
+import com.beust.klaxon.Klaxon
+import kotlinx.android.synthetic.main.activity_new_file_folder.*
+import kotlinx.android.synthetic.main.activity_new_file_folder.name_text
+import kotlinx.android.synthetic.main.activity_popup_info.*
+import kotlinx.coroutines.*
+
+class NewFileFolderActivity : Activity() {
+
+    var job = Job()
+    private val uiScope = CoroutineScope(Dispatchers.Main + job)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityNewFileFolderBinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_new_file_folder
+        )
+
+        binding.newFileFolderActivity = this
+    }
+
+    fun createFileFolder() {
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+                val fileType = if (file_radio_button.isSelected) {
+                    FileType.Document.toString()
+                } else {
+                    FileType.Document.toString()
+                }
+
+                createFileFolder(
+                    intent.getStringExtra("path"),
+                    intent.getStringExtra("parentUuid"),
+                    fileType,
+                    name_text.text.toString()
+                )
+
+                withContext(Dispatchers.Main) {
+                    finish()
+                }
+            }
+        }
+
+    }
+}
