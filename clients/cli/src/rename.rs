12917983acfb81c480f5a7633a0d0f886e39ef7c
Parth <parth@mehrotra.me>
2020-07-12 00:17:03 -0400

    cargo fmt

--- clients/cli/src/rename.rs
+++ clients/cli/src/rename.rs
@@ -1,8 +1,8 @@
 use crate::utils::{connect_to_db, get_account};
-use lockbook_core::repo::file_metadata_repo::{FileMetadataRepo};
+use lockbook_core::repo::file_metadata_repo::FileMetadataRepo;
+use lockbook_core::service::file_service::{DocumentRenameError, FileService};
 use lockbook_core::{DefaultFileMetadataRepo, DefaultFileService};
 use std::process::exit;
-use lockbook_core::service::file_service::{FileService, DocumentRenameError};
 
 pub fn rename(path: &str, new_name: &str) {
     let db = connect_to_db();
@@ -12,30 +12,28 @@ pub fn rename(path: &str, new_name: &str) {
         Ok(maybe_fm) => match maybe_fm {
             None => {
                 eprintln!("That path does not exist!");
-            },
-            Some(fm) => {
-                match DefaultFileService::rename_file(&db, fm.id, new_name) {
-                    Ok(_) => {
-                        db.flush().unwrap();
-                    },
-                    Err(rename_error) => match rename_error {
-                        DocumentRenameError::FileNameContainsSlash => {
-                            eprintln!("The new name cannot contain a slash.");
-                        },
-                        DocumentRenameError::FileNameNotAvailable => {
-                            eprintln!("A file with this name exists at this location already.");
-                        },
-                        DocumentRenameError::FileDoesNotExist |
-                        DocumentRenameError::DbError(_) |
-                        DocumentRenameError::FailedToRecordChange(_) => {
-                            eprintln!("An unexpected error occurred! {:#?}", rename_error);
-                        },
-                    },
+            }
+            Some(fm) => match DefaultFileService::rename_file(&db, fm.id, new_name) {
+                Ok(_) => {
+                    db.flush().unwrap();
                 }
+                Err(rename_error) => match rename_error {
+                    DocumentRenameError::FileNameContainsSlash => {
+                        eprintln!("The new name cannot contain a slash.");
+                    }
+                    DocumentRenameError::FileNameNotAvailable => {
+                        eprintln!("A file with this name exists at this location already.");
+                    }
+                    DocumentRenameError::FileDoesNotExist
+                    | DocumentRenameError::DbError(_)
+                    | DocumentRenameError::FailedToRecordChange(_) => {
+                        eprintln!("An unexpected error occurred! {:#?}", rename_error);
+                    }
+                },
             },
         },
         Err(err) => {
             eprintln!("An unexpected error occurred! {:#?}", err);
-        },
+        }
     }
 }
