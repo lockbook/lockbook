41f72be4f12417ea67278bb6dbda7f9601670e37
Smail <SmailBarkouch1@gmail.com>
2020-07-12 18:22:17 -0400

    added qr code scanner and made opimizations

--- clients/android/app/src/main/java/app/lockbook/login/ImportAccountActivity.kt
+++ clients/android/app/src/main/java/app/lockbook/login/ImportAccountActivity.kt
@@ -9,16 +9,22 @@ import app.lockbook.loggedin.mainscreen.MainScreenActivity
 import app.lockbook.R
 import app.lockbook.core.importAccount
 import app.lockbook.databinding.ActivityImportAccountBinding
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_import_account.*
 import kotlinx.coroutines.*
 
 class ImportAccountActivity : AppCompatActivity() {
 
+    companion object {
+
+        private const val QR_CODE_SCANNER_REQUEST_CODE = 101
+        private const val OK = 0 // should handle
+        private const val ACCOUNT_STRING_INVALID = 2    // should handle
+    }
+
     private var job = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + job)
-
-    private val success = 0                 // should handle
-    private val accountStringInvalid = 2    // should handle
+    private val intentIntegrator: IntentIntegrator = IntentIntegrator(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,30 +32,61 @@ class ImportAccountActivity : AppCompatActivity() {
         val binding: ActivityImportAccountBinding = DataBindingUtil.setContentView(this,
             R.layout.activity_import_account
         )
+
         binding.importAccountActivity = this
     }
 
     fun importAccountFromAccountString() {
         uiScope.launch {
             withContext(Dispatchers.IO) {
-                when (importAccount(filesDir.absolutePath, account_string.text.toString())) {
-                    success -> {
-                        startActivity(Intent(applicationContext, MainScreenActivity::class.java))
-                        finishAffinity()
-                    }
-                    accountStringInvalid -> Toast.makeText(
-                        applicationContext,
-                        "Account String invalid!",
-                        Toast.LENGTH_LONG
-                    ).show()
-                    else -> Toast.makeText(
-                        applicationContext,
-                        "Unexpected error occured, please create a bug report (activity_settings)",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
+            handleImportResult(importAccountFromString(account_string.text.toString()))
             }
         }
+    }
 
+    fun navigateToQRCodeScanner() {
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+                intentIntegrator.setBeepEnabled(true).setRequestCode(QR_CODE_SCANNER_REQUEST_CODE).initiateScan()
+            }
+        }
+    }
+    
+    private fun importAccountFromString(account: String): Int {
+        return importAccount(filesDir.absolutePath, account)
+    }
+
+    private fun handleImportResult(resultCode: Int) {
+        when (resultCode) {
+            OK -> {
+                startActivity(Intent(applicationContext, MainScreenActivity::class.java))
+                finishAffinity()
+            }
+            ACCOUNT_STRING_INVALID -> Toast.makeText(
+                applicationContext,
+                "Account String invalid!",
+                Toast.LENGTH_LONG
+            ).show()
+            else -> Toast.makeText(
+                applicationContext,
+                "Unexpected error occured, please create a bug report (activity_settings)",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        uiScope.launch {
+            withContext(Dispatchers.IO) {
+                if (requestCode == QR_CODE_SCANNER_REQUEST_CODE) {
+                    val intentResult =
+                        IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+                    if (intentResult.contents != null) {
+                        handleImportResult(importAccountFromString(intentResult.contents))
+                    }
+                }
+                super.onActivityResult(requestCode, resultCode, data)
+            }
+        }
     }
 }
