5ad782942b30593c14f81e10bd79883db35702cb
Parth <parth@mehrotra.me>
2020-07-13 19:21:50 -0400

    format and expose db

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,23 +2,23 @@ extern crate reqwest;
 
 #[macro_use]
 extern crate log;
-
-use crate::service::crypto_service::{RsaImpl, AesImpl};
-use crate::repo::db_provider::DiskBackedDB;
 use crate::client::ClientImpl;
 use crate::repo::account_repo::AccountRepoImpl;
-use crate::service::clock_service::ClockImpl;
-use crate::service::auth_service::AuthServiceImpl;
-use crate::service::account_service::AccountServiceImpl;
+use crate::repo::db_provider::DiskBackedDB;
+use crate::repo::document_repo::DocumentRepoImpl;
 use crate::repo::file_metadata_repo::FileMetadataRepoImpl;
 use crate::repo::local_changes_repo::LocalChangesRepoImpl;
-use crate::repo::document_repo::DocumentRepoImpl;
+use crate::service::account_service::AccountServiceImpl;
+use crate::service::auth_service::AuthServiceImpl;
+use crate::service::clock_service::ClockImpl;
+use crate::service::crypto_service::{AesImpl, RsaImpl};
 use crate::service::file_encryption_service::FileEncryptionServiceImpl;
-use crate::service::sync_service::FileSyncService;
 use crate::service::file_service::FileServiceImpl;
+use crate::service::sync_service::FileSyncService;
+pub use sled::Db;
 
-pub mod client;
 pub mod c_interface;
+pub mod client;
 pub mod model;
 pub mod repo;
 pub mod service;
