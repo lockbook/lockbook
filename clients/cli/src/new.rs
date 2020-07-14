fa424d23f2a04e169fbf3d04cc6fb32a77f9a235
Parth <parth@mehrotra.me>
2020-07-13 22:42:49 -0400

    cargo fmt

--- clients/cli/src/new.rs
+++ clients/cli/src/new.rs
@@ -6,14 +6,20 @@ use lockbook_core::model::crypto::DecryptedValue;
 use lockbook_core::model::file_metadata::FileType::Folder;
 use lockbook_core::repo::file_metadata_repo::FileMetadataRepo;
 use lockbook_core::service::file_service::{FileService, NewFileFromPathError};
-use lockbook_core::{get_account, DefaultFileMetadataRepo, DefaultFileService, GetAccountError, create_file_at_path, CreateFileAtPathEnum};
+use lockbook_core::{
+    create_file_at_path, get_account, CreateFileAtPathEnum, DefaultFileMetadataRepo,
+    DefaultFileService, GetAccountError,
+};
 use uuid::Uuid;
 
 use crate::utils::{connect_to_db, edit_file_with_editor, exit_with, get_config};
-use crate::{NO_ACCOUNT, UNEXPECTED_ERROR, FILE_ALREADY_EXISTS, NO_ROOT, PATH_NO_ROOT, DOCUMENT_TREATED_AS_FOLDER, SUCCESS};
-use std::process::exit;
+use crate::{
+    DOCUMENT_TREATED_AS_FOLDER, FILE_ALREADY_EXISTS, NO_ACCOUNT, NO_ROOT, PATH_NO_ROOT, SUCCESS,
+    UNEXPECTED_ERROR,
+};
 use lockbook_core::model::file_metadata::FileMetadata;
 use std::io::Error;
+use std::process::exit;
 
 pub fn new(file_name: &str) {
     match get_account(&get_config()) {
@@ -40,19 +46,36 @@ pub fn new(file_name: &str) {
         Ok(file_metadata) => file_metadata,
         Err(err) => {
             match fs::remove_file(&temp_file_path) {
-                Ok(_) => eprintln!("Aborted due to a problem, temp file cleaned up successfully: {}", file_location),
-                Err(io_err) => eprintln!("Aborted due to problem, temp file not cleaned up! Location: {}, error: {}", file_location, io_err),
+                Ok(_) => eprintln!(
+                    "Aborted due to a problem, temp file cleaned up successfully: {}",
+                    file_location
+                ),
+                Err(io_err) => eprintln!(
+                    "Aborted due to problem, temp file not cleaned up! Location: {}, error: {}",
+                    file_location, io_err
+                ),
             }
 
             match err {
-                CreateFileAtPathEnum::FileAlreadyExists => exit_with("File already exists!", FILE_ALREADY_EXISTS),
-                CreateFileAtPathEnum::NoAccount => exit_with("No account! Run init or import to get started!", NO_ACCOUNT),
-                CreateFileAtPathEnum::NoRoot => exit_with("No root folder, have you synced yet?", NO_ROOT),
-                CreateFileAtPathEnum::PathDoesntStartWithRoot => exit_with("Path doesn't start with your root folder.", PATH_NO_ROOT),
-                CreateFileAtPathEnum::DocumentTreatedAsFolder => exit_with("A file within your path is a document that was treated as a folder", DOCUMENT_TREATED_AS_FOLDER),
+                CreateFileAtPathEnum::FileAlreadyExists => {
+                    exit_with("File already exists!", FILE_ALREADY_EXISTS)
+                }
+                CreateFileAtPathEnum::NoAccount => {
+                    exit_with("No account! Run init or import to get started!", NO_ACCOUNT)
+                }
+                CreateFileAtPathEnum::NoRoot => {
+                    exit_with("No root folder, have you synced yet?", NO_ROOT)
+                }
+                CreateFileAtPathEnum::PathDoesntStartWithRoot => {
+                    exit_with("Path doesn't start with your root folder.", PATH_NO_ROOT)
+                }
+                CreateFileAtPathEnum::DocumentTreatedAsFolder => exit_with(
+                    "A file within your path is a document that was treated as a folder",
+                    DOCUMENT_TREATED_AS_FOLDER,
+                ),
                 CreateFileAtPathEnum::UnexpectedError(msg) => exit_with(&msg, UNEXPECTED_ERROR),
             }
-        },
+        }
     };
 
     if file_metadata.file_type == Folder {
