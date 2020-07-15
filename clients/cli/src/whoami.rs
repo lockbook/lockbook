47288f479b759d7fd2e25b2ac57b5cec482ceeb2
Parth <parth@mehrotra.me>
2020-07-14 21:35:38 -0400

    removed the rest

--- clients/cli/src/whoami.rs
+++ clients/cli/src/whoami.rs
@@ -1,18 +1,14 @@
-use lockbook_core::repo::account_repo::{AccountRepo, AccountRepoError};
-use lockbook_core::DefaultAccountRepo;
+use lockbook_core::{get_account, GetAccountError};
 
-use crate::utils::connect_to_db;
+use crate::utils::{exit_with, exit_with_no_account, get_config};
+use crate::UNEXPECTED_ERROR;
 
 pub fn whoami() {
-    match DefaultAccountRepo::get_account(&connect_to_db()) {
+    match get_account(&get_config()) {
         Ok(account) => println!("{}", account.username),
         Err(err) => match err {
-            AccountRepoError::SledError(_) | AccountRepoError::SerdeError(_) => {
-                eprintln!("Sled error: {:?}", err)
-            }
-            AccountRepoError::NoAccount(_) => {
-                eprintln!("No account found, run init, import or help.")
-            }
+            GetAccountError::NoAccount => exit_with_no_account(),
+            GetAccountError::UnexpectedError(msg) => exit_with(&msg, UNEXPECTED_ERROR),
         },
     }
 }
