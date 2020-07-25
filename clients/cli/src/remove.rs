8ec7a7f722fee21c35ebae1d0989c3298885882e
Parth <parth@mehrotra.me>
2020-07-25 19:10:51 -0400

    public site

--- clients/cli/src/remove.rs
+++
@@ -1,9 +0,0 @@
-use crate::utils::exit_with;
-use crate::UNIMPLEMENTED;
-
-pub fn remove(_file_name: &str) {
-    exit_with(
-        "Removing files has not been implemented yet!",
-        UNIMPLEMENTED,
-    )
-}
