bb221783f6585fd5588e951c365fea5781517ba6
Smail <SmailBarkouch1@gmail.com>
2020-08-03 12:32:28 -0400

    new settings work entirely with biometrics

--- clients/android/app/src/main/java/app/lockbook/loggedin/settings/SettingsFragment.kt
+++ clients/android/app/src/main/java/app/lockbook/loggedin/settings/SettingsFragment.kt
@@ -1,22 +1,31 @@
 package app.lockbook.loggedin.settings
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
+import android.annotation.SuppressLint
+import android.content.*
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.FrameLayout
 import android.widget.PopupWindow
 import android.widget.Toast
+import androidx.biometric.BiometricConstants
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.MutableLiveData
+import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 import app.lockbook.R
 import app.lockbook.utils.AccountExportError
 import app.lockbook.utils.Config
 import app.lockbook.utils.CoreModel
+import app.lockbook.utils.SharedPreferences.BIOMETRIC_NONE
+import app.lockbook.utils.SharedPreferences.BIOMETRIC_OPTION_KEY
+import app.lockbook.utils.SharedPreferences.BIOMETRIC_RECOMMENDED
+import app.lockbook.utils.SharedPreferences.BIOMETRIC_STRICT
+import app.lockbook.utils.SharedPreferences.EXPORT_ACCOUNT_QR_KEY
+import app.lockbook.utils.SharedPreferences.EXPORT_ACCOUNT_RAW_KEY
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import com.google.zxing.BarcodeFormat
@@ -26,21 +35,116 @@ import kotlinx.android.synthetic.main.activity_account_qr_code.view.*
 class SettingsFragment(private val config: Config) : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_preference, rootKey)
+
+        findPreference<Preference>(BIOMETRIC_OPTION_KEY)?.setOnPreferenceChangeListener { preference, newValue ->
+            if (newValue is String) {
+                Log.i("SmailBarkouch", "HERE1")
+                performBiometricFlow(preference.key, newValue)
+            }
+
+            false
+        }
     }
 
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
-        if (preference is Preference) {
-            when (preference.key) {
-                "export_account_raw" -> exportAccountRaw()
-                "export_account_qr" -> exportAccountQR()
-            }
-        } else {
-            Toast.makeText(context, "An unexpected error has occurred!", Toast.LENGTH_LONG).show()
-            return false
+        when (preference?.key) {
+            EXPORT_ACCOUNT_QR_KEY, EXPORT_ACCOUNT_RAW_KEY -> performBiometricFlow(preference.key)
+            else -> super.onPreferenceTreeClick(preference)
         }
 
         return true
     }
+
+    private fun performBiometricFlow(key: String, newValue: String = "") {
+        when (
+            PreferenceManager.getDefaultSharedPreferences(
+                requireContext()
+            ).getString(
+                BIOMETRIC_OPTION_KEY,
+                BIOMETRIC_NONE
+            )
+            ) {
+            BIOMETRIC_RECOMMENDED -> {
+                if (BiometricManager.from(requireContext())
+                        .canAuthenticate() != BiometricManager.BIOMETRIC_SUCCESS
+                ) {
+                    Toast.makeText(
+                        requireContext(),
+                        "An unexpected error has occurred!",
+                        Toast.LENGTH_LONG
+                    )
+                        .show()
+                    return
+                }
+
+                val executor = ContextCompat.getMainExecutor(requireContext())
+                val biometricPrompt = BiometricPrompt(
+                    this, executor,
+                    object : BiometricPrompt.AuthenticationCallback() {
+                        override fun onAuthenticationError(
+                            errorCode: Int,
+                            errString: CharSequence
+                        ) {
+                            super.onAuthenticationError(errorCode, errString)
+                            when (errorCode) {
+                                BiometricConstants.ERROR_HW_UNAVAILABLE, BiometricConstants.ERROR_UNABLE_TO_PROCESS, BiometricConstants.ERROR_NO_BIOMETRICS, BiometricConstants.ERROR_HW_NOT_PRESENT -> {
+                                    Log.i(
+                                        "SettingsFragment",
+                                        "Biometric authentication error: $errString"
+                                    )
+                                    Toast.makeText(
+                                        requireContext(),
+                                        "An unexpected error has occurred!", Toast.LENGTH_SHORT
+                                    )
+                                        .show()
+                                }
+                                BiometricConstants.ERROR_LOCKOUT, BiometricConstants.ERROR_LOCKOUT_PERMANENT -> {
+                                    Toast.makeText(
+                                        requireContext(),
+                                        "Too many tries, try again later!", Toast.LENGTH_SHORT
+                                    )
+                                        .show()
+                                }
+                            }
+                        }
+
+                        override fun onAuthenticationSucceeded(
+                            result: BiometricPrompt.AuthenticationResult
+                        ) {
+                            super.onAuthenticationSucceeded(result)
+                            matchKey(key, newValue)
+                        }
+                    }
+                )
+
+                val promptInfo = BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Lockbook Biometric Verification")
+                    .setSubtitle("Enter your fingerprint to modify this biometric sensitive setting.")
+                    .setDeviceCredentialAllowed(true)
+                    .build()
+
+                biometricPrompt.authenticate(promptInfo)
+
+            }
+            BIOMETRIC_NONE, BIOMETRIC_STRICT -> {
+                Log.i("SmailBarkouch", "HERE2")
+                matchKey(key, newValue)
+            }
+        }
+    }
+
+    private fun matchKey(key: String, newValue: String) {
+        when (key) {
+            EXPORT_ACCOUNT_RAW_KEY -> exportAccountRaw()
+            EXPORT_ACCOUNT_QR_KEY -> exportAccountQR()
+            BIOMETRIC_OPTION_KEY -> changeBiometricPreference(newValue)
+        }
+    }
+
+    private fun changeBiometricPreference(newValue: String) {
+        findPreference<ListPreference>(BIOMETRIC_OPTION_KEY)?.value = newValue
+    }
+
 
     private fun exportAccountQR() {
         when (val exportResult = CoreModel.exportAccount(config)) {
