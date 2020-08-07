8427efda1d45d78f7f0e4e23eea8886eed55b44b
Smail <SmailBarkouch1@gmail.com>
2020-08-07 11:49:05 -0400

    fmt

--- clients/android/app/src/main/java/app/lockbook/loggedin/texteditor/TextEditorViewModelFactory.kt
+++ clients/android/app/src/main/java/app/lockbook/loggedin/texteditor/TextEditorViewModelFactory.kt
@@ -3,7 +3,7 @@ package app.lockbook.loggedin.texteditor
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class TextEditorViewModelFactory(val initialContents: String): ViewModelProvider.Factory {
+class TextEditorViewModelFactory(val initialContents: String) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TextEditorViewModel::class.java))
