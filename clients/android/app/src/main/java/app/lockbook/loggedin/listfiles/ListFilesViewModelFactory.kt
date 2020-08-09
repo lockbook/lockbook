7c1bd2506c1500b5db155f2bb1e3d8fc2f823394
Smail <SmailBarkouch1@gmail.com>
2020-08-09 10:39:31 -0400

    fully working sorting with preferences

--- clients/android/app/src/main/java/app/lockbook/loggedin/listfiles/ListFilesViewModelFactory.kt
+++ clients/android/app/src/main/java/app/lockbook/loggedin/listfiles/ListFilesViewModelFactory.kt
@@ -1,15 +1,17 @@
 package app.lockbook.loggedin.listfiles
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 class ListFilesViewModelFactory(
-    private val path: String
+    private val path: String,
+    private val application: Application
 ) : ViewModelProvider.Factory {
     @Suppress("unchecked_cast")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ListFilesViewModel::class.java))
-            return ListFilesViewModel(path) as T
+            return ListFilesViewModel(path, application) as T
         throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
