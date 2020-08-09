1c671a65c4dfc59e57e4ff678cb385ecc1ac2574
Smail <SmailBarkouch1@gmail.com>
2020-08-09 10:45:23 -0400

    fmt

--- clients/android/app/src/main/java/app/lockbook/loggedin/listfiles/ListFilesFragment.kt
+++ clients/android/app/src/main/java/app/lockbook/loggedin/listfiles/ListFilesFragment.kt
@@ -3,7 +3,6 @@ package app.lockbook.loggedin.listfiles
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
