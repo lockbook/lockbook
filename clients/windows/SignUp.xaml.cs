4e05e2ea2d742e012c8538a0ffc75e13763f55c9
Parth <parth@mehrotra.me>
2020-08-10 23:50:51 -0400

    creating account and doing ffi with freeing memory

--- clients/windows/SignUp.xaml.cs
+++ clients/windows/SignUp.xaml.cs
@@ -1,20 +1,64 @@
-﻿using Windows.Foundation;
+﻿using System;
+using Windows.Foundation;
+using Windows.Storage;
+using Windows.UI.Popups;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using static lockbook.Core;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+namespace lockbook {
 
-namespace lockbook
-{
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class SignUp : Page
-    {
-        public SignUp()
-        {
+    public sealed partial class SignUp : Page {
+        public SignUp() {
             InitializeComponent();
+        }
+
+        private void ImportAccount(object sender, RoutedEventArgs e) {
+            System.Diagnostics.Debug.WriteLine("Import");
+        }
+
+        private async void CreateAccount(object sender, RoutedEventArgs e) {
+            createAccount.IsEnabled = false;
+            progressGroup.Visibility = Visibility.Visible;
+            progressRing.Visibility = Visibility.Visible;
+            progressRing.IsActive = true;
+            error.Visibility = Visibility.Collapsed;
+            var (result, message) = await Core.CreateAccount(username.Text);
+
+            switch (result) {
+                case CreateAccountResult.Success:
+                    Frame.Navigate(typeof(FileExplorer));
+                    break;
+                case CreateAccountResult.UnexpectedError:
+                    await new MessageDialog(message, "Unexpected Error!").ShowAsync();
+                    break;
+                case CreateAccountResult.ContractError:
+                    await new MessageDialog("See logs and file a bug report!", "Contract Error!").ShowAsync();
+                    break;
+                case CreateAccountResult.InvalidUsername:
+                    error.Text = "Invalid username!";
+                    error.Visibility = Visibility.Visible;
+                    break;
+                case CreateAccountResult.UsernameTaken:
+                    error.Text = "Username taken!";
+                    error.Visibility = Visibility.Visible;
+
+                    break;
+                case CreateAccountResult.CouldNotReachServer:
+                    error.Text = "Could not reach server!";
+                    error.Visibility = Visibility.Visible;
+                    break;
+                case CreateAccountResult.AccountExistsAlready:
+                    error.Text = "An account exists already!";
+                    error.Visibility = Visibility.Visible;
+                    break;
+            }
+
+            createAccount.IsEnabled = true;
+            progressGroup.Visibility = Visibility.Collapsed;
+            progressRing.Visibility = Visibility.Collapsed;
+            progressRing.IsActive = false;
         }
     }
 }
