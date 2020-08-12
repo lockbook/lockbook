f3e8ecd0cbd282d9c5ef9dc66f7adb4ba86d0095
Parth <parth@mehrotra.me>
2020-08-11 23:08:20 -0400

    refactored c# <> c# interface to be a bit nicer

---
+++ clients/windows/CoreModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Core {
+    namespace CreateAccount {
+        abstract class Result { }
+
+        class Success : Result { }
+
+        public enum PossibleErrors {
+            UsernameTaken,
+            InvalidUsername,
+            CouldNotReachServer,
+            AccountExistsAlready,
+        }
+
+        class ExpectedError : Result {
+            public PossibleErrors error;
+        }
+
+        class UnexpectedError : Result {
+            public String errorMessage;
+        }
+    }
+}
+
+// Unexpected Error:
+// {"Err":{"UnexpectedError":"Could not connect to db, config: Config {\n    writeable_path: \"\",\n}, error: SledError(\n    Io(\n        Os {\n            code: 5,\n            kind: PermissionDenied,\n            message: \"Access is denied.\",\n        },\n    ),\n)"}}
+
+// Expected Error:
+// {"Err":"InvalidUsername"}
+
+// OK
+// {"Ok":null}
