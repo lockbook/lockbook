3b4631e27e08ed607b72b8f47b7fba6d2142b2ab
Parth <parth@mehrotra.me>
2020-08-13 14:16:18 -0400

    working on account import

--- clients/windows/Core.cs
+++ clients/windows/Core.cs
@@ -1,28 +1,25 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using Windows.Data.Json;
-using Windows.UI.Popups;
-using Windows.Web.AtomPub;
 
 namespace lockbook {
     class CoreService {
         static String path = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
 
         [DllImport("lockbook_core.dll")]
-        private static extern System.IntPtr create_account(string path, string username);
+        private static extern IntPtr create_account(string path, string username);
 
         [DllImport("lockbook_core.dll")]
-        private static extern System.IntPtr get_account(string path);
-
+        private static extern IntPtr get_account(string path);
 
         [DllImport("lockbook_core.dll")]
-        private unsafe static extern void release_pointer(System.IntPtr str_pointer);
+        private static extern IntPtr import_account(string path, string account_string);
 
-        private static String getStringAndRelease(System.IntPtr pointer) {
+        [DllImport("lockbook_core.dll")]
+        private unsafe static extern void release_pointer(IntPtr str_pointer);
+
+        private static String getStringAndRelease(IntPtr pointer) {
             String temp_string = Marshal.PtrToStringAnsi(pointer);
             String result = (String)temp_string.Clone();
             release_pointer(pointer);
@@ -73,14 +70,92 @@ namespace lockbook {
                 errorMessage = "Contract error!"
             };
         }
+
+        public static bool AccountExists() {
+            String result = getStringAndRelease(get_account(path));
+            JObject obj = JObject.Parse(result);
+            JToken ok = obj.SelectToken("Ok", errorWhenNoMatch: false);
+            return ok != null;
+        }
+
+        public static async Task<Core.GetAccount.Result> GetAccount() {
+            String result = await Task.Run(() => getStringAndRelease(get_account(path)));
+
+
+            JObject obj = JObject.Parse(result);
+
+            JToken unexpectedError = obj.SelectToken("Err.UnexpectedError", errorWhenNoMatch: false);
+            JToken expectedError = obj.SelectToken("Err", errorWhenNoMatch: false);
+            JToken ok = obj.SelectToken("Ok", errorWhenNoMatch: false);
+
+            if (unexpectedError != null) {
+                return new Core.GetAccount.UnexpectedError {
+                    errorMessage = result
+                };
+            }
+
+            if (expectedError != null) {
+                switch (expectedError.ToString()) {
+                    case "NoAccount":
+                        return new Core.GetAccount.ExpectedError {
+                            error = Core.GetAccount.PossibleErrors.NoAccount
+                        };
+                };
+            }
+
+            if (ok != null) {
+                return new Core.GetAccount.Success {
+                    accountJson = ok.ToString()
+                };
+            }
+
+            return new Core.GetAccount.UnexpectedError {
+                errorMessage = "Contract error!"
+            };
+        }
+        public static async Task<Core.CreateAccount.Result> ImportAccount(String account_string) {
+            String result = await Task.Run(() => getStringAndRelease(import_account(path, account_string)));
+
+            JObject obj = JObject.Parse(result);
+
+            JToken unexpectedError = obj.SelectToken("Err.UnexpectedError", errorWhenNoMatch: false);
+            JToken expectedError = obj.SelectToken("Err", errorWhenNoMatch: false);
+            JToken ok = obj.SelectToken("Ok", errorWhenNoMatch: false);
+
+            if (unexpectedError != null) {
+                return new Core.CreateAccount.UnexpectedError {
+                    errorMessage = result
+                };
+            }
+
+            if (expectedError != null) {
+                switch (expectedError.ToString()) {
+                    case "InvalidUsername":
+                        return new Core.CreateAccount.ExpectedError {
+                            error = Core.CreateAccount.PossibleErrors.InvalidUsername
+                        };
+                    case "UsernameTaken":
+                        return new Core.CreateAccount.ExpectedError {
+                            error = Core.CreateAccount.PossibleErrors.UsernameTaken
+                        };
+                    case "CouldNotReachServer":
+                        return new Core.CreateAccount.ExpectedError {
+                            error = Core.CreateAccount.PossibleErrors.CouldNotReachServer
+                        };
+                    case "AccountExistsAlready":
+                        return new Core.CreateAccount.ExpectedError {
+                            error = Core.CreateAccount.PossibleErrors.AccountExistsAlready
+                        };
+                }
+            }
+
+            if (ok != null) {
+                return new Core.CreateAccount.Success { };
+            }
+
+            return new Core.CreateAccount.UnexpectedError {
+                errorMessage = "Contract error!"
+            };
+        }
     }
 }
-
-// Unexpected Error:
-// {"Err":{"UnexpectedError":"Could not connect to db, config: Config {\n    writeable_path: \"\",\n}, error: SledError(\n    Io(\n        Os {\n            code: 5,\n            kind: PermissionDenied,\n            message: \"Access is denied.\",\n        },\n    ),\n)"}}
-
-// Expected Error:
-// {"Err":"InvalidUsername"}
-
-// OK
-// {"Ok":null}
