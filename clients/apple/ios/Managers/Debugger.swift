1379dae40b504c9698275704e87d8524189a426c
Raayan Pillai <raayan@raayanpillai.com>
2020-08-22 01:50:36 -0400

    obviously use tail recursion dummy! incremental sync much less janky

--- clients/apple/ios/Managers/Debugger.swift
+++ clients/apple/ios/Managers/Debugger.swift
@@ -18,4 +18,16 @@ final class Debugger: ObservableObject {
     init(lockbookApi: LockbookApi) {
         self.lockbookApi = lockbookApi
     }
+    
+    func createFiles(count: Int) -> Void {
+        switch self.lockbookApi.getRoot() {
+        case .success(let dir):
+            for _ in 0..<5 {
+                let _ = self.lockbookApi.createFile(name: String(UUID.init().uuidString.prefix(5)), dirId: dir.id, isFolder: false)
+            }
+            return
+        case .failure(_):
+            return
+        }
+    }
 }
