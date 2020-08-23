09041b7295a1dd18b4a32b6f2df4fd41f116942a
Travis Vanderstad <t.vanderstad@gmail.com>
2020-08-23 17:34:07 -0400

    adds test

--- core/tests/create_document_tests.rs
+++ core/tests/create_document_tests.rs
@@ -183,4 +183,52 @@ mod create_document_tests {
             ))
         );
     }
+
+    #[test]
+    fn create_document_parent_not_found() {
+        // new account
+        let account = generate_account();
+        let folder_id = Uuid::new_v4();
+        let folder_key = AesImpl::generate_key();
+
+        assert_matches!(
+            ClientImpl::new_account(
+                &account.username,
+                &sign(&account),
+                account.keys.to_public_key(),
+                folder_id,
+                FolderAccessInfo {
+                    folder_id: folder_id,
+                    access_key: aes_key(&folder_key, &folder_key),
+                },
+                rsa_key(&account.keys.to_public_key(), &folder_key)
+            ),
+            Ok(_)
+        );
+
+        let parent_folder_id = Uuid::new_v4();
+
+        // create document
+        let doc_id = Uuid::new_v4();
+        let doc_key = AesImpl::generate_key();
+        let doc_name = random_filename();
+
+        assert_matches!(
+            ClientImpl::create_document(
+                &account.username,
+                &sign(&account),
+                doc_id,
+                &doc_name,
+                parent_folder_id,
+                aes_str(&doc_key, "doc content"),
+                FolderAccessInfo {
+                    folder_id: parent_folder_id,
+                    access_key: aes_key(&folder_key, &doc_key),
+                },
+            ),
+            Err(Error::<CreateDocumentError>::Api(
+                CreateDocumentError::ParentNotFound
+            ))
+        );
+    }
 }
