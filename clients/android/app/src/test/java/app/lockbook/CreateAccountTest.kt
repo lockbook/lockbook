a982f9cf1751db640d0a6bd4f7e19dbc00b81232
Smail <SmailBarkouch1@gmail.com>
2020-08-25 14:57:37 -0400

    made every single test

--- clients/android/app/src/test/java/app/lockbook/CreateAccountTest.kt
+++ clients/android/app/src/test/java/app/lockbook/CreateAccountTest.kt
@@ -4,15 +4,26 @@ import app.lockbook.core.loadLockbookCore
 import app.lockbook.utils.Config
 import app.lockbook.utils.CoreModel
 import app.lockbook.utils.CreateAccountError
+import com.beust.klaxon.Klaxon
+import org.junit.After
 import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 
 
 class CreateAccountTest {
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun loadLib() {
+            loadLockbookCore()
+            Runtime.getRuntime().exec("mkdir $path")
+        }
+    }
 
-    @Before
-    fun loadLib() {
-        loadLockbookCore()
+    @After
+    fun resetDirectory() {
+        Runtime.getRuntime().exec("rm -rf $path/*")
     }
 
     @Test
@@ -27,11 +38,51 @@ class CreateAccountTest {
     fun createAccountUsernameTaken() {
         val username = generateAlphaString()
         CoreModel.generateAccount(Config(path), username).component1()!!
+        Runtime.getRuntime().exec("rm -rf $path")
 
         val secondAccountError = CoreModel.generateAccount(
             Config(path),
             username
         ).component2()!!
         require(secondAccountError is CreateAccountError.UsernameTaken)
+    }
+
+    @Test
+    fun createAccountInvalidUsername() {
+        val firstCreateAccountError = CoreModel.generateAccount(
+            Config(path),
+            "!@#$%^&*()"
+        ).component2()!!
+        require(firstCreateAccountError is CreateAccountError.InvalidUsername)
+        val secondCreateAccountError = CoreModel.generateAccount(
+            Config(path),
+            "œ∑´´†¥¨ˆˆπåß∂ƒ"
+        ).component2()!!
+        require(secondCreateAccountError is CreateAccountError.InvalidUsername)
+        val thirdCreateAccountError = CoreModel.generateAccount(
+            Config(path),
+            "Ω≈ç√∫˜˜¬˚∆˙©"
+        ).component2()!!
+        require(thirdCreateAccountError is CreateAccountError.InvalidUsername)
+        val fourthCreateAccountError = CoreModel.generateAccount(
+            Config(path),
+            "☺️☠️✋☝️✊"
+        ).component2()!!
+        require(fourthCreateAccountError is CreateAccountError.InvalidUsername)
+    }
+
+    @Test
+    fun createAccountExistsAlready() {
+        throw Throwable(
+            Klaxon().toJsonString(
+                CoreModel.generateAccount(
+                    Config(path),
+                    generateAlphaString()
+                )
+            )
+        )
+        val createAccountError =
+            CoreModel.generateAccount(Config(path), generateAlphaString()).component2()!!
+        require(createAccountError is CreateAccountError.AccountExistsAlready)
     }
 }
