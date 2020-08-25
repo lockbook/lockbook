66dce48806a250a1a51da28f4b93a5823d640701
Raayan Pillai <raayan@raayanpillai.com>
2020-08-24 22:30:34 -0400

    fix logger stuff touch up java lib usage touch up c lib usage

--- clients/android/core/src/main/java/app/lockbook/core/core.kt
+++ clients/android/core/src/main/java/app/lockbook/core/core.kt
@@ -1,6 +1,6 @@
 package app.lockbook.core
 
-external fun initLogger()
+external fun initLogger(config: String)
 external fun createAccount(config: String, username: String): String
 external fun importAccount(config: String, account: String): String
 external fun exportAccount(config: String): String
@@ -22,5 +22,6 @@ external fun executeSyncWork(config: String, account: String, workUnit: String):
 
 fun loadLockbookCore() {
     System.loadLibrary("lockbook_core")
-    initLogger()
+    // TODO: @smail put the real config here
+    initLogger("JUNK_CONFIG")
 }
