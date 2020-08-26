b11155be14bf00a280f2057dfd5ff9f017fdfaaa
Smail <SmailBarkouch1@gmail.com>
2020-08-26 10:57:53 -0400

    All tests work (except for delete file)

--- clients/android/app/src/test/java/app/lockbook/GetChildrenTest.kt
+++ clients/android/app/src/test/java/app/lockbook/GetChildrenTest.kt
@@ -4,29 +4,34 @@ import app.lockbook.core.loadLockbookCore
 import app.lockbook.utils.Config
 import app.lockbook.utils.CoreModel
 import org.junit.After
+import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
 
 class GetChildrenTest {
-
-    private val coreModel = CoreModel(Config(path))
 
     companion object {
         @BeforeClass
         @JvmStatic
         fun loadLib() {
             loadLockbookCore()
-            Runtime.getRuntime().exec("mkdir $path")
+            Runtime.getRuntime().exec("rm -rf $path")
         }
+    }
+
+    @Before
+    fun createDirectory() {
+        Runtime.getRuntime().exec("mkdir $path")
     }
 
     @After
     fun resetDirectory() {
-        Runtime.getRuntime().exec("rm -rf $path/*")
+        Runtime.getRuntime().exec("rm -rf $path")
     }
 
     @Test
     fun getChildrenOk() {
+        val coreModel = CoreModel(Config(path))
         CoreModel.generateAccount(
             Config(path),
             generateAlphaString()
--- clients/android/app/src/test/java/app/lockbook/WriteToDocumentTest.kt
+++ clients/android/app/src/test/java/app/lockbook/WriteToDocumentTest.kt
@@ -7,6 +7,7 @@ import app.lockbook.utils.FileType
 import app.lockbook.utils.WriteToDocumentError
 import com.beust.klaxon.Klaxon
 import org.junit.After
+import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
 
@@ -19,13 +20,19 @@ class WriteToDocumentTest {
         @JvmStatic
         fun loadLib() {
             loadLockbookCore()
-            Runtime.getRuntime().exec("mkdir $path")
+            Runtime.getRuntime().exec("rm -rf $path")
+            Runtime.getRuntime().exec("rm -rf $path")
         }
+    }
+
+    @Before
+    fun createDirectory() {
+        Runtime.getRuntime().exec("mkdir $path")
     }
 
     @After
     fun resetDirectory() {
-        Runtime.getRuntime().exec("rm -rf $path/*")
+        Runtime.getRuntime().exec("rm -rf $path")
     }
 
     @Test
@@ -37,21 +44,7 @@ class WriteToDocumentTest {
         coreModel.setParentToRoot().component1()!!
         val document = coreModel.createFile(generateAlphaString(), Klaxon().toJsonString(FileType.Document)).component1()!!
         coreModel.insertFile(document).component1()!!
-        CoreModel.writeContentToDocument(Config(path), "", document.id).component1()!!
-    }
-
-    @Test
-    fun writeToDocumentNoAccount() {
-        CoreModel.generateAccount(
-            Config(path),
-            generateAlphaString()
-        ).component1()!!
-        coreModel.setParentToRoot().component1()!!
-        val document = coreModel.createFile(generateAlphaString(), Klaxon().toJsonString(FileType.Document)).component1()!!
-        coreModel.insertFile(document).component1()!!
-        Runtime.getRuntime().exec("rm -rf $path/*")
-        val writeToDocumentError = CoreModel.writeContentToDocument(Config(path), "", document.id).component2()!!
-        require(writeToDocumentError is WriteToDocumentError.NoAccount)
+        CoreModel.writeContentToDocument(Config(path), document.id, "").component1()!!
     }
 
     @Test
@@ -61,7 +54,7 @@ class WriteToDocumentTest {
             generateAlphaString()
         ).component1()!!
         coreModel.setParentToRoot().component1()!!
-        val writeToDocumentError = CoreModel.writeContentToDocument(Config(path), "", generateAlphaString()).component2()!!
+        val writeToDocumentError = CoreModel.writeContentToDocument(Config(path), generateId(), "").component2()!!
         require(writeToDocumentError is WriteToDocumentError.FileDoesNotExist)
     }
 
@@ -74,7 +67,7 @@ class WriteToDocumentTest {
         coreModel.setParentToRoot().component1()!!
         val folder = coreModel.createFile(generateAlphaString(), Klaxon().toJsonString(FileType.Folder)).component1()!!
         coreModel.insertFile(folder).component1()!!
-        val writeToDocumentError = CoreModel.writeContentToDocument(Config(path), "", folder.id).component2()!!
+        val writeToDocumentError = CoreModel.writeContentToDocument(Config(path), folder.id, "").component2()!!
         require(writeToDocumentError is WriteToDocumentError.FolderTreatedAsDocument)
     }
 }
