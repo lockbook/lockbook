8939cd68d33d9bdf2e253dffb634bf6d8b175e1a
travis <t.vanderstad@gmail.com>
2020-08-26 12:30:57 -0400

    adds test project, moves other project into folder

---
+++ clients/windows/test/CoreServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace test {
+    [TestClass]
+    public class CoreServiceTest {
+        [TestMethod]
+        public void Pass() {
+
+        }
+
+        [TestMethod]
+        public void Fail() {
+            throw new Exception();
+        }
+    }
+}
