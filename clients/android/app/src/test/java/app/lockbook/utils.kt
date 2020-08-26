b87900cb497cd3d2befc2f7436a078268d0072d6
Smail <SmailBarkouch1@gmail.com>
2020-08-26 11:42:07 -0400

    fixed comment

--- clients/android/app/src/test/java/app/lockbook/utils.kt
+++ clients/android/app/src/test/java/app/lockbook/utils.kt
@@ -3,7 +3,7 @@ package app.lockbook
 import java.util.*
 // You have to build the macos jni from core first to be able to run the tests.
 // Next you have to add a vm option that helps java find the library:
-// -ea -Djava.library.path="//lockbook/clients/android/core/src/main/jniLibs/desktop/"
+// -ea -Djava.library.path="<path to lockbook>/lockbook/clients/android/core/src/main/jniLibs/desktop/"
 
 fun generateAlphaString(): String =
     (1..20).map { (('A'..'Z') + ('a'..'z')).random() }.joinToString("")
