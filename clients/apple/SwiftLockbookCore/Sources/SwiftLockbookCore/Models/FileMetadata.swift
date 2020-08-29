db2d84419a6b1faa53cd860cdd372c06fbd8caed
Raayan Pillai <raayan@raayanpillai.com>
2020-08-29 13:31:53 -0400

    Linked and working

---
+++ clients/apple/SwiftLockbookCore/Sources/SwiftLockbookCore/Models/FileMetadata.swift
@@ -0,0 +1,58 @@
+//
+//  FileMetadata.swift
+//  ios_client
+//
+//  Created by Raayan Pillai on 4/11/20.
+//  Copyright © 2020 Lockbook. All rights reserved.
+//
+
+import Foundation
+
+public struct FileMetadata: Codable, Identifiable {
+    public var fileType: FileType
+    public var id: UUID
+    public var parent: UUID
+    public var name: String
+    public var owner: String
+    public var contentVersion: Int
+    public var metadataVersion: Int
+    public var deleted: Bool
+    public var signature: SignedValue = SignedValue(content: "", signature: "")
+    public var userAccessKeys: [Account.Username : UserAccessInfo] = .init()
+    public var folderAccessKeys: FolderAccessInfo = FolderAccessInfo(folderId: .init(), accessKey: .init(garbage: "", nonce: ""))
+}
+
+public enum FileType: String, Codable {
+    case Document
+    case Folder
+}
+
+public struct FolderAccessInfo: Codable {
+    var folderId: UUID
+    var accessKey: EncryptedValueWithNonce
+}
+
+public struct UserAccessInfo: Codable {
+    var username: Account.Username
+    var publicKey: RSAPublicKey
+    var accessKey: EncryptedValue
+}
+
+public struct SignedValue: Codable {
+    var content: String
+    var signature: String
+}
+
+public struct EncryptedValueWithNonce: Codable {
+    var garbage: String
+    var nonce: String
+}
+
+public struct EncryptedValue: Codable {
+    var garbage: String
+}
+
+public struct RSAPublicKey: Codable {
+    var n: [UInt]
+    var e: [UInt]
+}
