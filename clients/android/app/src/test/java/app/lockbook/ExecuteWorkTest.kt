93a4b9b181767f05901576b93d036ed73208373b
Smail <SmailBarkouch1@gmail.com>
2020-09-01 13:55:55 -0400

    added another test to check if import syncs

--- clients/android/app/src/test/java/app/lockbook/ExecuteWorkTest.kt
+++ clients/android/app/src/test/java/app/lockbook/ExecuteWorkTest.kt
@@ -65,24 +65,106 @@ class ExecuteWorkTest {
             this::executeWorkOk.name,
             CoreModel.insertFile(config, folder).component1()
         )
+        repeat(10) {
+            val syncWork = assertTypeReturn<WorkCalculated>(
+                this::executeWorkOk.name,
+                CoreModel.calculateFileSyncWork(config).component1()
+            )
 
-        val syncWork = assertTypeReturn<WorkCalculated>(
-            this::executeWorkOk.name,
-            CoreModel.calculateFileSyncWork(config).component1()
+            for (workUnit in syncWork.work_units) {
+                assertType<Unit>(
+                    this::executeWorkOk.name,
+                    CoreModel.executeFileSyncWork(
+                        config,
+                        assertTypeReturn(
+                            this::executeWorkOk.name,
+                            CoreModel.getAccount(config).component1()
+                        ),
+                        workUnit
+                    ).component1()
+                )
+            }
+        }
+    }
+
+    @Test
+    fun executeWorkImportAccountOk() {
+        assertType<Unit>(
+            this::executeWorkImportAccountOk.name,
+            CoreModel.generateAccount(config, generateAlphaString()).component1()
         )
 
-        for (workUnit in syncWork.work_units) {
-            assertType<Unit>(
-                this::executeWorkOk.name,
-                CoreModel.executeFileSyncWork(
-                    config,
-                    assertTypeReturn(
-                        this::executeWorkOk.name,
-                        CoreModel.getAccount(config).component1()
-                    ),
-                    workUnit
-                ).component1()
+        val rootFileMetadata = assertTypeReturn<FileMetadata>(
+            this::executeWorkImportAccountOk.name,
+            CoreModel.getRoot(config).component1()
+        )
+
+        val document = assertTypeReturn<FileMetadata>(
+            this::executeWorkImportAccountOk.name,
+            CoreModel.createFile(
+                config,
+                rootFileMetadata.id,
+                generateAlphaString(),
+                Klaxon().toJsonString(FileType.Document)
+            ).component1()
+        )
+
+        val folder = assertTypeReturn<FileMetadata>(
+            this::executeWorkImportAccountOk.name,
+            CoreModel.createFile(
+                config,
+                rootFileMetadata.id,
+                generateAlphaString(),
+                Klaxon().toJsonString(FileType.Folder)
+            ).component1()
+        )
+
+        assertType<Unit>(
+            this::executeWorkImportAccountOk.name,
+            CoreModel.insertFile(config, document).component1()
+        )
+
+        assertType<Unit>(
+            this::executeWorkImportAccountOk.name,
+            CoreModel.insertFile(config, folder).component1()
+        )
+
+        assertType<Unit>(
+            this::executeWorkImportAccountOk.name,
+            CoreModel.syncAllFiles(config).component1()
+        )
+
+        val exportAccountString = assertTypeReturn<String>(
+            this::executeWorkImportAccountOk.name,
+            CoreModel.exportAccount(config).component1()
+        )
+
+        config = Config(createRandomPath())
+
+        assertType<Unit>(
+            this::executeWorkImportAccountOk.name,
+            CoreModel.importAccount(config, exportAccountString).component1()
+        )
+
+        repeat(10) {
+            val syncWork = assertTypeReturn<WorkCalculated>(
+                this::executeWorkImportAccountOk.name,
+                CoreModel.calculateFileSyncWork(config).component1()
             )
+
+            for (workUnit in syncWork.work_units) {
+                assertType<Unit>(
+                    this::executeWorkImportAccountOk.name,
+                    CoreModel.executeFileSyncWork(
+                        config,
+                        assertTypeReturn(
+                            this::executeWorkImportAccountOk.name,
+                            CoreModel.getAccount(config).component1()
+                        ),
+                        workUnit
+                    ).component1()
+                )
+            }
         }
     }
 
