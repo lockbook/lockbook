de95d2c13d7384c78d2f66ace3ed7dc560e5c726
Parth <parth@mehrotra.me>
2020-09-09 21:07:46 -0400

    state service created

---
+++ core/src/service/db_state_service.rs
@@ -0,0 +1,49 @@
+use sled::Db;
+use crate::repo::{db_version_repo, account_repo};
+use crate::repo::account_repo::AccountRepo;
+use crate::service::db_state_service::GetStateError::AccountDbError;
+use crate::model::account::Account;
+use crate::service::db_state_service::State::Empty;
+
+#[derive(Debug)]
+pub enum State {
+    ReadyToUse,
+    Empty,
+    MigrationRequired,
+}
+
+#[derive(Debug)]
+pub enum GetStateError {
+    AccountDbError(account_repo::DbError),
+    RepoError(db_version_repo::Error),
+}
+
+#[derive(Debug)]
+pub enum MigrationError {
+    RepoError(db_version_repo::Error)
+}
+
+pub trait DbStateService {
+    fn get_state(db: &Db) -> Result<State, GetStateError>;
+    fn perform_migration(db: &Db) -> Result<MigrationError, ()>;
+}
+
+pub struct DbStateServiceImpl<
+    AccountDb: AccountRepo
+> {
+    _account: AccountDb
+}
+
+impl<AccountDb: AccountRepo> DbStateService for DbStateServiceImpl<AccountDb> {
+    fn get_state(db: &Db) -> Result<State, GetStateError> {
+        if AccountDb::maybe_get_account(&db).map_err(AccountDbError)?.is_none() {
+            return Ok(Empty);
+        }
+
+
+    }
+
+    fn perform_migration(db: &Db) -> Result<MigrationError, ()> {
+        unimplemented!()
+    }
+}
