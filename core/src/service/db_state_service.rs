04078199e2106bbe02965f447f42f39de764423c
Parth <parth@mehrotra.me>
2020-09-11 20:16:39 -0400

    added sanity check

--- core/src/service/db_state_service.rs
+++ core/src/service/db_state_service.rs
@@ -5,9 +5,10 @@ use crate::service::db_state_service::GetStateError::{AccountDbError, RepoError}
 use crate::service::db_state_service::State::{
     Empty, MigrationRequired, ReadyToUse, StateRequiresClearing,
 };
+use crate::CODE_VERSION;
 use sled::Db;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum State {
     ReadyToUse,
     Empty,
@@ -41,20 +42,18 @@ impl<AccountDb: AccountRepo, VersionDb: DbVersionRepo> DbStateService
     for DbStateServiceImpl<AccountDb, VersionDb>
 {
     fn get_state(db: &Db) -> Result<State, GetStateError> {
-        let code_version = env!("CARGO_PKG_VERSION").to_string();
-
         if AccountDb::maybe_get_account(&db)
             .map_err(AccountDbError)?
             .is_none()
         {
-            VersionDb::set(&db, &code_version).map_err(RepoError)?;
+            VersionDb::set(&db, CODE_VERSION).map_err(RepoError)?;
             return Ok(Empty);
         }
 
         match VersionDb::get(&db).map_err(RepoError)? {
             None => Ok(StateRequiresClearing),
             Some(state_version) => {
-                if state_version == code_version {
+                if state_version == CODE_VERSION.to_string() {
                     Ok(ReadyToUse)
                 } else {
                     Ok(MigrationRequired)
@@ -65,7 +64,7 @@ impl<AccountDb: AccountRepo, VersionDb: DbVersionRepo> DbStateService
 
     fn perform_migration(db: &Db) -> Result<(), MigrationError> {
         loop {
-            let code_version = env!("CARGO_PKG_VERSION").to_string();
+            let code_version = CODE_VERSION.to_string();
 
             let db_version = match VersionDb::get(&db).map_err(MigrationError::RepoError)? {
                 None => return Err(MigrationError::StateRequiresClearing),
@@ -83,4 +82,27 @@ impl<AccountDb: AccountRepo, VersionDb: DbVersionRepo> DbStateService
             }
         }
     }
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use crate::model::state::dummy_config;
+    use crate::repo::db_provider::{DbProvider, TempBackedDB};
+    use crate::repo::db_version_repo::{DbVersionRepo, DbVersionRepoImpl};
+    use crate::service::db_state_service::DbStateService;
+    use crate::service::db_state_service::State::Empty;
+    use crate::{DefaultDbStateService, CODE_VERSION};
+
+    #[test]
+    fn test_initial_state() {
+        let config = dummy_config();
+        let db = TempBackedDB::connect_to_db(&config).unwrap();
+
+        assert!(DbVersionRepoImpl::get(&db).unwrap().is_none());
+        assert_eq!(DefaultDbStateService::get_state(&db).unwrap(), Empty);
+        assert_eq!(DefaultDbStateService::get_state(&db).unwrap(), Empty);
+        assert_eq!(DbVersionRepoImpl::get(&db).unwrap().unwrap(), CODE_VERSION);
+    }
+
+    // The rest are integration tests
 }
