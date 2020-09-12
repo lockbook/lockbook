bf40829b778dcd85795fb5ac3a562a57dee643e9
Parth <parth@mehrotra.me>
2020-09-12 15:18:57 -0400

    fmt

--- clients/cli/src/list.rs
+++ clients/cli/src/list.rs
@@ -1,7 +1,7 @@
-use lockbook_core::repo::file_metadata_repo::Filter;
 use lockbook_core::list_paths;
+use lockbook_core::repo::file_metadata_repo::Filter;
 
-use crate::utils::{get_config, print_last_successful_sync, prepare_db_and_get_account_or_exit};
+use crate::utils::{get_config, prepare_db_and_get_account_or_exit, print_last_successful_sync};
 
 pub fn list(file_filter: Option<Filter>) {
     prepare_db_and_get_account_or_exit();
