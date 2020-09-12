ed8f377dccd5f6b4136f67234f0513edc07a12b7
Smail <SmailBarkouch1@gmail.com>
2020-09-12 19:39:16 -0400

    svg intermediate changes

--- clients/android/app/src/main/java/app/lockbook/loggedin/editor/HandwritingEditorActivity.kt
+++ clients/android/app/src/main/java/app/lockbook/loggedin/editor/HandwritingEditorActivity.kt
@@ -1,9 +1,7 @@
 package app.lockbook.loggedin.editor
 
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -11,32 +9,74 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import app.lockbook.R
+import app.lockbook.utils.TEXT_EDITOR_BACKGROUND_SAVE_PERIOD
+import com.beust.klaxon.Klaxon
+import com.github.nwillc.ksvg.elements.Container
+import com.github.nwillc.ksvg.elements.Element
+import com.github.nwillc.ksvg.elements.G
 import kotlinx.android.synthetic.main.activity_handwriting_editor.*
+import timber.log.Timber
+import java.util.*
 
 class HandwritingEditorActivity: AppCompatActivity() {
     private lateinit var handwritingEditorViewModel: HandwritingEditorViewModel
+    private var timer: Timer = Timer()
+    private val handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_handwriting_editor)
 
-//        val id = intent.getStringExtra("id")
-//        val contents = intent.getStringExtra("contents")
-//
-//        if (id == null) {
-//            errorHasOccurred("Unable to retrieve id.")
-//            finish()
-//            return
-//        }
-//        if (contents == null) {
-//            errorHasOccurred("Unable to retrieve contents.")
-//            finish()
-//            return
-//        }
+        val id = intent.getStringExtra("id")
+        val contents = intent.getStringExtra("contents")
+
+        if (id == null) {
+            errorHasOccurred("Unable to retrieve id.")
+            finish()
+            return
+        }
+        if (contents == null) {
+            errorHasOccurred("Unable to retrieve contents.")
+            finish()
+            return
+        }
 
         handwritingEditorViewModel =
-            ViewModelProvider(this, HandwritingEditorViewModelFactory(application, "id", "contents")).get(HandwritingEditorViewModel::class.java)
+            ViewModelProvider(
+                this,
+                HandwritingEditorViewModelFactory(application, id)
+            ).get(HandwritingEditorViewModel::class.java)
 
+        handwritingEditorViewModel.errorHasOccurred.observe(
+            this,
+            { errorHasOccurred ->
+                errorHasOccurred(errorHasOccurred)
+            }
+        )
+
+        setUpHandwritingToolbar()
+        Timber.e("SMAIL3: $contents")
+        val g = G()
+        g.body = contents
+        handwriting_editor.svgObject.children.add(g)
+        startBackgroundSave()
+    }
+
+    private fun startBackgroundSave() {
+        timer.schedule(
+            object : TimerTask() {
+                override fun run() {
+                    handler.post {
+                        handwritingEditorViewModel.saveSVG(handwriting_editor.svgObject.toString())
+                    }
+                }
+            },
+            1000,
+            TEXT_EDITOR_BACKGROUND_SAVE_PERIOD
+        )
+    }
+
+    private fun setUpHandwritingToolbar() {
         ArrayAdapter.createFromResource(
             this,
             R.array.handwriting_editor_pen_size,
@@ -46,33 +86,20 @@ class HandwritingEditorActivity: AppCompatActivity() {
             handwriting_editor_pen_size_spinner.adapter = adapter
         }
 
-        handwriting_editor_pen_size_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                val paint = Paint()
-                paint.isAntiAlias = true
-                paint.color = handwriting_editor_canvas.paints.last().color
-                paint.style = Paint.Style.STROKE
-                paint.strokeJoin = Paint.Join.MITER
+        handwriting_editor_pen_size_spinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
 
-                when(parent?.getItemAtPosition(position).toString()) {
-                    getString(R.string.handwriting_editor_pen_1) ->  paint.strokeWidth = resources.getInteger(R.integer.handwriting_editor_pen_1).toFloat()
-                    getString(R.string.handwriting_editor_pen_2) -> paint.strokeWidth = resources.getInteger(R.integer.handwriting_editor_pen_2).toFloat()
-                    getString(R.string.handwriting_editor_pen_3) -> paint.strokeWidth = resources.getInteger(R.integer.handwriting_editor_pen_3).toFloat()
-                    getString(R.string.handwriting_editor_pen_4) -> paint.strokeWidth = resources.getInteger(R.integer.handwriting_editor_pen_4).toFloat()
                 }
 
-                handwriting_editor_canvas.paints.add(paint)
-                handwriting_editor_canvas.paths.add(Path())
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+
             }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-
-        }
 
         ArrayAdapter.createFromResource(
             this,
@@ -83,33 +110,20 @@ class HandwritingEditorActivity: AppCompatActivity() {
             handwriting_editor_pallete_spinner.adapter = adapter
         }
 
-        handwriting_editor_pallete_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                val paint = Paint()
-                paint.isAntiAlias = true
-                paint.style = Paint.Style.STROKE
-                paint.strokeJoin = Paint.Join.MITER
-                paint.strokeWidth = handwriting_editor_canvas.paints.last().strokeWidth
+        handwriting_editor_pallete_spinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
 
-                when(parent?.getItemAtPosition(position).toString()) {
-                    getString(R.string.handwriting_editor_pallete_white) -> paint.color = Color.WHITE
-                    getString(R.string.handwriting_editor_pallete_blue) -> paint.color = Color.BLUE
-                    getString(R.string.handwriting_editor_pallete_red) -> paint.color = Color.RED
-                    getString(R.string.handwriting_editor_pallete_yellow) -> paint.color = Color.YELLOW
                 }
 
-                handwriting_editor_canvas.paints.add(paint)
-                handwriting_editor_canvas.paths.add(Path())
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+
             }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-
-        }
     }
 
     private fun errorHasOccurred(errorText: String) {
