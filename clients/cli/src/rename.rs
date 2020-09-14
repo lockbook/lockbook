cb962af8ae37e8d0700d6a7ca5c52ed4be2972d9
steverusso <steverusso@protonmail.com>
2020-09-14 11:52:33 -0400

    Account for a path containing an empty file and for new file names being empty.

--- clients/cli/src/rename.rs
+++ clients/cli/src/rename.rs
@@ -1,5 +1,7 @@
 use crate::utils::{exit_with, get_account_or_exit, get_config};
-use crate::{FILE_NAME_NOT_AVAILABLE, FILE_NOT_FOUND, NAME_CONTAINS_SLASH, UNEXPECTED_ERROR};
+use crate::{
+    FILE_NAME_NOT_AVAILABLE, FILE_NOT_FOUND, NAME_CONTAINS_SLASH, NAME_EMPTY, UNEXPECTED_ERROR,
+};
 use lockbook_core::{get_file_by_path, rename_file, GetFileByPathError, RenameFileError};
 use std::process::exit;
 
@@ -10,6 +12,7 @@ pub fn rename(path: &str, new_name: &str) {
         Ok(file_metadata) => match rename_file(&get_config(), file_metadata.id, new_name) {
             Ok(_) => exit(0),
             Err(err) => match err {
+                RenameFileError::NewNameEmpty => exit_with("New name is empty!", NAME_EMPTY),
                 RenameFileError::NewNameContainsSlash => {
                     exit_with("New name cannot contain a slash!", NAME_CONTAINS_SLASH)
                 }
