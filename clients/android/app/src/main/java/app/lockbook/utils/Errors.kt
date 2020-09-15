ec077e7b575e6cc1dfeae0b6cd712878677a70df
Parth <parth@mehrotra.me>
2020-09-15 17:21:56 -0400

    handle change for android

--- clients/android/app/src/main/java/app/lockbook/utils/Errors.kt
+++ clients/android/app/src/main/java/app/lockbook/utils/Errors.kt
@@ -48,6 +48,7 @@ sealed class CreateFileError {
     object CouldNotFindAParent : CreateFileError()
     object FileNameNotAvailable : CreateFileError()
     object FileNameContainsSlash : CreateFileError()
+    object FileNameEmpty: CreateFileError()
     data class UnexpectedError(val error: String) : CreateFileError()
 }
 
