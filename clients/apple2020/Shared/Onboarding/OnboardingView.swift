9f9b7e760fbd93e3be89b62505cd7270679b06fa
Raayan Pillai <raayan@raayanpillai.com>
2020-09-20 00:57:28 -0400

    tree file navigation

--- clients/apple2020/Shared/Onboarding/OnboardingView.swift
+++ clients/apple2020/Shared/Onboarding/OnboardingView.swift
@@ -22,41 +22,17 @@ struct OnboardingView: View {
                 // For iPad and macOS
                 Text("You need an account!")
             }
-            self.core.message.map { MessageBanner(core: self.core, message: $0) }
         }
-        .ignoresSafeArea()
     }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        /// This allows us to use the real Rust FFI in SwiftUI Previews!
-//         OnboardingView(core: Core(documenstDirectory: "/Users/raayanpillai/ios_preview"))
+        /// You can point this to a real directory with:
+        // OnboardingView(core: Core(documenstDirectory: "<somedir>"))
         OnboardingView(core: Core())
         
         MessageBanner(core: Core(), message: Message(words: "Oof!", icon: "exclamationmark.bubble", color: .yellow))
             .previewLayout(.sizeThatFits)
-    }
-}
-
-struct MessageBanner: View {
-    @ObservedObject var core: Core
-    let message: Message
-    
-    var body: some View {
-        HStack {
-            Spacer()
-            Label(message.words, systemImage: message.icon ?? "")
-                .font(.headline)
-                .foregroundColor(.black)
-                .padding(.vertical, 20)
-            Spacer()
-        }
-        .background(message.color)
-        .onTapGesture {
-            withAnimation {
-                self.core.message = nil
-            }
-        }
     }
 }
