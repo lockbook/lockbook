3ff0bedd894b2b1464a2676272dc00511be1146a
Raayan Pillai <raayan@raayanpillai.com>
2020-09-20 00:57:28 -0400

    tree file navigation

--- clients/apple2020/Shared/Core.swift
+++ clients/apple2020/Shared/Core.swift
@@ -7,12 +7,23 @@ class Core: ObservableObject {
     let api: LockbookApi
     @Published var account: Account?
     @Published var message: Message? = nil
+    @Published var files: [FileMetadata] = []
+    @Published var grouped: [FileMetadataWithChildren] = []
     
     func purge() {
         let lockbookDir = URL(fileURLWithPath: documenstDirectory).appendingPathComponent("lockbook.sled")
         if let _ = try? FileManager.default.removeItem(at: lockbookDir) {
             print("Deleted \(lockbookDir) and logging out")
             self.account = nil
+        }
+    }
+    
+    func sync() {
+        switch api.synchronize() {
+        case .success(_):
+            updateFiles()
+        case .failure(let err):
+            displayError(error: err)
         }
     }
     
@@ -29,6 +40,25 @@ class Core: ObservableObject {
         }
     }
     
+    private func buildTree(meta: FileMetadata) -> FileMetadataWithChildren {
+        return FileMetadataWithChildren(meta: meta, children: files.filter({ $0.parent == meta.id && $0.id != meta.id }).map(buildTree))
+    }
+    
+    func updateFiles() {
+        switch api.getRoot() {
+        case .success(let root):
+            switch api.listFiles() {
+            case .success(let metas):
+                self.files = metas
+                self.grouped = buildTree(meta: root).children ?? []
+            case .failure(let err):
+                displayError(error: err)
+            }
+        case .failure(let err):
+            displayError(error: err)
+        }
+    }
+    
     init(documenstDirectory: String) {
         self.documenstDirectory = documenstDirectory
         let api = CoreApi(documentsDirectory: documenstDirectory)
@@ -40,15 +70,31 @@ class Core: ObservableObject {
             print(err)
         }
         self.api = api
+        self.updateFiles()
     }
     
     init() {
         self.documenstDirectory = "<USING-FAKE-API>"
         self.api = FakeApi()
+        self.updateFiles()
     }
 }
 
-
+struct FileMetadataWithChildren: Identifiable {
+    let id: UUID
+    let meta: FileMetadata
+    let children: [FileMetadataWithChildren]?
+    
+    init(meta: FileMetadata, children: [FileMetadataWithChildren]) {
+        self.id = meta.id
+        self.meta = meta
+        if !children.isEmpty {
+            self.children = children
+        } else {
+            self.children = nil
+        }
+    }
+}
 
 struct Message {
     let words: String
