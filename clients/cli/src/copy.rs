2d9a2eaea41c2bbc3526eafc906f35f8ca4b7ddd
Parth <parth@mehrotra.me>
2020-09-26 22:14:41 -0400

    closes #309

--- clients/cli/src/copy.rs
+++ clients/cli/src/copy.rs
@@ -1,18 +1,18 @@
 use std::fs;
 use std::path::PathBuf;
 
+use lockbook_core::{create_file_at_path, CreateFileAtPathError, write_document};
 use lockbook_core::model::crypto::DecryptedValue;
-use lockbook_core::{create_file_at_path, write_document, CreateFileAtPathError};
 
-use crate::utils::{exit_with, exit_with_no_account, get_account_or_exit, get_config};
 use crate::{
     COULD_NOT_GET_OS_ABSOLUTE_PATH, COULD_NOT_READ_OS_FILE, COULD_NOT_READ_OS_METADATA,
     DOCUMENT_TREATED_AS_FOLDER, FILE_ALREADY_EXISTS, NO_ROOT, PATH_CONTAINS_EMPTY_FILE,
     PATH_NO_ROOT, SUCCESS, UNEXPECTED_ERROR, UNIMPLEMENTED,
 };
+use crate::utils::{exit_with, exit_with_no_account, get_account_or_exit, get_config};
 
-pub fn copy(path: PathBuf) {
-    let account = get_account_or_exit();
+pub fn copy(path: PathBuf, import_dest: &str) {
+    get_account_or_exit();
 
     let metadata = fs::metadata(&path).unwrap_or_else(|err| {
         exit_with(
@@ -36,19 +36,22 @@ pub fn copy(path: PathBuf) {
             )
         });
 
-        let absolute_path_string = absolute_path_maybe.to_str().unwrap_or_else(|| {
-            exit_with(
-                "Absolute path not a valid utf-8 sequence!",
-                UNEXPECTED_ERROR,
-            )
-        });
+        let import_dest_with_filename = if import_dest.ends_with('/') {
+            match absolute_path_maybe.file_name() {
+                Some(name) => match name.to_os_string().into_string() {
+                    Ok(string) => string,
+                    Err(err) => exit_with(
+                        format!("Unexpected error while trying to convert an OsString -> Rust String: {:?}", err).as_str(),
+                        UNEXPECTED_ERROR,
+                    ),
+                },
+                None => exit_with("Import target does not contain a file name!", UNEXPECTED_ERROR),
+            }
+        } else {
+            import_dest.to_string()
+        };
 
-        let import_dest = format!(
-            "{}/imported/cli-copy{}",
-            account.username, absolute_path_string
-        );
-
-        let file_metadata = match create_file_at_path(&get_config(), &import_dest) {
+        let file_metadata = match create_file_at_path(&get_config(), import_dest_with_filename.as_str()) {
             Ok(file_metadata) => file_metadata,
             Err(err) => match err {
                 CreateFileAtPathError::FileAlreadyExists => exit_with(&format!("Input destination {} not available within lockbook!", import_dest), FILE_ALREADY_EXISTS),
@@ -56,7 +59,7 @@ pub fn copy(path: PathBuf) {
                 CreateFileAtPathError::NoRoot => exit_with("No root folder, have you synced yet?", NO_ROOT),
                 CreateFileAtPathError::DocumentTreatedAsFolder => exit_with(&format!("A file along the target destination is a document that cannot be used as a folder: {}", import_dest), DOCUMENT_TREATED_AS_FOLDER),
                 CreateFileAtPathError::PathContainsEmptyFile => exit_with(&format!("Input destination {} contains an empty file!", import_dest), PATH_CONTAINS_EMPTY_FILE),
-                CreateFileAtPathError::PathDoesntStartWithRoot => exit_with("Unexpected: PathDoesntStartWithRoot", PATH_NO_ROOT),
+                CreateFileAtPathError::PathDoesntStartWithRoot => exit_with("Import destination doesn't start with your root folder.", PATH_NO_ROOT),
                 CreateFileAtPathError::UnexpectedError(msg) => exit_with(&msg, UNEXPECTED_ERROR),
             },
         };
