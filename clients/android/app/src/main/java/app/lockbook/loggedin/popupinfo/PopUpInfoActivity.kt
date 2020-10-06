8aa8005dfd3132be70b91e4d929ace18899c2b15
Smail <SmailBarkouch1@gmail.com>
2020-10-06 12:18:06 -0400

    better handled null types

--- clients/android/app/src/main/java/app/lockbook/loggedin/popupinfo/PopUpInfoActivity.kt
+++ clients/android/app/src/main/java/app/lockbook/loggedin/popupinfo/PopUpInfoActivity.kt
@@ -31,22 +31,39 @@ class PopUpInfoActivity : Activity() {
         val tempMetadataVersion = intent.getStringExtra("metadataVersion") ?: "ERROR"
         val tempContentVersion = intent.getStringExtra("contentVersion") ?: "ERROR"
         val fileType = intent.getStringExtra("fileType") ?: "ERROR"
-        val dateMetadataVersion = Date(Timestamp(tempMetadataVersion.toLongOrNull() ?: 0L).time)
-        val dateContentVersion = Date(Timestamp(tempContentVersion.toLongOrNull() ?: 0L).time)
+        val metadataVersion = tempMetadataVersion.toLongOrNull()
+        val contentVersionError = tempContentVersion.toLongOrNull()
+        if (metadataVersion == null) {
+            popup_info_metadata_version.text = getString(
+                R.string.popup_info_metadata_version,
+                "ERROR"
+            )
+        } else {
+            val dateMetadataVersion = Date(Timestamp(metadataVersion).time)
+            popup_info_metadata_version.text = getString(
+                R.string.popup_info_metadata_version,
+                if (dateMetadataVersion.time != 0L) dateMetadataVersion else resources.getString(R.string.pop_up_info_never_synced)
+            )
+        }
+
+        if (contentVersionError == null) {
+            popup_info_content_version.text = getString(
+                R.string.popup_info_content_version,
+                "Error"
+            )
+        } else {
+            val dateContentVersion = Date(Timestamp(contentVersionError).time)
+            popup_info_content_version.text = getString(
+                R.string.popup_info_content_version,
+                if (dateContentVersion.time != 0L) dateContentVersion else resources.getString(R.string.pop_up_info_never_synced)
+            )
+        }
 
         popup_info_name.text = getString(R.string.popup_info_name, name)
         popup_info_id.text = getString(R.string.popup_info_id, id)
         popup_info_file_type.text =
             getString(R.string.popup_info_file_type, fileType)
-        popup_info_metadata_version.text = getString(
-            R.string.popup_info_metadata_version,
-            if (dateMetadataVersion.time != 0L) dateMetadataVersion else resources.getString(R.string.pop_up_info_never_synced)
-        )
-        popup_info_content_version.text =
-            getString(
-                R.string.popup_info_content_version,
-                if (dateContentVersion.time != 0L) dateContentVersion else resources.getString(R.string.pop_up_info_never_synced)
-            )
+
     }
 
     private fun rename() {
