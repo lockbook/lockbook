b9baff84beebccefb0bfb988335a4af44910edc6
Jake Hecker <heckerj02@gmail.com>
2020-10-05 22:23:30 -0400

    test problem and fixes

--- dev_utils/create_core_for_windows.bat
+++ dev_utils/create_core_for_windows.bat
@@ -1,5 +1,4 @@
-set "API_URL=http://api.lockbook.app:8000"
+set "API_URL=http://qa.lockbook.app:8000"
 cd ..\core
-cargo clean
 cargo build --release
 copy target\release\lockbook_core.dll ..\clients\windows\
