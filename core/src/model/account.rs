8f2dc46d99f61abee96d81b13087017a614b64ca
Raayan Pillai <raayan@raayanpillai.com>
2020-10-06 22:18:44 -0400

    core stores api_url in the account object

--- core/src/model/account.rs
+++ core/src/model/account.rs
@@ -2,9 +2,11 @@ use rsa::RSAPrivateKey;
 use serde::{Deserialize, Serialize};
 
 pub type Username = String;
+pub type ApiUrl = String;
 
 #[derive(PartialEq, Debug, Serialize, Deserialize)]
 pub struct Account {
     pub username: Username,
+    pub api_url: ApiUrl,
     pub keys: RSAPrivateKey,
 }
