c7a29ac91c9ac01626e9d75b736f192e3dc0c583
Raayan Pillai <raayan@raayanpillai.com>
2020-10-12 16:58:41 -0400

    removing old usage of init logger and update linux main

--- clients/apple/SwiftLockbookCore/Tests/SwiftLockbookCoreTests/SwiftLockbookCoreTests.swift
+++ clients/apple/SwiftLockbookCore/Tests/SwiftLockbookCoreTests/SwiftLockbookCoreTests.swift
@@ -10,7 +10,6 @@ struct CoreScenario {
     init() {
         dir = FileManager.default.temporaryDirectory.appendingPathComponent("SwiftLockbookCoreTests", isDirectory: false)
         api = CoreApi(documentsDirectory: dir.path)
-        api.initializeLogger()
     }
     
     /// Creates a working directory for your testing, deletes if one already exists
