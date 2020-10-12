ab2ee2192636760bb9faa0c6fc8173d96a57d6c9
Raayan Pillai <raayan@raayanpillai.com>
2020-10-12 01:11:14 -0400

    SwiftLockbookCore recalibrated

--- clients/apple/SwiftLockbookCore/Sources/SwiftLockbookCore/Apis/FakeApi.swift
+++ clients/apple/SwiftLockbookCore/Sources/SwiftLockbookCore/Apis/FakeApi.swift
@@ -5,51 +5,51 @@ public struct FakeApi: LockbookApi {
         
     }
     
-    public func getAccount() -> CoreResult<Account> {
+    public func getAccount() -> CoreResult<Account, GetAccountError> {
         CoreResult.success(.fake(username: username))
     }
     
-    public func createAccount(username: String, apiLocation: String) -> CoreResult<Account> {
-        CoreResult.failure(ApplicationError.lazy())
+    public func createAccount(username: String, apiLocation: String) -> CoreResult<Account, CreateAccountError> {
+        CoreResult.failure(CoreError.lazy())
     }
     
-    public func importAccount(accountString: String) -> CoreResult<Account> {
-        CoreResult.failure(ApplicationError.lazy())
+    public func importAccount(accountString: String) -> CoreResult<Account, ImportError> {
+        CoreResult.failure(CoreError.lazy())
     }
     
-    public func exportAccount() -> CoreResult<String> {
-        CoreResult.failure(ApplicationError.lazy())
+    public func exportAccount() -> CoreResult<String, AccountExportError> {
+        CoreResult.failure(CoreError.lazy())
     }
     
-    public func getUsage() -> CoreResult<[FileUsage]> {
+    public func getUsage() -> CoreResult<[FileUsage], GetUsageError> {
         CoreResult.success([FileUsage(fileId: .init(), byteSecs: UInt64(100), secs: UInt64(1))])
     }
     
-    public func synchronize() -> CoreResult<Empty> {
-        CoreResult.failure(ApplicationError.lazy())
+    public func synchronize() -> CoreResult<Empty, SyncAllError> {
+        CoreResult.failure(CoreError.lazy())
     }
     
-    public func calculateWork() -> CoreResult<WorkMetadata> {
-        CoreResult.failure(ApplicationError.lazy())
+    public func calculateWork() -> CoreResult<WorkMetadata, CalculateWorkError> {
+        CoreResult.failure(CoreError.lazy())
     }
     
-    public func executeWork(work: WorkUnit) -> CoreResult<Empty> {
-        CoreResult.failure(ApplicationError.lazy())
+    public func executeWork(work: WorkUnit) -> CoreResult<Empty, ExecuteWorkError> {
+        CoreResult.failure(CoreError.lazy())
     }
     
-    public func setLastSynced(lastSync: UInt64) -> CoreResult<Empty> {
-        CoreResult.failure(ApplicationError.lazy())
+    public func setLastSynced(lastSync: UInt64) -> CoreResult<Empty, SetLastSyncedError> {
+        CoreResult.failure(CoreError.lazy())
     }
     
-    public func getRoot() -> CoreResult<FileMetadata> {
+    public func getRoot() -> CoreResult<FileMetadata, GetRootError> {
         return CoreResult.success(FileMetadata(fileType: .Folder, id: root.id, parent: root.id, name: "first_file.md", owner: "root", contentVersion: 1587384000000, metadataVersion: 1587384000000, deleted: false))
     }
     
-    public func listFiles() -> CoreResult<[FileMetadata]> {
-        return Result.success(fileMetas)
+    public func listFiles() -> CoreResult<[FileMetadata], ListMetadatasError> {
+        return CoreResult.success(fileMetas)
     }
     
-    public func getFile(id: UUID) -> CoreResult<DecryptedValue> {
+    public func getFile(id: UUID) -> CoreResult<DecryptedValue, ReadDocumentError> {
         CoreResult.success(DecryptedValue(secret: """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis mattis arcu a commodo. Maecenas dapibus mollis lacinia. Nunc ut mi felis. Donec efficitur, nulla venenatis sodales sagittis, elit tellus ullamcorper leo, in fringilla turpis nisl at sapien. Morbi et sagittis dolor, auctor sollicitudin lorem. In porttitor vulputate mi quis mattis. Suspendisse potenti. In leo sem, tincidunt ut diam sed, malesuada aliquet ipsum. Mauris pretium sapien non erat pulvinar, id dapibus dui convallis. Etiam maximus tellus ac nunc hendrerit vulputate. Vestibulum placerat ligula sit amet eleifend interdum. Pellentesque dignissim ipsum lectus, vitae ultricies mi accumsan id. Morbi ullamcorper gravida justo eu maximus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
 
@@ -57,21 +57,21 @@ Nulla facilisi. Fusce ac risus ut sem vulputate euismod vitae ac massa. Quisque 
 """))
     }
     
-    public func createFile(name: String, dirId: UUID, isFolder: Bool) -> CoreResult<FileMetadata> {
+    public func createFile(name: String, dirId: UUID, isFolder: Bool) -> CoreResult<FileMetadata, CreateFileError> {
         let now = Date().timeIntervalSince1970
         return CoreResult.success(FileMetadata(fileType: .Document, id: UUID(uuidString: "c30a513a-0d75-4f10-ba1e-7a261ebbbe05").unsafelyUnwrapped, parent: dirId, name: "new_file.md", owner: username, contentVersion: UInt64(now), metadataVersion: UInt64(now), deleted: false))
     }
     
-    public func updateFile(id: UUID, content: String) -> CoreResult<Empty> {
-        CoreResult.failure(ApplicationError.lazy())
+    public func updateFile(id: UUID, content: String) -> CoreResult<Empty, WriteToDocumentError> {
+        CoreResult.failure(CoreError.lazy())
     }
     
-    public func markFileForDeletion(id: UUID) -> CoreResult<Bool> {
-        CoreResult.failure(ApplicationError.lazy())
+    public func markFileForDeletion(id: UUID) -> CoreResult<Bool, DeleteFileError> {
+        CoreResult.failure(CoreError.lazy())
     }
     
-    public func renameFile(id: UUID, name: String) -> CoreResult<Empty> {
-        CoreResult.failure(ApplicationError.lazy())
+    public func renameFile(id: UUID, name: String) -> CoreResult<Empty, RenameFileError> {
+        CoreResult.failure(CoreError.lazy())
     }
     
     public let username: Account.Username = "jeff"
--- clients/apple/SwiftLockbookCore/Sources/SwiftLockbookCore/Models/Error.swift
+++ clients/apple/SwiftLockbookCore/Sources/SwiftLockbookCore/Models/Error.swift
@@ -1,63 +1,180 @@
 import Foundation
 
-public enum ApplicationError: Error, Equatable {
-    case Lockbook(CoreError)
-    case Serialization(String)
-    case Deserialization(String, String)
-    case State(String)
-    
-    public func message() -> String {
-        switch self {
-        case .Lockbook(let coreErr):
-            return coreErr.rawValue
-        case .Serialization(let errMsg):
-            return errMsg
-        case .Deserialization(let errMsg, let json):
-            return "\(errMsg) >>>> JSON: \(json)"
-        case .State(let errMsg):
-            return errMsg
+public enum CoreError<E: Decodable & Equatable>: Error, Equatable, Decodable {
+    public static func == (lhs: CoreError<E>, rhs: CoreError<E>) -> Bool {
+        switch (lhs, rhs) {
+        case (UIError(let lui), UIError(let rui)):
+            return lui == rui
+        case (Unexpected(let lui), Unexpected(let rui)):
+            return lui == rui
+        default:
+            return false
+        }
+    }
+
+    case UIError(E)
+    case Unexpected(String)
+
+    enum ErrorTypes: String, Decodable {
+        case UiError
+        case Unexpected
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: TagContentKeys.self)
+        let error = try container.decode(ErrorTypes.self, forKey: .tag)
+        switch error {
+        case .UiError:
+            self = .UIError(try container.decode(E.self, forKey: .content))
+        case .Unexpected:
+            self = .Unexpected(try container.decode(String.self, forKey: .content))
         }
     }
     
-    public static func lazy() -> ApplicationError {
-        State("Lazy!")
+    public static func lazy() -> Self {
+        Unexpected("Lazy!")
     }
 }
 
-public enum CoreError: String, Decodable {
+public enum InitLoggerError: String, Decodable {
+    case Stub
+}
+
+public enum GetStateError: String, Decodable {
+    case Stub
+}
+
+public enum MigrationError: String, Decodable {
+    case StateRequiresCleaning
+}
+
+public enum CreateAccountError: String, Decodable {
+    case AccountExistsAlready
+    case ClientUpdateRequired
+    case CouldNotReachServer
+    case InvalidUsername
+    case UsernameTaken
+}
+
+public enum ImportError: String, Decodable {
     case AccountDoesNotExist
     case AccountExistsAlready
     case AccountStringCorrupted
-    case CannotMoveRoot
-    case CannotRenameRoot
     case ClientUpdateRequired
-    case CouldNotFindAParent
     case CouldNotReachServer
+    case UsernamePKMismatch
+}
+
+public enum AccountExportError: String, Decodable {
+    case NoAccount
+}
+
+public enum GetAccountError: String, Decodable {
+    case NoAccount
+}
+
+public enum CreateFileAtPathError: String, Decodable {
     case DocumentTreatedAsFolder
     case FileAlreadyExists
-    case FileDoesNotExist
-    case FileNameContainsSlash
-    case FileNameEmpty
-    case FileNameNotAvailable
-    case FolderTreatedAsDocument
-    case InvalidUsername
-    case NewNameContainsSlash
-    case NewNameEmpty
     case NoAccount
-    case NoFileAtThatPath
-    case NoFileWithThatId
     case NoRoot
     case PathContainsEmptyFile
     case PathDoesntStartWithRoot
-    case StateRequiresCleaning
+}
+
+public enum WriteToDocumentError: String, Decodable {
+    case FileDoesNotExist
+    case FolderTreatedAsDocument
+    case NoAccount
+}
+
+public enum CreateFileError: String, Decodable {
+    case CouldNotFindAParent
+    case DocumentTreatedAsFolder
+    case FileNameContainsSlash
+    case FileNameEmpty
+    case FileNameNotAvailable
+    case NoAccount
+}
+
+public enum GetRootError: String, Decodable {
+    case NoRoot
+}
+
+public enum GetChildrenError: String, Decodable {
+    case Stub
+}
+
+public enum GetFileByIdError: String, Decodable {
+    case NoFileWithThatId
+}
+
+public enum GetFileByPathError: String, Decodable {
+    case NoFileAtThatPath
+}
+
+public enum InsertFileError: String, Decodable {
+    case Stub
+}
+
+public enum DeleteFileError: String, Decodable {
+    case NoFileWithThatId
+}
+
+public enum ReadDocumentError: String, Decodable {
+    case FileDoesNotExist
+    case NoAccount
+    case TreatedFolderAsDocument
+}
+
+public enum ListPathsError: String, Decodable {
+    case Stub
+}
+
+public enum ListMetadatasError: String, Decodable {
+    case Stub
+}
+
+public enum RenameFileError: String, Decodable {
+    case CannotRenameRoot
+    case FileDoesNotExist
+    case FileNameNotAvailable
+    case NewNameContainsSlash
+    case NewNameEmpty
+}
+
+public enum MoveFileError: String, Decodable {
+    case CannotMoveRoot
+    case DocumentTreatedAsFolder
+    case FileDoesNotExist
+    case NoAccount
     case TargetParentDoesNotExist
     case TargetParentHasChildNamedThat
-    case TreatedFolderAsDocument
-    case UsernamePKMismatch
-    case UsernameTaken
-    // Sort of junk
-    case BadAccount // Contains (GetAccountError)
-    case ExecuteWorkError // Contains ([ExecuteWorkError])
-    case UnexpectedError // Conttains (String)
+}
 
+public enum SyncAllError: String, Decodable {
+    case NoAccount
+    case CouldNotReachServer
+    case ExecuteWorkError
+}
+public enum CalculateWorkError: String, Decodable {
+    case NoAccount
+    case CouldNotReachServer
+    case ClientUpdateRequired
+}
+public enum ExecuteWorkError: String, Decodable {
+    case CouldNotReachServer
+    case ClientUpdateRequired
+    case BadAccount
+}
+public enum SetLastSyncedError: String, Decodable {
+    case Stub
+}
+public enum GetLastSyncedError: String, Decodable {
+    case Stub
+}
+public enum GetUsageError: String, Decodable {
+    case NoAccount
+    case CouldNotReachServer
+    case ClientUpdateRequired
 }
