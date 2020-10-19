b29fae07a7c4dac280026a2b664453c378d4ff8f
Travis Vanderstad <t.vanderstad@gmail.com>
2020-10-19 18:20:16 -0400

    cleanup

--- core/tests/get_updates_test.rs
+++ core/tests/get_updates_test.rs
@@ -24,7 +24,7 @@ mod get_updates_test {
                 folder_id: folder_id,
                 access_key: aes_encrypt(&folder_key, &folder_key),
             },
-            rsa_encrypt::<AESKey>(&account.keys.to_public_key(), &folder_key),
+            rsa_encrypt(&account.keys.to_public_key(), &folder_key),
         )
         .unwrap();
 
