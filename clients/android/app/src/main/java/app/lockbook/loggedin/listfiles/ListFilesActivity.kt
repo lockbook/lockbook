560174098bd62412a761ba79fa61352eadd8623e
Smail <SmailBarkouch1@gmail.com>
2020-10-20 16:09:10 -0400

    merged handwriting with launch with core

--- clients/android/app/src/main/java/app/lockbook/loggedin/listfiles/ListFilesActivity.kt
+++ clients/android/app/src/main/java/app/lockbook/loggedin/listfiles/ListFilesActivity.kt
@@ -6,13 +6,12 @@ import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import androidx.preference.PreferenceManager
 import app.lockbook.R
 import app.lockbook.loggedin.settings.SettingsActivity
-import app.lockbook.utils.Messages.UNEXPECTED_ERROR_OCCURRED
+import app.lockbook.utils.Messages.UNEXPECTED_CLIENT_ERROR
 import app.lockbook.utils.SharedPreferences.SORT_FILES_A_Z
 import app.lockbook.utils.SharedPreferences.SORT_FILES_FIRST_CHANGED
 import app.lockbook.utils.SharedPreferences.SORT_FILES_KEY
@@ -22,9 +21,10 @@ import app.lockbook.utils.SharedPreferences.SORT_FILES_Z_A
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_list_files.*
+import kotlinx.android.synthetic.main.splash_screen.*
 import timber.log.Timber
-
 
 class ListFilesActivity : AppCompatActivity() {
     private var menu: Menu? = null
@@ -34,7 +34,8 @@ class ListFilesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_list_files)
 
         val title = SpannableString("Lockbook")
-        title.setSpan(ForegroundColorSpan(
+        title.setSpan(
+            ForegroundColorSpan(
             ResourcesCompat.getColor(
             resources,
             R.color.light,
@@ -69,8 +70,11 @@ class ListFilesActivity : AppCompatActivity() {
             SORT_FILES_TYPE -> menu?.findItem(R.id.menu_list_files_sort_type)?.isChecked = true
             else -> {
                 Timber.e("File sorting shared preference does not match every supposed option: $optionValue")
-                Toast.makeText(this, UNEXPECTED_ERROR_OCCURRED, Toast.LENGTH_LONG)
-                    .show()
+                Snackbar.make(
+                    splash_screen,
+                    UNEXPECTED_CLIENT_ERROR,
+                    Snackbar.LENGTH_SHORT
+                ).show()
             }
         }
     }
@@ -92,7 +96,7 @@ class ListFilesActivity : AppCompatActivity() {
                     fragment.onSortPressed(item.itemId)
                 } else {
                     Timber.e("Unable to retrieve ListFilesFragment.")
-                    Toast.makeText(this, UNEXPECTED_ERROR_OCCURRED, Toast.LENGTH_LONG).show()
+                    Snackbar.make(list_files_activity_layout, UNEXPECTED_CLIENT_ERROR, Snackbar.LENGTH_SHORT).show()
                 }
                 true
             }
@@ -117,7 +121,7 @@ class ListFilesActivity : AppCompatActivity() {
             }
             null -> {
                 Timber.e("Unable to get result of back press.")
-                Toast.makeText(this, UNEXPECTED_ERROR_OCCURRED, Toast.LENGTH_LONG).show()
+                Snackbar.make(list_files_activity_layout, UNEXPECTED_CLIENT_ERROR, Snackbar.LENGTH_SHORT).show()
             }
         }
     }
