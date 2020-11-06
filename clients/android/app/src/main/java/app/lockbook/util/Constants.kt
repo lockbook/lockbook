d395512a5506f070a08306a61c07768fbe7517db
Smail Barkouch <SmailBarkouch1@gmail.com>
2020-11-06 13:42:21 -0500

    lots of bug fixes and cleaner code

--- clients/android/app/src/main/java/app/lockbook/util/Constants.kt
+++ clients/android/app/src/main/java/app/lockbook/util/Constants.kt
@@ -50,7 +50,6 @@ object WorkManagerTags {
     const val PERIODIC_SYNC_TAG = "periodic_sync"
 }
 
-const val CANVAS_SIZE = 2500
 const val CANVAS_WIDTH = 2125
 const val CANVAS_HEIGHT = 2750
 const val TEXT_EDITOR_BACKGROUND_SAVE_PERIOD: Long = 5000
