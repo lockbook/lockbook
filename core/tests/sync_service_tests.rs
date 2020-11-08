6232139dfb8b9577c534d5fe42dc6f2bb7767e9f
Parth <parth@mehrotra.me>
2020-11-08 14:41:07 -0500

    fix delete metadata

--- core/tests/sync_service_tests.rs
+++ core/tests/sync_service_tests.rs
@@ -2,7 +2,6 @@ mod integration_test;
 
 #[cfg(test)]
 mod sync_tests {
-    use crate::integration_test::{generate_account, test_db};
     use lockbook_core::model::crypto::DecryptedValue;
     use lockbook_core::model::work_unit::WorkUnit;
     use lockbook_core::repo::file_metadata_repo::FileMetadataRepo;
@@ -12,6 +11,8 @@ mod sync_tests {
     use lockbook_core::{
         DefaultAccountService, DefaultFileMetadataRepo, DefaultFileService, DefaultSyncService,
     };
+
+    use crate::integration_test::{generate_account, test_db};
 
     #[test]
     fn test_create_files_and_folders_sync() {
@@ -1042,7 +1043,12 @@ mod sync_tests {
             .work_units
             .is_empty());
 
-        assert!(DefaultFileService::rename_file(&db, file.id, "file.md").is_err())
+        assert!(DefaultFileService::rename_file(&db, file.id, "file.md").is_err());
+
+        assert!(DefaultSyncService::calculate_work(&db)
+            .unwrap()
+            .work_units
+            .is_empty());
     }
 
     #[test]
@@ -1067,14 +1073,44 @@ mod sync_tests {
             .work_units
             .is_empty());
 
-        assert!(DefaultFileService::move_file(&db, file.id, file.parent).is_err())
+        assert!(DefaultFileService::move_file(&db, file.id, file.parent).is_err());
     }
 
     #[test]
     // Test that documents are deleted when a fresh sync happens
-    fn delete_document_test() {}
+    fn delete_document_test_sync() {
+        let db1 = test_db();
+        let db2 = test_db();
+        let generated_account = generate_account();
+        let account = DefaultAccountService::create_account(
+            &db1,
+            &generated_account.username,
+            &generated_account.api_url,
+        )
+        .unwrap();
 
+        let file =
+            DefaultFileService::create_at_path(&db1, &format!("{}/file.md", account.username))
+                .unwrap();
+
+        DefaultSyncService::sync(&db1).unwrap();
+        DefaultFileService::delete_document(&db1, file.id).unwrap();
+        DefaultSyncService::sync(&db1).unwrap();
+
+        DefaultAccountService::import_account(
+            &db2,
+            &DefaultAccountService::export_account(&db1).unwrap(),
+        )
+        .unwrap();
+        DefaultSyncService::sync(&db2).unwrap();
+
+        assert!(DefaultFileService::read_document(&db2, file.id).is_err());
+    }
+
+    #[test]
     // Test that documents are deleted between syncs
+    fn delete_document_test_after_sync() {}
+
     // Test that usage reflects a reduction in usage
     // Test that new documents that are deleted are never synced
 
