4944b980956baf95524d822129a157299f5d0d03
Smail Barkouch <SmailBarkouch1@gmail.com>
2020-11-09 16:29:14 -0500

    working tests and delete functionality

--- clients/android/app/src/main/java/app/lockbook/App.kt
+++ clients/android/app/src/main/java/app/lockbook/App.kt
@@ -12,7 +12,7 @@ import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import app.lockbook.model.CoreModel
-import app.lockbook.ui.FileModel
+import app.lockbook.model.FileModel
 import app.lockbook.util.SharedPreferences.BACKGROUND_SYNC_ENABLED_KEY
 import app.lockbook.util.SharedPreferences.BACKGROUND_SYNC_PERIOD_KEY
 import app.lockbook.util.SharedPreferences.IS_THIS_AN_IMPORT_KEY
