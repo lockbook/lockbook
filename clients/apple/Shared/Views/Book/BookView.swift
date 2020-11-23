013e95376fce1a416dc501da0d9ae317c106b442
Raayan Pillai <raayan@raayanpillai.com>
2020-11-22 20:55:59 -0500

    moving actions and indications to the bottom bar

--- clients/apple/Shared/Views/Book/BookView.swift
+++ clients/apple/Shared/Views/Book/BookView.swift
@@ -6,24 +6,9 @@ struct BookView: View {
     let account: Account
     
     var body: some View {
-        #if os(iOS)
-        let placement = ToolbarItemPlacement.navigationBarTrailing
-        #else
-        let placement = ToolbarItemPlacement.status
-        #endif
-
         NavigationView {
             makeList()
                 .navigationTitle(account.username)
-                .toolbar {
-                    ToolbarItem(placement: placement) {
-                        Button(action: core.sync) {
-                            SyncIndicator(syncing: $core.syncing)
-                                .foregroundColor(core.syncing ? .pink : .accentColor)
-                        }
-                        .disabled(core.syncing)
-                    }
-                }
 
             Text("Pick a file!")
         }
