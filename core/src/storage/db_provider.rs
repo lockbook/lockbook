6072e565a333b354de965b4c683717ddb1a0b621
Raayan Pillai <raayan@raayanpillai.com>
2020-11-28 13:09:43 -0500

    removing TempBackedDb in favor of temp config

--- core/src/storage/db_provider.rs
+++ core/src/storage/db_provider.rs
@@ -1,7 +1,4 @@
-use std::io;
-
 use sled::Db;
-use tempfile::tempdir;
 
 use crate::model::state::Config;
 use crate::DB_NAME;
@@ -9,7 +6,6 @@ use crate::DB_NAME;
 #[derive(Debug)]
 pub enum Error {
     SledError(sled::Error),
-    TempFileError(io::Error), // TODO ungroup these
 }
 
 pub trait DbProvider {
@@ -18,22 +14,11 @@ pub trait DbProvider {
 
 pub struct DiskBackedDB;
 
-pub struct TempBackedDB;
-
 impl DbProvider for DiskBackedDB {
     fn connect_to_db(config: &Config) -> Result<Db, Error> {
         let db_path = format!("{}/{}", &config.writeable_path, DB_NAME.to_string());
         debug!("DB Location: {}", db_path);
         Ok(sled::open(db_path.as_str()).map_err(Error::SledError)?)
-    }
-}
-
-impl DbProvider for TempBackedDB {
-    fn connect_to_db(_config: &Config) -> Result<Db, Error> {
-        let dir = tempdir().map_err(Error::TempFileError)?;
-        let dir_path = dir.path().join(DB_NAME);
-        debug!("DB Location: {:?}", dir_path);
-        Ok(sled::open(dir_path).map_err(Error::SledError)?)
     }
 }
 
