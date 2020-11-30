3bd707fc99a72a9b9236657d90ece79f67cf875e
Smail Barkouch <SmailBarkouch1@gmail.com>
2020-11-30 17:12:39 -0500

    fixed import error

--- clients/android/app/src/main/java/app/lockbook/model/CoreModel.kt
+++ clients/android/app/src/main/java/app/lockbook/model/CoreModel.kt
@@ -10,7 +10,7 @@ object CoreModel {
 
     private const val QA_API_URL = "http://qa.lockbook.app:8000"
     private const val PROD_API_URL = "http://api.lockbook.app:8000"
-    private fun getAPIURL(): String = System.getenv("API_URL") ?: QA_API_URL
+    fun getAPIURL(): String = System.getenv("API_URL") ?: QA_API_URL
 
     fun setUpInitLogger(path: String): Result<Unit, InitLoggerError> {
         val initLoggerResult: Result<Unit, InitLoggerError>? =
