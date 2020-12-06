511f7133d11b04fca49a6fc8173f72d12ceb0831
Smail Barkouch <SmailBarkouch1@gmail.com>
2020-12-06 11:22:02 -0500

    pulled in android branch for easy merge to master

--- clients/android/app/src/main/java/app/lockbook/model/FileModel.kt
+++ clients/android/app/src/main/java/app/lockbook/model/FileModel.kt
@@ -9,7 +9,6 @@ import androidx.work.WorkerParameters
 import app.lockbook.App
 import app.lockbook.util.*
 import app.lockbook.util.Messages.UNEXPECTED_CLIENT_ERROR
-import com.beust.klaxon.Klaxon
 import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Ok
 import timber.log.Timber
@@ -58,7 +57,7 @@ class FileModel(path: String) {
                 ) {
                     is Ok -> {
                         parentFileMetadata = getParentOfParentResult.value
-                        if(filePath.size != 1) {
+                        if (filePath.size != 1) {
                             filePath.remove(filePath.last())
                         }
                         _updateBreadcrumbBar.postValue(filePath)
