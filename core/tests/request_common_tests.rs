59b3957cfd66b6720da030fca007023aab3ddd71
Parth <parth@mehrotra.me>
2020-12-08 22:41:41 -0500

    fmt

--- core/tests/request_common_tests.rs
+++ core/tests/request_common_tests.rs
@@ -5,16 +5,22 @@ mod request_common_tests {
     use crate::assert_matches;
     use crate::integration_test::{generate_account, generate_root_metadata, test_config};
     use lockbook_core::client::{ApiError, Client, ClientImpl};
-    use lockbook_core::model::api::{GetPublicKeyError, GetPublicKeyRequest, GetPublicKeyResponse, NewAccountRequest, NewAccountError};
-    use lockbook_core::service::code_version_service::CodeVersion;
-    use lockbook_core::{create_account, get_account, DefaultCrypto, DefaultCodeVersion};
-    use rsa::{RSAPublicKey, RSAPrivateKey};
-    use lockbook_core::service::crypto_service::{RSAImpl, PubKeyCryptoService, RSADecryptError, RSAVerifyError, RSAEncryptError, RSASignError};
+    use lockbook_core::model::api::{
+        GetPublicKeyError, GetPublicKeyRequest, GetPublicKeyResponse, NewAccountError,
+        NewAccountRequest,
+    };
+    use lockbook_core::model::crypto::{RSAEncrypted, RSASigned};
     use lockbook_core::service::clock_service::{Clock, ClockImpl};
-    use lockbook_core::model::crypto::{RSASigned, RSAEncrypted};
+    use lockbook_core::service::code_version_service::CodeVersion;
+    use lockbook_core::service::crypto_service::{
+        PubKeyCryptoService, RSADecryptError, RSAEncryptError, RSAImpl, RSASignError,
+        RSAVerifyError,
+    };
+    use lockbook_core::{create_account, get_account, DefaultCodeVersion, DefaultCrypto};
     use rsa::errors::Error;
-    use serde::Serialize;
+    use rsa::{RSAPrivateKey, RSAPublicKey};
     use serde::de::DeserializeOwned;
+    use serde::Serialize;
 
     struct MockCodeVersion;
     impl CodeVersion for MockCodeVersion {
@@ -51,7 +57,7 @@ mod request_common_tests {
     }
 
     struct MockClock;
-    impl Clock for MockClock{
+    impl Clock for MockClock {
         fn get_time() -> i64 {
             ClockImpl::get_time() - 3600000
         }
@@ -61,34 +67,47 @@ mod request_common_tests {
     fn expired_request() {
         let account = generate_account();
         let (root, _) = generate_root_metadata(&account);
-        let result = ClientImpl::<RSAImpl<MockClock>, DefaultCodeVersion>::request(&account, NewAccountRequest::new(&account, &root));
-        assert_matches!(
-            result,
-            Err(ApiError::<NewAccountError>::ExpiredAuth)
+        let result = ClientImpl::<RSAImpl<MockClock>, DefaultCodeVersion>::request(
+            &account,
+            NewAccountRequest::new(&account, &root),
         );
+        assert_matches!(result, Err(ApiError::<NewAccountError>::ExpiredAuth));
     }
 
     struct MockRSA;
-    impl PubKeyCryptoService for MockRSA{
+    impl PubKeyCryptoService for MockRSA {
         fn generate_key() -> Result<RSAPrivateKey, Error> {
             unimplemented!()
         }
 
-        fn encrypt<T: Serialize + DeserializeOwned>(_: &RSAPublicKey, _: &T) -> Result<RSAEncrypted<T>, RSAEncryptError> {
+        fn encrypt<T: Serialize + DeserializeOwned>(
+            _: &RSAPublicKey,
+            _: &T,
+        ) -> Result<RSAEncrypted<T>, RSAEncryptError> {
             unimplemented!()
         }
 
-        fn decrypt<T: DeserializeOwned>(_: &RSAPrivateKey, _: &RSAEncrypted<T>) -> Result<T, RSADecryptError> {
+        fn decrypt<T: DeserializeOwned>(
+            _: &RSAPrivateKey,
+            _: &RSAEncrypted<T>,
+        ) -> Result<T, RSADecryptError> {
             unimplemented!()
         }
 
-        fn sign<T: Serialize>(private_key: &RSAPrivateKey, to_sign: T) -> Result<RSASigned<T>, RSASignError> {
+        fn sign<T: Serialize>(
+            private_key: &RSAPrivateKey,
+            to_sign: T,
+        ) -> Result<RSASigned<T>, RSASignError> {
             let mut result = RSAImpl::<ClockImpl>::sign(private_key, to_sign).unwrap();
             result.signature.pop();
             Ok(result)
         }
 
-        fn verify<T: Serialize>(_: &RSAPublicKey, _: &RSASigned<T>, _: u64) -> Result<(), RSAVerifyError> {
+        fn verify<T: Serialize>(
+            _: &RSAPublicKey,
+            _: &RSASigned<T>,
+            _: u64,
+        ) -> Result<(), RSAVerifyError> {
             unimplemented!()
         }
     }
@@ -97,10 +116,10 @@ mod request_common_tests {
     fn bad_signature() {
         let account = generate_account();
         let (root, _) = generate_root_metadata(&account);
-        let result = ClientImpl::<MockRSA, DefaultCodeVersion>::request(&account, NewAccountRequest::new(&account, &root));
-        assert_matches!(
-            result,
-            Err(ApiError::<NewAccountError>::InvalidAuth)
+        let result = ClientImpl::<MockRSA, DefaultCodeVersion>::request(
+            &account,
+            NewAccountRequest::new(&account, &root),
         );
+        assert_matches!(result, Err(ApiError::<NewAccountError>::InvalidAuth));
     }
 }
