5b11ff65f38fb08b3ed9561d1d9386ce0925a5f1
steverusso <steverusso@protonmail.com>
2020-12-15 10:46:56 -0500

    Refactor how Errors are sent through the Msg channel and displayed.

--- clients/linux/src/error.rs
+++ clients/linux/src/error.rs
@@ -4,3 +4,19 @@ pub enum LbError {
     User(String),
     Program(String),
 }
+
+impl LbError {
+    pub fn msg(&self) -> &String {
+        match self {
+            Self::User(msg) => msg,
+            Self::Program(msg) => msg,
+        }
+    }
+
+    pub fn is_prog(&self) -> bool {
+        match self {
+            Self::User(_) => false,
+            Self::Program(_) => true,
+        }
+    }
+}
