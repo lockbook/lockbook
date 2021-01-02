aadc8de922e0beab8a213fda1a42a7c202e50e34
Parth <parth@mehrotra.me>
2021-01-02 17:39:44 -0500

    don't recycle, don't use list

--- clients/apple/Shared/Components/FileCell.swift
+++ clients/apple/Shared/Components/FileCell.swift
@@ -3,42 +3,15 @@ import SwiftLockbookCore
 
 struct FileCell: View {
     let meta: FileMetadata
-
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text(meta.name)
-            Label(intEpochToString(epoch: meta.contentVersion), systemImage: meta.fileType == .Folder ? "folder" : "doc")
-                .font(.footnote)
-                .foregroundColor(.secondary)
-        }
-    }
-}
-
-struct SyntheticFileCell: View {
-    let params: (FileMetadata, FileType)
-    @Binding var nameField: String
-    let onCreate: () -> Void
-    let onCancel: () -> Void
-
+    
     var body: some View {
         HStack {
-            VStack(alignment: .leading) {
-                TextField("new file...", text: $nameField)
-                    .autocapitalization(.none)
-                Label("New \(params.1.rawValue) in \(params.0.name)", systemImage: params.1 == .Folder ? "folder" : "doc")
-                    .font(.footnote)
-                    .foregroundColor(.secondary)
-            }
-            Button(action: onCreate) {
-                Image(systemName: "plus")
-                    .foregroundColor(.green)
-            }
-            .padding(.horizontal, 10)
-            Button(action: onCancel) {
-                Image(systemName: "xmark")
-                    .foregroundColor(.red)
-            }
-        }
+            Image(systemName: meta.fileType == .Folder ? "folder" : "doc")
+                .frame(width: 40, height: 40, alignment: .center)
+                .foregroundColor( meta.fileType == .Folder ? .blue : .white)
+            Text(meta.name)
+            Spacer()
+        }.background(Color(UIColor.tertiarySystemFill))
     }
 }
 
@@ -46,7 +19,6 @@ struct FileCell_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             FileCell(meta: Core().files[0])
-            SyntheticFileCell(params: (Core().files[0], .Document), nameField: .constant(""), onCreate: {}, onCancel: {})
         }
         .previewLayout(.fixed(width: 300, height: 50))
     }
--- clients/apple/Shared/Views/Book/FileListView.swift
+++ clients/apple/Shared/Views/Book/FileListView.swift
@@ -3,177 +3,67 @@ import SwiftLockbookCore
 
 struct FileListView: View {
     @ObservedObject var core: Core
+    @State var showingAccount: Bool = false
+    let currentFolder: FileMetadata
     let account: Account
-    let root: FileMetadata
-    @State var creating: FileType?
-    @State var creatingName: String = ""
-    @State var path: [FileMetadata]
-    @State var currentFolder: FileMetadata
-
-    var body: some View {
-        let filtered = computeFileList()
-        let baseView = List {
-            Button(action: handleNavigateUp) {
-                HStack {
-                    Image(systemName: "arrow.turn.left.up")
-                        .foregroundColor(.accentColor)
-                    ForEach(path) {
-                        Text($0.name)
-                        Text("/")
-                            .foregroundColor(.accentColor)
-                    }
-                    Text(currentFolder.name)
-                }
-            }
-            creating.map { creatingType in
-                SyntheticFileCell(params: (currentFolder, creatingType), nameField: $creatingName, onCreate: {
-                    handleCreate(meta: currentFolder, type: creatingType)
-                }, onCancel: doneCreating)
-            }
-            ForEach(filtered) { meta in
-                renderCell(meta: meta)
-                    .contextMenu(menuItems: {
-                        Button(action: {
-                            handleDelete(meta: meta)
-                        }) {
-                            Label("Delete", systemImage: "trash.fill")
-                        }
-                    })
-            }
-            .onDelete(perform: {
-                handleDelete(meta: filtered[$0.first!])
-            })
-        }
-        .onReceive(core.timer, perform: { _ in
-            core.sync()
-        })
-
-        #if os(iOS)
-        return baseView
-            .toolbar {
-                ToolbarItemGroup(placement: .bottomBar) {
-                    ProgressView()
-                        .opacity(core.syncing ? 1.0 : 0)
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    Text("\(core.files.count) items")
-                        .foregroundColor(.secondary)
-                    Spacer()
-                    Spacer()
-                    Button(action: { creating = .Folder }) {
-                        Image(systemName: "folder.badge.plus")
-                    }
-                    Button(action: { creating = .Document }) {
-                        Image(systemName: "square.and.pencil")
-                    }
-                }
-            }
-        #else
-        return VStack {
-            baseView
-                .toolbar {
-                    ToolbarItemGroup(placement: .primaryAction, content: { HStack { } })
-                }
-            Spacer()
-            HStack {
-                Button(action: { creating = .Folder }) {
-                    Image(systemName: "folder.badge.plus")
-                }
-                Button(action: { creating = .Document }) {
-                    Image(systemName: "doc.on.doc")
-                }
-                Spacer()
-                Text("\(core.files.count) items")
-                    .foregroundColor(.secondary)
-                Spacer()
-                ProgressView()
-                    .controlSize(.small)
-                    .opacity(core.syncing ? 1.0 : 0)
-            }
-            .padding()
-        }
-        #endif
-    }
-
-    init(core: Core, account: Account, root: FileMetadata) {
-        self.core = core
-        self.account = account
-        self.root = root
-        self._path = .init(initialValue: [])
-        self._currentFolder = .init(initialValue: root)
-    }
-
-    func handleCreate(meta: FileMetadata, type: FileType) {
-        switch core.api.createFile(name: creatingName, dirId: meta.id, isFolder: type == .Folder) {
-        case .success(_):
-            doneCreating()
-            core.updateFiles()
-        case .failure(let err):
-            core.handleError(err)
-        }
-    }
-
-    func handleDelete(meta: FileMetadata) {
-        switch core.api.deleteFile(id: meta.id) {
-        case .success(_):
-            core.updateFiles()
-        case .failure(let err):
-            core.handleError(err)
-        }
-    }
-
-    func handleSelectFolder(meta: FileMetadata) {
-        withAnimation {
-            path.append(currentFolder)
-            currentFolder = meta
-        }
-    }
-
-    func handleNavigateUp() {
-        withAnimation {
-            path.popLast().map {
-                currentFolder = $0
-            }
-        }
-    }
-
-    func computeFileList() -> [FileMetadata] {
+    
+    var children: [FileMetadata] {
         core.files.filter {
             $0.parent == currentFolder.id && $0.id != currentFolder.id
         }
     }
-
-    func doneCreating() {
-        withAnimation {
-            creating = .none
-            creatingName = ""
-        }
-    }
-
-    func renderCell(meta: FileMetadata) -> AnyView {
-        if meta.fileType == .Folder {
-            return AnyView(
-                Button(action: { handleSelectFolder(meta: meta) }) {
-                    FileCell(meta: meta)
+    
+    var body: some View {
+        ScrollView {
+            HStack {
+                Spacer()
+            }
+            VStack {
+                ForEach(children) { meta in
+                    if meta.fileType == .Folder {
+                        NavigationLink(destination: FileListView(core: core, currentFolder: meta, account: account)) {
+                            FileCell(meta: meta)
+                        }.isDetailLink(false)
+                    } else {
+                        NavigationLink(destination: EditorView(core: core, meta: meta).equatable()) {
+                            FileCell(meta: meta)
+                        }
+                    }
                 }
-            )
-        } else {
-            return AnyView(
-                NavigationLink(destination: EditorView(core: core, meta: meta).equatable()) {
-                    FileCell(meta: meta)
-                }
-            )
+            }
+            .cornerRadius(10)
         }
+        .padding(.leading, 10)
+        .sheet(isPresented: $showingAccount, content: {
+            AccountView(core: core, account: account)
+        })
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: { showingAccount.toggle() }) {
+                    Image(systemName: "person.circle.fill")
+                }
+            }
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: core.sync) {
+                    Image(systemName: "arrow.right.arrow.left.circle.fill")
+                }
+            }
+            ToolbarItemGroup(placement: .bottomBar) {
+                ProgressView()
+                    .opacity(core.syncing ? 1.0 : 0)
+                Spacer()
+                Text("\(core.files.count) items")
+                    .foregroundColor(.secondary)
+                Spacer()
+                Button(action: { }) {
+                    Image(systemName: "folder.badge.plus")
+                }
+                Button(action: { }) {
+                    Image(systemName: "square.and.pencil")
+                }
+            }
+        }
+        .navigationTitle(currentFolder.name)
     }
 }
 
-struct FileListView_Previews: PreviewProvider {
-    static let core = Core()
-
-    static var previews: some View {
-        NavigationView {
-            FileListView(core: core, account: core.account!, root: core.root!)
-        }
-    }
-}
