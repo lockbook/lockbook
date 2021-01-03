53569411d215fb30ccb69661521fa965533e67d1
travis <t.vanderstad@gmail.com>
2021-01-03 17:46:54 -0500

    settings factoring

--- clients/windows/lockbook/Settings.xaml.cs
+++ clients/windows/lockbook/Settings.xaml.cs
@@ -6,16 +6,53 @@ using Windows.Storage.Streams;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace lockbook {
 
     public sealed partial class SignInContentDialog : ContentDialog {
         const ulong BYTE = 1;
-        const ulong KILOBYTES = BYTE* 1000;
-        const ulong MEGABYTES = KILOBYTES* 1000;
-        const ulong GIGABYTES = MEGABYTES* 1000;
-        const ulong TERABYTES = GIGABYTES* 1000;
+        const ulong KILOBYTES = BYTE * 1000;
+        const ulong MEGABYTES = KILOBYTES * 1000;
+        const ulong GIGABYTES = MEGABYTES * 1000;
+        const ulong TERABYTES = GIGABYTES * 1000;
+
+        public string Username {
+            get {
+                return usernameTextBlock.Text;
+            }
+            set {
+                usernameTextBlock.Text = value;
+            }
+        }
+
+        public string ServerLocation {
+            get {
+                return serverLocationTextBlock.Text;
+            }
+            set {
+                serverLocationTextBlock.Text = value;
+            }
+        }
+
+        public string SpaceUsed {
+            get {
+                return spaceUsedTextBlock.Text;
+            }
+            set {
+                spaceUsedTextBlock.Text = value;
+            }
+        }
+
+        public ImageSource AccountQRCode {
+            get {
+                return qrCodeImg.Source;
+            }
+            set {
+                qrCodeImg.Source = value;
+            }
+        }
 
         public SignInContentDialog() {
             InitializeComponent();
@@ -23,9 +60,24 @@ namespace lockbook {
             setUsernameAndApiUrl();
         }
 
+        public static string SpaceUsedStringFromUsageBytes(ulong usageBytes) {
+            switch (usageBytes) {
+                case ulong inBytes when inBytes < KILOBYTES:
+                    return "" + usageBytes + " Bytes";
+                case ulong inKiloBytes when inKiloBytes < MEGABYTES:
+                    return "" + usageBytes / (double)KILOBYTES + " KB";
+                case ulong inMegabytes when inMegabytes < GIGABYTES:
+                    return "" + usageBytes / (double)MEGABYTES + " MB";
+                case ulong inGigaytes when inGigaytes < TERABYTES:
+                    return "" + usageBytes / (double)GIGABYTES + " GB";
+                default:
+                    return "" + usageBytes / (double)TERABYTES + " TB";
+            }
+        }
+
         private async void setUsage() {
             var usageString = "";
-            switch(await App.CoreService.GetUsage()) {
+            switch (await App.CoreService.GetUsage()) {
                 case Core.GetUsage.Success success:
                     ulong bytes = 0;
                     foreach (var usage in success.usage) {
@@ -34,23 +86,7 @@ namespace lockbook {
 
                     System.Diagnostics.Debug.WriteLine(bytes + " bytes");
 
-                    switch (bytes) {
-                        case ulong inBytes when inBytes < KILOBYTES:
-                            usageString = "" + inBytes + " Bytes";
-                            break; 
-                        case ulong inKiloBytes when inKiloBytes < MEGABYTES:
-                            usageString = "" + bytes / (double) KILOBYTES + " KB";
-                            break; 
-                        case ulong inMegabytes when inMegabytes < GIGABYTES:
-                            usageString = "" + bytes / (double) MEGABYTES + " MB";
-                            break;
-                        case ulong inGigaytes when inGigaytes < TERABYTES:
-                            usageString = "" + bytes / (double) GIGABYTES + " GB";
-                            break;
-                        default:
-                            usageString = "" + bytes / (double) TERABYTES + " TB";
-                            break;
-                    }
+                    usageString = SpaceUsedStringFromUsageBytes(bytes);
                     break;
 
                 case Core.GetUsage.ExpectedError expectedError:
@@ -72,21 +108,21 @@ namespace lockbook {
                     break;
             }
 
-            spaceUsedTextBlock.Text = usageString;
+            SpaceUsed = usageString;
         }
 
         public async void setUsernameAndApiUrl() {
             switch (await App.CoreService.GetAccount()) {
                 case Core.GetAccount.Success success:
-                    usernameTextBlock.Text = success.account.username;
-                    apiTextBlock.Text = success.account.apiUrl;
+                    Username = success.account.username;
+                    ServerLocation = success.account.apiUrl;
                     break;
 
                 case Core.GetAccount.ExpectedError expectedError:
                     switch (expectedError.Error) {
                         case Core.GetAccount.PossibleErrors.NoAccount:
                             usernameTextBlock.Text = "No Account!";
-                            apiTextBlock.Text = "No Account!";
+                            serverLocationTextBlock.Text = "No Account!";
                             break;
                     }
                     break;
@@ -97,21 +133,25 @@ namespace lockbook {
             }
         }
 
+        private static void CopyToClipboard(string s) {
+            var dataPackage = new DataPackage {
+                RequestedOperation = DataPackageOperation.Copy
+            };
+            dataPackage.SetText(s);
+            Clipboard.SetContent(dataPackage);
+        }
+
         private async void CopyAccountStringToClipboard(object sender, RoutedEventArgs e) {
             switch (await App.CoreService.ExportAccount()) {
                 case Core.ExportAccount.Success success:
-                    var dataPackage = new DataPackage {
-                        RequestedOperation = DataPackageOperation.Copy
-                    };
-                    dataPackage.SetText(success.accountString);
-                    Clipboard.SetContent(dataPackage);
+                    CopyToClipboard(success.accountString);
                     break;
 
                 case Core.ExportAccount.ExpectedError expectedError:
                     switch (expectedError.Error) {
                         case Core.ExportAccount.PossibleErrors.NoAccount:
                             usernameTextBlock.Text = "No Account!";
-                            apiTextBlock.Text = "No Account!";
+                            serverLocationTextBlock.Text = "No Account!";
                             break;
                     }
                     break;
@@ -122,33 +162,37 @@ namespace lockbook {
             }
         }
 
+        private static async Task<ImageSource> GenerateQRCode(string accountString) {
+            // https://github.com/codebude/QRCoder/wiki/Advanced-usage---QR-Code-renderers#24-bitmapbyteqrcode-renderer-in-detail
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(accountString, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new BitmapByteQRCode(qrCodeData);
+            var qrCodeAsBitmapByteArr = await Task.Run(() => qrCode.GetGraphic(20));
+
+            using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream()) {
+                using (DataWriter writer = new DataWriter(stream.GetOutputStreamAt(0))) {
+                    writer.WriteBytes(qrCodeAsBitmapByteArr);
+                    await writer.StoreAsync();
+                }
+                var image = new BitmapImage();
+                await image.SetSourceAsync(stream);
+
+                return image;
+            }
+        }
+
         private async void ShowQRCode(object sender, RoutedEventArgs e) {
             switch (await App.CoreService.ExportAccount()) {
                 case Core.ExportAccount.Success success:
                     showQRCode.IsEnabled = false;
-                    // https://github.com/codebude/QRCoder/wiki/Advanced-usage---QR-Code-renderers#24-bitmapbyteqrcode-renderer-in-detail
-                    var qrGenerator = new QRCodeGenerator();
-                    var qrCodeData = qrGenerator.CreateQrCode(success.accountString, QRCodeGenerator.ECCLevel.Q);
-                    var qrCode = new BitmapByteQRCode(qrCodeData);
-                    var qrCodeAsBitmapByteArr = await Task.Run(() => qrCode.GetGraphic(20));
-
-                    using (InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream()) {
-                        using (DataWriter writer = new DataWriter(stream.GetOutputStreamAt(0))) {
-                            writer.WriteBytes(qrCodeAsBitmapByteArr);
-                            await writer.StoreAsync();
-                        }
-                        var image = new BitmapImage();
-                        await image.SetSourceAsync(stream);
-
-                        qrCodeImg.Source = image;
-                    }
+                    qrCodeImg.Source = await GenerateQRCode(success.accountString);
                     break;
 
                 case Core.ExportAccount.ExpectedError expectedError:
                     switch (expectedError.Error) {
                         case Core.ExportAccount.PossibleErrors.NoAccount:
                             usernameTextBlock.Text = "No Account!";
-                            apiTextBlock.Text = "No Account!";
+                            serverLocationTextBlock.Text = "No Account!";
                             break;
                     }
                     break;
