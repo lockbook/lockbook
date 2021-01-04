fe5047f30b5543964a2ef037a9b78f8d5cf8d203
Parth <parth@mehrotra.me>
2021-01-03 21:02:35 -0500

    added back synthetic cell

--- clients/apple/Shared/Components/FileCell.swift
+++ clients/apple/Shared/Components/FileCell.swift
@@ -6,12 +6,56 @@ struct FileCell: View {
     
     var body: some View {
         HStack {
-            Image(systemName: meta.fileType == .Folder ? "folder" : "doc")
-                .frame(width: 40, height: 40, alignment: .center)
-                .foregroundColor( meta.fileType == .Folder ? .blue : .white)
-            Text(meta.name)
+            VStack(alignment: .leading, spacing: 5) {
+                Text(meta.name)
+                    .font(.title3)
+                HStack {
+                    Image(systemName: meta.fileType == .Folder ? "folder" : "doc")
+                        .foregroundColor(meta.fileType == .Folder ? .blue : .secondary)
+                    Text(intEpochToString(epoch: meta.contentVersion))
+                        .foregroundColor(.secondary)
+                    
+                }.font(.footnote)
+            }
+            .padding(.vertical, 5)
             Spacer()
-        }.background(Color(UIColor.tertiarySystemFill))
+            
+            if meta.fileType == .Folder {
+                Image(systemName: "chevron.right")
+                    .padding(.trailing, 10)
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+}
+
+struct SyntheticFileCell: View {
+    let params: (FileMetadata, FileType)
+    @Binding var nameField: String
+    let onCreate: () -> Void
+    let onCancel: () -> Void
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                TextField(params.1 == .Folder ? "folder name" : "document name", text: $nameField,
+                          onCommit: onCreate)
+                    .autocapitalization(.none)
+                    .font(.title3)
+                HStack {
+                    Image(systemName: params.1 == .Folder ? "folder" : "doc")
+                        .foregroundColor(params.1 == .Folder ? .blue : .secondary)
+                    Text("New \(params.1.rawValue) in \(params.0.name)")
+                        .foregroundColor(.gray)
+                    
+                }.font(.footnote)
+            }
+            Button(action: onCancel) {
+                Image(systemName: "xmark")
+                    .foregroundColor(.red)
+            }
+        }
+        
     }
 }
 
@@ -19,6 +63,8 @@ struct FileCell_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             FileCell(meta: Core().files[0])
+            SyntheticFileCell(params: (Core().files[0], .Document), nameField: .constant(""), onCreate: {}, onCancel: {})
+            
         }
         .previewLayout(.fixed(width: 300, height: 50))
     }
--- clients/apple/Shared/Views/Book/FileListView.swift
+++ clients/apple/Shared/Views/Book/FileListView.swift
@@ -4,10 +4,12 @@ import SwiftLockbookCore
 struct FileListView: View {
     @ObservedObject var core: Core
     @State var showingAccount: Bool = false
+    @State var creating: FileType?
+    @State var creatingName: String = ""
     let currentFolder: FileMetadata
     let account: Account
     
-    var children: [FileMetadata] {
+    func computeFileList() -> [FileMetadata] {
         core.files.filter {
             $0.parent == currentFolder.id && $0.id != currentFolder.id
         }
@@ -15,11 +17,14 @@ struct FileListView: View {
     
     var body: some View {
         ScrollView {
-            HStack {
-                Spacer()
-            }
             VStack {
-                ForEach(children) { meta in
+                creating.map { type in
+                    SyntheticFileCell(params: (currentFolder, type), nameField: $creatingName, onCreate: {
+                        handleCreate(meta: currentFolder, type: type)
+                    }, onCancel: doneCreating)
+                }
+                
+                ForEach(computeFileList()) { meta in
                     if meta.fileType == .Folder {
                         NavigationLink(destination: FileListView(core: core, currentFolder: meta, account: account)) {
                             FileCell(meta: meta)
@@ -31,9 +36,9 @@ struct FileListView: View {
                     }
                 }
             }
-            .cornerRadius(10)
+            .padding(.leading, 20)
         }
-        .padding(.leading, 10)
+        
         .sheet(isPresented: $showingAccount, content: {
             AccountView(core: core, account: account)
         })
@@ -55,15 +60,51 @@ struct FileListView: View {
                 Text("\(core.files.count) items")
                     .foregroundColor(.secondary)
                 Spacer()
-                Button(action: { }) {
-                    Image(systemName: "folder.badge.plus")
+                Menu {
+                    Button(action: {creating = .Document}) {
+                        Label("Create a document", systemImage: "doc")
+                    }
+                    
+                    Button(action: {creating = .Folder}) {
+                        Label("Create a folder", systemImage: "folder")
+                    }
                 }
-                Button(action: { }) {
-                    Image(systemName: "square.and.pencil")
+                label: {
+                    Label("Add", systemImage: "plus")
+                        .frame(width: 40, height: 40)
                 }
             }
         }
         .navigationTitle(currentFolder.name)
     }
+    
+    func handleCreate(meta: FileMetadata, type: FileType) {
+        switch core.api.createFile(name: creatingName, dirId: meta.id, isFolder: type == .Folder) {
+        case .success(_):
+            doneCreating()
+            core.updateFiles()
+        case .failure(let err):
+            core.handleError(err)
+        }
+    }
+    
+    func doneCreating() {
+        withAnimation {
+            creating = .none
+            creatingName = ""
+        }
+    }
+    
 }
 
+struct FileListView_Previews: PreviewProvider {
+    static let core = Core()
+    
+    static var previews: some View {
+        NavigationView {
+            FileListView(core: core,
+                         showingAccount: false, currentFolder: core.root!,
+                         account: core.account!)
+        }
+    }
+}
