c9b1bb53e1f5fd626180f5d41e390a31a407e7d7
Smail Barkouch <SmailBarkouch1@gmail.com>
2021-01-07 17:52:12 -0500

    better handled expect

--- clients/cli/src/utils.rs
+++ clients/cli/src/utils.rs
@@ -2,8 +2,8 @@ use std::{env, fs};
 
 use lockbook_core::model::state::Config;
 use lockbook_core::{
-    get_account, get_db_state, get_last_synced_human_string, init_logger, migrate_db,
-    GetAccountError, GetStateError, MigrationError,
+    get_account, get_db_state, get_last_synced_human_string, init_logger, migrate_db, Error,
+    GetAccountError, GetLastSyncedError, GetStateError, MigrationError,
 };
 use lockbook_core::{write_document, Error as CoreError, WriteToDocumentError};
 
@@ -168,8 +168,13 @@ pub fn edit_file_with_editor(file_location: &str) -> bool {
 
 pub fn print_last_successful_sync() {
     if atty::is(atty::Stream::Stdout) {
-        let last_updated = get_last_synced_human_string(&get_config())
-            .expect("Failed to retrieve content from FileMetadataRepo");
+        let last_updated = match get_last_synced_human_string(&get_config()) {
+            Ok(ok) => ok,
+            Err(_) => exit_with(
+                "Unexpected error while attempting to retrieve usage: {:#?}",
+                UNEXPECTED_ERROR,
+            ),
+        };
 
         println!("Last successful sync: {}", last_updated);
     }
