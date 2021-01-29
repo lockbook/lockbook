817b146c3f5e9c7f3dfe973c17d2bb618437f57f
steverusso <steverusso@protonmail.com>
2021-01-29 09:12:10 -0500

    cli: tidy rename error matching.

--- clients/cli/src/rename.rs
+++ clients/cli/src/rename.rs
@@ -11,21 +11,19 @@ pub fn rename(path: &str, new_name: &str) {
         Ok(file_metadata) => match rename_file(&get_config(), file_metadata.id, new_name) {
             Ok(_) => exit_success(""),
             Err(err) => match err {
-                CoreError::UiError(RenameFileError::NewNameEmpty) => {
-                    exitlb!(FileNameEmpty, "New name is empty!")
-                }
-                CoreError::UiError(RenameFileError::CannotRenameRoot) => {
-                    exitlb!(NoRootOps("rename".to_string()))
-                }
-                CoreError::UiError(RenameFileError::NewNameContainsSlash) => {
-                    exitlb!(FileNameHasSlash, "New name cannot contain a slash!")
-                }
-                CoreError::UiError(RenameFileError::FileNameNotAvailable) => {
-                    exitlb!(FileNameNotAvailable, "File name not available!")
-                }
-                CoreError::UiError(RenameFileError::FileDoesNotExist) => {
-                    err_unexpected!("FileDoesNotExist!").exit()
-                }
+                CoreError::UiError(err) => match err {
+                    RenameFileError::NewNameEmpty => exitlb!(FileNameEmpty, "New name is empty!"),
+                    RenameFileError::CannotRenameRoot => exitlb!(NoRootOps("rename".to_string())),
+                    RenameFileError::NewNameContainsSlash => {
+                        exitlb!(FileNameHasSlash, "New name cannot contain a slash!")
+                    }
+                    RenameFileError::FileNameNotAvailable => {
+                        exitlb!(FileNameNotAvailable, "File name not available!")
+                    }
+                    RenameFileError::FileDoesNotExist => {
+                        err_unexpected!("FileDoesNotExist!").exit()
+                    }
+                },
                 CoreError::Unexpected(msg) => err_unexpected!("{}", msg).exit(),
             },
         },
