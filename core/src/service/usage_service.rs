d2c48d2ebe71f4df1bde0dde2c73cf1afdc5114a
Smail Barkouch <SmailBarkouch@users.noreply.github.com>
2021-01-31 10:35:11 -0500

    core: added sanity check for the lib.rs function get usage human string  (#540)
    * added get_usage human string sanity check
    
    * made sanity check rely on a more specific output
    
    * forgot to add letter to check
    
    * fixed sanity test

--- core/src/service/usage_service.rs
+++ core/src/service/usage_service.rs
@@ -1,10 +1,10 @@
+use crate::client;
 use crate::client::Client;
 use crate::model::api;
 use crate::model::api::{GetUsageRequest, GetUsageResponse};
 use crate::repo::account_repo;
 use crate::repo::account_repo::AccountRepo;
 use crate::storage::db_provider::Backend;
-use crate::{client, DefaultClient};
 
 pub const BYTE: u64 = 1;
 pub const KILOBYTE: u64 = BYTE * 1000;
@@ -17,12 +17,13 @@ pub const MEGABYTE_PLUS_ONE: u64 = MEGABYTE + 1;
 pub const GIGABYTE_PLUS_ONE: u64 = GIGABYTE + 1;
 pub const TERABYTE_PLUS_ONE: u64 = TERABYTE + 1;
 
+#[derive(Debug)]
 pub enum GetUsageError<MyBackend: Backend> {
     AccountRetrievalError(account_repo::AccountRepoError<MyBackend>),
     ApiError(client::ApiError<api::GetUsageError>),
 }
 
-pub trait UsageService<MyBackend: Backend, AccountDb: AccountRepo<MyBackend>> {
+pub trait UsageService<MyBackend: Backend, AccountDb: AccountRepo<MyBackend>, ApiClient: Client> {
     fn get_usage(backend: &MyBackend::Db) -> Result<GetUsageResponse, GetUsageError<MyBackend>>;
     fn get_usage_human_string(
         backend: &MyBackend::Db,
@@ -30,18 +31,24 @@ pub trait UsageService<MyBackend: Backend, AccountDb: AccountRepo<MyBackend>> {
     ) -> Result<String, GetUsageError<MyBackend>>;
 }
 
-pub struct UsageServiceImpl<MyBackend: Backend, AccountDb: AccountRepo<MyBackend>> {
+pub struct UsageServiceImpl<
+    MyBackend: Backend,
+    AccountDb: AccountRepo<MyBackend>,
+    ApiClient: Client,
+> {
     _accounts: AccountDb,
     _backend: MyBackend,
+    _client: ApiClient,
 }
 
-impl<MyBackend: Backend, AccountDb: AccountRepo<MyBackend>> UsageService<MyBackend, AccountDb>
-    for UsageServiceImpl<MyBackend, AccountDb>
+impl<MyBackend: Backend, AccountDb: AccountRepo<MyBackend>, ApiClient: Client>
+    UsageService<MyBackend, AccountDb, ApiClient>
+    for UsageServiceImpl<MyBackend, AccountDb, ApiClient>
 {
     fn get_usage(backend: &MyBackend::Db) -> Result<GetUsageResponse, GetUsageError<MyBackend>> {
         let acc = AccountDb::get_account(backend).map_err(GetUsageError::AccountRetrievalError)?;
 
-        DefaultClient::request(&acc, GetUsageRequest {}).map_err(GetUsageError::ApiError)
+        ApiClient::request(&acc, GetUsageRequest {}).map_err(GetUsageError::ApiError)
     }
 
     fn get_usage_human_string(
@@ -71,5 +78,149 @@ impl<MyBackend: Backend, AccountDb: AccountRepo<MyBackend>> UsageService<MyBacke
                 abbr
             ))
         }
+    }
+}
+
+#[cfg(test)]
+mod unit_tests {
+    use crate::client::{ApiError, Client};
+    use crate::model::account::{Account, ApiUrl};
+    use crate::model::api::{FileUsage, GetUsageResponse, Request};
+    use crate::model::state::Config;
+    use crate::repo::account_repo::{AccountRepo, AccountRepoError};
+    use crate::service::clock_service::ClockImpl;
+    use crate::service::crypto_service::{PubKeyCryptoService, RSAImpl};
+    use crate::service::usage_service::{UsageService, UsageServiceImpl};
+    use crate::storage::db_provider::{Backend, FileBackend};
+    use serde::de::DeserializeOwned;
+    use serde::Serialize;
+    use uuid::Uuid;
+
+    const BYTES_SMALL: u64 = 1000;
+    const BYTES_MEDIUM: u64 = 1000000;
+    const BYTES_LARGE: u64 = 1000000000;
+
+    struct MockAccountRepo<MyBackend: Backend> {
+        _backend: MyBackend,
+    }
+
+    impl<MyBackend: Backend> AccountRepo<MyBackend> for MockAccountRepo<MyBackend> {
+        fn insert_account(
+            _backend: &MyBackend::Db,
+            _account: &Account,
+        ) -> Result<(), AccountRepoError<MyBackend>> {
+            unimplemented!()
+        }
+
+        fn maybe_get_account(
+            _backend: &MyBackend::Db,
+        ) -> Result<Option<Account>, AccountRepoError<MyBackend>> {
+            unimplemented!()
+        }
+
+        fn get_account(_backend: &MyBackend::Db) -> Result<Account, AccountRepoError<MyBackend>> {
+            Ok(Account {
+                username: "".to_string(),
+                api_url: "".to_string(),
+                private_key: RSAImpl::<ClockImpl>::generate_key().unwrap(),
+            })
+        }
+
+        fn get_api_url(_backend: &MyBackend::Db) -> Result<ApiUrl, AccountRepoError<MyBackend>> {
+            unimplemented!()
+        }
+    }
+
+    trait BytesHelper {
+        fn get_bytes_count() -> u64;
+    }
+
+    struct BytesHelperSmall;
+
+    impl BytesHelper for BytesHelperSmall {
+        fn get_bytes_count() -> u64 {
+            BYTES_SMALL
+        }
+    }
+
+    struct BytesHelperMedium;
+
+    impl BytesHelper for BytesHelperMedium {
+        fn get_bytes_count() -> u64 {
+            BYTES_MEDIUM
+        }
+    }
+
+    struct BytesHelperLarge;
+
+    impl BytesHelper for BytesHelperLarge {
+        fn get_bytes_count() -> u64 {
+            BYTES_LARGE
+        }
+    }
+
+    struct MockClient<MyByteHelper: BytesHelper> {
+        _byte_helper: MyByteHelper,
+    }
+
+    impl<MyByteHelper: BytesHelper> Client for MockClient<MyByteHelper> {
+        fn request<
+            T: Request<Response = impl DeserializeOwned, Error = impl DeserializeOwned> + Serialize,
+        >(
+            _account: &Account,
+            _request: T,
+        ) -> Result<<T as Request>::Response, ApiError<<T as Request>::Error>> {
+            let file_usage = FileUsage {
+                file_id: Uuid::nil(),
+                byte_secs: MyByteHelper::get_bytes_count(),
+                secs: 0,
+            };
+
+            let get_usage_response = GetUsageResponse {
+                usages: vec![file_usage.clone(), file_usage],
+            };
+
+            let serialized = serde_json::to_string(&get_usage_response).unwrap();
+            let deserialized: T::Response = serde_json::from_str(&serialized).unwrap();
+
+            Ok(deserialized)
+        }
+    }
+
+    fn get_usage_of_size<T: BytesHelper>(exact: bool) -> String {
+        UsageServiceImpl::<
+            FileBackend,
+            MockAccountRepo<FileBackend>,
+            MockClient<T>,
+        >::get_usage_human_string(
+            &Config {
+                writeable_path: "".to_string(),
+            },
+            exact,
+        ).unwrap()
+    }
+
+    #[test]
+    fn usage_human_string_sanity_check() {
+        let bytes_small_result = get_usage_of_size::<BytesHelperSmall>(false);
+        let bytes_small_exact_result = get_usage_of_size::<BytesHelperSmall>(true);
+        let bytes_small_total = BYTES_SMALL * 2;
+
+        assert_eq!(bytes_small_result, format!("{}.000 KB", 2));
+        assert_eq!(bytes_small_exact_result, bytes_small_total.to_string());
+
+        let bytes_medium_result = get_usage_of_size::<BytesHelperMedium>(false);
+        let bytes_medium_exact_result = get_usage_of_size::<BytesHelperMedium>(true);
+        let bytes_medium_total = BYTES_MEDIUM * 2;
+
+        assert_eq!(bytes_medium_result, format!("{}.000 MB", 2));
+        assert_eq!(bytes_medium_exact_result, bytes_medium_total.to_string());
+
+        let bytes_large_result = get_usage_of_size::<BytesHelperLarge>(false);
+        let bytes_large_exact_result = get_usage_of_size::<BytesHelperLarge>(true);
+        let bytes_large_total = BYTES_LARGE * 2;
+
+        assert_eq!(bytes_large_result, format!("{}.000 GB", 2));
+        assert_eq!(bytes_large_exact_result, bytes_large_total.to_string());
     }
 }
