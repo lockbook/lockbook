c5767a577c9dc07abeb11c7c7134223171ffa99c
Smail Barkouch <SmailBarkouch@users.noreply.github.com>
2021-02-06 16:28:27 -0500

    android: fixed mass erasing error in HandwritingEditorView (#578)
    * working correct erasing
    
    * removed random gradle and unused logging
    
    * moved into const

--- clients/android/app/src/main/java/app/lockbook/ui/HandwritingEditorView.kt
+++ clients/android/app/src/main/java/app/lockbook/ui/HandwritingEditorView.kt
@@ -112,6 +112,7 @@ class HandwritingEditorView(context: Context, attributeSet: AttributeSet?) :
     init {
         setUpPaint()
     }
+
     private fun setUpPaint() {
         strokePaint.isAntiAlias = true
         strokePaint.style = Paint.Style.STROKE
@@ -267,13 +268,13 @@ class HandwritingEditorView(context: Context, attributeSet: AttributeSet?) :
         val pressure = compressPressure(event.pressure)
 
         if (isErasing || event.buttonState == MotionEvent.BUTTON_STYLUS_PRIMARY) {
-            eraseAtPoint(modelPoint)
-        } else {
-            if (!erasePoints.first.x.isNaN() || !erasePoints.second.x.isNaN()) {
+            if (event.action == SPEN_ACTION_DOWN && (!erasePoints.first.x.isNaN() || !erasePoints.second.x.isNaN())) {
                 erasePoints.first.set(PointF(Float.NaN, Float.NaN))
                 erasePoints.second.set(PointF(Float.NaN, Float.NaN))
             }
 
+            eraseAtPoint(modelPoint)
+        } else {
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> moveTo(modelPoint)
                 MotionEvent.ACTION_MOVE -> lineTo(modelPoint, pressure)
--- clients/android/app/src/main/java/app/lockbook/util/Constants.kt
+++ clients/android/app/src/main/java/app/lockbook/util/Constants.kt
@@ -53,5 +53,8 @@ object WorkManagerTags {
 
 const val CANVAS_WIDTH = 2125
 const val CANVAS_HEIGHT = 2750
+const val SPEN_ACTION_DOWN = 211
+
 const val TEXT_EDITOR_BACKGROUND_SAVE_PERIOD: Long = 5000
+
 const val LOG_FILE_NAME = "lockbook.log"
