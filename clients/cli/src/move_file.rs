8c3f75bc22ec4038ce47fe09ae0bd456aa71a893
steverusso <steverusso@protonmail.com>
2021-02-26 10:36:59 -0500

    refactor error matching

--- clients/cli/src/move_file.rs
+++ clients/cli/src/move_file.rs
@@ -23,26 +23,22 @@ pub fn move_file(path1: &str, path2: &str) -> CliResult<()> {
 
     lockbook_core::move_file(&cfg, file_metadata.id, target_file_metadata.id).map_err(|err| {
         match err {
-            CoreError::UiError(MoveFileError::NoAccount) => err!(NoAccount),
-            CoreError::UiError(MoveFileError::CannotMoveRoot) => err!(NoRootOps("move")),
-            CoreError::UiError(MoveFileError::FileDoesNotExist) => {
-                err!(FileNotFound(path1.to_string()))
-            }
-            CoreError::UiError(MoveFileError::TargetParentDoesNotExist) => {
-                err!(FileNotFound(path2.to_string()))
-            }
-            CoreError::UiError(MoveFileError::FolderMovedIntoItself) => {
-                err!(CannotMoveFolderIntoItself)
-            }
-            CoreError::UiError(MoveFileError::TargetParentHasChildNamedThat) => {
-                err!(FileNameNotAvailable(target_file_metadata.name))
-            }
-            CoreError::UiError(MoveFileError::DocumentTreatedAsFolder) => err_extra!(
-                DocTreatedAsFolder(path2.to_string()),
-                "{} cannot be moved to {}",
-                file_metadata.name,
-                target_file_metadata.name
-            ),
+            CoreError::UiError(err) => match err {
+                MoveFileError::NoAccount => err!(NoAccount),
+                MoveFileError::CannotMoveRoot => err!(NoRootOps("move")),
+                MoveFileError::FileDoesNotExist => err!(FileNotFound(path1.to_string())),
+                MoveFileError::TargetParentDoesNotExist => err!(FileNotFound(path2.to_string())),
+                MoveFileError::FolderMovedIntoItself => err!(CannotMoveFolderIntoItself),
+                MoveFileError::TargetParentHasChildNamedThat => {
+                    err!(FileNameNotAvailable(target_file_metadata.name))
+                }
+                MoveFileError::DocumentTreatedAsFolder => err_extra!(
+                    DocTreatedAsFolder(path2.to_string()),
+                    "{} cannot be moved to {}",
+                    file_metadata.name,
+                    target_file_metadata.name
+                ),
+            },
             CoreError::Unexpected(msg) => err_unexpected!("{}", msg),
         }
     })
