b3232283fee54a8ebc68cbc38560bd09bd4ed70e
travis <t.vanderstad@gmail.com>
2021-02-28 14:20:42 -0500

    feature flag for tls

--- dev_utils/create_core_for_windows_aarch64.bat
+++ dev_utils/create_core_for_windows_aarch64.bat
@@ -1,3 +1,3 @@
 cd ..\core
-cargo build --target=aarch64-pc-windows-msvc --release
+cargo build --target=aarch64-pc-windows-msvc --release --no-default-features --features "native-tls"
 copy target\aarch64-pc-windows-msvc\release\lockbook_core.dll ..\clients\windows\core\
