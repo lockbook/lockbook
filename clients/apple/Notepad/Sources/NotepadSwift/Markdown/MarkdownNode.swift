41d0d13b7ebd1f7df7b0fea61c2c97aa9ef8e946
Raayan Pillai <raayanp01@gmail.com>
2021-03-08 23:15:02 -0500

    apple: markdown bugs

--- clients/apple/Notepad/Sources/NotepadSwift/Markdown/MarkdownNode.swift
+++ clients/apple/Notepad/Sources/NotepadSwift/Markdown/MarkdownNode.swift
@@ -1,23 +1,11 @@
 import Foundation
+import Down
 
 public struct MarkdownNode: Equatable {
     public let range: NSRange
     public let rawType: Int
     public let type: MarkdownType
     public let headingLevel: Int
-
-    public init?(_ s: (Int, Int), _ e: (Int, Int), _ rawType: Int, lines: [Int], headingLevel: Int = 0) {
-        self.rawType = rawType
-        if let type = MarkdownType(rawValue: self.rawType) {
-            let s = lines[s.0] + s.1
-            let e = lines[e.0] + e.1
-            self.range = NSMakeRange(s, e - s + 1)
-            self.headingLevel = headingLevel
-            self.type = type
-        } else {
-            return nil
-        }
-    }
 
     public init(range: NSRange, type: MarkdownType, headingLevel: Int = 0) {
         self.range = range
---
+++ clients/apple/Notepad/Tests/LinuxMain.swift
@@ -0,0 +1,8 @@
+import XCTest
+
+import SwiftLockbookCoreTests
+
+var tests = [XCTestCaseEntry]()
+tests += SwiftLockbookCoreTests.__allTests()
+
+XCTMain(tests)
