2f18739c32bc2807882be7288e5a524979808312
Smail Barkouch <SmailBarkouch@users.noreply.github.com>
2021-03-18 17:58:27 -0400

    core: drawing support (#603)
    * some basic structs and service
    
    * exposed api and seemingly working
    
    * created new function in drawing service to get drawing data
    
    * deleted comment
    
    * new endpoint in cli, runs does not work
    
    * image is successfully created though not correct
    
    * successfully exports to png
    
    * supports multiple image formats and has better error handeling
    
    * some opimizations with strings and error handeling
    
    * working color
    
    * able to optionally use a directory to export the drawing to
    
    * able to either get bytes or directly output it into a file
    
    * better handled errors and renamed drawing field
    
    * fulling working endpoints in core
    
    * fixed lint problems
    
    * added new dependencies for swift tests so it builds correctly
    
    * got rid of raqote text feature
    
    * core successfully switched to exporting bytes for drawings
    
    * working dynamic size
    
    * got rid of tests code, added errors and matched succesfully on them
    
    * optimizations and some fixes
    
    * got rid of hdr format
    
    * successfully switched core to new format
    
    * default theme works
    
    * got rid of bug where drawing misses the last few points
    
    * drawing accounts for boldness
    
    * should be working now
    
    * updated message
    
    * bumped cli version and fixed formatting
    
    * cleaned up get_drawing_bounds
    
    * added tests
    
    Co-authored-by: Parth <parth@mehrotra.me>

--- core/src/service/mod.rs
+++ core/src/service/mod.rs
@@ -3,6 +3,7 @@ pub mod clock_service;
 pub mod code_version_service;
 pub mod crypto_service;
 pub mod db_state_service;
+pub mod drawing_service;
 pub mod file_compression_service;
 pub mod file_encryption_service;
 pub mod file_service;
