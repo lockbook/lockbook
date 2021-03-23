9721bd53f9f06cbc44f82e229ec200b89c697992
Parth <parth@mehrotra.me>
2021-03-23 15:23:54 -0400

    copy refactors

--- clients/cli/src/copy.rs
+++ clients/cli/src/copy.rs
@@ -95,11 +95,11 @@ fn copy_file(
     let content = fs::read_to_string(&filesystem_path)
         .map_err(|err| err!(OsCouldNotReadFile(path_string!(filesystem_path), err)))?;
 
-    let absolute_path = fs::canonicalize(&filesystem_path)
+    let fs_absolute_path = fs::canonicalize(&filesystem_path)
         .map_err(|err| err!(OsCouldNotGetAbsPath(path_string!(filesystem_path), err)))?;
 
-    let import_dest_with_filename = if lockbook_path.ends_with('/') {
-        match absolute_path.file_name() {
+    let lb_path_with_filename = if lockbook_path.ends_with('/') {
+        match fs_absolute_path.file_name() {
             Some(name) => match name.to_os_string().into_string() {
                 Ok(string) => format!("{}{}", &lockbook_path, string),
                 Err(err) => err_unexpected!("converting an OsString to String: {:?}", err).exit(),
@@ -110,37 +110,37 @@ fn copy_file(
         lockbook_path.to_string()
     };
 
-    let file_metadata = match create_file_at_path(config, &import_dest_with_filename) {
+    let file_metadata = match create_file_at_path(config, &lb_path_with_filename) {
         Ok(file_metadata) => file_metadata,
         Err(err) => match err {
             CoreError::UiError(err) => match err {
                 CreateFileAtPathError::FileAlreadyExists => {
                     if edit {
-                        get_file_by_path(config, &import_dest_with_filename).unwrap_or_else(
-                            |get_err| match get_err {
+                        get_file_by_path(config, &lb_path_with_filename).unwrap_or_else(|get_err| {
+                            match get_err {
                                 CoreError::UiError(GetFileByPathError::NoFileAtThatPath)
                                 | CoreError::Unexpected(_) => {
                                     err_unexpected!("{:?}", get_err).exit()
                                 }
-                            },
-                        )
+                            }
+                        })
                     } else {
-                        return Err(err_extra!(FileAlreadyExists(import_dest_with_filename), "The input destination is not available within lockbook. Use --edit to overwrite the contents of this file!"));
+                        return Err(err_extra!(FileAlreadyExists(lb_path_with_filename), "The input destination is not available within lockbook. Use --edit to overwrite the contents of this file!"));
                     }
                 }
                 CreateFileAtPathError::NoAccount => err!(NoAccount).exit(),
                 CreateFileAtPathError::NoRoot => err!(NoRoot).exit(),
                 CreateFileAtPathError::DocumentTreatedAsFolder => {
-                    return Err(err!(DocTreatedAsFolder(import_dest_with_filename)));
+                    return Err(err!(DocTreatedAsFolder(lb_path_with_filename)));
                 }
                 CreateFileAtPathError::PathContainsEmptyFile => {
                     return Err(err_extra!(
-                        PathContainsEmptyFile(import_dest_with_filename),
+                        PathContainsEmptyFile(lb_path_with_filename),
                         "The input destination path contains an empty file!"
                     ));
                 }
                 CreateFileAtPathError::PathDoesntStartWithRoot => {
-                    err!(PathNoRoot(import_dest_with_filename.to_string())).exit()
+                    err!(PathNoRoot(lb_path_with_filename.to_string())).exit()
                 }
             },
             CoreError::Unexpected(msg) => err_unexpected!("{}", msg).exit(),
@@ -148,7 +148,7 @@ fn copy_file(
     };
 
     match write_document(config, file_metadata.id, content.as_bytes()) {
-        Ok(_) => Ok(format!("imported to {}", import_dest_with_filename)),
+        Ok(_) => Ok(format!("imported to {}", lb_path_with_filename)),
         Err(err) => Err(err_unexpected!("{:#?}", err)),
     }
 }
