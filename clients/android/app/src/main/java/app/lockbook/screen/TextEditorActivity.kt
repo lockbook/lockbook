142d27ab68fc4253deb6238adafde87781f32b6e
Smail Barkouch <SmailBarkouch@users.noreply.github.com>
2021-03-25 13:11:58 -0400

    android: refactor code (#653)
    * moved out errors into a single object
    
    * moved out all snackbar logic and biometric verification into their own objects
    
    * simplified syncing info and sorted out functions in listfilesviewmodel
    
    * optimized drawing, moved sync functions into its own model, and more
    
    * fixed import
    
    * fixed parameter problem

--- clients/android/app/src/main/java/app/lockbook/screen/TextEditorActivity.kt
+++ clients/android/app/src/main/java/app/lockbook/screen/TextEditorActivity.kt
@@ -7,18 +7,17 @@ import android.text.style.ForegroundColorSpan
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
-import app.lockbook.App.Companion.UNEXPECTED_CLIENT_ERROR
-import app.lockbook.App.Companion.UNEXPECTED_ERROR
 import app.lockbook.R
+import app.lockbook.model.AlertModel
+import app.lockbook.model.OnFinishAlert
 import app.lockbook.model.TextEditorViewModel
 import app.lockbook.modelfactory.TextEditorViewModelFactory
+import app.lockbook.util.BASIC_ERROR
 import app.lockbook.util.exhaustive
-import com.google.android.material.snackbar.Snackbar
 import io.noties.markwon.Markwon
 import io.noties.markwon.editor.MarkwonEditor
 import io.noties.markwon.editor.MarkwonEditorTextWatcher
@@ -45,7 +44,7 @@ class TextEditorActivity : AppCompatActivity() {
         val id = intent.getStringExtra("id")
 
         if (id == null) {
-            errorHasOccurred("Unable to retrieve id.")
+            AlertModel.unexpectedCoreErrorHasOccurred(this, "Unable to retrieve id.", OnFinishAlert.DoSomethingOnFinishAlert(::finish))
             return
         }
 
@@ -75,14 +74,14 @@ class TextEditorActivity : AppCompatActivity() {
         textEditorViewModel.errorHasOccurred.observe(
             this,
             { errorText ->
-                errorHasOccurred(errorText)
+                AlertModel.errorHasOccurred(text_editor_layout, errorText, OnFinishAlert.DoNothingOnFinishAlert)
             }
         )
 
         textEditorViewModel.unexpectedErrorHasOccurred.observe(
             this,
             { errorText ->
-                unexpectedErrorHasOccurred(errorText)
+                AlertModel.unexpectedCoreErrorHasOccurred(this, errorText, OnFinishAlert.DoSomethingOnFinishAlert(::finish))
             }
         )
 
@@ -103,29 +102,10 @@ class TextEditorActivity : AppCompatActivity() {
         )
     }
 
-    private fun errorHasOccurred(error: String) {
-        Snackbar.make(text_editor_layout, error, Snackbar.LENGTH_SHORT).addCallback(object : Snackbar.Callback() {
-            override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
-                super.onDismissed(transientBottomBar, event)
-                finish()
-            }
-        }).show()
-    }
-
-    private fun unexpectedErrorHasOccurred(error: String) {
-        AlertDialog.Builder(this, R.style.Main_Widget_Dialog)
-            .setTitle(UNEXPECTED_ERROR)
-            .setMessage(error)
-            .setOnCancelListener {
-                finish()
-            }
-            .show()
-    }
-
     private fun setUpView(id: String) {
         val name = intent.getStringExtra("name")
         if (name == null) {
-            errorHasOccurred("Unable to retrieve file name.")
+            AlertModel.errorHasOccurred(text_editor_layout, "Unable to retrieve file name.", OnFinishAlert.DoSomethingOnFinishAlert(::finish))
             return
         }
 
@@ -272,12 +252,11 @@ class TextEditorActivity : AppCompatActivity() {
             R.id.menu_text_editor_undo -> handleTextUndo()
             else -> {
                 Timber.e("Menu item not matched: ${item.itemId}")
-                Snackbar.make(
+                AlertModel.errorHasOccurred(
                     text_editor_layout,
-                    UNEXPECTED_CLIENT_ERROR,
-                    Snackbar.LENGTH_SHORT
+                    BASIC_ERROR,
+                    OnFinishAlert.DoNothingOnFinishAlert
                 )
-                    .show()
             }
         }.exhaustive
 
