93a15bb8f9b0430271522d44a9a7e2cdb766b078
raayan <raayan@raayanpillai.com>
2021-03-28 17:29:26 -0400

    Adding drawing errors to macro and Swift api

---
+++ clients/apple/macOS/ImageLoader.swift
@@ -0,0 +1,20 @@
+//
+//  ImageLoader.swift
+//  Lockbook (macOS)
+//
+//  Created by Raayan Pillai on 3/28/21.
+//
+
+import SwiftUI
+
+struct ImageLoader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageLoader_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageLoader()
+    }
+}
