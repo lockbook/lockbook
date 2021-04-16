d0ca3388cadb0ba786047afe356c06f9e0c27f8c
Steve Russo <steverusso@protonmail.com>
2021-04-16 11:15:54 -0400

    linux: tidy code regarding the updating of the sync status (#702)
    * linux: use LbApp::refresh_sync_status method where possible
    * linux: refresh sync status in closures by sending the RefreshSyncStatus message

--- clients/linux/src/app.rs
+++ clients/linux/src/app.rs
@@ -193,10 +193,7 @@ impl LbApp {
             } else if let Err(err) = lb.gui.show_account_screen(&lb.core) {
                 lb.messenger.send_err("showing account screen", err);
             } else {
-                match lb.core.sync_status() {
-                    Ok(s) => lb.gui.account.sync().set_status(&s),
-                    Err(err) => lb.messenger.send_err("getting sync status", err),
-                }
+                lb.messenger.send(Msg::RefreshSyncStatus);
             }
             glib::Continue(true)
         });
@@ -267,10 +264,7 @@ impl LbApp {
                 sync_ui.sync_progress(&msg);
             } else {
                 sync_ui.set_syncing(false);
-                match lb.core.sync_status() {
-                    Ok(s) => sync_ui.set_status(&s),
-                    Err(err) => lb.messenger.send_err("getting sync status", err),
-                }
+                lb.messenger.send(Msg::RefreshSyncStatus);
             }
             glib::Continue(true)
         });
@@ -299,9 +293,7 @@ impl LbApp {
         let file = self.core.create_file_at_path(&path)?;
         self.gui.account.add_file(&self.core, &file)?;
 
-        let status = self.core.sync_status()?;
-        self.gui.account.sync().set_status(&status);
-
+        self.refresh_sync_status()?;
         self.open_file(Some(file.id))
     }
 
@@ -456,14 +448,11 @@ impl LbApp {
                 let id = f.id;
                 let content = acctscr.text_content();
 
-                let ch = make_glib_chan!(self.core as c, self.messenger as m => move |result: LbResult<()>| {
+                let ch = make_glib_chan!(self.messenger as m => move |result: LbResult<()>| {
                     match result {
                         Ok(_) => {
                             acctscr.set_saving(false);
-                            match c.sync_status() {
-                                Ok(s) => acctscr.sync().set_status(&s),
-                                Err(err) => m.send_err("getting sync status", err),
-                            }
+                            m.send(Msg::RefreshSyncStatus);
                         }
                         Err(err) => m.send_err("saving file", err),
                     }
@@ -563,10 +552,7 @@ impl LbApp {
         }
 
         d.close();
-
-        let s = self.core.sync_status()?;
-        self.gui.account.sync().set_status(&s);
-        Ok(())
+        self.refresh_sync_status()
     }
 
     fn rename_file(&self) -> LbResult<()> {
@@ -617,10 +603,7 @@ impl LbApp {
                     d.close();
                     let acctscr = &lb.gui.account;
                     acctscr.sidebar.tree.set_name(&id, &name);
-                    match lb.core.sync_status() {
-                        Ok(s) => acctscr.sync().set_status(&s),
-                        Err(err) => lb.messenger.send_err("getting sync status", err),
-                    }
+                    lb.messenger.send(Msg::RefreshSyncStatus);
                 }
                 Err(err) => match err.kind() {
                     UserErr => {
