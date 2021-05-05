0acd8612d1e996991fd72b827dd225050c3e0ec1
Parth <parth@mehrotra.me>
2021-05-05 17:54:30 -0400

    server: billing part 2 (#719)
    * project initiation doc
    
    * Create db scripts for new tables
    
    * Better local dev experience
    
    * Usage ledger replaced with column on file_metadata
    
    * Apply suggestions from code review
    
    Co-authored-by: Travis Vanderstad <t.vanderstad@gmail.com>
    
    * accept travis suggestion
    
    Co-authored-by: Travis Vanderstad <t.vanderstad@gmail.com>

---
+++ utils/dev/psql_local.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# brew install postgresql
+
+set -a
+. ../../containers/local.env
+
+PGPASSWORD=$INDEX_DB_PASS psql \
+	--host=$INDEX_DB_HOST \
+	--username=$INDEX_DB_USER \
+	--port=$INDEX_DB_PORT \
+	$INDEX_DB_DB
