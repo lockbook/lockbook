2b86f4aa0d78ed32bcad3b8e4400cd824e589ff5
Parth <parth@mehrotra.me>
2021-05-13 16:27:44 -0400

    server: connection pools & compile time sql checks & dependency cleanup (#731)
    * successful build with sqlx pg client
    
    * server comes up, updated tokio deps, removed dep on raayan
    
    * some more compiler errors for travis
    
    * aligned versions, perhaps still a pd issue, actual postgres errors now
    
    * test fixes
    
    * infra work for sqlx
    
    * fix admin client
    
    * make clippy happy round 1
    
    * make clippy happy round 2
    
    * fmt fml
    
    * fix test
    
    * fix tests & fix pd reactor
    
    * fmt
    
    Co-authored-by: Travis Vanderstad <t.vanderstad@gmail.com>

--- server/index_db/create_db.sql
+++ server/index_db/create_db.sql
@@ -1,74 +1,79 @@
 CREATE TABLE IF NOT EXISTS stripe_payees
 (
-    payee_id bigserial NOT NULL PRIMARY KEY
+    id BIGSERIAL NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS apple_payees
 (
-    payee_id bigserial NOT NULL PRIMARY KEY
+    id BIGSERIAL NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS google_payees
 (
-    payee_id bigserial NOT NULL PRIMARY KEY
+    id BIGSERIAL NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS account_tiers
 (
-    account_tier_id bigserial PRIMARY KEY,
-    bytes_cap       bigint NOT NULL,
-    valid_until     timestamptz,
-    payee_id_stripe bigint REFERENCES stripe_payees (payee_id),
-    payee_id_apple  bigint REFERENCES apple_payees (payee_id),
-    payee_id_google bigint REFERENCES google_payees (payee_id),
+    id           BIGSERIAL,
+    bytes_cap    BIGINT NOT NULL,
+    valid_until  TIMESTAMPTZ,
+    payee_stripe BIGINT,
+    payee_apple  BIGINT,
+    payee_google BIGINT,
+    CONSTRAINT pk_account_tiers PRIMARY KEY (id),
+    CONSTRAINT fk_account_tiers_payee_stripe_stripe_payees_id FOREIGN KEY (payee_stripe) REFERENCES stripe_payees (id),
+    CONSTRAINT fk_account_tiers_payee_apple_apple_payees_id FOREIGN KEY (payee_apple) REFERENCES apple_payees (id),
+    CONSTRAINT fk_account_tiers_payee_google_google_payees_id FOREIGN KEY (payee_google) REFERENCES google_payees (id),
     CONSTRAINT at_most_one_payment_method CHECK (
         NOT (
-                (payee_id_stripe IS NOT NULL AND payee_id_apple IS NOT NULL) AND
-                (payee_id_stripe IS NOT NULL AND payee_id_google IS NOT NULL) AND
-                (payee_id_apple IS NOT NULL AND payee_id_google IS NOT NULL) AND
-                (payee_id_stripe IS NOT NULL AND payee_id_apple IS NOT NULL AND payee_id_google IS NOT NULL)
+                (payee_stripe IS NOT NULL AND payee_apple IS NOT NULL) AND
+                (payee_stripe IS NOT NULL AND payee_google IS NOT NULL) AND
+                (payee_apple IS NOT NULL AND payee_google IS NOT NULL) AND
+                (payee_stripe IS NOT NULL AND payee_apple IS NOT NULL AND payee_google IS NOT NULL
             )
         )
+    )
 );
 
 CREATE TABLE IF NOT EXISTS accounts
 (
-    name         text   NOT NULL,
-    public_key   text   NOT NULL,
-    account_tier bigint NOT NULL REFERENCES account_tiers (account_tier_id),
+    name         TEXT   NOT NULL,
+    public_key   TEXT   NOT NULL,
+    account_tier BIGINT NOT NULL,
     CONSTRAINT pk_accounts PRIMARY KEY (name),
+    CONSTRAINT fk_accounts_account_tier_account_tiers_id FOREIGN KEY (account_tier) REFERENCES account_tiers (id),
     CONSTRAINT uk_public_key UNIQUE (public_key)
 );
 
 CREATE TABLE IF NOT EXISTS files
 (
-    id                text    NOT NULL,
-    parent            text    NOT NULL,
-    parent_access_key text    NOT NULL,
-    is_folder         boolean NOT NULL,
-    name              text    NOT NULL,
-    owner             text    NOT NULL,
-    signature         text    NOT NULL,
-    deleted           boolean,
-    metadata_version  bigint  NOT NULL,
-    content_version   bigint  NOT NULL,
-    document_size     bigint,
+    id                TEXT    NOT NULL,
+    parent            TEXT    NOT NULL,
+    parent_access_key TEXT    NOT NULL,
+    is_folder         BOOLEAN NOT NULL,
+    name              TEXT    NOT NULL,
+    owner             TEXT    NOT NULL,
+    signature         TEXT    NOT NULL,
+    deleted           BOOLEAN NOT NULL,
+    metadata_version  BIGINT  NOT NULL,
+    content_version   BIGINT  NOT NULL,
+    document_size     BIGINT,
     CONSTRAINT pk_files PRIMARY KEY (id),
     CONSTRAINT fk_files_parent_files_id FOREIGN KEY (parent) REFERENCES files (id),
     CONSTRAINT fk_files_owner_accounts_name FOREIGN KEY (owner) REFERENCES accounts (name),
-    CONSTRAINT documents_must_have_size
-        CHECK (
-            is_folder OR document_size IS NOT NULL
-            )
+    CONSTRAINT documents_must_have_size CHECK (
+        is_folder OR document_size IS NOT NULL
+    )
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS uk_files_name_parent ON files (parent, name) WHERE (NOT deleted);
 
 CREATE TABLE IF NOT EXISTS user_access_keys
 (
-    file_id       text NOT NULL,
-    sharee_id     text NOT NULL,
-    encrypted_key text NOT NULL,
+    file_id       TEXT NOT NULL,
+    sharee_id     TEXT NOT NULL,
+    encrypted_key TEXT NOT NULL,
     CONSTRAINT pk_user_access_keys PRIMARY KEY (file_id, sharee_id),
     CONSTRAINT fk_user_access_keys_file_id_files_id FOREIGN KEY (file_id) REFERENCES files (id),
     CONSTRAINT fk_user_access_keys_sharee_id_accounts_name FOREIGN KEY (sharee_id) REFERENCES accounts (name)
