69de186c92b18d1b39eb7d0d9b45a1d8b24764e0
Parth <parth@mehrotra.me>
2021-05-13 16:27:44 -0400

    server: connection pools & compile time sql checks & dependency cleanup (#731)
    * successful build with sqlx pg client
    
    * server comes up, updated tokio deps, removed dep on raayan
    
    * some more compiler errors for travis
    
    * aligned versions, perhaps still a pd issue, actual postgres errors now
    
    * test fixes
    
    * infra work for sqlx
    
    * fix admin client
    
    * make clippy happy round 1
    
    * make clippy happy round 2
    
    * fmt fml
    
    * fix test
    
    * fix tests & fix pd reactor
    
    * fmt
    
    Co-authored-by: Travis Vanderstad <t.vanderstad@gmail.com>

--- server/server/src/account_service.rs
+++ server/server/src/account_service.rs
@@ -1,4 +1,3 @@
-use crate::file_index_repo::create_free_account_tier_row;
 use crate::utils::username_is_valid;
 use crate::{file_index_repo, RequestContext};
 
@@ -16,7 +15,8 @@ pub async fn new_account(
     if !username_is_valid(&request.username) {
         return Err(Some(NewAccountError::InvalidUsername));
     }
-    let transaction = match server_state.index_db_client.transaction().await {
+
+    let mut transaction = match server_state.index_db_client.begin().await {
         Ok(t) => t,
         Err(e) => {
             error!("Internal server error! Cannot begin transaction: {:?}", e);
@@ -24,26 +24,11 @@ pub async fn new_account(
         }
     };
 
-    let tier_id = create_free_account_tier_row(&transaction)
-        .await
-        .map_err(|e| {
-            error!(
-                "Internal server error! Could not create tier row for new account {:?}",
-                e
-            );
-            None
-        })?;
-
-    let new_account_result = file_index_repo::new_account(
-        &transaction,
-        &request.username,
-        &serde_json::to_string(&request.public_key)
-            .map_err(|_| Some(NewAccountError::InvalidPublicKey))?,
-        tier_id,
-    )
-    .await;
+    let new_account_result =
+        file_index_repo::new_account(&mut transaction, &request.username, &request.public_key)
+            .await;
     new_account_result.map_err(|e| match e {
-        file_index_repo::AccountError::UsernameTaken => Some(NewAccountError::UsernameTaken),
+        file_index_repo::NewAccountError::UsernameTaken => Some(NewAccountError::UsernameTaken),
         _ => {
             error!(
                 "Internal server error! Cannot create account in Postgres: {:?}",
@@ -54,7 +39,7 @@ pub async fn new_account(
     })?;
 
     let create_folder_result = file_index_repo::create_file(
-        &transaction,
+        &mut transaction,
         request.folder_id,
         request.folder_id,
         FileType::Folder,
@@ -65,7 +50,7 @@ pub async fn new_account(
     )
     .await;
     let new_version = create_folder_result.map_err(|e| match e {
-        file_index_repo::FileError::IdTaken => Some(NewAccountError::FileIdTaken),
+        file_index_repo::CreateFileError::IdTaken => Some(NewAccountError::FileIdTaken),
         _ => {
             error!(
                 "Internal server error! Cannot create account root folder in Postgres: {:?}",
@@ -75,11 +60,10 @@ pub async fn new_account(
         }
     })?;
     let new_user_access_key_result = file_index_repo::create_user_access_key(
-        &transaction,
+        &mut transaction,
         &request.username,
         request.folder_id,
-        &serde_json::to_string(&request.user_access_key)
-            .map_err(|_| Some(NewAccountError::InvalidUserAccessKey))?,
+        &request.user_access_key,
     )
     .await;
     new_user_access_key_result.map_err(|e| {
@@ -106,14 +90,14 @@ pub async fn get_public_key(
 ) -> Result<GetPublicKeyResponse, Option<GetPublicKeyError>> {
     let request = &context.request;
     let server_state = &mut context.server_state;
-    let transaction = match server_state.index_db_client.transaction().await {
+    let mut transaction = match server_state.index_db_client.begin().await {
         Ok(t) => t,
         Err(e) => {
             error!("Internal server error! Cannot begin transaction: {:?}", e);
             return Err(None);
         }
     };
-    let result = file_index_repo::get_public_key(&transaction, &request.username).await;
+    let result = file_index_repo::get_public_key(&mut transaction, &request.username).await;
     let key = result.map_err(|e| match e {
         file_index_repo::PublicKeyError::UserNotFound => Some(GetPublicKeyError::UserNotFound),
         _ => {
@@ -138,7 +122,7 @@ pub async fn get_usage(
     context: &mut RequestContext<'_, GetUsageRequest>,
 ) -> Result<GetUsageResponse, Option<GetUsageError>> {
     let server_state = &mut context.server_state;
-    let transaction = match server_state.index_db_client.transaction().await {
+    let mut transaction = match server_state.index_db_client.begin().await {
         Ok(t) => t,
         Err(e) => {
             error!("Internal server error! Cannot begin transaction: {:#?}", e);
@@ -146,14 +130,14 @@ pub async fn get_usage(
         }
     };
 
-    let usages = file_index_repo::get_file_usages(&transaction, &context.public_key)
+    let usages = file_index_repo::get_file_usages(&mut transaction, &context.public_key)
         .await
         .map_err(|e| {
             error!("Usage calculation error: {:#?}", e);
             None
         })?;
 
-    let cap = file_index_repo::get_account_data_cap(&transaction, &context.public_key)
+    let cap = file_index_repo::get_account_data_cap(&mut transaction, &context.public_key)
         .await
         .map_err(|e| {
             error!("Data cap calculation error: {:#?}", e);
