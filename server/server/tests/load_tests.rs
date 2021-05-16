7556ca18dcf76e6610c21db047ba01c9911a2f81
raayan <raayan@raayanpillai.com>
2021-05-16 12:58:36 -0400

    Testing calculate work performance

--- server/server/tests/load_tests.rs
+++ server/server/tests/load_tests.rs
@@ -4,45 +4,59 @@ mod integration_test;
 #[cfg(test)]
 mod load_tests {
     use crate::integration_test::{api_url, random_uuid, test_config};
-    use std::{thread, time};
-    use atomic_counter::{RelaxedCounter, AtomicCounter};
-    use std::sync::Arc;
+    use atomic_counter::{AtomicCounter, RelaxedCounter};
+    use indicatif::{ProgressBar, ProgressStyle};
     use std::ops::Add;
-    use std::io::Write;
-    use indicatif::{ProgressIterator, ProgressBar, ProgressStyle};
+    use std::sync::Arc;
     use std::time::Duration;
+    use std::{thread, time};
 
     #[test]
     fn create_and_sync() {
-        let cfg = test_config();
-        // let account = lockbook_core::create_account(&cfg, &random_uuid(), &api_url()).unwrap();
-
         let cpu_count = num_cpus::get();
         println!("Threads: {}", cpu_count);
 
         let counter = Arc::new(RelaxedCounter::new(0));
-        let duration = time::Duration::from_secs(5);
-        println!("Spawning {} threads and working for {} seconds", cpu_count, duration.as_secs());
+        let duration = time::Duration::from_secs(60);
+        println!(
+            "Spawning {} threads and working for {} seconds",
+            cpu_count,
+            duration.as_secs()
+        );
 
         let mut children = vec![];
         for _ in 0..cpu_count {
             let counter_clone = counter.clone();
             children.push(thread::spawn(move || {
                 // Setup
+                let cfg = test_config();
+                lockbook_core::create_account(
+                    &cfg,
+                    format!("loadtest{}", &random_uuid()).as_str(),
+                    &api_url(),
+                )
+                .expect("Could not create account!");
+                lockbook_core::sync_all(&cfg, None).expect("Could not sync!");
                 let end_time = time::Instant::now().add(duration);
+                // let root = lockbook_core::get_root(&cfg).expect("Could not get root!");
+                // let root_id = root.id;
+                // let file = lockbook_core::create_file(&cfg, random_uuid().as_str(), root_id, FileType::Document).expect("Could not create file!");
                 // Let the horses run
                 while time::Instant::now() < end_time {
-                    // Is Done then break
+                    // lockbook_core::write_document(&cfg, file.id, random_uuid().as_bytes()).unwrap();
+                    // lockbook_core::sync_all(&cfg, None).unwrap();
+                    lockbook_core::calculate_work(&cfg).unwrap();
                     counter_clone.inc();
                 }
             }));
         }
 
-
         let bar = ProgressBar::new(duration.as_secs());
-        bar.set_style(ProgressStyle::default_bar()
-            .template("{spinner:.green} [{elapsed_precise}] {bar:40.cyan/blue} ({eta}) {msg}")
-            .progress_chars("#>-"));
+        bar.set_style(
+            ProgressStyle::default_bar()
+                .template("{spinner:.green} [{elapsed_precise}] {bar:40.cyan/blue} ({eta}) {msg}")
+                .progress_chars("#>-"),
+        );
         let start_time = time::Instant::now();
         let end_time = start_time.add(duration);
         while time::Instant::now() < end_time {
@@ -50,7 +64,7 @@ mod load_tests {
             let elapsed = (time::Instant::now() - start_time).as_secs();
             if elapsed > 0 {
                 bar.set_position(elapsed);
-                bar.set_message(format!("{} ops/s", (counter.get() as u64)/elapsed));
+                bar.set_message(format!("{} ops/s", (counter.get() as u64) / elapsed));
             } else {
                 bar.set_message(format!("{} ops/s", 0));
             }
@@ -59,17 +73,12 @@ mod load_tests {
         for child in children {
             let _ = child.join();
         }
-        bar.finish_with_message(format!("{} ops/s", (counter.get() as u64)/duration.as_secs()));
+        bar.finish_with_message(format!(
+            "{} ops/s",
+            (counter.get() as u64) / duration.as_secs()
+        ));
         bar.finish();
 
         println!("Completed Operations: {}", counter.get());
-
-        // lockbook_core::create_file_at_path(
-        //     &cfg1,
-        //     &format!("{}/a/b/c/d/test.txt", account1.username),
-        // )
-        // .unwrap();
-        // lockbook_core::sync_all(&cfg1, None).unwrap();
-
     }
 }
