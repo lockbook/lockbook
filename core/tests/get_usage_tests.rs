82f5398c6300a97506e5888afd75557c4408d05b
Smail Barkouch <SmailBarkouch@users.noreply.github.com>
2021-05-22 17:50:46 -0400

    core: remove sled (#742)
    * removed sled
    
    * removed Backend trait
    
    * resolved test errors
    
    * renamed backend variables to config, fixed server import, removed unused import

--- core/tests/get_usage_tests.rs
+++ core/tests/get_usage_tests.rs
@@ -4,10 +4,9 @@ mod integration_test;
 mod get_usage_tests {
     use crate::integration_test::{generate_account, random_filename, test_config};
     use lockbook_core::repo::document_repo::DocumentRepo;
-    use lockbook_core::storage::db_provider::Backend;
     use lockbook_core::{
         create_account, create_file, delete_file, get_root, get_usage, init_logger, sync_all,
-        write_document, DefaultBackend, DefaultDocumentRepo,
+        write_document, DefaultDocumentRepo,
     };
     use lockbook_models::file_metadata::FileType;
     use lockbook_models::file_metadata::FileType::Folder;
@@ -45,10 +44,7 @@ mod get_usage_tests {
 
         sync_all!(config).unwrap();
 
-        let local_encrypted = {
-            let backend = DefaultBackend::connect_to_db(config).unwrap();
-            DefaultDocumentRepo::get(&backend, file.id).unwrap().value
-        };
+        let local_encrypted = DefaultDocumentRepo::get(&config, file.id).unwrap().value;
 
         assert_eq!(get_usage(config).unwrap()[0].file_id, file.id);
         assert_eq!(get_usage(config).unwrap().len(), 1);
@@ -107,10 +103,7 @@ mod get_usage_tests {
         delete_file(config, folder.id).unwrap();
         sync_all!(config).unwrap();
 
-        let local_encrypted = {
-            let backend = DefaultBackend::connect_to_db(config).unwrap();
-            DefaultDocumentRepo::get(&backend, file.id).unwrap().value
-        };
+        let local_encrypted = DefaultDocumentRepo::get(&config, file.id).unwrap().value;
 
         let usages = get_usage(config).unwrap();
         let mut total_usage = 0;
