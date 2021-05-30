18b6e229cdc93fb693a067507f6d03ca3beff799
Parth <parth@mehrotra.me>
2021-05-30 17:19:52 -0400

    core: reduce core to it's spiritual essence (#778)
    * Author a single Cargo.toml at the project root
    
    + Will cause tooling to work better (cross project refactors)
    + Allows us to `cargo check --all-targets` at the project root
    + Fix broken benchmarks
    
    * remove .lock files from dockerfiles
    
    * test_utils package to centralize all test related helper code
    
    * got rid of dummy connect_to_db
    
    * ungenerified code version
    
    * ungenerified account repo
    
    * ungenerified db version repo
    
    * ungenerified db state service
    
    * ungenerified file_metadata_repo
    
    * fixed ci
    
    * ungenerify document repo
    
    * ungenerified local_changes_repo
    
    * ungenerified file_encryption_service
    
    * ungenerified account_service
    
    * ungenerified file_compression_service
    
    * ungenerified file_service
    
    * ungenerified drawing_service
    
    * ungenerified sync_service

--- core/src/service/file_compression_service.rs
+++ core/src/service/file_compression_service.rs
@@ -3,38 +3,27 @@ use flate2::write::ZlibEncoder;
 use flate2::Compression;
 use std::io::{Read, Write};
 
-pub trait FileCompressionService {
-    fn compress(content: &[u8]) -> Result<Vec<u8>, std::io::Error>;
-    fn decompress(content: &[u8]) -> Result<Vec<u8>, std::io::Error>;
+pub fn compress(content: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(content)?;
+    encoder.finish()
 }
 
-pub struct FileCompressionServiceImpl;
-
-impl FileCompressionService for FileCompressionServiceImpl {
-    fn compress(content: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-        let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
-        encoder.write_all(content)?;
-        encoder.finish()
-    }
-
-    fn decompress(content: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-        let mut decoder = ZlibDecoder::new(content);
-        let mut result = Vec::<u8>::new();
-        decoder.read_to_end(&mut result)?;
-        Ok(result)
-    }
+pub fn decompress(content: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+    let mut decoder = ZlibDecoder::new(content);
+    let mut result = Vec::<u8>::new();
+    decoder.read_to_end(&mut result)?;
+    Ok(result)
 }
 
 #[cfg(test)]
 mod unit_tests {
-    use crate::service::file_compression_service::{
-        FileCompressionService, FileCompressionServiceImpl,
-    };
+    use crate::service::file_compression_service;
 
     #[test]
     fn compress_decompress() {
-        FileCompressionServiceImpl::decompress(
-            &FileCompressionServiceImpl::compress(b"hello").unwrap(),
+        file_compression_service::decompress(
+            &file_compression_service::compress(b"hello").unwrap(),
         )
         .unwrap();
     }
