09c1eb6c2d98e5cb008c064dee7cf869331d04f5
Parth <parth@mehrotra.me>
2021-06-17 19:19:27 -0400

    bump versions

--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -305,10 +305,11 @@ fn deserialize_request<TRequest: Request + DeserializeOwned>(
 fn verify_client_version<TRequest: Request>(request: &RequestWrapper<TRequest>) -> Result<(), ()> {
     match &request.client_version as &str {
         "0.0.0" => Err(()),
-        "0.1.0" => Ok(()),
-        "0.1.1" => Ok(()),
-        "0.1.2" => Ok(()),
-        "0.1.3" => Ok(()),
+        "0.1.0" => Err(()),
+        "0.1.1" => Err(()),
+        "0.1.2" => Err(()),
+        "0.1.3" => Err(()),
+        "0.1.4" => Ok(()),
         _ => Err(()),
     }
 }
