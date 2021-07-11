5510b0ce861025976609623c0bf4581edaf469b2
Parth <parth@mehrotra.me>
2021-07-11 11:14:14 -0400

    apple: refinements & bugfixes (#804)
    * remove random flashing
    
    * get rid of horrible bug in ScrollView -> VStack
    
    * do the same for drawings
    
    * fix purge
    
    * sync button was behaving very strangely, it was becoming small, this fixes
    
    * fix bottom bar and created new, new file experience for iOS
    
    * handle errors explicitly && close #802
    
    * restore titles for non-drawings
    
    * dotted grid for drawings

--- clients/apple/iOS/Drawing/DrawingView.swift
+++ clients/apple/iOS/Drawing/DrawingView.swift
@@ -5,14 +5,11 @@ import Combine
 
 struct DrawingView: UIViewRepresentable {
     @Environment(\.colorScheme) var colorScheme
-    let frame: CGRect
     @State var drawing: PKDrawing = PKDrawing()
     @State var zoom: CGFloat = 1
     @ObservedObject var toolPicker: ToolbarModel
     let pencilInteraction = UIPencilInteraction()
     let view = PKCanvasView()
-    let gridView = GridUIView()
-    let backgroundView = UIView()
 
     let onChange: (PKDrawing) -> Void
 
@@ -32,17 +29,13 @@ struct DrawingView: UIViewRepresentable {
         pencilInteraction.delegate = toolPicker
         view.addInteraction(pencilInteraction)
 
-        backgroundView.backgroundColor = colorScheme == .light ? .white : .black
-        backgroundView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: self.frame.size)
-        view.addSubview(backgroundView)
-        view.sendSubviewToBack(backgroundView)
-
-        gridView.backgroundColor = .clear
-        gridView.frame = self.frame
-        view.addSubview(gridView)
-        view.sendSubviewToBack(gridView)
-
-
+        let imageView = UIImageView(image: UIImage(named: "grid")?.resizableImage(withCapInsets: UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0), resizingMode: .tile))
+        imageView.alpha = 0.3
+        imageView.frame = CGRect(x: 0, y: 0, width: view.contentSize.width, height: view.contentSize.height)
+        let contentView = view.subviews[0]
+        contentView.addSubview(imageView)
+        contentView.sendSubviewToBack(imageView)
+        
         return view
     }
 
@@ -55,13 +48,11 @@ struct DrawingView: UIViewRepresentable {
         @Binding var drawing: PKDrawing
         @Binding var scaleFactor: CGFloat
         let onChange: (PKDrawing) -> ()
-        let didZoom: () -> ()
 
-        init(drawing: Binding<PKDrawing>, scaleFactor: Binding<CGFloat>, onChange: @escaping (PKDrawing) -> Void, didZoom: @escaping () -> Void) {
+        init(drawing: Binding<PKDrawing>, scaleFactor: Binding<CGFloat>, onChange: @escaping (PKDrawing) -> Void) {
             _drawing = drawing
             _scaleFactor = scaleFactor
             self.onChange = onChange
-            self.didZoom = didZoom
         }
 
         func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
@@ -75,16 +66,14 @@ struct DrawingView: UIViewRepresentable {
 
         func scrollViewDidZoom(_ scrollView: UIScrollView) {
             scaleFactor = scrollView.zoomScale
-            didZoom()
+            let offsetX = max((scrollView.bounds.width - scrollView.contentSize.width) * 0.5, 0)
+            let offsetY = max((scrollView.bounds.height - scrollView.contentSize.height) * 0.5, 0)
+            scrollView.contentInset = UIEdgeInsets(top: offsetY, left: offsetX, bottom: 0, right: 0)
         }
     }
 
     func makeCoordinator() -> Coordinator {
-        Coordinator(drawing: $drawing, scaleFactor: $zoom, onChange: onChange, didZoom: {
-            backgroundView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: view.contentSize)
-            let gsz = view.visibleSize.multiply(factor: 2)
-            gridView.frame = CGRect(origin: CGPoint(x: -gsz.width/3, y: -gsz.height/3), size: gsz)
-        })
+        Coordinator(drawing: $drawing, scaleFactor: $zoom, onChange: onChange)
     }
 
 }
