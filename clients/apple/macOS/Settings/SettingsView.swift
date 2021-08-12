c32274226db3a4b3d03e7c72ba7503c305e76633
Parth <parth@mehrotra.me>
2021-08-11 20:02:43 -0400

    apple: settings overhaul (#845)
    * fixes #810
    
    * macOS settings structure
    
    * added uncomressed usage
    
    * abstracting away things
    
    * did the same for iOS

---
+++ clients/apple/macOS/Settings/SettingsView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+import SwiftLockbookCore
+
+struct SettingsView: View {
+    @ObservedObject var core: GlobalState
+    
+    var body: some View {
+        switch core.account {
+        case .none:
+            VStack(spacing: 20) {
+                Text("You need an account for settings!")
+                Text("Default API: \(ConfigHelper.safeGet(.apiLocation) ?? "None!")")
+            }.padding(100)
+        case .some(let account):
+            TabView {
+                AccountSettingsView(core: core, settingsState: SettingsState(core:core), account: account)
+                    .tabItem {
+                        Label("Account", systemImage: "person")
+                    }
+                UsageSettingsView(core: core, settingsState: SettingsState(core:core))
+                    .tabItem {
+                        Label("Usage", systemImage: "externaldrive")
+                    }
+            }
+            .padding(20)
+            .frame(width: 600)
+            
+        }
+    }
+}
