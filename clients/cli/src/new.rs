d21be40ec6fe0e5fa1136014cd70f1e5cd073057
Smail Barkouch <SmailBarkouch@users.noreply.github.com>
2021-08-17 22:57:01 -0400

    cli: tmp is cleaned up even if an error occurs  (#851)
    * fixed tmp cleanup
    
    * replaced prints with just 1
    
    * made changes based on review

--- clients/cli/src/new.rs
+++ clients/cli/src/new.rs
@@ -53,7 +53,10 @@ pub fn new(file_name: &str) -> CliResult<()> {
     }
 
     if edit_was_successful {
-        save_temp_file_contents(file_metadata.id, &file_location, false)
+        match save_temp_file_contents(file_metadata.id, &file_location) {
+            Ok(_) => println!("Document encrypted and saved. Cleaning up temporary file."),
+            Err(err) => err.print(),
+        }
     } else {
         eprintln!("Your editor indicated a problem, aborting and cleaning up");
     }
