01d6f945f366fea27d92f33b7aa3efd4bb0aea7b
Travis Vanderstad <t.vanderstad@gmail.com>
2021-08-22 12:39:40 -0400

    local storage unit tests

--- core/src/repo/local_storage.rs
+++ core/src/repo/local_storage.rs
@@ -123,3 +123,68 @@ where
     let k = String::from_utf8_lossy(key.as_ref()).to_string();
     format!("{}/{}", namespace_path(db, namespace), k)
 }
+
+#[cfg(test)]
+mod unit_tests {
+    use crate::model::state::temp_config;
+    use crate::repo::local_storage;
+
+    #[test]
+    fn read_write() {
+        let db = &temp_config();
+
+        local_storage::write(db, "namespace", "key", "value".as_bytes()).unwrap();
+        let result: Vec<u8> = local_storage::read(db, "namespace", "key").unwrap().unwrap();
+
+        assert_eq!(String::from_utf8_lossy(&result), "value");
+    }
+
+    #[test]
+    fn delete() {
+        let db = &temp_config();
+
+        local_storage::write(db, "namespace", "key-1", "value-1".as_bytes()).unwrap();
+        local_storage::write(db, "namespace", "key-2", "value-2".as_bytes()).unwrap();
+        local_storage::delete(db, "namespace", "key-2").unwrap();
+        let result1: Vec<u8> = local_storage::read(db, "namespace", "key-1").unwrap().unwrap();
+        let result2: Option<Vec<u8>> = local_storage::read(db, "namespace", "key-2").unwrap();
+
+        assert_eq!(String::from_utf8_lossy(&result1), "value-1");
+        assert_eq!(result2, None);
+    }
+
+    #[test]
+    fn delete_all() {
+        let db = &temp_config();
+
+        local_storage::write(db, "namespace", "key-1", "value-1".as_bytes()).unwrap();
+        local_storage::write(db, "namespace", "key-2", "value-2".as_bytes()).unwrap();
+        local_storage::delete_all(db, "namespace").unwrap();
+        let result1: Option<Vec<u8>> = local_storage::read(db, "namespace", "key-1").unwrap();
+        let result2: Option<Vec<u8>> = local_storage::read(db, "namespace", "key-2").unwrap();
+
+        assert_eq!(result1, None);
+        assert_eq!(result2, None);
+    }
+
+    #[test]
+    fn dump() {
+        let db = &temp_config();
+
+        local_storage::write(db, "namespace", "key-1", "value-1".as_bytes()).unwrap();
+        local_storage::write(db, "namespace", "key-4", "value-4".as_bytes()).unwrap();
+        local_storage::write(db, "namespace", "key-3", "value-3".as_bytes()).unwrap();
+        local_storage::write(db, "namespace", "key-2", "value-2".as_bytes()).unwrap();
+        local_storage::write(db, "namespace", "key-5", "value-5".as_bytes()).unwrap();
+
+        let result: Vec<Vec<u8>> = local_storage::dump(db, "namespace").unwrap();
+
+        assert_eq!(result, vec![
+            "value-1".as_bytes(),
+            "value-2".as_bytes(),
+            "value-3".as_bytes(),
+            "value-4".as_bytes(),
+            "value-5".as_bytes(),
+        ]);
+    }
+}
