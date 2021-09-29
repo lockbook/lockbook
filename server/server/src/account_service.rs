c27b864c631844ebd27f63d0fd7c16c8426af8b7
Travis Vanderstad <t.vanderstad@gmail.com>
2021-09-29 08:32:01 -0400

    fix change document content tests

--- server/server/src/account_service.rs
+++ server/server/src/account_service.rs
@@ -68,7 +68,10 @@ pub async fn new_account(
         }),
         Err(sqlx::Error::Database(db_err)) => match db_err.constraint() {
             Some("uk_name") => Err(Ok(NewAccountError::UsernameTaken)),
-            _ => Err(Err(format!("Cannot commit transaction due to constraint violation: {:?}", db_err))),
+            _ => Err(Err(format!(
+                "Cannot commit transaction due to constraint violation: {:?}",
+                db_err
+            ))),
         },
         Err(e) => Err(Err(format!("Cannot commit transaction: {:?}", e))),
     }
