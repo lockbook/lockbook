d4c2362f580e9f23948288df914970e8f6affd94
Parth <parth@mehrotra.me>
2021-10-19 17:03:23 -0400

    apple: onboarding refinements (#896)
    * first of before you start page
    
    * progress backup
    
    * more progress
    
    * Update clients/apple/Shared/Config/Development.xcconfig
    
    * onboarding refined

--- clients/apple/macOS/OutlineView/OutlineSection.swift
+++ clients/apple/macOS/OutlineView/OutlineSection.swift
@@ -4,6 +4,7 @@ import SwiftLockbookCore
 struct OutlineSection: View {
     
     @EnvironmentObject var files: FileService
+    @EnvironmentObject var onboarding: OnboardingService
     @ObservedObject var state: OutlineState
     
     var root: ClientFileMetadata
@@ -33,6 +34,6 @@ struct OutlineSection: View {
             // A hack for list row insets not working. This hack also applies to the section header though.
         }.contextMenu {
             OutlineContextMenu.getContextView(meta: root, outlineState: state, branchState: nil)
-        }
+        }.sheet(isPresented: $onboarding.anAccountWasCreatedThisSession, content: { BeforeYouStart() })
     }
 }
