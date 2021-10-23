261a0d4c5b7a7c7892e6c3fede62b6930063f428
Parth <parth@mehrotra.me>
2021-10-23 15:41:00 -0400

    wrote another failing test

--- core/tests/delete_and_list_tests.rs
+++ core/tests/delete_and_list_tests.rs
@@ -10,9 +10,9 @@ mod delete_and_list_tests {
     use lockbook_models::file_metadata::FileType;
 
     use lockbook_core::{
-        assert_matches, create_file, create_file_at_path, delete_file, list_paths, make_account,
-        path, read_document, write_document, CreateFileError, ReadDocumentError,
-        WriteToDocumentError,
+        assert_matches, create_file, create_file_at_path, delete_file, get_root, list_paths,
+        make_account, path, read_document, save_document_to_disk, write_document, CreateFileError,
+        FileDeleteError, ReadDocumentError, SaveDocumentToDiskError, WriteToDocumentError,
     };
 
     #[test]
@@ -85,6 +85,45 @@ mod delete_and_list_tests {
         assert_matches!(
             create_file(&db, "document", folder.id, FileType::Document),
             Err(UiError(CreateFileError::CouldNotFindAParent))
+        );
+    }
+
+    #[test]
+    fn try_to_delete_root() {
+        let db = test_config();
+        let account = make_account!(db);
+
+        assert_matches!(
+            delete_file(&db, get_root(&db).unwrap().id),
+            Err(UiError(FileDeleteError::CannotDeleteRoot))
+        );
+    }
+
+    #[test]
+    fn test_create_parent_delete_parent_read_doc() {
+        let db = test_config();
+        let account = make_account!(db);
+        let doc = create_file_at_path(&db, path!(account, "folder/test.md")).unwrap();
+        write_document(&db, doc.id, "content".as_bytes()).unwrap();
+        assert_eq!(read_document(&db, doc.id).unwrap(), "content".as_bytes());
+        delete_file(&db, doc.parent).unwrap();
+        assert_matches!(
+            read_document(&db, doc.id),
+            Err(UiError(ReadDocumentError::FileDoesNotExist))
+        );
+    }
+
+    #[test]
+    fn test_create_parent_delete_parent_save_doc() {
+        let db = test_config();
+        let account = make_account!(db);
+        let doc = create_file_at_path(&db, path!(account, "folder/test.md")).unwrap();
+        write_document(&db, doc.id, "content".as_bytes()).unwrap();
+        assert_eq!(read_document(&db, doc.id).unwrap(), "content".as_bytes());
+        delete_file(&db, doc.parent).unwrap();
+        assert_matches!(
+            save_document_to_disk(&db, doc.id, "/dev/null".to_string()),
+            Err(UiError(SaveDocumentToDiskError::FileDoesNotExist))
         );
     }
 }
