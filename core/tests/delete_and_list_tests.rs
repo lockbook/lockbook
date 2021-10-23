b7c57b4902ac3e9c8d34713451c474f71ae6a819
Parth <parth@mehrotra.me>
2021-10-23 15:09:01 -0400

    wrote another failing test

--- core/tests/delete_and_list_tests.rs
+++ core/tests/delete_and_list_tests.rs
@@ -6,10 +6,14 @@ mod delete_and_list_tests {
     use lockbook_core::service::path_service::{create_at_path, Filter};
     use lockbook_core::service::test_utils::generate_account;
     use lockbook_core::service::test_utils::test_config;
-    use lockbook_core::{delete_file, list_paths, make_account, path};
+    use lockbook_core::Error::UiError;
+    use lockbook_core::{
+        assert_matches, delete_file, list_paths, make_account, path, read_document,
+        ReadDocumentError,
+    };
 
     #[test]
-    fn test_create_and_delete() {
+    fn test_create_and_delete_and_list() {
         let db = test_config();
         let account = make_account!(db);
         let doc = create_at_path(&db, path!(account, "doc.md")).unwrap();
@@ -23,6 +27,24 @@ mod delete_and_list_tests {
         assert_eq!(
             list_paths(&db, Some(Filter::LeafNodesOnly)).unwrap().len(),
             0
+        );
+    }
+
+    #[test]
+    fn test_create_and_delete() {
+        let db = test_config();
+        let account = make_account!(db);
+        let doc = create_at_path(&db, path!(account, "doc.md")).unwrap();
+        assert_eq!(
+            list_paths(&db, Some(Filter::LeafNodesOnly)).unwrap().len(),
+            1
+        );
+
+        delete_file(&db, doc.id).unwrap();
+
+        assert_matches!(
+            read_document(&db, doc.id),
+            Err(UiError(ReadDocumentError::FileDoesNotExist))
         );
     }
 }
