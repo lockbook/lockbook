88ad0620a7f2ff05e101311f4dac6d42540d1328
Parth <parth@mehrotra.me>
2021-10-23 15:15:25 -0400

    wrote another failing test

--- core/tests/delete_and_list_tests.rs
+++ core/tests/delete_and_list_tests.rs
@@ -8,12 +8,12 @@ mod delete_and_list_tests {
     use lockbook_core::service::test_utils::test_config;
     use lockbook_core::Error::UiError;
     use lockbook_core::{
-        assert_matches, delete_file, list_paths, make_account, path, read_document,
-        ReadDocumentError,
+        assert_matches, delete_file, list_paths, make_account, path, read_document, write_document,
+        ReadDocumentError, WriteToDocumentError,
     };
 
     #[test]
-    fn test_create_and_delete_and_list() {
+    fn test_create_delete_list() {
         let db = test_config();
         let account = make_account!(db);
         let doc = create_at_path(&db, path!(account, "doc.md")).unwrap();
@@ -31,7 +31,7 @@ mod delete_and_list_tests {
     }
 
     #[test]
-    fn test_create_and_delete() {
+    fn test_create_delete_read() {
         let db = test_config();
         let account = make_account!(db);
         let doc = create_at_path(&db, path!(account, "doc.md")).unwrap();
@@ -45,6 +45,24 @@ mod delete_and_list_tests {
         assert_matches!(
             read_document(&db, doc.id),
             Err(UiError(ReadDocumentError::FileDoesNotExist))
+        );
+    }
+
+    #[test]
+    fn test_create_delete_write() {
+        let db = test_config();
+        let account = make_account!(db);
+        let doc = create_at_path(&db, path!(account, "doc.md")).unwrap();
+        assert_eq!(
+            list_paths(&db, Some(Filter::LeafNodesOnly)).unwrap().len(),
+            1
+        );
+
+        delete_file(&db, doc.id).unwrap();
+
+        assert_matches!(
+            write_document(&db, doc.id, "document content".as_bytes()),
+            Err(UiError(WriteToDocumentError::FileDoesNotExist))
         );
     }
 }
