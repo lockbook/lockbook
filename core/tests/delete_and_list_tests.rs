da4985493d7127011dd534e3f6219cbdd25b1633
Parth <parth@mehrotra.me>
2021-10-23 15:32:04 -0400

    wrote another failing test

--- core/tests/delete_and_list_tests.rs
+++ core/tests/delete_and_list_tests.rs
@@ -3,20 +3,23 @@ mod integration_test;
 #[cfg(test)]
 mod delete_and_list_tests {
     use lockbook_core::service::account_service;
-    use lockbook_core::service::path_service::{create_at_path, Filter};
+    use lockbook_core::service::path_service::Filter;
     use lockbook_core::service::test_utils::generate_account;
     use lockbook_core::service::test_utils::test_config;
     use lockbook_core::Error::UiError;
+    use lockbook_models::file_metadata::FileType;
+
     use lockbook_core::{
-        assert_matches, delete_file, list_paths, make_account, path, read_document, write_document,
-        ReadDocumentError, WriteToDocumentError,
+        assert_matches, create_file, create_file_at_path, delete_file, list_paths, make_account,
+        path, read_document, write_document, CreateFileError, ReadDocumentError,
+        WriteToDocumentError,
     };
 
     #[test]
     fn test_create_delete_list() {
         let db = test_config();
         let account = make_account!(db);
-        let doc = create_at_path(&db, path!(account, "doc.md")).unwrap();
+        let doc = create_file_at_path(&db, path!(account, "doc.md")).unwrap();
         assert_eq!(
             list_paths(&db, Some(Filter::LeafNodesOnly)).unwrap().len(),
             1
@@ -34,7 +37,7 @@ mod delete_and_list_tests {
     fn test_create_delete_read() {
         let db = test_config();
         let account = make_account!(db);
-        let doc = create_at_path(&db, path!(account, "doc.md")).unwrap();
+        let doc = create_file_at_path(&db, path!(account, "doc.md")).unwrap();
         assert_eq!(
             list_paths(&db, Some(Filter::LeafNodesOnly)).unwrap().len(),
             1
@@ -52,7 +55,7 @@ mod delete_and_list_tests {
     fn test_create_delete_write() {
         let db = test_config();
         let account = make_account!(db);
-        let doc = create_at_path(&db, path!(account, "doc.md")).unwrap();
+        let doc = create_file_at_path(&db, path!(account, "doc.md")).unwrap();
         assert_eq!(
             list_paths(&db, Some(Filter::LeafNodesOnly)).unwrap().len(),
             1
@@ -63,6 +66,25 @@ mod delete_and_list_tests {
         assert_matches!(
             write_document(&db, doc.id, "document content".as_bytes()),
             Err(UiError(WriteToDocumentError::FileDoesNotExist))
+        );
+    }
+
+    #[test]
+    fn test_create_parent_delete_create_in_parent() {
+        let db = test_config();
+        let account = make_account!(db);
+        let folder = create_file_at_path(&db, path!(account, "folder/")).unwrap();
+
+        assert_eq!(
+            list_paths(&db, Some(Filter::LeafNodesOnly)).unwrap().len(),
+            1
+        );
+
+        delete_file(&db, folder.id).unwrap();
+
+        assert_matches!(
+            create_file(&db, "document", folder.id, FileType::Document),
+            Err(UiError(CreateFileError::CouldNotFindAParent))
         );
     }
 }
