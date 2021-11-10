b16a76c6c15604c39f084dedddf2199ca805e63e
Parth <parth@mehrotra.me>
2021-11-10 18:08:58 -0500

    core: restore get dirty local files

--- clients/apple/Shared/Stateful Logic/StatusService.swift
+++ clients/apple/Shared/Stateful Logic/StatusService.swift
@@ -32,4 +32,24 @@ class StatusService: ObservableObject {
             }
         }
     }
+    
+    @objc func setLastSynced() {
+           DispatchQueue.global(qos: .userInteractive).async {
+               if DI.accounts.account == nil {
+                   print("No account yet, but tried to update last synced, ignoring")
+                   return
+               }
+               
+               let lastSynced = self.core.getLastSyncedHumanString()
+               
+               DispatchQueue.main.async {
+                   switch lastSynced {
+                   case .success(let lastSynced):
+                       self.lastSynced = lastSynced
+                   case .failure(let error):
+                       DI.errors.handleError(error)
+                   }
+               }
+           }
+       }
 }
