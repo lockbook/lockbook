30cc5f0287302236105925118dc2824411053556
Parth <parth@mehrotra.me>
2021-12-18 19:09:56 -0500

    i can tls if you want it

--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -15,6 +15,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let config = Config::from_env_vars();
     pretty_env_logger::init();
 
+    // *** Things this server connects to ***
     let index_db_client = file_index_repo::connect(&config.index_db)
         .await
         .expect("Failed to connect to index_db");
@@ -28,8 +29,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         files_db_client,
     });
 
-    warp::serve(core_routes(&server_state))
-        .run(([127, 0, 0, 1], 8000))
-        .await;
+    let server = warp::serve(core_routes(&server_state));
+
+    // *** How people can connect to this server ***
+    match (config.server.ssl_cert_location, config.server.ssl_private_key_location) {
+        (Some(cert), Some(key)) => {
+            server.tls()
+                .cert_path(&cert)
+                .key_path(&key)
+                .run(([0, 0, 0, 0], config.server.port))
+                .await
+        },
+        _ => {
+            println!("binding to localhost (not 0) without tls for local development");
+            server.run(([127, 0, 0, 1], config.server.port))
+                .await
+        }
+    };
+
     Ok(())
 }
