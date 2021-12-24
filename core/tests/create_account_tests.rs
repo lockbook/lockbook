3ed453c509dcdcc8e5fe604ea07f42cc3e80209e
Parth <parth@mehrotra.me>
2021-12-24 00:32:20 -0500

    server: make sure usernames are case sensitive (#939)
    * server: make sure usernames are not case sensitive
    
    * fix tests
    
    * fix tests
    
    * cargo fmt
    
    * cargo fmt
    
    * clippy
    
    * fmt
    
    * lowercase test assertion
    
    * lowercase test assertion

--- core/tests/create_account_tests.rs
+++ core/tests/create_account_tests.rs
@@ -3,6 +3,7 @@ mod integration_test;
 #[cfg(test)]
 mod change_document_content_tests {
     use lockbook_core::service::api_service;
+    use lockbook_core::service::api_service::ApiError;
     use lockbook_core::service::test_utils::{generate_account, generate_root_metadata};
     use lockbook_models::api::*;
 
@@ -12,5 +13,22 @@ mod change_document_content_tests {
         let account = generate_account();
         let (root, _root_key) = generate_root_metadata(&account);
         api_service::request(&account, NewAccountRequest::new(&account, &root)).unwrap();
+    }
+
+    #[test]
+    fn create_account_username_case() {
+        // new account
+        let mut account = generate_account();
+        let (root, _root_key) = generate_root_metadata(&account);
+        api_service::request(&account, NewAccountRequest::new(&account, &root)).unwrap();
+        let old_username = account.username;
+        let mut account = generate_account();
+        account.username = old_username.to_uppercase();
+        let (root, _root_key) = generate_root_metadata(&account);
+        let operation = api_service::request(&account, NewAccountRequest::new(&account, &root));
+        match operation {
+            Err(ApiError::Endpoint(NewAccountError::UsernameTaken)) => {} // Test pass
+            _ => panic!("Usernames must be case sensitive {:?}", operation),
+        }
     }
 }
