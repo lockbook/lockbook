d58f74acc5422ddd931187810f87d935ef77f46a
Parth <parth@mehrotra.me>
2021-12-24 22:25:24 -0500

    core: check for cycles before checking for path conflicts (#940)
    * fix code that didn't handle cycles well
    
    * warning

--- core/tests/create_account_tests.rs
+++ core/tests/create_account_tests.rs
@@ -18,7 +18,7 @@ mod change_document_content_tests {
     #[test]
     fn create_account_username_case() {
         // new account
-        let mut account = generate_account();
+        let account = generate_account();
         let (root, _root_key) = generate_root_metadata(&account);
         api_service::request(&account, NewAccountRequest::new(&account, &root)).unwrap();
         let old_username = account.username;
