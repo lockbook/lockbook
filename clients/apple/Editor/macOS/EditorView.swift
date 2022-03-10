6a01226dd205158f123f33bbd3d7d14913548a75
Parth <parth@mehrotra.me>
2022-03-09 22:10:03 -0500

    apple: massively improve markdown editing experience (#968)
    * remove un-needed files
    
    * early gains with markdown editor
    
    * remove old notepad
    
    * add a log
    
    * initial structure for new editor
    
    * custom text storage
    
    * bullets wip
    
    * new parser
    
    * finding seg faults
    
    * changing parser strategy
    
    * trying to write some goddamn swift tests
    
    * trying to write some goddamn swift tests
    
    * indexer seems reliable now
    
    * styles looking good, need to make the model more complex
    
    * new styler working well
    
    * just need to implement all the elements now
    
    * list wrapping working excellently
    
    * perf optimizations
    
    * nicer code background
    
    * maybe found a bug in down?
    
    * using my own version of Down due to a bug, and only updating the document when it's actually needed
    
    * iOS done

---
+++ clients/apple/Editor/macOS/EditorView.swift
@@ -0,0 +1,78 @@
+import Foundation
+import SwiftUI
+
+struct EditorView: NSViewRepresentable {
+    
+    @EnvironmentObject var model: DocumentLoader
+    let storage = Storage()
+    
+    func makeNSView(context: Context) -> NSScrollView {
+        let scrollView = NSScrollView()
+
+        let layoutManager = NSLayoutManager()
+        storage.addLayoutManager(layoutManager)
+
+        let textContainer = NSTextContainer(containerSize: scrollView.frame.size)
+        textContainer.widthTracksTextView = true
+        textContainer.containerSize = NSSize(
+            width: scrollView.contentSize.width,
+            height: CGFloat.greatestFiniteMagnitude
+        )
+        layoutManager.addTextContainer(textContainer)
+        
+        let textView = NSTextView(frame: .zero, textContainer: textContainer)
+        textView.autoresizingMask = .width
+        textView.isVerticallyResizable = true
+        textView.maxSize = NSSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude)
+        textView.minSize = NSSize(width: 0, height: scrollView.contentSize.height)
+        textView.delegate = context.coordinator
+        textView.string = model.textDocument!
+        textView.allowsUndo = true
+        storage.syntaxHighlight()
+        
+        scrollView.documentView = textView
+        scrollView.hasVerticalScroller = true
+        return scrollView
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(textChange: updateModel)
+    }
+    
+    func updateModel(update: String) {
+        model.textDocument = update
+    }
+    
+    func updateNSView(_ scroll: NSScrollView, context: Context) {
+        if let nsView = scroll.documentView as? NSTextView {
+            if model.reloadContent {
+                model.reloadContent = false
+                
+                nsView.string = model.textDocument!
+            }
+        }
+    }
+}
+
+class Coordinator: NSObject, NSTextViewDelegate {
+    public var onTextChange: (String) -> Void
+    
+    init(textChange: @escaping (String) -> Void) {
+        self.onTextChange = textChange
+    }
+    
+    public func textDidChange(_ notification: Notification) {
+        guard let textView = notification.object as? NSTextView else {
+            print("textview not a textview")
+            return
+        }
+        guard let storage = textView.textStorage as? Storage else {
+            print("Wrong storage type attached to this textview")
+            return
+        }
+        
+        storage.syntaxHighlight()
+        
+        onTextChange(textView.string)
+    }
+}
