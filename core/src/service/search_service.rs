153247b4cde2817ce6c663cdc7d5644317ecfc95
Steve Russo <steverusso@pm.me>
2022-04-03 09:55:36 -0400

    improve search code in core

--- core/src/service/search_service.rs
+++ core/src/service/search_service.rs
@@ -10,11 +10,42 @@ use crate::model::state::Config;
 use crate::path_service;
 use crate::CoreError;
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 pub struct SearchResultItem {
     pub id: Uuid,
     pub path: String,
     pub score: i64,
+}
+
+impl Ord for SearchResultItem {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.score.cmp(&other.score) {
+            Ordering::Greater => Ordering::Less,
+            Ordering::Less => Ordering::Greater,
+            Ordering::Equal => {
+                let chars1: Vec<char> = self.path.chars().collect();
+                let chars2: Vec<char> = other.path.chars().collect();
+
+                let n_chars1 = chars1.len();
+                let n_chars2 = chars2.len();
+
+                for i in 0..std::cmp::min(n_chars1, n_chars2) {
+                    let ord = chars1[i].cmp(&chars2[i]);
+                    if ord != Ordering::Equal {
+                        return ord;
+                    }
+                }
+
+                n_chars1.cmp(&n_chars2)
+            }
+        }
+    }
+}
+
+impl PartialOrd for SearchResultItem {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 pub fn search_file_paths(config: &Config, input: &str) -> Result<Vec<SearchResultItem>, CoreError> {
@@ -22,45 +53,19 @@ pub fn search_file_paths(config: &Config, input: &str) -> Result<Vec<SearchResul
         return Ok(Vec::new());
     }
 
-    let mut possibs = Vec::new();
-    for f in file_service::get_all_not_deleted_metadata(config, RepoSource::Local)? {
-        possibs.push((f.id, path_service::get_path_by_id(config, f.id)?));
-    }
-
     let root_name = file_service::get_root(config)?.decrypted_name;
     let matcher = SkimMatcherV2::default();
 
-    let mut results = possibs
-        .into_iter()
-        .filter_map(|(id, path)| {
-            let path = path.strip_prefix(&root_name).unwrap_or(&path).to_string();
+    let mut results = Vec::new();
+    for f in file_service::get_all_not_deleted_metadata(config, RepoSource::Local)? {
+        let path = path_service::get_path_by_id(config, f.id)?;
+        let path_without_root = path.strip_prefix(&root_name).unwrap_or(&path).to_string();
 
-            matcher
-                .fuzzy_match(&path, input)
-                .map(|score| SearchResultItem { id, path, score })
-        })
-        .collect::<Vec<SearchResultItem>>();
-
-    results.sort_by(|a, b| match a.score.cmp(&b.score) {
-        Ordering::Greater => Ordering::Less,
-        Ordering::Less => Ordering::Greater,
-        Ordering::Equal => {
-            let chars1: Vec<char> = a.path.chars().collect();
-            let chars2: Vec<char> = b.path.chars().collect();
-
-            let n_chars1 = chars1.len();
-            let n_chars2 = chars2.len();
-
-            for i in 0..std::cmp::min(n_chars1, n_chars2) {
-                let ord = chars1[i].cmp(&chars2[i]);
-                if ord != Ordering::Equal {
-                    return ord;
-                }
-            }
-
-            n_chars1.cmp(&n_chars2)
+        if let Some(score) = matcher.fuzzy_match(&path_without_root, input) {
+            results.push(SearchResultItem { id: f.id, path: path_without_root, score });
         }
-    });
+    }
+    results.sort();
 
     Ok(results)
 }
