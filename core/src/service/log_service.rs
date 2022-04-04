ff86b377b77ee98a95790e4ee03c4e34c76e8492
Parth <parth@mehrotra.me>
2022-04-04 18:40:32 -0400

    core: remove stdout logger

--- core/src/service/log_service.rs
+++ core/src/service/log_service.rs
@@ -17,19 +17,13 @@ pub fn init(log_path: &Path) -> Result<(), CoreError> {
         .and_then(|s| LevelFilter::from_str(s.as_str()).ok())
         .unwrap_or(LevelFilter::DEBUG);
 
-    let subscriber = tracing_subscriber::Registry::default()
-        .with(
-            fmt::Layer::new()
-                .with_span_events(FmtSpan::ACTIVE)
-                .with_target(false)
-                .with_writer(tracing_appender::rolling::never(log_path, LOG_FILE))
-                .with_filter(lockbook_log_level),
-        )
-        .with(
-            fmt::Layer::new()
-                .with_writer(std::io::stdout)
-                .with_filter(LevelFilter::WARN),
-        );
+    let subscriber = tracing_subscriber::Registry::default().with(
+        fmt::Layer::new()
+            .with_span_events(FmtSpan::ACTIVE)
+            .with_target(false)
+            .with_writer(tracing_appender::rolling::never(log_path, LOG_FILE))
+            .with_filter(lockbook_log_level),
+    );
 
     tracing::subscriber::set_global_default(subscriber).map_err(core_err_unexpected)?;
 
