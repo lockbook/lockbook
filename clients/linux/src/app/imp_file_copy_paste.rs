86ff19b63f71ef8a7e24d1f81637eba008ff5cc7
Steve Russo <steverusso@pm.me>
2022-04-06 11:39:03 -0400

    linux: implement move via right click cut & paste (#1052)

---
+++ clients/linux/src/app/imp_file_copy_paste.rs
@@ -0,0 +1,50 @@
+impl super::App {
+    pub fn cut_selected_file(&self) {
+        let t = &self.account.tree;
+        match t.get_selected_uuid() {
+            Some(id) => *t.clipboard.borrow_mut() = Some(id),
+            None => self.show_err_dialog("a single file must be selected to copy or cut!"),
+        }
+    }
+
+    pub fn paste_into_selected_file(&self) {
+        let t = &self.account.tree;
+
+        let dest_id = match t.get_selected_uuid() {
+            Some(id) => match self.api.file_by_id(id) {
+                Ok(meta) => match meta.file_type {
+                    lb::FileType::Document => meta.parent,
+                    lb::FileType::Folder => meta.id,
+                },
+                Err(err) => {
+                    self.show_err_dialog(&format!("{:?}", err));
+                    return;
+                }
+            },
+            None => {
+                self.show_err_dialog("a single file must be selected to paste file!");
+                return;
+            }
+        };
+
+        let id = match t.clipboard.borrow_mut().take() {
+            Some(id) => id,
+            None => {
+                self.show_err_dialog("the clipboard is empty, there's nothing to paste!");
+                return;
+            }
+        };
+
+        match self.api.move_file(id, dest_id) {
+            Ok(_) => {
+                let iter = t.search(&id).unwrap();
+                t.model.remove(&iter);
+
+                let children = self.api.file_and_all_children(id).unwrap();
+                let parent_iter = t.search(&dest_id).unwrap();
+                t.append_any_children(&dest_id, &parent_iter, &children);
+            }
+            Err(err) => self.show_err_dialog(&format!("{:?}", err)),
+        }
+    }
+}
