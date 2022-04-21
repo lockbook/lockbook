b2d7442608d4e61ce6b1d4e670429df7f4f1f6e4
Steve Russo <steverusso@pm.me>
2022-04-21 09:14:50 -0400

    improve dialog ui, add icons, add link to wikipedia encryption

--- clients/linux/src/app/imp_account_create.rs
+++ clients/linux/src/app/imp_account_create.rs
@@ -41,9 +41,19 @@ impl super::App {
 
         let message = gtk::Label::new(Some(MESSAGE));
         message.set_wrap(true);
-        message.set_margin_bottom(16);
 
-        let goto_settings = gtk::Button::with_label("Backup Now");
+        let learn_more_e2e_btn = gtk::LinkButton::builder()
+            .uri("https://en.wikipedia.org/wiki/End-to-end_encryption")
+            .label("Learn more")
+            .build();
+        learn_more_e2e_btn.add_css_class("onboard-link");
+        let learn_more_e2e = gtk::Box::builder()
+            .orientation(gtk::Orientation::Horizontal)
+            .build();
+        learn_more_e2e.append(&learn_more_e2e_btn);
+        learn_more_e2e.append(&gtk::Label::new(Some(" about end-to-end encryption.")));
+
+        let goto_settings = icon_button("security-high", "Backup Now");
         goto_settings.connect_clicked({
             let info = info.clone();
             let app = self.clone();
@@ -54,7 +64,7 @@ impl super::App {
             }
         });
 
-        let goto_account_screen = gtk::Button::with_label("Skip and Backup Later");
+        let goto_account_screen = icon_button("security-low", "Backup Later");
         goto_account_screen.connect_clicked({
             let info = info.clone();
             move |_| info.close()
@@ -63,27 +73,53 @@ impl super::App {
         let btns = gtk::Grid::builder()
             .column_spacing(4)
             .column_homogeneous(true)
-            .margin_top(16)
             .build();
         btns.attach(&goto_settings, 0, 0, 1, 1);
         btns.attach(&goto_account_screen, 1, 0, 1, 1);
 
         let cntr = gtk::Box::builder()
             .orientation(gtk::Orientation::Vertical)
+            .spacing(20)
             .halign(gtk::Align::Center)
             .valign(gtk::Align::Center)
-            .margin_top(16)
-            .margin_bottom(16)
-            .margin_start(16)
-            .margin_end(16)
+            .margin_top(20)
+            .margin_bottom(20)
+            .margin_start(20)
+            .margin_end(20)
             .build();
         cntr.append(&message);
+        cntr.append(&learn_more_e2e);
         cntr.append(&gtk::Separator::new(gtk::Orientation::Horizontal));
         cntr.append(&btns);
 
+        let info_icon = gtk::Image::from_icon_name("emblem-important");
+        info_icon.set_pixel_size(32);
+
+        let info_hbar = gtk::HeaderBar::new();
+        info_hbar.pack_start(&info_icon);
+
+        info.set_titlebar(Some(&info_hbar));
         info.content_area().append(&cntr);
         info.present();
+        goto_settings.grab_focus();
     }
+}
+
+fn icon_button(icon_name: &str, label: &str) -> gtk::Button {
+    let icon = gtk::Image::from_icon_name(icon_name);
+    icon.set_pixel_size(24);
+
+    let content = gtk::Box::builder()
+        .orientation(gtk::Orientation::Horizontal)
+        .halign(gtk::Align::Center)
+        .margin_top(4)
+        .margin_bottom(4)
+        .spacing(8)
+        .build();
+    content.append(&icon);
+    content.append(&gtk::Label::new(Some(label)));
+
+    gtk::Button::builder().child(&content).build()
 }
 
 static MESSAGE: &str = "Lockbook encrypts your notes with a key that stays on your Lockbook devices. This makes your notes unreadable to everyone except you. However, if you lose this key, your notes are not recoverable. Therefore, we recommend you make a backup in case something happens to this device.";
