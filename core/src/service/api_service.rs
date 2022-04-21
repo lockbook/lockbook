7b26192837b8ac316f261fc2f63e00100b352542
Parth <parth@mehrotra.me>
2022-04-21 19:50:44 -0400

    static reqwest client

--- core/src/service/api_service.rs
+++ core/src/service/api_service.rs
@@ -1,3 +1,4 @@
+use lazy_static::lazy_static;
 use reqwest::blocking::Client as ReqwestClient;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
@@ -37,6 +38,10 @@ pub enum ApiError<E> {
     Deserialize(String),
 }
 
+lazy_static! {
+    static ref CLIENT: ReqwestClient = ReqwestClient::new();
+}
+
 pub fn request<
     T: Request<Response = impl DeserializeOwned, Error = impl DeserializeOwned> + Serialize,
 >(
@@ -51,7 +56,6 @@ pub fn request_helper<
     account: &Account, request: T, get_code_version: fn() -> &'static str,
     get_time: fn() -> Timestamp,
 ) -> Result<T::Response, ApiError<T::Error>> {
-    let client = ReqwestClient::new();
     let signed_request =
         pubkey::sign(&account.private_key, request, get_time).map_err(ApiError::Sign)?;
     let serialized_request = serde_json::to_vec(&RequestWrapper {
@@ -59,7 +63,7 @@ pub fn request_helper<
         client_version: String::from(get_code_version()),
     })
     .map_err(|err| ApiError::Serialize(err.to_string()))?;
-    let serialized_response = client
+    let serialized_response = CLIENT
         .request(T::METHOD, format!("{}{}", account.api_url, T::ROUTE).as_str())
         .body(serialized_request)
         .send()
