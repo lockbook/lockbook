f353922d33d71ca13eae20179ca32e0fb260cfbe
Steve Russo <steverusso@pm.me>
2022-04-24 08:56:37 -0400

    only untrack auto save on text content

--- clients/linux/src/app/imp_close_file.rs
+++ clients/linux/src/app/imp_close_file.rs
@@ -1,12 +1,15 @@
+use crate::ui;
+
 impl super::App {
     pub fn close_file(&self) {
         if let Some(tab) = self.account.current_tab() {
-            let id = tab.id();
-
-            if self.bg_state.is_dirty(id) {
-                self.save_file(Some(id));
+            if let Some(_txt_ed) = tab.content::<ui::TextEditor>() {
+                let id = tab.id();
+                if self.bg_state.is_dirty(id) {
+                    self.save_file(Some(id));
+                }
+                self.bg_state.untrack(id);
             }
-            self.bg_state.untrack(id);
 
             let t = &self.account.tabs;
             t.remove_page(t.current_page());
