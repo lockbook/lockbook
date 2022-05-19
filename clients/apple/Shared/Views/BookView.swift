5c0a792689fb0b2ffd5ae2cf871181cff91f75ab
Parth <parth@mehrotra.me>
2022-05-19 14:28:11 -0400

    apple: refine all things trees (#1093)
    * refactor iOS file tree for more code reuse
    
    * outline view for ipad
    
    * macos tree view looking good, icons & context & dnd remain
    
    * icons done, d&d done, now just need context menu
    
    * context menu figured out, we're over the hump, need to organize, and cleanup, then release
    
    * crossplatform sheets done, context menus done
    
    * file logic cleaned up, all files always sorted'

--- clients/apple/Shared/Views/BookView.swift
+++ clients/apple/Shared/Views/BookView.swift
@@ -2,20 +2,85 @@ import SwiftUI
 import SwiftLockbookCore
 
 struct BookView: View {
-    
+
+    @EnvironmentObject var sheets: SheetState
+    @EnvironmentObject var onboarding: OnboardingService
+
     let currentFolder: DecryptedFileMetadata
     let account: Account
-    
-    @State var moving: DecryptedFileMetadata?
+
+    #if os(iOS)
+    @Environment(\.horizontalSizeClass) var horizontal
+    @Environment(\.verticalSizeClass) var vertical
+    #endif
 
     var body: some View {
+        platformFileTree
+                .sheet(isPresented: $onboarding.anAccountWasCreatedThisSession, content: { BeforeYouStart() })
+                .sheet(isPresented: $sheets.creating) {
+                    NewFileSheet()
+                }
+                .iOSOnlySheet(isPresented: $sheets.moving)
+                .sheet(isPresented: $sheets.renaming) {
+                    RenamingSheet()
+                }
+    }
+
+    #if os(iOS)
+    var iOS: some View {
         NavigationView {
-            #if os(iOS)
-            FileListView(currentFolder: currentFolder, account: account, moving: $moving)
-            #else
             FileListView(currentFolder: currentFolder, account: account)
-            #endif
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            NavigationLink(
+                                    destination: SettingsView().equatable(), isActive: $onboarding.theyChoseToBackup) {
+                                Image(systemName: "gearshape.fill")
+                                        .foregroundColor(.blue)
+                            }
+                        }
+                    }
         }
+                .navigationViewStyle(.stack)
+    }
+
+    @ViewBuilder
+    var iPad: some View {
+        let _ = print("Switching to iPad, \(DI.currentDoc.selectedItem)")
+        NavigationView {
+            FileTreeView(currentFolder: currentFolder, account: account)
+        }
+    }
+    #else
+    var macOS: some View {
+        NavigationView {
+            FileListView()
+        }
+    }
+    #endif
+
+    @ViewBuilder
+    var platformFileTree: some View {
+        #if os(iOS)
+        if horizontal == .regular && vertical == .regular {
+            iPad
+        } else {
+            iOS
+        }
+        #else
+        macOS
+        #endif
+    }
+}
+
+extension View {
+    func iOSOnlySheet(isPresented: Binding<Bool>) -> some View {
+        #if os(iOS)
+        self.sheet(isPresented: isPresented) {
+            MoveSheet()
+        }
+        #else
+        self
+        #endif
     }
 }
 
@@ -23,7 +88,7 @@ struct BookView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             BookView(currentFolder: FakeApi.root, account: .fake(username: "jeff"))
-                .ignoresSafeArea()
+                    .ignoresSafeArea()
         }
     }
 }
---
+++ clients/apple/iOS/FileTreeView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+import SwiftLockbookCore
+
+struct FileTreeView: View {
+
+    @EnvironmentObject var sheets: SheetState
+    @EnvironmentObject var currentDoc: CurrentDocument
+    @EnvironmentObject var coreService: CoreService
+    @EnvironmentObject var files: FileService
+    @EnvironmentObject var onboarding: OnboardingService
+
+    let currentFolder: DecryptedFileMetadata
+    let account: Account
+    
+    var body: some View {
+        VStack {
+            OutlineSection(root: currentFolder)
+            HStack {
+                BottomBar(onCreating: {
+                    sheets.creatingInfo = CreatingInfo(parent: currentFolder, child_type: .Document)
+                })
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                NavigationLink(
+                    destination: SettingsView().equatable(), isActive: $onboarding.theyChoseToBackup) {
+                        Image(systemName: "gearshape.fill")
+                            .foregroundColor(.blue)
+                    }
+            }
+        }
+        if let item = currentDoc.selectedItem {
+            DocumentView(meta: item)
+        }
+    }
+}
