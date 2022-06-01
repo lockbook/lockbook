5739323a58003a6d39bf2e6c1db51d15ce0dffb1
Smail Barkouch <smailbarkouch1@gmail.com>
2022-06-01 18:04:00 -0400

    set back api url in android

--- clients/android/app/src/main/java/app/lockbook/model/CoreModel.kt
+++ clients/android/app/src/main/java/app/lockbook/model/CoreModel.kt
@@ -15,7 +15,7 @@ import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.modules.subclass
 
 object CoreModel {
-    private const val PROD_API_URL = "https://5a16-2601-86-680-28c0-00-6b7a.ngrok.io"
+    private const val PROD_API_URL = "https://api.prod.lockbook.net"
     private fun getAPIURL(): String = System.getenv("API_URL") ?: PROD_API_URL
 
     private fun <O, E : Enum<E>> SerializersModuleBuilder.createPolyRelation(
