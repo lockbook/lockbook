789782292065ee6d057ac4d1a2c605562ace8020
Steve Russo <steverusso@pm.me>
2022-06-02 11:35:00 -0400

    refactor the new file user experience

--- clients/linux/src/app/imp_new_file.rs
+++ clients/linux/src/app/imp_new_file.rs
@@ -1,48 +1,91 @@
 use gtk::prelude::*;
 
+use crate::ui;
+
 impl super::App {
-    pub fn new_file(&self, ftype: lb::FileType) {
-        let ftype_str = format!("{:?}", ftype);
+    pub fn prompt_new_file(&self) {
+        let selected_id = self.account.tree.get_selected_uuid();
 
-        let lbl = gtk::Label::builder()
-            .label(&format!("Enter {} name:", ftype_str.to_lowercase()))
+        let (parent_id, parent_path) = match lb::parent_info(&self.api, selected_id) {
+            Ok(v) => v,
+            Err(err) => {
+                self.show_err_dialog(&err);
+                return;
+            }
+        };
+
+        let parent_lbl = gtk::Label::builder()
+            .label("Parent:")
+            .margin_start(6)
             .halign(gtk::Align::Start)
             .build();
 
-        let errlbl = gtk::Label::builder()
-            .name("err")
-            .halign(gtk::Align::Start)
-            .margin_top(16)
-            .margin_bottom(8)
+        let parent_entry = gtk::Entry::builder()
+            .text(&parent_path)
+            .sensitive(false)
+            .hexpand(true)
             .build();
 
-        let entry = gtk::Entry::builder()
+        let name_lbl = gtk::Label::builder()
+            .label("Name:")
+            .margin_start(6)
+            .halign(gtk::Align::Start)
+            .build();
+
+        let name_entry = gtk::Entry::builder()
             .activates_default(true)
-            .margin_top(16)
+            .hexpand(true)
             .build();
 
-        let content = gtk::Box::builder()
-            .orientation(gtk::Orientation::Vertical)
-            .margin_start(8)
-            .margin_end(8)
-            .margin_top(8)
-            .margin_bottom(8)
+        let ext_lbl = gtk::Label::builder().visible(false).build();
+
+        let name_and_ext = gtk::Box::new(gtk::Orientation::Horizontal, 6);
+        name_and_ext.append(&name_entry);
+        name_and_ext.append(&ext_lbl);
+
+        let ftype_choices = ui::ToggleGroup::with_buttons(&[
+            ("Folder", NewFileType::Folder),
+            ("Plain Text", NewFileType::PlainText),
+            ("Markdown", NewFileType::Markdown),
+        ]);
+        ftype_choices.connect_changed(move |value: NewFileType| {
+            if let Some(ext) = value.ext() {
+                ext_lbl.set_text(ext);
+                ext_lbl.show();
+            } else {
+                ext_lbl.hide();
+            }
+        });
+
+        let form = gtk::Grid::builder()
+            .column_spacing(16)
+            .row_spacing(16)
             .build();
+        form.attach(&parent_lbl, 0, 0, 1, 1);
+        form.attach(&parent_entry, 1, 0, 1, 1);
+        form.attach(&name_lbl, 0, 1, 1, 1);
+        form.attach(&name_and_ext, 1, 1, 1, 1);
 
-        content.append(&lbl);
-        content.append(&entry);
+        let err_lbl = gtk::Label::builder().visible(false).name("err").build();
 
-        let d = gtk::Dialog::builder()
-            .title(&format!("New {}", ftype_str))
-            .transient_for(&self.window)
-            .default_width(300)
-            .modal(true)
-            .build();
+        let d = new_file_dialog(&self.window);
+        let ca = d.content_area();
+        ca.set_orientation(gtk::Orientation::Vertical);
+        ca.set_spacing(16);
+        ca.set_margin_top(16);
+        ca.set_margin_bottom(16);
+        ca.set_margin_start(16);
+        ca.set_margin_end(16);
+        ca.append(&ftype_choices.cntr);
+        ca.append(&form);
+        ca.append(&err_lbl);
 
-        d.content_area().set_orientation(gtk::Orientation::Vertical);
-        d.content_area().append(&content);
-        d.add_button("Ok", gtk::ResponseType::Ok);
-        d.set_default_response(gtk::ResponseType::Ok);
+        name_entry.grab_focus();
+
+        let display_error = move |err_msg: &str| {
+            err_lbl.set_text(err_msg);
+            err_lbl.show();
+        };
 
         let app = self.clone();
         d.connect_response(move |d, resp| {
@@ -51,42 +94,147 @@ impl super::App {
                 return;
             }
 
-            let parent_id = match app.account.tree.get_selected_uuid() {
-                Some(id) => match app.api.file_by_id(id) {
-                    Ok(file) => match file.file_type {
-                        lb::FileType::Document => file.parent,
-                        lb::FileType::Folder => file.id,
-                    },
-                    Err(err) => {
-                        errlbl.set_text(&format!("{:?}", err));
-                        content.append(&errlbl);
-                        entry.set_sensitive(false);
-                        return;
-                    }
-                },
-                None => {
-                    eprintln!("no destination is selected to create from!");
-                    return;
-                }
-            };
+            let ftype = ftype_choices.value();
+            let mut fname = name_entry.text().to_string();
+            if let Some(ext) = ftype.ext() {
+                fname = format!("{}{}", fname, ext);
+            }
 
-            let fname = entry.buffer().text();
-
-            match app.api.create_file(&fname, parent_id, ftype) {
+            match app.api.create_file(&fname, parent_id, ftype.lb_type()) {
                 Ok(new_file) => {
-                    d.close();
                     match app.account.tree.add_file(&new_file) {
                         Ok(_) => {
                             app.update_sync_status();
+                            d.close();
                             //open the file if doc?
                         }
-                        Err(err) => eprintln!("{}", err),
+                        Err(err) => display_error(&err),
                     }
                 }
-                Err(err) => eprintln!("{:?}", err),
+                Err(err) => display_error(&format!("{:?}", err)), //todo
             }
         });
 
         d.show();
     }
 }
+
+fn new_file_dialog(parent: &impl IsA<gtk::Window>) -> gtk::Dialog {
+    let new_icon = gtk::Image::from_icon_name("emblem-new");
+    new_icon.set_pixel_size(28);
+
+    let title = gtk::Label::new(None);
+    title.set_markup("<b>New File</b>");
+
+    let titlebar = gtk::HeaderBar::new();
+    titlebar.set_title_widget(Some(&title));
+    titlebar.pack_start(&new_icon);
+
+    let d = gtk::Dialog::builder()
+        .transient_for(parent)
+        .titlebar(&titlebar)
+        .modal(true)
+        .build();
+    d.add_button("Ok", gtk::ResponseType::Ok);
+    d.set_default_response(gtk::ResponseType::Ok);
+    d
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum NewFileType {
+    Folder,
+    PlainText,
+    Markdown,
+}
+
+impl Default for NewFileType {
+    fn default() -> Self {
+        Self::Folder
+    }
+}
+
+impl NewFileType {
+    fn lb_type(&self) -> lb::FileType {
+        match self {
+            Self::PlainText | Self::Markdown => lb::FileType::Document,
+            Self::Folder => lb::FileType::Folder,
+        }
+    }
+
+    fn ext(&self) -> Option<&str> {
+        match self {
+            Self::PlainText | Self::Folder => None,
+            Self::Markdown => Some(".md"),
+        }
+    }
+}
+
+/*
+#[derive(Clone)]
+struct FileTypeChoices {
+    folder: gtk::ToggleButton,
+    text: gtk::ToggleButton,
+    markdown: gtk::ToggleButton,
+    cntr: gtk::Box,
+}
+
+impl FileTypeChoices {
+    fn new() -> Self {
+        let type_group = gtk::ToggleButton::new();
+
+        let folder = gtk::ToggleButton::builder()
+            .group(&type_group)
+            .can_focus(false)
+            .label("Folder")
+            .active(true)
+            .build();
+
+        let text = gtk::ToggleButton::builder()
+            .group(&type_group)
+            .can_focus(false)
+            .label("Plain Text")
+            .build();
+
+        let markdown = gtk::ToggleButton::builder()
+            .group(&type_group)
+            .can_focus(false)
+            .label("Markdown")
+            .build();
+
+        let sg = gtk::SizeGroup::new(gtk::SizeGroupMode::Horizontal);
+        sg.add_widget(&folder);
+        sg.add_widget(&text);
+        sg.add_widget(&markdown);
+
+        let cntr = gtk::Box::new(gtk::Orientation::Horizontal, 0);
+        cntr.add_css_class("toggle_btn_group");
+        cntr.set_margin_bottom(4);
+        cntr.append(&folder);
+        cntr.append(&text);
+        cntr.append(&markdown);
+
+        Self { folder, text, markdown, cntr }
+    }
+
+    fn selected_type(&self) -> NewFileType {
+        if self.text.is_active() {
+            return NewFileType::PlainText;
+        }
+        if self.markdown.is_active() {
+            return NewFileType::Markdown;
+        }
+        return NewFileType::Folder;
+    }
+
+    fn connect_clicked<F: Fn(&Self) + 'static>(&self, f: F) {
+        use std::rc::Rc;
+        let f = Rc::new(f);
+
+        for btn in &[&self.folder, &self.text, &self.markdown] {
+            let f = f.clone();
+            let this = self.clone();
+            btn.connect_clicked(move |_| f(&this));
+        }
+    }
+}
+*/
