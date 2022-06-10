bea9392e1237471c6b5badbb69b4ba175877c843
Steve Russo <steverusso@pm.me>
2022-06-09 20:09:24 -0400

    remove comment, add whitespace, swap none some lines

--- core/src/service/file_service.rs
+++ core/src/service/file_service.rs
@@ -33,13 +33,12 @@ impl Tx<'_> {
         self.insert_metadatum(config, RepoSource::Local, &metadata)?;
         Ok(metadata)
     }
+
     pub fn root(&self) -> Result<DecryptedFileMetadata, CoreError> {
         let files = self.get_all_not_deleted_metadata(RepoSource::Local)?;
-        // let root = self.root.get(&OneKey {});
-        // files. work on this. Replace the following files with root (maybe)
         match files.maybe_find_root() {
-            None => Err(RootNonexistent),
             Some(file_metadata) => Ok(file_metadata),
+            None => Err(RootNonexistent),
         }
     }
 
