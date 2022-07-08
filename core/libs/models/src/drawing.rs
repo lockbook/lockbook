ac7bb986b5e0b4524f664ba0992f5ee73eb90ec1
Steve Russo <steverusso@pm.me>
2022-07-07 20:23:25 -0400

    derive Default on Drawing

--- core/libs/models/src/drawing.rs
+++ core/libs/models/src/drawing.rs
@@ -2,19 +2,13 @@ use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Drawing {
     pub scale: f32,
     pub translation_x: f32,
     pub translation_y: f32,
     pub strokes: Vec<Stroke>,
     pub theme: Option<HashMap<ColorAlias, ColorRGB>>,
-}
-
-impl Default for Drawing {
-    fn default() -> Self {
-        Drawing { scale: 0.0, translation_x: 0.0, translation_y: 0.0, strokes: vec![], theme: None }
-    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
