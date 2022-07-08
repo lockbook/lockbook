b53d00542ab84015d2d254b658d971199f7f4f07
Steve Russo <steverusso@pm.me>
2022-07-07 20:27:34 -0400

    add a constructor and an is_empty helper method onto Stroke

--- core/libs/models/src/drawing.rs
+++ core/libs/models/src/drawing.rs
@@ -20,6 +20,22 @@ pub struct Stroke {
     pub alpha: f32,
 }
 
+impl Stroke {
+    pub fn new(color: ColorAlias) -> Self {
+        Self {
+            points_x: Vec::new(),
+            points_y: Vec::new(),
+            points_girth: Vec::new(),
+            color,
+            alpha: 1.0,
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.points_x.is_empty() && self.points_y.is_empty() && self.points_girth.is_empty()
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
 pub enum ColorAlias {
     Black,
