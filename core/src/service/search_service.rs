8fb7e521a0f5cdf5458091455404d1291894240d
Smail Barkouch <SmailBarkouch@users.noreply.github.com>
2022-07-24 17:00:19 -0400

    core: search (#1224)
    * works
    
    * fixed missing matched indices on slices
    
    * fmt
    
    * resolved comments
    
    * resolved comments
    
    * added crossbeam
    
    * Update core/src/service/search_service.rs
    
    Co-authored-by: Steve Russo <steverusso@pm.me>
    
    * made steves ellipses changes to other statements
    
    * fixed single statement in previous commit
    
    * Apply suggestions from code review
    
    Co-authored-by: Steve Russo <steverusso@pm.me>
    
    * cleanup
    
    Co-authored-by: Steve Russo <steverusso@pm.me>

--- core/src/service/search_service.rs
+++ core/src/service/search_service.rs
@@ -1,9 +1,26 @@
+use crate::model::filename::DocumentType;
 use crate::model::repo::RepoSource;
-use crate::{CoreError, RequestContext};
+use crate::{CoreError, RequestContext, UnexpectedError};
+use crossbeam::channel::{self, Receiver, RecvTimeoutError, Sender};
 use fuzzy_matcher::skim::SkimMatcherV2;
 use fuzzy_matcher::FuzzyMatcher;
+use lockbook_models::file_metadata::DecryptedFiles;
+use lockbook_models::tree::FileMetadata;
+use serde::Serialize;
 use std::cmp::Ordering;
+use std::sync::atomic::{self, AtomicBool};
+use std::sync::Arc;
+use std::thread::{self, JoinHandle};
+use std::time::Duration;
+use sublime_fuzzy::{FuzzySearch, Scoring};
 use uuid::Uuid;
+
+const DEBOUNCE_MILLIS: u64 = 500;
+const LOWEST_CONTENT_SCORE_THRESHOLD: i64 = 170;
+
+const MAX_CONTENT_MATCH_LENGTH: usize = 400;
+const IDEAL_CONTENT_MATCH_LENGTH: usize = 150;
+const CONTENT_MATCH_PADDING: usize = 8;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct SearchResultItem {
@@ -49,4 +66,344 @@ impl RequestContext<'_, '_> {
 
         Ok(results)
     }
+
+    pub fn start_search(&mut self) -> Result<StartSearchInfo, CoreError> {
+        let files_not_deleted: DecryptedFiles =
+            self.get_all_not_deleted_metadata(RepoSource::Local)?;
+
+        let mut files_info = Vec::new();
+
+        for file in files_not_deleted.values() {
+            if file.is_document() {
+                let content =
+                    match DocumentType::from_file_name_using_extension(&file.decrypted_name) {
+                        DocumentType::Text => self
+                            .read_document(self.config, RepoSource::Local, file.id)
+                            .map(|bytes| Some(String::from_utf8_lossy(&bytes).to_string()))?,
+                        _ => None,
+                    };
+
+                files_info.push(SearchableFileInfo {
+                    id: file.id,
+                    path: RequestContext::path_by_id_helper(&files_not_deleted, file.id)?,
+                    content,
+                })
+            }
+        }
+
+        let (search_tx, search_rx) = channel::unbounded::<SearchRequest>();
+        let (results_tx, results_rx) = channel::unbounded::<SearchResult>();
+        let join_handle = thread::spawn(move || {
+            if let Err(search_err) = Self::search_loop(&results_tx, search_rx, files_info) {
+                if let Err(err) = results_tx.send(SearchResult::Error(search_err)) {
+                    warn!("Send failed: {:#?}", err);
+                }
+            }
+        });
+
+        Ok(StartSearchInfo { search_tx, results_rx, join_handle })
+    }
+
+    fn recv_with_debounce(
+        search_rx: &Receiver<SearchRequest>, debounce_duration: Duration,
+    ) -> Result<SearchRequest, UnexpectedError> {
+        let mut result = search_rx.recv()?;
+
+        loop {
+            match search_rx.recv_timeout(debounce_duration) {
+                Ok(new_result) => result = new_result,
+                Err(RecvTimeoutError::Timeout) => return Ok(result),
+                Err(err) => return Err(UnexpectedError::from(err)),
+            }
+        }
+    }
+
+    fn search_loop(
+        results_tx: &Sender<SearchResult>, search_rx: Receiver<SearchRequest>,
+        files_info: Vec<SearchableFileInfo>,
+    ) -> Result<(), UnexpectedError> {
+        let files_info = Arc::new(files_info);
+        let mut should_continue = Arc::new(AtomicBool::new(true));
+        let debounce_duration = Duration::from_millis(DEBOUNCE_MILLIS);
+
+        loop {
+            let search = RequestContext::recv_with_debounce(&search_rx, debounce_duration)?;
+            should_continue.store(false, atomic::Ordering::Relaxed);
+
+            match search {
+                SearchRequest::Search { input } => {
+                    should_continue = Arc::new(AtomicBool::new(true));
+
+                    let results_tx = results_tx.clone();
+                    let files_info = files_info.clone();
+                    let should_continue = should_continue.clone();
+                    let input = input.clone();
+
+                    thread::spawn(move || {
+                        if let Err(search_err) =
+                            RequestContext::search(&results_tx, files_info, should_continue, input)
+                        {
+                            if let Err(err) = results_tx.send(SearchResult::Error(search_err)) {
+                                warn!("Send failed: {:#?}", err);
+                            }
+                        }
+                    });
+                }
+                SearchRequest::EndSearch => return Ok(()),
+                SearchRequest::StopCurrentSearch => {}
+            }
+        }
+    }
+
+    fn search(
+        results_tx: &Sender<SearchResult>, files_info: Arc<Vec<SearchableFileInfo>>,
+        should_continue: Arc<AtomicBool>, search: String,
+    ) -> Result<(), UnexpectedError> {
+        let mut no_matches = true;
+
+        RequestContext::search_file_names(
+            results_tx,
+            &should_continue,
+            &files_info,
+            &search,
+            &mut no_matches,
+        )?;
+
+        if should_continue.load(atomic::Ordering::Relaxed) {
+            RequestContext::search_file_contents(
+                results_tx,
+                &should_continue,
+                &files_info,
+                &search,
+                &mut no_matches,
+            )?;
+
+            if no_matches && should_continue.load(atomic::Ordering::Relaxed) {
+                results_tx.send(SearchResult::NoMatch)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    fn search_file_names(
+        results_tx: &Sender<SearchResult>, should_continue: &Arc<AtomicBool>,
+        files_info: &Arc<Vec<SearchableFileInfo>>, search: &str, no_matches: &mut bool,
+    ) -> Result<(), UnexpectedError> {
+        for info in files_info.as_ref() {
+            if !should_continue.load(atomic::Ordering::Relaxed) {
+                return Ok(());
+            }
+
+            if let Some(fuzzy_match) = FuzzySearch::new(search, &info.path)
+                .case_insensitive()
+                .score_with(&Scoring::emphasize_distance())
+                .best_match()
+            {
+                if fuzzy_match.score().is_positive() {
+                    if *no_matches {
+                        *no_matches = false;
+                    }
+
+                    if !should_continue.load(atomic::Ordering::Relaxed) {
+                        return Ok(());
+                    }
+
+                    if results_tx
+                        .send(SearchResult::FileNameMatch {
+                            id: info.id,
+                            path: info.path.clone(),
+                            matched_indices: fuzzy_match.matched_indices().cloned().collect(),
+                            score: fuzzy_match.score() as i64,
+                        })
+                        .is_err()
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn search_file_contents(
+        results_tx: &Sender<SearchResult>, should_continue: &Arc<AtomicBool>,
+        files_info: &Arc<Vec<SearchableFileInfo>>, search: &str, no_matches: &mut bool,
+    ) -> Result<(), UnexpectedError> {
+        for info in files_info.as_ref() {
+            if !should_continue.load(atomic::Ordering::Relaxed) {
+                return Ok(());
+            }
+
+            if let Some(content) = &info.content {
+                let mut content_matches: Vec<ContentMatch> = Vec::new();
+
+                for paragraph in content.split("\n\n") {
+                    if let Some(fuzzy_match) = FuzzySearch::new(search, paragraph)
+                        .case_insensitive()
+                        .score_with(&Scoring::emphasize_distance())
+                        .best_match()
+                    {
+                        let score = fuzzy_match.score() as i64;
+
+                        if score >= LOWEST_CONTENT_SCORE_THRESHOLD {
+                            let (paragraph, matched_indices) =
+                                RequestContext::optimize_searched_text(
+                                    paragraph,
+                                    fuzzy_match.matched_indices().cloned().collect(),
+                                )?;
+
+                            content_matches.push(ContentMatch {
+                                paragraph,
+                                matched_indices,
+                                score,
+                            });
+                        }
+                    }
+                }
+
+                if !content_matches.is_empty() {
+                    if *no_matches {
+                        *no_matches = false;
+                    }
+
+                    if !should_continue.load(atomic::Ordering::Relaxed) {
+                        return Ok(());
+                    }
+
+                    if results_tx
+                        .send(SearchResult::FileContentMatches {
+                            id: info.id,
+                            path: info.path.clone(),
+                            content_matches,
+                        })
+                        .is_err()
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn optimize_searched_text(
+        paragraph: &str, matched_indices: Vec<usize>,
+    ) -> Result<(String, Vec<usize>), UnexpectedError> {
+        if paragraph.len() <= IDEAL_CONTENT_MATCH_LENGTH {
+            return Ok((paragraph.to_string(), matched_indices));
+        }
+
+        let mut index_offset: usize = 0;
+        let mut new_paragraph = paragraph.to_string();
+        let mut new_indices = matched_indices;
+
+        let first_match = new_indices.first().ok_or_else(|| {
+            warn!("A fuzzy match happened but there are no matched indices.");
+            UnexpectedError("No matched indices.".to_string())
+        })?;
+
+        let last_match = new_indices.last().ok_or_else(|| {
+            warn!("A fuzzy match happened but there are no matched indices.");
+            UnexpectedError("No matched indices.".to_string())
+        })?;
+
+        if *last_match < IDEAL_CONTENT_MATCH_LENGTH {
+            new_paragraph = new_paragraph
+                .chars()
+                .take(IDEAL_CONTENT_MATCH_LENGTH + CONTENT_MATCH_PADDING)
+                .chain("...".chars())
+                .collect();
+        } else {
+            if *first_match > CONTENT_MATCH_PADDING as usize {
+                let at_least_take = new_paragraph.len() - first_match + CONTENT_MATCH_PADDING;
+
+                let deleted_chars_len = if at_least_take > IDEAL_CONTENT_MATCH_LENGTH {
+                    first_match - CONTENT_MATCH_PADDING
+                } else {
+                    new_paragraph.len() - IDEAL_CONTENT_MATCH_LENGTH
+                };
+
+                index_offset = deleted_chars_len - 3;
+
+                new_paragraph = "..."
+                    .chars()
+                    .chain(new_paragraph.chars().skip(deleted_chars_len))
+                    .collect();
+            }
+
+            if new_paragraph.len() > IDEAL_CONTENT_MATCH_LENGTH + CONTENT_MATCH_PADDING + 3 {
+                let at_least_take = *last_match - index_offset + CONTENT_MATCH_PADDING;
+
+                let take_chars_len = if at_least_take > IDEAL_CONTENT_MATCH_LENGTH {
+                    at_least_take
+                } else {
+                    IDEAL_CONTENT_MATCH_LENGTH - (last_match - index_offset)
+                };
+
+                new_paragraph = new_paragraph
+                    .chars()
+                    .take(take_chars_len)
+                    .chain("...".chars())
+                    .collect();
+            }
+
+            if new_paragraph.len() > MAX_CONTENT_MATCH_LENGTH {
+                new_paragraph = new_paragraph
+                    .chars()
+                    .take(MAX_CONTENT_MATCH_LENGTH)
+                    .chain("...".chars())
+                    .collect();
+
+                new_indices = new_indices
+                    .into_iter()
+                    .filter(|index| (*index - index_offset) < MAX_CONTENT_MATCH_LENGTH)
+                    .collect()
+            }
+        }
+
+        Ok((
+            new_paragraph,
+            new_indices
+                .iter()
+                .map(|index| *index - index_offset)
+                .collect(),
+        ))
+    }
+}
+
+pub struct StartSearchInfo {
+    pub search_tx: Sender<SearchRequest>,
+    pub results_rx: Receiver<SearchResult>,
+    pub join_handle: JoinHandle<()>,
+}
+
+struct SearchableFileInfo {
+    id: Uuid,
+    path: String,
+    content: Option<String>,
+}
+
+#[derive(Clone)]
+pub enum SearchRequest {
+    Search { input: String },
+    EndSearch,
+    StopCurrentSearch,
+}
+
+#[derive(Debug)]
+pub enum SearchResult {
+    Error(UnexpectedError),
+    FileNameMatch { id: Uuid, path: String, matched_indices: Vec<usize>, score: i64 },
+    FileContentMatches { id: Uuid, path: String, content_matches: Vec<ContentMatch> },
+    NoMatch,
+}
+
+#[derive(Serialize, Debug)]
+pub struct ContentMatch {
+    pub paragraph: String,
+    pub matched_indices: Vec<usize>,
+    pub score: i64,
 }
--- docs/design-ux/search.md
+++ docs/design-ux/search.md
@@ -23,6 +23,59 @@ After this tag based search, we should expand the search criteria and decrypt & 
 that's what the user desires. We should communicate the various steps of the search process to the user as the operation
 proceeds.
 
+## Content Search Text Splitting
+
+We don't apply our matching algorithm to all the text of a file. Instead, we split a file into sections separated by 
+`\n\n` (much like markdown). When a section has a match, we pre-process the text so that we only return the most 
+relevant part of the section to a client. Ideally, we want to produce a 150-character window containing all 
+matched indices.
+
+When processing the text, different situations may arise, and in each one, we produce that window of characters 
+differently.
+1. The section matched on is less than 150 characters, so we return the whole area.
+2. The matched indices are less than 150 characters, so we return the first 150 characters.
+```markdown
+Example
+__Matched section:__
+*Lorem ipsum dolor sit amet, consectetur adipiscing elit.* Vivamus lorem purus, malesuada a dui a, auctor lobortis dolor. 
+Proin ut placerat lectus. Vestibulum massa orci, fermentum id nunc sit amet, scelerisque tempus enim. Duis tristique 
+imperdiet ex. Curabitur sagittis augue vel orci eleifend, sed cursus ante porta. Phasellus pellentesque vulputate ante 
+id fringilla. Suspendisse eu volutpat augue. Mauris massa nisl, venenatis eget viverra non, ultrices vel enim.
+__Returned Window:__
+*Lorem ipsum dolor sit amet, consectetur adipiscing elit.* Vivamus lorem purus, malesuada a dui a, auctor lobortis dolor.
+Proin ut placerat lectus. Vest...
+```
+3. The section matched on is greater than 150 characters, but most matched indices lie at the beginning or end.
+We remove the minimum number of characters, starting from the beginning, to get that 150-character window. 
+Suppose there are not enough characters before the first matched index. In that case, we leave an 8-character buffer 
+before the first matched index and remove as many characters as possible after the last matched index, following the 
+same rules.
+```markdown
+Example
+__Matched section:__
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lorem purus, malesuada a dui a, auctor lobortis dolor.
+Proin ut placerat lectus. Vestibulum massa orci, *fermentum id nunc sit amet, scelerisque tempus enim.* Duis tristique
+imperdiet ex. Curabitur sagittis augue vel orci eleifend, sed cursus ante porta. Phasellus pellentesque vulputate ante
+id fringilla. Suspendisse eu volutpat augue. Mauris massa nisl, venenatis eget viverra non, ultrices vel enim.
+__Returned Window:__
+...a orci, *fermentum id nunc sit amet, scelerisque tempus enim.* Duis tristique imperdiet ex. Curabitur sagittis augue 
+vel orci eleifend, sed cursus a...
+```
+4. If we still have more than 150 characters after step 3, we will only take the first 400 characters and matched 
+indices; if it is less, we will leave it as is.
+```markdown
+Example
+__Matched section:__
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lorem purus, malesuada a dui a, auctor lobortis dolor.
+*Proin ut placerat lectus. Vestibulum massa orci, fermentum id nunc sit amet, scelerisque tempus enim. Duis tristique
+imperdiet ex. Curabitur sagittis augue vel orci eleifend, sed cursus ante porta. Phasellus pellentesque vulputate* ante
+id fringilla. Suspendisse eu volutpat augue. Mauris massa nisl, venenatis eget viverra non, ultrices vel enim.
+__Returned Window:__
+... dolor. *Proin ut placerat lectus. Vestibulum massa orci, fermentum id nunc sit amet, scelerisque tempus enim. Duis tristique
+imperdiet ex. Curabitur sagittis augue vel orci eleifend, sed cursus ante porta. Phasellus pellentesque vulputate* ante
+id...
+```
+
 # Text editor search
 
 Implementation will be upto the text editors in question, will perform similar to "Search this page" in your web
