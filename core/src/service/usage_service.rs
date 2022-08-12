lockbook/lockbook

commit a678a37f61b59ac1c16f8dce44b07ca5625702d6

core: trunc human readable byte numbers to at most 2 decimal places

core/src/service/usage_service.rs
@@ -100,5 +100,9 @@ pub fn bytes_to_human(size: u64) -> String {
     let size_in_unit = size as f64 / unit as f64;
     let dec = f64::trunc(size_in_unit.fract() * 100.0) / 100.0;
 
-    format!("{} {}", size_in_unit.trunc() + dec, abbr)
+    let num = format!("{:.2}", size_in_unit.trunc() + dec)
+        .trim_end_matches(['.', '0'])
+        .to_owned();
+
+    format!("{} {}", num, abbr)
 }

commit 20d8322a6693605f8c3b0a8f8952457176d5bf08

core: fixed usage service human readable bug
I introduced this bug in a678a37f61b59ac1c16f8dce44b07ca5625702d6

core/src/service/usage_service.rs
@@ -101,7 +101,8 @@ pub fn bytes_to_human(size: u64) -> String {
     let dec = f64::trunc(size_in_unit.fract() * 100.0) / 100.0;
 
     let num = format!("{:.2}", size_in_unit.trunc() + dec)
-        .trim_end_matches(['.', '0'])
+        .trim_end_matches(['0'])
+        .trim_end_matches(['.'])
         .to_owned();
 
     format!("{} {}", num, abbr)