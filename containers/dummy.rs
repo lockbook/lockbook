2c0ca7c7ced84b1131acf4b7e9c275dec9df059a
Smail Barkouch <SmailBarkouch@users.noreply.github.com>
2022-08-12 16:29:12 -0400

    infra: new ci (#1249)

--- containers/dummy.rs
+++
@@ -1,5 +0,0 @@
-pub fn main() {}
-
-/// Intentionally leave out pub to generate a warning so you know when this file is being used
-/// Read more here: https://blog.mgattozzi.dev/caching-rust-docker-builds/
-fn you_are_using_dummy_rs_for_docker_cache() {}
