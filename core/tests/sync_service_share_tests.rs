6280cb5ea505f35acc839b05431e6d8bb301b463
Travis Vanderstad <t.vanderstad@gmail.com>
2022-08-14 15:45:00 -0400

    server: sharing bug fix (#1272)

--- core/tests/sync_service_share_tests.rs
+++ core/tests/sync_service_share_tests.rs
@@ -69,6 +69,88 @@ fn new_files() {
 }
 
 #[test]
+fn synced_files() {
+    let cores = vec![test_core_with_account(), test_core_with_account()];
+    let accounts = cores
+        .iter()
+        .map(|core| core.get_account().unwrap())
+        .collect::<Vec<_>>();
+
+    let a = cores[0].create_at_path("a/").unwrap();
+    let axx = cores[0].create_at_path("a/x/x").unwrap();
+    let e = cores[0].create_at_path("e/").unwrap();
+    cores[0].create_at_path("e/x/x").unwrap();
+
+    cores[0].sync(None).unwrap();
+    cores[1].sync(None).unwrap();
+
+    cores[0]
+        .share_file(a.id, &accounts[1].username, ShareMode::Write)
+        .unwrap();
+    cores[0]
+        .share_file(e.id, &accounts[1].username, ShareMode::Write)
+        .unwrap();
+    cores[0].sync(None).unwrap();
+
+    cores[1].sync(None).unwrap();
+    let shares = cores[1].get_pending_shares().unwrap();
+    cores[1].create_link_at_path("link1", shares[0].id).unwrap();
+    cores[1].create_link_at_path("link2", shares[1].id).unwrap();
+
+    cores[0]
+        .write_document(axx.id, b"document content")
+        .unwrap();
+    cores[0].sync(None).unwrap();
+
+    assert_stuff(&cores[0], &cores[1]);
+    assert::all_paths(
+        &cores[1],
+        &["/", "/link1/", "/link1/x/", "/link1/x/x", "/link2/", "/link2/x/", "/link2/x/x"],
+    );
+}
+
+#[test]
+fn synced_files_edit_after_share() {
+    let cores = vec![test_core_with_account(), test_core_with_account()];
+    let accounts = cores
+        .iter()
+        .map(|core| core.get_account().unwrap())
+        .collect::<Vec<_>>();
+
+    let a = cores[0].create_at_path("a/").unwrap();
+    let axx = cores[0].create_at_path("a/x/x").unwrap();
+    let e = cores[0].create_at_path("e/").unwrap();
+    cores[0].create_at_path("e/x/x").unwrap();
+
+    cores[0].sync(None).unwrap();
+    cores[1].sync(None).unwrap();
+
+    cores[0]
+        .share_file(a.id, &accounts[1].username, ShareMode::Write)
+        .unwrap();
+    cores[0]
+        .share_file(e.id, &accounts[1].username, ShareMode::Write)
+        .unwrap();
+    cores[0].sync(None).unwrap();
+
+    cores[1].sync(None).unwrap();
+    let shares = cores[1].get_pending_shares().unwrap();
+    cores[1].create_link_at_path("link1", shares[0].id).unwrap();
+    cores[1].create_link_at_path("link2", shares[1].id).unwrap();
+
+    cores[0]
+        .write_document(axx.id, b"document content")
+        .unwrap();
+    cores[0].sync(None).unwrap();
+
+    assert_stuff(&cores[0], &cores[1]);
+    assert::all_paths(
+        &cores[1],
+        &["/", "/link1/", "/link1/x/", "/link1/x/x", "/link2/", "/link2/x/", "/link2/x/x"],
+    );
+}
+
+#[test]
 fn edited_document() {
     let cores = vec![test_core_with_account(), test_core_with_account()];
     let accounts = cores
