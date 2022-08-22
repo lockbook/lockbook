beaa8605d7d892b243e729e2754b2999ff196089
Steve Russo <steverusso@pm.me>
2022-08-22 12:53:54 -0400

    infra: delete old windows scripts

--- utils/dev/create_windows_app_bundle.bat
+++
@@ -1,4 +0,0 @@
-call create_core_for_windows.bat
-cd ..\clients\windows
-msbuild lockbook.sln /p:Platform=x64;PlatformTarget=x64;Configuration=Release;AppxBundle=Always;AppxBundlePlatforms=x64
-explorer ./lockbook/bin/x64/Release
