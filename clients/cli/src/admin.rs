b5b585fa0fc0d80b8fb1ce99bd72c2595a0b9d8a
Parth <parth@mehrotra.me>
2022-08-24 20:13:56 -0400

    cli, server: admin can now disappear files (#1325)

--- clients/cli/src/admin.rs
+++ clients/cli/src/admin.rs
@@ -1,19 +1,50 @@
 use crate::CliError;
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::Confirm;
-use lockbook_core::{AdminDeleteAccountError, Core, Error};
 use structopt::StructOpt;
+
+use lockbook_core::Uuid;
+use lockbook_core::{AdminDeleteAccountError, AdminDisappearFileError, Core, Error};
 
 #[derive(Debug, PartialEq, Eq, StructOpt)]
 pub enum Admin {
     /// Delete a user
     DeleteAccount { username: String },
+
+    /// Disappear a file
+    ///
+    /// When you delete a file you flip that file's is_deleted flag to false. In a disaster recovery
+    /// scenario, you may want to *disappear* a file so that it never existed. This is useful in a
+    /// scenario where your server let in an invalid file.
+    DisappearFile { id: Uuid },
 }
 
 pub fn admin(core: &Core, admin: Admin) -> Result<(), CliError> {
     match admin {
         Admin::DeleteAccount { username } => delete_account(core, username),
+        Admin::DisappearFile { id } => disappear_file(core, id),
     }
+}
+
+fn disappear_file(core: &Core, id: Uuid) -> Result<(), CliError> {
+    let maybe_confirm = Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt(format!("Are you sure you want to delete '{}'?", id))
+        .interact_opt()?;
+
+    if maybe_confirm.unwrap_or(false) {
+        core.admin_disappear_file(id).map_err(|err| match err {
+            Error::UiError(err) => match err {
+                AdminDisappearFileError::InsufficientPermission => {
+                    CliError::insufficient_permission()
+                }
+                AdminDisappearFileError::FileNotFound => CliError::file_id_not_found(id),
+                AdminDisappearFileError::CouldNotReachServer => CliError::network_issue(),
+                AdminDisappearFileError::ClientUpdateRequired => CliError::update_required(),
+            },
+            Error::Unexpected(msg) => CliError::unexpected(msg),
+        })?;
+    }
+    Ok(())
 }
 
 fn delete_account(core: &Core, username: String) -> Result<(), CliError> {
---
+++ core/tests/admin_tests.rs
@@ -0,0 +1,37 @@
+use test_utils::*;
+
+#[test]
+#[ignore]
+fn admin_disappear_test() {
+    let admin_core = test_core();
+    admin_core.create_account("admin1", &url()).unwrap();
+
+    let customer_core = test_core_with_account();
+    let test1 = customer_core.create_at_path("test1.md").unwrap();
+    let test2 = customer_core.create_at_path("test2.md").unwrap();
+    customer_core.sync(None).unwrap();
+
+    let account_string = customer_core.export_account().unwrap();
+    let customer_core_2 = test_core();
+    customer_core_2.import_account(&account_string).unwrap();
+    assert_eq!(customer_core_2.calculate_work().unwrap().work_units.len(), 3);
+
+    admin_core.admin_disappear_file(test2.id).unwrap();
+
+    let account_string = customer_core.export_account().unwrap();
+    let customer_core_2 = test_core();
+    customer_core_2.import_account(&account_string).unwrap();
+    assert_eq!(customer_core_2.calculate_work().unwrap().work_units.len(), 2);
+    customer_core_2.sync(None).unwrap();
+
+    assert!(customer_core_2
+        .list_metadatas()
+        .unwrap()
+        .iter()
+        .any(|f| f.id == test1.id));
+    assert!(!customer_core_2
+        .list_metadatas()
+        .unwrap()
+        .iter()
+        .any(|f| f.id == test2.id));
+}
