bbc53c81c60840c54141211f96ff0dea5202deef
Steve Russo <steverusso@pm.me>
2022-08-27 17:29:44 -0400

    egui: polish settings tabs

--- clients/egui/src/account/modals/settings/mod.rs
+++ clients/egui/src/account/modals/settings/mod.rs
@@ -9,10 +9,13 @@ use eframe::egui;
 use egui_extras::{Size, StripBuilder};
 
 use crate::settings::Settings;
+use crate::theme::Icon;
+use crate::widgets::separator;
 
 use self::account_tab::*;
 use self::usage_tab::*;
 
+#[derive(PartialEq)]
 enum SettingsTab {
     Account,
     Usage,
@@ -59,21 +62,78 @@ impl SettingsModal {
         }))
     }
 
-    fn draw_tab_labels(&mut self, ui: &mut egui::Ui) {
-        ui.vertical(|ui| {
-            if ui.button("Account").clicked() {
-                self.active_tab = SettingsTab::Account;
-            }
-            if ui.button("Usage").clicked() {
-                self.active_tab = SettingsTab::Usage;
-            }
-            if ui.button("Appearance").clicked() {
-                self.active_tab = SettingsTab::Appearance;
-            }
-            if ui.button("General").clicked() {
-                self.active_tab = SettingsTab::General;
-            }
-        });
+    fn show_tab_labels(&mut self, ui: &mut egui::Ui) {
+        ui.add_space(1.0);
+        egui::Frame::none()
+            .fill(ui.visuals().faint_bg_color)
+            .rounding(egui::Rounding {
+                sw: ui.style().visuals.window_rounding.sw,
+                ..Default::default()
+            })
+            .show(ui, |ui| {
+                ui.set_min_height(ui.available_size_before_wrap().y);
+                ui.spacing_mut().item_spacing = egui::vec2(0.0, 0.0);
+
+                self.tab_label(ui, SettingsTab::Account, Icon::ACCOUNT, "Account");
+                self.tab_label(ui, SettingsTab::Usage, Icon::SAVE, "Usage");
+                self.tab_label(ui, SettingsTab::Appearance, Icon::SPARKLE, "Appearance");
+                self.tab_label(ui, SettingsTab::General, Icon::SETTINGS, "General");
+            });
+    }
+
+    fn tab_label(&mut self, ui: &mut egui::Ui, tab: SettingsTab, icon: Icon, text: &str) {
+        const PADDING: f32 = 15.0;
+        const SPACING: f32 = 10.0;
+
+        let text_height = ui.text_style_height(&egui::TextStyle::Body);
+        let height = 24.0 + text_height + SPACING + PADDING * 2.0;
+
+        let response = ui
+            .scope(|ui| {
+                ui.spacing_mut().item_spacing = egui::vec2(0.0, 0.0);
+                ui.set_height(height);
+
+                StripBuilder::new(ui)
+                    .size(Size::remainder())
+                    .size(Size::exact(4.0))
+                    .horizontal(|mut strip| {
+                        strip.cell(|ui| {
+                            ui.vertical_centered(|ui| {
+                                ui.add_space(PADDING);
+                                icon.size(24.0).show(ui);
+                                ui.add_space(SPACING);
+                                ui.label(text);
+                                ui.add_space(PADDING);
+                            });
+                        });
+                        strip.cell(|ui| {
+                            let available_space = ui.available_size_before_wrap();
+                            let size = egui::vec2(4.0, available_space.y);
+
+                            let (rect, response) = ui.allocate_at_least(size, egui::Sense::hover());
+
+                            if ui.is_rect_visible(response.rect) {
+                                let color = if self.active_tab == tab {
+                                    ui.visuals().widgets.active.bg_fill
+                                } else {
+                                    ui.visuals().widgets.noninteractive.bg_stroke.color
+                                };
+
+                                let stroke = egui::Stroke::new(4.0, color);
+
+                                ui.painter().vline(rect.center().x, rect.y_range(), stroke);
+                            }
+                        });
+                    });
+            })
+            .response;
+
+        let response = response.interact(egui::Sense::click());
+        if response.clicked() {
+            self.active_tab = tab;
+        }
+
+        separator(ui);
     }
 }
 
@@ -90,14 +150,14 @@ impl super::Modal for SettingsModal {
     fn show(&mut self, ui: &mut egui::Ui) -> Self::Response {
         let mut resp = None;
 
-        ui.set_max_height(ui.available_size().y - 300.0);
+        ui.set_max_height(ui.available_size().y - 400.0);
         ui.set_width(520.0);
 
         StripBuilder::new(ui)
-            .size(Size::exact(100.0))
+            .size(Size::exact(115.0))
             .size(Size::remainder())
             .horizontal(|mut strip| {
-                strip.cell(|ui| self.draw_tab_labels(ui));
+                strip.cell(|ui| self.show_tab_labels(ui));
                 strip.cell(|ui| {
                     ui.add_space(12.0);
                     match &self.active_tab {
