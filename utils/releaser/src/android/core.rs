25f48e293daafb98e243bb5c5fdb4c9b4c29070a
Smail Barkouch <SmailBarkouch@users.noreply.github.com>
2022-08-28 16:11:27 -0400

    infra: android release automation (#1334)

---
+++ utils/releaser/src/android/core.rs
@@ -0,0 +1,71 @@
+use crate::utils::CommandRunner;
+use std::fs;
+use std::process::Command;
+
+const MIN_NDK_VERSION: u32 = 22;
+const NDK_LIB_NAME: &str = "liblockbook_core.so";
+const JNI_LIB: &str = "clients/android/core/src/main/jniLibs";
+
+const ARCH64: &str = "aarch64-linux-android";
+const ARMV7: &str = "armv7-linux-androideabi";
+const I686: &str = "i686-linux-android";
+const X86_64: &str = "x86_64-linux-android";
+
+const SHORT_ARCH64: &str = "arm64-v8a";
+const SHORT_ARMV7: &str = "armeabi-v7a";
+const SHORT_I686: &str = "x86";
+const SHORT_X86_64: &str = "x86_64";
+
+pub fn build_libs() {
+    build_core_for_android_arch(ARCH64);
+    build_core_for_android_arch(ARMV7);
+    build_core_for_android_arch(I686);
+    build_core_for_android_arch(X86_64);
+
+    let android_arch64 = format!("{JNI_LIB}/{SHORT_ARCH64}");
+    let android_armv7 = format!("{JNI_LIB}/{SHORT_ARMV7}");
+    let android_i686 = format!("{JNI_LIB}/{SHORT_I686}");
+    let android_x86_64 = format!("{JNI_LIB}/{SHORT_X86_64}");
+
+    fs::create_dir_all(&android_arch64).unwrap();
+    fs::create_dir_all(&android_armv7).unwrap();
+    fs::create_dir_all(&android_i686).unwrap();
+    fs::create_dir_all(&android_x86_64).unwrap();
+
+    fs::copy(
+        format!("target/{ARCH64}/release/{NDK_LIB_NAME}"),
+        format!("{android_arch64}/{NDK_LIB_NAME}"),
+    )
+    .unwrap();
+    fs::copy(
+        format!("target/{ARMV7}/release/{NDK_LIB_NAME}"),
+        format!("{android_armv7}/{NDK_LIB_NAME}"),
+    )
+    .unwrap();
+    fs::copy(
+        format!("target/{I686}/release/{NDK_LIB_NAME}"),
+        format!("{android_i686}/{NDK_LIB_NAME}"),
+    )
+    .unwrap();
+    fs::copy(
+        format!("target/{X86_64}/release/{NDK_LIB_NAME}"),
+        format!("{android_x86_64}/{NDK_LIB_NAME}"),
+    )
+    .unwrap();
+}
+
+fn build_core_for_android_arch(platform: &str) {
+    Command::new("cargo")
+        .args([
+            "ndk",
+            "--target",
+            platform,
+            "--platform",
+            &MIN_NDK_VERSION.to_string(),
+            "--",
+            "build",
+            "--release",
+        ])
+        .current_dir("core")
+        .assert_success();
+}
--- utils/releaser/src/main.rs
+++ utils/releaser/src/main.rs
@@ -1,20 +1,23 @@
+mod android;
 mod apple;
 mod secrets;
 mod server;
 mod utils;
 
-use crate::secrets::{AppStore, Github};
+use crate::secrets::{AppStore, Github, PlayStore};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
 enum Releaser {
     DeployServer,
     ReleaseApple,
+    ReleaseAndroid,
 }
 
 fn main() {
     match Releaser::from_args() {
         Releaser::DeployServer => server::deploy_server(),
         Releaser::ReleaseApple => apple::release_apple(&Github::env(), &AppStore::env()),
+        Releaser::ReleaseAndroid => android::release_android(&Github::env(), &PlayStore::env()),
     }
 }
