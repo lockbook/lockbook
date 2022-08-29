44dd28ff782ba8159dc2a0da8092d2e34772017a
Steve Russo <steverusso@pm.me>
2022-08-29 13:57:17 -0700

    infra: add windows to releaser (#1338)

---
+++ utils/releaser/src/windows/egui.rs
@@ -0,0 +1,49 @@
+use std::fs::File;
+use std::path::Path;
+use std::process::Command;
+
+use gh_release::ReleaseClient;
+
+use crate::utils::{core_version, lb_repo, CommandRunner};
+use crate::Github;
+
+pub fn release_installers(gh: &Github) {
+    build_x86(gh);
+}
+
+fn build_x86(gh: &Github) {
+    Command::new("cargo")
+        .args(["build", "-p", "lockbook-egui", "--release", "--target=x86_64-pc-windows-msvc"])
+        .assert_success();
+
+    Command::new("cargo")
+        .env("LB_TARGET", "x86_64-pc-windows-msvc")
+        .args(["build", "-p", "winstaller", "--release", "--target=x86_64-pc-windows-msvc"])
+        .assert_success();
+
+    upload(
+        gh,
+        "lockbook-windows-setup-x86_64.exe",
+        "target/x86_64-pc-windows-msvc/release/winstaller.exe",
+    );
+}
+
+fn upload<P: AsRef<Path>>(gh: &Github, name: &str, fpath: P) {
+    let client = ReleaseClient::new(gh.0.clone()).unwrap();
+
+    let release = client
+        .get_release_by_tag_name(&lb_repo(), &core_version())
+        .unwrap();
+
+    let file = File::open(fpath).unwrap();
+    client
+        .upload_release_asset(
+            &lb_repo(),
+            release.id as u64,
+            name,
+            "application/vnd.microsoft.portable-executable",
+            file,
+            None,
+        )
+        .unwrap();
+}
