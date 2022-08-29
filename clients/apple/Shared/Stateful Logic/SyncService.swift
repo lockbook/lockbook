ab60833e79e7e1608a9b7f5637ada11969304989
Parth <parth@mehrotra.me>
2022-08-29 16:39:31 -0400

    apple: fix onboarding accountnonexistent errors

--- clients/apple/Shared/Stateful Logic/SyncService.swift
+++ clients/apple/Shared/Stateful Logic/SyncService.swift
@@ -20,7 +20,7 @@ class SyncService: ObservableObject {
         syncTimer?.invalidate()
         syncTimer = Timer.scheduledTimer(timeInterval: 30*60, target: self, selector: #selector(syncTimerTick), userInfo: nil, repeats: true)
     }
-
+    
     @objc func syncTimerTick() {
         sync()
     }
@@ -40,36 +40,40 @@ class SyncService: ObservableObject {
         if syncing {
             return
         }
+        if DI.accounts.account == nil {
+            print("tried to sync before having an account, ignoring")
+            return
+        }
         
         syncing = true
         
         DispatchQueue.global(qos: .userInteractive).async {
-            let result = self.core.syncAll()
-            
-            DispatchQueue.main.async {
-                self.syncing = false
-
-                switch result {
-                case .success(_):
-                    self.offline = false
-                    self.postSyncSteps()
-                case .failure(let error):
-                    switch error.kind {
-                    case .UiError(let uiError):
-                        switch uiError {
-                        case .CouldNotReachServer:
-                            self.offline = true
-                        case .ClientUpdateRequired:
-                            self.upgrade = true
-                        case .Retry:
-                            // TODO
-                            DI.errors.handleError(ErrorWithTitle(title: "Retry", message: "SyncService wants retry"))
+                let result = self.core.syncAll()
+                
+                DispatchQueue.main.async {
+                    self.syncing = false
+                    
+                    switch result {
+                    case .success(_):
+                        self.offline = false
+                        self.postSyncSteps()
+                    case .failure(let error):
+                        switch error.kind {
+                        case .UiError(let uiError):
+                            switch uiError {
+                            case .CouldNotReachServer:
+                                self.offline = true
+                            case .ClientUpdateRequired:
+                                self.upgrade = true
+                            case .Retry:
+                                // TODO
+                                DI.errors.handleError(ErrorWithTitle(title: "Retry", message: "SyncService wants retry"))
+                            }
+                        default:
+                            DI.errors.handleError(error)
                         }
-                    default:
-                        DI.errors.handleError(error)
                     }
                 }
-            }
         }
     }
 }
