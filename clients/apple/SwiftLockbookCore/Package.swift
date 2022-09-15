cf8c4aa282587742f7ec08bd834a5c7cc67e537e
Parth <parth@mehrotra.me>
2022-09-15 17:58:07 -0400

    is this fine?

--- clients/apple/SwiftLockbookCore/Package.swift
+++ clients/apple/SwiftLockbookCore/Package.swift
@@ -21,8 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftLockbookCore",
-            dependencies: ["CLockbookCore"],
-            linkerSettings: [LinkerSetting.unsafeFlags(["-L", "../CLockbookCore/Sources/CLockbookCore/lib"])]),
+            dependencies: ["CLockbookCore"]),
         .testTarget(
             name: "SwiftLockbookCoreTests",
             dependencies: ["SwiftLockbookCore"]),
