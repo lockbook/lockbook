49aa67acfe3854cc649ab08ee2859ce3046b01c4
Steve Russo <steverusso@pm.me>
2022-09-17 06:23:12 -0700

    egui: ui tidy and refactors (#1376)

--- clients/egui/src/widgets/separator.rs
+++ clients/egui/src/widgets/separator.rs
@@ -4,15 +4,15 @@ pub fn separator(ui: &mut egui::Ui) {
     let is_horizontal_line = !ui.layout().main_dir().is_horizontal();
 
     let available_space = ui.available_size_before_wrap();
-    let spacing = 1.0;
+    let ln_size = 1.0;
 
-    let size = if is_horizontal_line {
-        egui::vec2(available_space.x, spacing)
+    let desired_size = if is_horizontal_line {
+        egui::vec2(available_space.x, ln_size)
     } else {
-        egui::vec2(spacing, available_space.y)
+        egui::vec2(ln_size, available_space.y)
     };
 
-    let (rect, response) = ui.allocate_at_least(size, egui::Sense::hover());
+    let (rect, response) = ui.allocate_at_least(desired_size, egui::Sense::hover());
 
     if ui.is_rect_visible(response.rect) {
         let stroke = ui.visuals().widgets.noninteractive.bg_stroke;
