cae71d715b0ea3d87c665e39ac95f92108ef71a2
Travis Vanderstad <t.vanderstad@gmail.com>
2022-09-19 11:21:00 -0400

    regenerate owners index

--- server/server/src/schema.rs
+++ server/server/src/schema.rs
@@ -32,6 +32,7 @@ pub mod v2 {
     use lockbook_shared::file_metadata::Owner;
     use lockbook_shared::server_file::ServerFile;
     use std::collections::HashSet;
+    use tracing::info;
     use uuid::Uuid;
 
     hmdb::schema! {
@@ -53,35 +54,39 @@ pub mod v2 {
     ) {
         // copy existing tables
         for (k, v) in source.usernames.get_all() {
-            destination.usernames.insert(k.clone(), v.clone());
+            destination.usernames.insert(k.clone(), *v);
         }
         for (k, v) in source.accounts.get_all() {
-            destination.accounts.insert(k.clone(), v.clone());
-        }
-        for (k, v) in source.owned_files.get_all() {
-            destination.owned_files.insert(k.clone(), v.clone());
+            destination.accounts.insert(*k, v.clone());
         }
         for (k, v) in source.metas.get_all() {
-            destination.metas.insert(k.clone(), v.clone());
+            destination.metas.insert(*k, v.clone());
         }
         for (k, v) in source.sizes.get_all() {
-            destination.sizes.insert(k.clone(), v.clone());
+            if source.metas.get(k).is_some() {
+                destination.sizes.insert(*k, *v);
+            }
         }
         for (k, v) in source.google_play_ids.get_all() {
-            destination.google_play_ids.insert(k.clone(), v.clone());
+            destination.google_play_ids.insert(k.clone(), *v);
         }
         for (k, v) in source.stripe_ids.get_all() {
-            destination.stripe_ids.insert(k.clone(), v.clone());
+            destination.stripe_ids.insert(k.clone(), *v);
         }
 
-        // populate new indexes
+        // populate new indexes (and regenerate owned files, which is corrupt)
+        let mut owned_files = HashMap::new();
         let mut shared_files = HashMap::new();
         let mut file_children = HashMap::new();
-        for owner in source.owned_files.keys() {
+        for owner in source.accounts.keys() {
+            owned_files.insert(*owner, HashSet::new());
             shared_files.insert(*owner, HashSet::new());
         }
         for (id, file) in source.metas.get_all() {
             file_children.insert(*id, HashSet::new());
+            if let Some(owned_files) = owned_files.get_mut(&file.owner()) {
+                owned_files.insert(*id);
+            }
             for user_access_key in file.user_access_keys() {
                 if user_access_key.encrypted_for != user_access_key.encrypted_by {
                     if let Some(shared_files) =
@@ -97,11 +102,15 @@ pub mod v2 {
                 file_children.insert(*id);
             }
         }
-        println!("migration: indexed shares of {} users", shared_files.len());
+        info!("migration: indexed owned files of {} users", owned_files.len());
+        for (k, v) in owned_files {
+            destination.owned_files.insert(k, v);
+        }
+        info!("migration: indexed shared files of {} users", shared_files.len());
         for (k, v) in shared_files {
             destination.shared_files.insert(k, v);
         }
-        println!("migration: indexed children of {} files", file_children.len());
+        info!("migration: indexed children of {} files", file_children.len());
         for (k, v) in file_children {
             destination.file_children.insert(k, v);
         }
