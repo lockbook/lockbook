83c612bdf5a048c1fd1d9e3ca1983cd73dac6806
Travis Vanderstad <t.vanderstad@gmail.com>
2022-09-27 20:08:11 -0400

    core: validate tests

---
+++ core/tests/validate_tests.rs
@@ -0,0 +1,51 @@
+use hmdb::transaction::{Transaction, TransactionTable};
+use lockbook_core::repo::schema::helper_log::{base_metadata, local_metadata};
+use lockbook_shared::account::Account;
+use lockbook_shared::file::File;
+use lockbook_shared::file_metadata::{FileType, Owner};
+use lockbook_shared::signed_file::SignedFile;
+use lockbook_shared::tree_like::Stagable;
+use lockbook_shared::{SharedError, SharedResult, ValidationFailure};
+use test_utils::*;
+use uuid::Uuid;
+
+type BaseMetadata<'a> = TransactionTable<'a, Uuid, SignedFile, base_metadata>;
+type LocalMetadata<'a> = TransactionTable<'a, Uuid, SignedFile, local_metadata>;
+
+fn run(
+    f: fn(Account, Owner, File, &mut BaseMetadata, &mut LocalMetadata) -> SharedResult<()>,
+) -> SharedResult<()> {
+    let core = test_core_with_account();
+    let account = core.get_account().unwrap();
+    let owner = Owner(account.public_key());
+    let root = core.get_root().unwrap();
+    core.db
+        .transaction::<_, SharedResult<_>>(|tx| {
+            f(account, owner, root, &mut tx.base_metadata, &mut tx.local_metadata)
+        })
+        .unwrap()
+}
+
+#[test]
+fn create_two_files_with_same_path() {
+    let result = run(|account, owner, root, base_metadata, local_metadata| {
+        let mut tree = base_metadata
+            .stage(local_metadata)
+            .stage(Vec::new())
+            .to_lazy();
+        let (staged_tree, _) = tree
+            .stage_create(&root.id, "document", FileType::Document, &account)
+            .unwrap();
+        tree = staged_tree.promote();
+        let (staged_tree, _) = tree
+            .stage_create(&root.id, "document", FileType::Document, &account)
+            .unwrap();
+        tree = staged_tree.promote();
+        tree.validate(owner)?;
+        Ok(())
+    });
+    assert_matches!(
+        result,
+        Err(SharedError::ValidationFailure(ValidationFailure::PathConflict(_)))
+    );
+}
