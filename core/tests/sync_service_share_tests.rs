1c0beb27b0e9a10a040c82dddf70305bbc92a906
Travis Vanderstad <t.vanderstad@gmail.com>
2022-09-28 00:38:07 -0400

    add failing tests

--- core/tests/sync_service_share_tests.rs
+++ core/tests/sync_service_share_tests.rs
@@ -327,3 +327,129 @@ fn move_existing_edited_document_into_shared_folder() {
     assert::all_paths(&cores[0], &["/", "/folder/", "/folder/document"]);
     assert::all_document_contents(&cores[0], &[("/folder/document", b"document content")]);
 }
+
+#[test]
+fn create_link_in_unshared_folder() {
+    let cores = vec![test_core_with_account(), test_core_with_account()];
+    let accounts = cores
+        .iter()
+        .map(|core| core.get_account().unwrap())
+        .collect::<Vec<_>>();
+
+    let folder = cores[0].create_at_path("folder/").unwrap();
+    cores[0]
+        .share_file(folder.id, &accounts[1].username, ShareMode::Write)
+        .unwrap();
+
+    cores[0].sync(None).unwrap();
+    cores[1].sync(None).unwrap();
+
+    let shares = cores[1].get_pending_shares().unwrap();
+    cores[1].delete_pending_share(shares[0].id).unwrap();
+    let document = cores[1].create_at_path("document").unwrap();
+    cores[1]
+        .share_file(document.id, &accounts[0].username, ShareMode::Write)
+        .unwrap();
+
+    cores[1].sync(None).unwrap();
+    cores[0].sync(None).unwrap();
+
+    let shares = cores[0].get_pending_shares().unwrap();
+    let _link = cores[0]
+        .create_link_at_path("folder/link", shares[0].id)
+        .unwrap();
+
+    cores[0].sync(None).unwrap();
+    cores[1].sync(None).unwrap();
+
+    assert_stuff(&cores[0], &cores[1]);
+    assert::all_paths(&cores[0], &["/", "/folder/", "/folder/link"]);
+    assert::all_paths(&cores[1], &["/", "/document"]); // NOTE: fails here; document has vanished because it's path s resolved using a shared link
+}
+
+#[test]
+fn move_file_out_of_shared_folder_and_delete() {
+    let cores = vec![test_core_with_account(), test_core_with_account()];
+    let accounts = cores
+        .iter()
+        .map(|core| core.get_account().unwrap())
+        .collect::<Vec<_>>();
+    let roots = cores
+        .iter()
+        .map(|core| core.get_root().unwrap())
+        .collect::<Vec<_>>();
+
+    let folder = cores[0].create_at_path("folder/").unwrap();
+    let document = cores[0].create_at_path("folder/document").unwrap();
+    cores[0]
+        .share_file(folder.id, &accounts[1].username, ShareMode::Write)
+        .unwrap();
+
+    cores[0].sync(None).unwrap();
+    cores[1].sync(None).unwrap();
+
+    let shares = cores[1].get_pending_shares().unwrap();
+    let _link = cores[1].create_link_at_path("link/", shares[0].id).unwrap();
+
+    cores[1].sync(None).unwrap();
+    cores[0].sync(None).unwrap();
+
+    cores[0].move_file(document.id, roots[0].id).unwrap();
+
+    cores[0].sync(None).unwrap();
+
+    cores[0].delete_file(document.id).unwrap();
+    cores[0].create_at_path("/folder/document").unwrap(); // this will conflict with the now-moved document whose move isn't synced to the other client
+
+    cores[0].sync(None).unwrap();
+    cores[1].sync(None).unwrap();
+
+    assert_stuff(&cores[0], &cores[1]);
+    assert::all_paths(&cores[1], &["/", "/link/"]); // NOTE: fails here; deletion isn't synced because file is no longer in user's tree
+}
+
+#[test]
+fn move_file_out_of_shared_folder_and_create_path_conflict() {
+    let cores = vec![test_core_with_account(), test_core_with_account()];
+    let accounts = cores
+        .iter()
+        .map(|core| core.get_account().unwrap())
+        .collect::<Vec<_>>();
+    let roots = cores
+        .iter()
+        .map(|core| core.get_root().unwrap())
+        .collect::<Vec<_>>();
+
+    let folder = cores[0].create_at_path("folder/").unwrap();
+    let document = cores[0].create_at_path("folder/document").unwrap();
+    cores[0]
+        .share_file(folder.id, &accounts[1].username, ShareMode::Write)
+        .unwrap();
+
+    cores[0].sync(None).unwrap();
+    cores[1].sync(None).unwrap();
+
+    let shares = cores[1].get_pending_shares().unwrap();
+    let _link = cores[1].create_link_at_path("link/", shares[0].id).unwrap();
+
+    cores[1].sync(None).unwrap();
+    cores[0].sync(None).unwrap();
+
+    cores[0].move_file(document.id, roots[0].id).unwrap();
+
+    cores[0].sync(None).unwrap();
+
+    cores[0].delete_file(document.id).unwrap();
+    cores[0].create_at_path("/folder/document").unwrap(); // this will conflict with the now-moved document whose move isn't synced to the other client
+
+    cores[0].sync(None).unwrap();
+    cores[1].sync(None).unwrap();
+
+    cores[1]
+        .write_document(document.id, b"document content")
+        .unwrap(); // NOTE: fails with Unexpected("PathTaken")
+
+    cores[1].sync(None).unwrap();
+
+    assert_stuff(&cores[0], &cores[1]); // NOTE: if the test did make it here, validation would fail with a path conflict
+}
