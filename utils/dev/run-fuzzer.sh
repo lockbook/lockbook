d4712cf69e58bcdfc32c6dba3297f9d0af0b21e3
Parth <parth@mehrotra.me>
2022-09-29 18:02:47 -0400

    fuzzer: in process server (#1407)

--- utils/dev/run-fuzzer.sh
+++ utils/dev/run-fuzzer.sh
@@ -9,4 +9,4 @@ projRoot=$(git rev-parse --show-toplevel)
 cd "$projRoot"/core
 . ../containers/local.env
 
-cargo test --release exhaustive_test_sync -- --nocapture --ignored
+cargo test --test exhaustive_sync_check --release -- --nocapture --ignored
