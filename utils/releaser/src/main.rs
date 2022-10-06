aac014e08425ca2b0be332e6f06b600e44a1d90d
Smail Barkouch <smailbarkouch1@gmail.com>
2022-10-06 12:59:39 -0400

    added platform check

--- utils/releaser/src/main.rs
+++ utils/releaser/src/main.rs
@@ -10,11 +10,14 @@ mod windows;
 use crate::secrets::*;
 use crate::utils::root;
 
+use enum_iterator::{all, Sequence};
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(PartialEq, StructOpt, Sequence)]
 enum Releaser {
+    All,
     DeployServer,
+    #[cfg(target_os = "macos")]
     ReleaseApple,
     ReleaseAndroid,
     ReleaseWindows,
@@ -25,13 +28,26 @@ enum Releaser {
 fn main() {
     // Fail fast if we're invoking from the wrong location
     root();
+    from_args(Releaser::from_args());
+}
 
-    match Releaser::from_args() {
+fn from_args(releaser: Releaser) {
+    match releaser {
         Releaser::DeployServer => server::deploy_server(),
+        #[cfg(target_os = "macos")]
         Releaser::ReleaseApple => apple::release_apple(&Github::env(), &AppStore::env()),
         Releaser::ReleaseAndroid => android::release_android(&Github::env(), &PlayStore::env()),
         Releaser::ReleaseWindows => windows::release(&Github::env()),
         Releaser::ReleasePublicSite => public_site::release(),
         Releaser::ReleaseLinux => linux::release_linux(),
+        Releaser::All => {
+            for releaser in all::<Releaser>()
+                .collect::<Vec<Releaser>>()
+                .into_iter()
+                .filter(|release| *release != Releaser::All)
+            {
+                from_args(releaser);
+            }
+        }
     }
 }
