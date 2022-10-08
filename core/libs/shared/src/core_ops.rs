4f840e0e3a1659133842dff7b8d3d9a41994e5de
Travis Vanderstad <t.vanderstad@gmail.com>
2022-10-08 15:19:47 -0400

    resolve paths for links instead of linked files; resolve links to deleted files

--- core/libs/shared/src/core_ops.rs
+++ core/libs/shared/src/core_ops.rs
@@ -559,6 +559,26 @@ where
         }
         Ok(result)
     }
+
+    pub fn delete_links_to_deleted_files(
+        self, account: &Account,
+    ) -> SharedResult<TreeWithOps<Base, Local>> {
+        let mut result = self.stage(Vec::new());
+
+        for id in result.owned_ids() {
+            if result.calculate_deleted(&id)? {
+                continue;
+            }
+            let file = result.find(&id)?;
+            if let FileType::Link { target } = file.file_type() {
+                if result.calculate_deleted(&target)? {
+                    // delete link to deleted file file
+                    result = result.stage_delete(&id, account)?.promote();
+                }
+            }
+        }
+        Ok(result)
+    }
 }
 
 impl<Base, Remote, Local> LazyStage2<Base, Remote, Local>
@@ -870,6 +890,7 @@ where
         result = result.deduplicate_links(account)?.promote();
         result = result.resolve_shared_links(account)?.promote();
         result = result.resolve_owned_links(account)?.promote();
+        result = result.delete_links_to_deleted_files(account)?.promote();
 
         Ok((result, merge_document_changes))
     }
