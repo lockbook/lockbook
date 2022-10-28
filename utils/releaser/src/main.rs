a86748f7bc01835b859a19ec63d1c17bbce4d55e
Smail Barkouch <smailbarkouch1@gmail.com>
2022-10-28 13:36:51 -0400

    infra: add linux cli to releaser (#1427)

--- utils/releaser/src/main.rs
+++ utils/releaser/src/main.rs
@@ -1,5 +1,6 @@
 mod android;
 mod apple;
+mod linux;
 mod public_site;
 mod secrets;
 mod server;
@@ -11,24 +12,50 @@ use crate::utils::root;
 
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
+#[derive(PartialEq, StructOpt)]
 enum Releaser {
+    All,
     DeployServer,
     ReleaseApple,
     ReleaseAndroid,
     ReleaseWindows,
     ReleasePublicSite,
+    ReleaseLinux,
 }
 
 fn main() {
     // Fail fast if we're invoking from the wrong location
     root();
 
-    match Releaser::from_args() {
+    from_args(Releaser::from_args());
+}
+
+fn from_args(releaser: Releaser) {
+    match releaser {
         Releaser::DeployServer => server::deploy_server(),
         Releaser::ReleaseApple => apple::release_apple(&Github::env(), &AppStore::env()),
         Releaser::ReleaseAndroid => android::release_android(&Github::env(), &PlayStore::env()),
         Releaser::ReleaseWindows => windows::release(&Github::env()),
         Releaser::ReleasePublicSite => public_site::release(),
+        Releaser::ReleaseLinux => linux::release_linux(),
+        Releaser::All => {
+            let releases = if cfg!(target_os = "macos") {
+                vec![Releaser::ReleaseApple]
+            } else if cfg!(target_os = "linux") {
+                vec![
+                    Releaser::DeployServer,
+                    Releaser::ReleaseLinux,
+                    Releaser::ReleaseAndroid,
+                    Releaser::ReleaseWindows,
+                    Releaser::ReleasePublicSite,
+                ]
+            } else {
+                vec![Releaser::ReleaseWindows]
+            };
+
+            for releaser in releases {
+                from_args(releaser);
+            }
+        }
     }
 }
