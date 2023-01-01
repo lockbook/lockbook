f98473297cc8af91099c65d833c78d63526088ab
ad-tra <aew31gcmuy@gmail.com>
2023-01-01 14:21:10 -0500

    modified windows download link

--- public_site/index.js
+++ public_site/index.js
@@ -1,6 +1,6 @@
 let OSName = "Unknown";
 let OSDownloadMap = {
-    "Windows":"https://github.com/lockbook/lockbook/blob/master/docs/guides/install/windows.md",
+    "Windows":"https://github.com/lockbook/lockbook/releases/download/0.5.6/lockbook-windows-setup-x86_64.exe",
     "Mac/iOS":"https://apps.apple.com/us/app/lockbook/id1526775001",
     "Linux": "https://github.com/lockbook/lockbook/blob/master/docs/guides/install/linux.md",
     "Android": "https://play.google.com/store/apps/details?id=app.lockbook"
