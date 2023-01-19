c19abb6ae2659e048b876604799c3e5faa2321f9
Parth <parth@mehrotra.me>
2023-01-19 11:37:05 -0500

    added test

--- clients/admin/src/indexes.rs
+++ clients/admin/src/indexes.rs
@@ -8,7 +8,7 @@ pub enum CliIndex {
     OwnedFiles,
 }
 
-fn rebuild(core: &Core, index: CliIndex) -> Res<()> {
+pub fn rebuild(core: &Core, index: CliIndex) -> Res<()> {
     match index {
         CliIndex::OwnedFiles => core.admin_rebuild_index(ServerIndex::OwnedFiles)?,
     }
