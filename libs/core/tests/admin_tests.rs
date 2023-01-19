b40d3fc0b79ce8301ced508a954b76e0881ad7ec
“Travis <t.vanderstad@gmail.com>
2023-01-19 17:19:59 -0500

    add test

--- libs/core/tests/admin_tests.rs
+++ libs/core/tests/admin_tests.rs
@@ -40,7 +40,7 @@ fn admin_disappear_test() {
 
 #[test]
 #[ignore]
-fn admin_rebuild_index_test() {
+fn admin_rebuild_owned_files_index_test() {
     let admin_core = test_core();
     admin_core.create_account("admin1", &url(), false).unwrap();
 
@@ -78,4 +78,46 @@ fn admin_rebuild_index_test() {
 
     let cust1_new_device = test_core_from(&customer1);
     cust1_new_device.validate().unwrap();
+}
+
+#[test]
+#[ignore]
+fn admin_rebuild_shared_files_index_test() {
+    let admin_core = test_core();
+    admin_core.create_account("admin1", &url(), false).unwrap();
+
+    let customer1 = test_core_with_account();
+    let customer2 = test_core_with_account();
+
+    let doc = customer1.create_at_path("test.md").unwrap();
+    customer1
+        .share_file(doc.id, &customer2.get_account().unwrap().username, ShareMode::Read)
+        .unwrap();
+    customer1.sync(None).unwrap();
+    customer2.sync(None).unwrap();
+
+    assert!(admin_core
+        .admin_validate_server()
+        .unwrap()
+        .users_with_validation_failures
+        .is_empty());
+
+    admin_core
+        .admin_disappear_account(&customer1.get_account().unwrap().username)
+        .unwrap();
+
+    // this should be uncommented after: https://github.com/lockbook/lockbook/issues/1521
+    // assert!(admin_core.admin_validate_server().unwrap().users_with_validation_failures.is_empty());
+
+    admin_core
+        .admin_rebuild_index(ServerIndex::SharedFiles)
+        .unwrap();
+    assert!(admin_core
+        .admin_validate_server()
+        .unwrap()
+        .users_with_validation_failures
+        .is_empty());
+
+    let cust2_new_device = test_core_from(&customer2);
+    cust2_new_device.validate().unwrap();
 }
