98d0d7ba858a3ef42b8880e7c806b790d1bdec3b
Steve Russo <steverusso@pm.me>
2023-02-21 10:31:30 -0500

    change the lib name to the new external interface wrapper

--- utils/dev-tool/src/android.rs
+++ utils/dev-tool/src/android.rs
@@ -6,7 +6,7 @@ use std::path::Path;
 use std::process::Command;
 
 const MIN_NDK_VERSION: u32 = 22;
-const NDK_LIB_NAME: &str = "liblockbook_core.so";
+const NDK_LIB_NAME: &str = "liblockbook_core_external_interface.so";
 
 pub fn fmt_android(tool_env: &ToolEnvironment) {
     let android_dir = utils::android_dir(&tool_env.root_dir);
