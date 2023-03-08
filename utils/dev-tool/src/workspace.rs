82587b3906dd393a5f32c9e5a2d65934bb59028d
Steve Russo <steverusso@pm.me>
2023-03-07 22:08:00 -0500

    ci: check for unused deps with udeps

--- utils/dev-tool/src/workspace.rs
+++ utils/dev-tool/src/workspace.rs
@@ -16,6 +16,13 @@ pub fn clippy_workspace(tool_env: &ToolEnvironment) {
         .assert_success();
 }
 
+pub fn udeps_workspace(tool_env: &ToolEnvironment) {
+    Command::new("cargo")
+        .args(["udeps", "--all-targets", "--all-features"])
+        .current_dir(&tool_env.root_dir)
+        .assert_success();
+}
+
 pub fn check_lockfile(tool_env: &ToolEnvironment) {
     Command::new("cargo")
         .args(["check", "--locked"])
