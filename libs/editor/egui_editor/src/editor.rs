7aa19c43a390f3ae4937d321e08f15c3f92514df
Steve Russo <steverusso@pm.me>
2023-03-07 20:41:55 -0500

    ladies and gentlemen, we got him

--- libs/editor/egui_editor/src/editor.rs
+++ libs/editor/egui_editor/src/editor.rs
@@ -1,17 +1,13 @@
-use std::time::Instant;
-
 use egui::{Context, FontDefinitions, Response, Ui, Vec2};
 
 use crate::appearance::Appearance;
 use crate::ast::Ast;
-use crate::buffer::{Buffer, SubModification};
-use crate::cursor::Cursor;
+use crate::buffer::Buffer;
 use crate::debug::DebugInfo;
-use crate::element::ItemType;
-use crate::events::{self, pos_to_char_offset};
+use crate::events;
 use crate::galleys::Galleys;
 use crate::images::ImageCache;
-use crate::layouts::{Annotation, Layouts};
+use crate::layouts::Layouts;
 use crate::styles::StyleInfo;
 use crate::test_input::TEST_MARKDOWN;
 use crate::{ast, galleys, images, layouts, register_fonts, styles};
@@ -75,6 +71,8 @@ impl Editor {
         // update theme
         let theme_updated = self.appearance.set_theme(ui.visuals());
 
+        println!("{:?}", ui.ctx().input().events);
+
         // process events
         let (text_updated, cursor_pos_updated, selection_updated) = if self.initialized {
             let prior_cursor_pos = self.buffer.current.cursor.pos;
@@ -128,12 +126,24 @@ impl Editor {
         self.draw_text(ui_size, ui);
         let resp = ui.interact(rect, id, egui::Sense::click_and_drag());
         if let Some(pos) = resp.interact_pointer_pos() {
-            ui.memory().request_focus(id);
-            ui.memory().lock_focus(id, true);
-            self.process_pointer_click(ui_size, &pos, ui);
-
-            // self.layouts = layouts::calc(&self.buffer.current, &self.styles, &self.appearance);
-            // self.galleys = galleys::calc(&self.layouts, &self.images, &self.appearance, ui);
+            if !ui.memory().has_focus(id) {
+                events::process(
+                    &[egui::Event::PointerButton {
+                        pos,
+                        button: egui::PointerButton::Primary,
+                        pressed: true,
+                        modifiers: Default::default(),
+                    }],
+                    &self.layouts,
+                    &self.galleys,
+                    &self.appearance,
+                    ui_size,
+                    &mut self.buffer,
+                    &mut self.debug,
+                );
+                ui.memory().request_focus(id);
+                ui.memory().lock_focus(id, true);
+            }
         } else if resp.clicked_elsewhere() {
             ui.memory().surrender_focus(id);
         }
@@ -147,7 +157,7 @@ impl Editor {
         }
 
         // scroll
-        /*if cursor_pos_updated {
+        if cursor_pos_updated {
             ui.scroll_to_rect(
                 self.buffer
                     .current
@@ -155,118 +165,9 @@ impl Editor {
                     .rect(&self.buffer.current.segs, &self.galleys),
                 None,
             );
-        }*/
+        }
 
         resp
-    }
-
-    fn process_pointer_click(&mut self, ui_size: egui::Vec2, pos: &egui::Pos2, ui: &mut egui::Ui) {
-        let modifiers = ui.ctx().input().modifiers;
-        let galleys = &self.galleys;
-        let appearance = &self.appearance;
-        let buffer = &mut self.buffer;
-        let mut modifications = Vec::new();
-        let mut cursor = buffer.current.cursor;
-        let mut previous_cursor = buffer.current.cursor;
-
-        /*cursor.fix(false, &buffer.current.segs, galleys);
-        if cursor != previous_cursor {
-            modifications.push(SubModification::Cursor { cursor });
-            previous_cursor = cursor;
-        }*/
-
-        // do not process scrollbar clicks
-        if pos.x <= ui_size.x {
-            // process checkbox clicks
-            let checkbox_click = {
-                let mut checkbox_click = false;
-                for galley in &galleys.galleys {
-                    if let Some(Annotation::Item(ItemType::Todo(checked), ..)) = galley.annotation {
-                        if galley.checkbox_bounds(appearance).contains(*pos) {
-                            modifications.push(SubModification::Cursor {
-                                cursor: Cursor {
-                                    pos: buffer
-                                        .current
-                                        .segs
-                                        .byte_offset_to_char(galley.range.start + galley.head_size),
-                                    selection_origin: Some(
-                                        buffer.current.segs.byte_offset_to_char(
-                                            galley.range.start + galley.head_size - 6,
-                                        ),
-                                    ),
-                                    ..Default::default()
-                                },
-                            });
-                            modifications.push(SubModification::Insert {
-                                text: if checked { "- [ ] " } else { "- [x] " }.to_string(),
-                            });
-                            modifications.push(SubModification::Cursor { cursor });
-
-                            checkbox_click = true;
-                            break;
-                        }
-                    }
-                }
-                checkbox_click
-            };
-            if !checkbox_click {
-                // record instant for double/triple click
-                cursor.process_click_instant(Instant::now());
-
-                let mut double_click = false;
-                let mut triple_click = false;
-                if !modifiers.shift {
-                    // click: end selection
-                    cursor.selection_origin = None;
-
-                    double_click = cursor.double_click();
-                    triple_click = cursor.triple_click();
-                } else {
-                    // shift+click: begin selection
-                    cursor.set_selection_origin();
-                }
-                // any click: begin drag; update cursor
-                cursor.set_click_and_drag_origin();
-                if triple_click {
-                    cursor.pos = pos_to_char_offset(*pos, galleys, &buffer.current.segs);
-
-                    let (galley_idx, cur_cursor) =
-                        galleys.galley_and_cursor_by_char_offset(cursor.pos, &buffer.current.segs);
-                    let galley = &galleys[galley_idx];
-                    let begin_of_row_cursor = galley.galley.cursor_begin_of_row(&cur_cursor);
-                    let end_of_row_cursor = galley.galley.cursor_end_of_row(&cur_cursor);
-
-                    cursor.selection_origin = Some(galleys.char_offset_by_galley_and_cursor(
-                        galley_idx,
-                        &begin_of_row_cursor,
-                        &buffer.current.segs,
-                    ));
-                    cursor.pos = galleys.char_offset_by_galley_and_cursor(
-                        galley_idx,
-                        &end_of_row_cursor,
-                        &buffer.current.segs,
-                    );
-                } else if double_click {
-                    cursor.pos = pos_to_char_offset(*pos, galleys, &buffer.current.segs);
-
-                    cursor.advance_word(false, &buffer.current, &buffer.current.segs, galleys);
-                    let end_of_word_pos = cursor.pos;
-                    cursor.advance_word(true, &buffer.current, &buffer.current.segs, galleys);
-                    let begin_of_word_pos = cursor.pos;
-
-                    cursor.selection_origin = Some(begin_of_word_pos);
-                    cursor.pos = end_of_word_pos;
-                } else {
-                    cursor.pos = pos_to_char_offset(*pos, galleys, &buffer.current.segs);
-                }
-            }
-        }
-        if cursor != previous_cursor {
-            modifications.push(SubModification::Cursor { cursor });
-            // previous_cursor = cursor;
-            // buffer.current.cursor = cursor;
-        }
-        buffer.apply(modifications, &mut self.debug);
     }
 
     pub fn set_text(&mut self, new_text: String) {
