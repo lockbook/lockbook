dbbf597afc047ea495d24448d60495fcc7495782
Steve Russo <steverusso@pm.me>
2023-04-05 13:22:42 -0700

    egui: save tabs when closing them (#1690)

--- clients/egui/src/account/workspace.rs
+++ clients/egui/src/account/workspace.rs
@@ -46,18 +46,6 @@ impl Workspace {
         self.tabs.get(self.active_tab)
     }
 
-    pub fn close_tab(&mut self, i: usize) {
-        self.tabs.remove(i);
-        let n_tabs = self.tabs.len();
-        if self.active_tab >= n_tabs && n_tabs > 0 {
-            self.active_tab = n_tabs - 1;
-        }
-    }
-
-    pub fn close_current_tab(&mut self) {
-        self.close_tab(self.active_tab);
-    }
-
     pub fn goto_tab_id(&mut self, id: lb::Uuid) -> bool {
         for (i, tab) in self.tabs.iter().enumerate() {
             if tab.id == id {
@@ -204,13 +192,12 @@ impl super::AccountScreen {
         ui.vertical(|ui| {
             if self.workspace.tabs.len() > 1 {
                 ui.horizontal(|ui| {
-                    let ws = &mut self.workspace;
-
-                    for (i, maybe_resp) in ws
+                    for (i, maybe_resp) in self
+                        .workspace
                         .tabs
                         .iter()
                         .enumerate()
-                        .map(|(i, t)| tab_label(ui, t, ws.active_tab == i))
+                        .map(|(i, t)| tab_label(ui, t, self.workspace.active_tab == i))
                         .collect::<Vec<Option<TabLabelResponse>>>()
                         .iter()
                         .enumerate()
@@ -218,12 +205,12 @@ impl super::AccountScreen {
                         if let Some(resp) = maybe_resp {
                             match resp {
                                 TabLabelResponse::Clicked => {
-                                    ws.active_tab = i;
-                                    frame.set_window_title(&ws.tabs[i].name);
+                                    self.workspace.active_tab = i;
+                                    frame.set_window_title(&self.workspace.tabs[i].name);
                                 }
                                 TabLabelResponse::Closed => {
-                                    ws.close_tab(i);
-                                    frame.set_window_title(match ws.current_tab() {
+                                    self.close_tab(i);
+                                    frame.set_window_title(match self.workspace.current_tab() {
                                         Some(tab) => &tab.name,
                                         None => "Lockbook",
                                     });
@@ -280,8 +267,8 @@ impl super::AccountScreen {
         });
     }
 
-    pub fn save_current_tab(&self) {
-        if let Some(tab) = self.workspace.current_tab() {
+    pub fn save_tab(&self, i: usize) {
+        if let Some(tab) = self.workspace.tabs.get(i) {
             if let Some(content) = &tab.content {
                 if let TabContent::Drawing(d) = content {
                     self.core.save_drawing(tab.id, &d.drawing).unwrap(); // TODO
@@ -291,12 +278,21 @@ impl super::AccountScreen {
                         TabContent::PlainText(txt) => Some(txt.content.as_bytes()),
                         _ => None,
                     };
-
                     if let Some(bytes) = maybe_bytes {
                         self.core.write_document(tab.id, bytes).unwrap(); // TODO
                     }
                 }
             }
+        }
+    }
+
+    pub fn close_tab(&mut self, i: usize) {
+        self.save_tab(i);
+        let ws = &mut self.workspace;
+        ws.tabs.remove(i);
+        let n_tabs = ws.tabs.len();
+        if ws.active_tab >= n_tabs && n_tabs > 0 {
+            ws.active_tab = n_tabs - 1;
         }
     }
 }
