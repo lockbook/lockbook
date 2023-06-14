8675489a13df1fa831b459c49f5dd559fa7578d4
ssapra2 <ssapra2@andrew.cmu.edu>
2023-06-14 19:44:40 -0400

    Comments added for next steps

--- clients/drive/src/main.rs
+++ clients/drive/src/main.rs
@@ -21,6 +21,23 @@ fn main(){
     match cmd {
         Command::LocalSync{location} => {
             println!("{:?}", location);
+            // Step 1: Either make sure directory is empty or empty it
+            // Step 2: Initialize a core - Core::Init pointed at .lockbook, location = .lockbook/drive
+            // Step 3: Determine whether they are signed in - core.get_account
+            // Step 4: Initially they shouldn't be signed in - provide another subcommand where they can import their account
+            // Step 4.5: If you import an account rn, call core.sync 
+            // Step 5: Write them to the user specified location
+            // Step 6: On this user specified location, watch for changes
+            // Step 7: Based on what happens, do the corresponding thing inside core (eg if they create file, call core.create)
+            // If updated, read contents of core and write the contents to the corresponding place in core
+            // Step 8: Call core.sync - should give summary of what happened - apply those changes from core back onto file system
+            // When you make changes, how do you know how to ignore them (maybe you just wait for errors and see if they need to be ignored)
+            // Alternatively, when they are reduplicated, stop watching the file (lock directory), make changes, and then unlock it
+            // Source ID - maybe know your own process ID and ignore it
+            // Step 9: To make this safe, determine how to lock the file system so that no one can make changes when the file system is not being watched
+            // Naive approach: detect shutdown, delete everything
+            // Better approach: Put the file system into a read only state when we shutdown
+            // Step 10: Work on the UI
             check_for_changes().unwrap();
         }
     }
