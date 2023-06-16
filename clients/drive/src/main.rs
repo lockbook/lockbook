2e64a1717d755a0d320432c604f33f05a49ab114
ssapra2 <ssapra2@andrew.cmu.edu>
2023-06-16 13:30:54 -0400

    formatted

--- clients/drive/src/main.rs
+++ clients/drive/src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 
 //extern crate notify;
 
@@ -27,12 +27,10 @@ fn main() {
     }
 }
 
-fn core() -> Core{
-
+fn core() -> Core {
     let writeable_path = format!("{}/.lockbook/drive", std::env::var("HOME").unwrap());
 
     Core::init(&lb::Config { writeable_path, logs: true, colored_logs: true }).unwrap()
-
 }
 
 fn check_for_changes() -> notify::Result<()> {
@@ -56,21 +54,20 @@ fn check_for_changes() -> notify::Result<()> {
     Ok(())
 }
 
-
 // Step 1: Either make sure directory is empty or empty it
-            // Step 2: Initialize a core - Core::Init pointed at .lockbook, location = .lockbook/drive
-            // Step 3: Determine whether they are signed in - core.get_account
-            // Step 4: Initially they shouldn't be signed in - provide another subcommand where they can import their account
-            // Step 4.5: If you import an account rn, call core.sync
-            // Step 5: Write them to the user specified location
-            // Step 6: On this user specified location, watch for changes
-            // Step 7: Based on what happens, do the corresponding thing inside core (eg if they create file, call core.create)
-            // If updated, read contents of core and write the contents to the corresponding place in core
-            // Step 8: Call core.sync - should give summary of what happened - apply those changes from core back onto file system
-            // When you make changes, how do you know how to ignore them (maybe you just wait for errors and see if they need to be ignored)
-            // Alternatively, when they are reduplicated, stop watching the file (lock directory), make changes, and then unlock it
-            // Source ID - maybe know your own process ID and ignore it
-            // Step 9: To make this safe, determine how to lock the file system so that no one can make changes when the file system is not being watched
-            // Naive approach: detect shutdown, delete everything
-            // Better approach: Put the file system into a read only state when we shutdown
-            // Step 10: Work on the UI
+// Step 2: Initialize a core - Core::Init pointed at .lockbook, location = .lockbook/drive
+// Step 3: Determine whether they are signed in - core.get_account
+// Step 4: Initially they shouldn't be signed in - provide another subcommand where they can import their account
+// Step 4.5: If you import an account rn, call core.sync
+// Step 5: Write them to the user specified location
+// Step 6: On this user specified location, watch for changes
+// Step 7: Based on what happens, do the corresponding thing inside core (eg if they create file, call core.create)
+// If updated, read contents of core and write the contents to the corresponding place in core
+// Step 8: Call core.sync - should give summary of what happened - apply those changes from core back onto file system
+// When you make changes, how do you know how to ignore them (maybe you just wait for errors and see if they need to be ignored)
+// Alternatively, when they are reduplicated, stop watching the file (lock directory), make changes, and then unlock it
+// Source ID - maybe know your own process ID and ignore it
+// Step 9: To make this safe, determine how to lock the file system so that no one can make changes when the file system is not being watched
+// Naive approach: detect shutdown, delete everything
+// Better approach: Put the file system into a read only state when we shutdown
+// Step 10: Work on the UI
