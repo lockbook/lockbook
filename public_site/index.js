ce3271fd3ebf2f7a4b211d6cd5a38b80512818fe
ad-tra <aew31gcmuy@gmail.com>
2023-08-23 16:29:19 -0400

    almost done

--- public_site/index.js
+++ public_site/index.js
@@ -1,9 +1,9 @@
 let OSName = "Unknown";
 let OSDownloadMap = {
 	Windows:
-		"https://github.com/lockbook/lockbook/releases/download/0.5.7/lockbook-windows-setup-x86_64.exe",
+		"https://github.com/lockbook/lockbook/releases/latest/download/lockbook-windows-setup-x86_64.exe",
 	"Mac/iOS": "https://apps.apple.com/us/app/lockbook/id1526775001",
-	Linux: "https://github.com/lockbook/lockbook/blob/master/docs/guides/install/linux.md",
+	Linux: "https://github.com/lockbook/lockbook/releases/latest/download/lockbook-egui",
 	Android: "https://play.google.com/store/apps/details?id=app.lockbook",
 };
 if (window.navigator.userAgent.includes("Windows NT")) OSName = "Windows";
@@ -11,7 +11,22 @@ if (window.navigator.userAgent.includes("Mac")) OSName = "Mac/iOS";
 if (window.navigator.userAgent.includes("Android")) OSName = "Android";
 if (window.navigator.userAgent.includes("Linux")) OSName = "Linux";
 
-document.querySelectorAll(".get-lockbook").forEach((el) => {
+document.querySelectorAll(".get-lockbook-gui").forEach((el) => {
 	el.textContent = `Get for ${OSName}`;
 	el.href = OSDownloadMap[OSName];
+  if (el.nextElementSibling === null) return
+  let secondaryDownloadMap = {...OSDownloadMap};
+  delete secondaryDownloadMap[OSName];
+  secondaryDownloadMap = Object.entries(secondaryDownloadMap) 
+
+  el.nextElementSibling.querySelectorAll("a").forEach((secondaryLink,i) =>{
+    console.log(secondaryDownloadMap[i])
+ 
+    secondaryLink.href = secondaryDownloadMap[i][1];
+    secondaryLink.textContent = secondaryDownloadMap[i][0];
+  })
 });
+
+
+/** gui download center navigation **/
+
