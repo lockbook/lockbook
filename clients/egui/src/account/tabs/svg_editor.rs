853d4497400ab2f747a59e2b8c8cbbc277e2c65a
ad-tra <aew31gcmuy@gmail.com>
2023-11-03 04:00:39 -0400

    refactor to enable dark theme aware color switching

--- clients/egui/src/account/tabs/svg_editor.rs
+++ clients/egui/src/account/tabs/svg_editor.rs
@@ -1,15 +1,15 @@
-use std::rc::Rc;
+use std::borrow::BorrowMut;
+use std::fmt::Write;
 use std::sync::mpsc;
 
 use eframe::egui;
-use resvg::tiny_skia::{PathBuilder, Pixmap};
-use resvg::usvg::{
-    self, Align, Color, Fill, NodeExt, NodeKind, NonZeroPositiveF32, NonZeroRect, NormalizedF32,
-    Paint, Path, Size, Stroke, Transform, TreeWriting, ViewBox, XmlOptions,
-};
+use minidom::Element;
+use resvg::tiny_skia::{Path, PathBuilder, PathSegment, Pixmap};
+use resvg::usvg;
 
 pub struct SVGEditor {
-    raw: Vec<u8>,
+    svg: String,
+    root: Element,
     draw_rx: mpsc::Receiver<(egui::Pos2, usize)>,
     draw_tx: mpsc::Sender<(egui::Pos2, usize)>,
     path_builder: PathBuilder,
@@ -20,69 +20,84 @@ impl SVGEditor {
     pub fn boxed(bytes: &[u8], _ctx: &egui::Context) -> Box<Self> {
         let (draw_tx, draw_rx) = mpsc::channel();
 
+        // todo: handle invalid utf8
+        let svg = std::str::from_utf8(bytes).unwrap().to_string();
+
+        let root: Element = svg.parse().unwrap();
+
         Box::new(Self {
-            raw: bytes.to_vec(),
+            svg,
             draw_rx,
             draw_tx,
             id_counter: 0,
+            root,
             path_builder: PathBuilder::new(),
         })
     }
 
     pub fn show(&mut self, ui: &mut egui::Ui) {
+        self.setup_draw_events(ui);
+        self.draw_event_handler();
+
+        if self.root.attr("data-dark-mode").is_none() {
+            self.root
+                .set_attr("data-dark-mode", format!("{}", ui.visuals().dark_mode));
+            self.build_color_defs(ui);
+        }
+
+        if let Some(svg_flag) = self.root.attr("data-dark-mode") {
+            let svg_flag: bool = svg_flag.parse().unwrap_or(false);
+
+            if svg_flag != ui.visuals().dark_mode {
+                self.build_color_defs(ui);
+                self.root
+                    .set_attr("data-dark-mode", format!("{}", ui.visuals().dark_mode));
+            }
+        }
+
+        self.render_svg(ui);
+    }
+
+    fn build_color_defs(&mut self, ui: &mut egui::Ui) {
+        let text_color = ui.visuals().text_color();
+        let color = format!("rgb({} {} {})", text_color.r(), text_color.g(), text_color.b());
+
+        let gradient = Element::builder("linearGradient", "")
+            .attr("id", "fg")
+            .append(
+                Element::builder("stop", "")
+                    .attr("stop-color", color)
+                    .build(),
+            )
+            .build();
+
+        self.root.borrow_mut().append_child(gradient);
+    }
+
+    fn render_svg(&self, ui: &mut egui::Ui) {
         let mut utree: usvg::Tree =
-            usvg::TreeParsing::from_data(&self.raw, &usvg::Options::default()).unwrap();
-
+            usvg::TreeParsing::from_data(&self.svg.as_bytes(), &usvg::Options::default()).unwrap();
         let available_rect = ui.available_rect_before_wrap();
-        utree.size = utree
-            .size
-            .scale_to(Size::from_wh(available_rect.width(), available_rect.height()).unwrap());
+        utree.size = utree.size.scale_to(
+            usvg::Size::from_wh(available_rect.width(), available_rect.height()).unwrap(),
+        );
 
-        utree.view_box.rect = NonZeroRect::from_ltrb(
+        utree.view_box.rect = usvg::NonZeroRect::from_ltrb(
             available_rect.left(),
             available_rect.top(),
             available_rect.right(),
             available_rect.bottom(),
         )
         .unwrap();
-        self.raw = utree.to_string(&XmlOptions::default()).as_bytes().to_vec();
 
-        self.setup_path_events(ui);
-        self.path_handler(&utree, ui);
+        // println!("{}", utree.to_string(&XmlOptions::default()));
 
-        self.show_svg_render(&utree, ui);
-        /*
-         * how to detect if there's a theme switch?
-         * 1. mismatch between egui dark_mode flag and  svg custom attribute data-theme says
-         * I think this is impossible with resvg because it erases any custom attributes
-         * 2. store global memory in egui cache
-         * 3. fire an event when setting up the light/dark in the settings modal
-         *
-         * ---
-         *
-         * how to change color dynamically?
-         * add css styles. difficulties will arise from the fact that resvg erases css after applying
-         * use stroke=url() to a def variable and ensure 2 way binding between variable value and theme state
-         *
-         * ==> resvg is pretty poor for manipulating and composing a tree. I'm going with Bodoni/svg
-         */
-        // approach 1
-        //, then go through each node and update color
-        // have data-color='label' store a hex that informs color
-
-        // approach 2
-        // if the egui says it's dark, but svg says it's light, then update defs
-
-        println!("{}", ui.visuals().dark_mode);
-    }
-
-    fn show_svg_render(&self, utree: &usvg::Tree, ui: &mut egui::Ui) {
         let tree = resvg::Tree::from_usvg(&utree);
 
         let pixmap_size = tree.size.to_int_size();
         let mut pixmap = Pixmap::new(pixmap_size.width(), pixmap_size.height()).unwrap();
 
-        tree.render(Transform::default(), &mut pixmap.as_mut());
+        tree.render(usvg::Transform::default(), &mut pixmap.as_mut());
         let image = egui::ColorImage::from_rgba_unmultiplied(
             [pixmap.width() as usize, pixmap.height() as usize],
             &pixmap.data(),
@@ -90,7 +105,7 @@ impl SVGEditor {
 
         let texture = ui
             .ctx()
-            .load_texture("pdf_image", image, egui::TextureOptions::LINEAR);
+            .load_texture("svg_image", image, egui::TextureOptions::LINEAR);
         ui.add(
             egui::Image::new(
                 &texture,
@@ -100,57 +115,51 @@ impl SVGEditor {
         );
     }
 
-    fn path_handler(&mut self, utree: &usvg::Tree, ui: &mut egui::Ui) {
+    fn draw_event_handler(&mut self) {
         while let Ok((pos, id)) = self.draw_rx.try_recv() {
-            let text_color = ui.visuals().text_color();
-            if let Some(node) = utree.node_by_id(&id.to_string()) {
-                if let NodeKind::Path(ref mut p) = *node.borrow_mut() {
-                    self.path_builder.line_to(pos.x, pos.y);
-                    if let Some(stroke) = &mut p.stroke {
-                        // stroke.paint = Paint::Color(Color::new_rgb(
-                        //     text_color.r(),
-                        //     text_color.g(),
-                        //     text_color.b(),
-                        // ));
-                        stroke.paint = Paint::Color(Color::black());
-                        stroke.opacity = NormalizedF32::new(0.3).unwrap();
-                    }
-                    p.data = Rc::new(self.path_builder.clone().finish().unwrap());
+            let mut current_path = self.root.children_mut().find(|e| {
+                if let Some(id) = e.attr("id") {
+                    id == self.id_counter.to_string()
+                } else {
+                    false
                 }
+            });
+
+            // let a = current_path.unwrap().to;
+            if let Some(node) = current_path.as_mut() {
+                self.path_builder.line_to(pos.x, pos.y);
+                let path = self.path_builder.clone().finish().unwrap();
+                let data = get_path_data(path);
+
+                node.set_attr("d", data);
+                node.set_attr("stroke", "url(#fg)");
             } else {
                 self.path_builder.clear();
 
                 self.path_builder.move_to(pos.x, pos.y);
                 self.path_builder.line_to(pos.x, pos.y);
                 let path = self.path_builder.clone().finish().unwrap();
+                let data = get_path_data(path);
+                let child = Element::builder("path", "")
+                    .attr("stroke-width", 3)
+                    .attr("fill", "none")
+                    .attr("id", id)
+                    .attr("d", data)
+                    .build();
 
-                let mut stroke = Stroke::default();
-                stroke.width = NonZeroPositiveF32::new(4.0).unwrap();
-
-                stroke.paint =
-                    Paint::Color(Color::new_rgb(text_color.r(), text_color.g(), text_color.b()));
-
-                let node = NodeKind::Path(Path {
-                    id: id.to_string(),
-                    transform: Transform::default(),
-                    visibility: usvg::Visibility::Visible,
-                    fill: None,
-                    stroke: Some(stroke),
-                    paint_order: usvg::PaintOrder::StrokeAndFill,
-                    rendering_mode: usvg::ShapeRendering::GeometricPrecision,
-                    text_bbox: None,
-                    data: Rc::new(path),
-                });
-
-                let new_child = usvg::Node::new(node);
-                utree.root.append(new_child);
+                self.root.append_child(child);
             }
-            println!("{}", utree.to_string(&XmlOptions::default()));
-            self.raw = utree.to_string(&XmlOptions::default()).as_bytes().to_vec();
+
+            let mut buffer = Vec::new();
+            self.root.write_to(&mut buffer).unwrap();
+            self.svg = std::str::from_utf8(&buffer).unwrap().to_string();
+            self.svg = self.svg.replace("xmlns=''", "");
+
+            // println!("{}", self.svg);
         }
     }
 
-    fn setup_path_events(&mut self, ui: &mut egui::Ui) {
+    fn setup_draw_events(&mut self, ui: &mut egui::Ui) {
         if let Some(cursor_pos) = ui.ctx().pointer_hover_pos() {
             if ui.input(|i| i.pointer.primary_down()) {
                 self.draw_tx.send((cursor_pos, self.id_counter)).unwrap();
@@ -160,4 +169,25 @@ impl SVGEditor {
             }
         }
     }
+}
+
+fn get_path_data(path: Path) -> String {
+    let mut s = String::new();
+    for segment in path.segments() {
+        match segment {
+            PathSegment::MoveTo(p) => s.write_str(format!("M {} {} ", p.x, p.y).as_str()),
+            PathSegment::LineTo(p) => s.write_str(format!("L {} {} ", p.x, p.y).as_str()),
+            PathSegment::QuadTo(p0, p1) => {
+                s.write_str(format!("Q {} {} {} {} ", p0.x, p0.y, p1.x, p1.y).as_str())
+            }
+            PathSegment::CubicTo(p0, p1, p2) => s.write_str(
+                format!("C {} {} {} {} {} {} ", p0.x, p0.y, p1.x, p1.y, p2.x, p2.y).as_str(),
+            ),
+            PathSegment::Close => s.write_str(format!("Z ").as_str()),
+        }
+        .unwrap();
+    }
+
+    s.pop(); // ' '
+    return s;
 }
