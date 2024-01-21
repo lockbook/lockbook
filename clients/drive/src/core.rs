147400ae6af00670667f1bda891549d5987b317f
Parth <parth@mehrotra.me>
2024-01-21 17:42:21 -0500

    almost working

--- clients/drive/src/core.rs
+++ clients/drive/src/core.rs
@@ -1,14 +1,15 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Mutex};
 
-use lb_rs::{Core, File, Uuid};
-use nfsserve::nfs::{fattr3, fileid3, ftype3};
+use lb_rs::{Core, File, FileType, Uuid};
+use nfsserve::nfs::{fattr3, fileid3, filename3, ftype3};
 use tokio::task::spawn_blocking;
 
 // todo: this is not ideal, realistically core should just get the async await treatment
 pub struct AsyncCore {
     core: Core,
 
-    f3_uid: HashMap<fileid3, Uuid>,
+    f3_uid: Mutex<HashMap<fileid3, Uuid>>,
+    sizes: Mutex<HashMap<Uuid, usize>>,
 }
 
 impl AsyncCore {
@@ -18,11 +19,13 @@ impl AsyncCore {
         let core =
             Core::init(&lb_rs::Config { writeable_path, logs: true, colored_logs: true }).unwrap();
 
-        let mut ac = Self { core, f3_uid: Default::default() };
+        let mut ac = Self { core, f3_uid: Default::default(), sizes: Default::default() };
 
+        println!("preparing cache (are you in a release build?)");
         let files = ac.core.list_metadatas().unwrap();
-
-        ac.populate_caches(&files);
+        let sizes = ac.core.get_uncompressed_usage_breakdown().unwrap();
+        ac.populate_caches(&files, sizes);
+        println!("cache prepared");
 
         ac
     }
@@ -31,12 +34,14 @@ impl AsyncCore {
         self.core.get_root().unwrap()
     }
 
-    pub fn populate_caches(&mut self, files: &[File]) {
+    pub fn populate_caches(&mut self, files: &[File], sizes: HashMap<Uuid, usize>) {
         for file in files {
             let id = file.id;
             let first_part = id.as_u64_pair().0;
-            self.f3_uid.insert(first_part, file.id);
+            self.f3_uid.lock().unwrap().insert(first_part, file.id);
         }
+
+        *self.sizes.lock().unwrap() = sizes;
     }
 
     pub async fn get_all_files(&self) -> Vec<File> {
@@ -48,7 +53,7 @@ impl AsyncCore {
 
     pub async fn get_by_id(&self, id: fileid3) -> File {
         let core = self.c();
-        let id = *self.f3_uid.get(&id).unwrap();
+        let id = *self.f3_uid.lock().unwrap().get(&id).unwrap();
 
         spawn_blocking(move || core.get_file_by_id(id).unwrap())
             .await
@@ -57,7 +62,7 @@ impl AsyncCore {
 
     pub async fn get_children(&self, id: fileid3) -> Vec<File> {
         let core = self.c();
-        let id = *self.f3_uid.get(&id).unwrap();
+        let id = *self.f3_uid.lock().unwrap().get(&id).unwrap();
 
         spawn_blocking(move || core.get_children(id).unwrap())
             .await
@@ -73,13 +78,66 @@ impl AsyncCore {
             .unwrap();
     }
 
+    pub async fn read_document(&self, id: fileid3) -> Vec<u8> {
+        let core = self.c();
+        let id = *self.f3_uid.lock().unwrap().get(&id).unwrap();
+
+        spawn_blocking(move || core.read_document(id).unwrap())
+            .await
+            .unwrap()
+    }
+
+    // the lock exists on sizes because this needs to be &self because of the trait
+    pub async fn write_document(&self, id: fileid3, data: Vec<u8>) {
+        let core = self.c();
+        let id = *self.f3_uid.lock().unwrap().get(&id).unwrap();
+        let new_size = data.len();
+
+        spawn_blocking(move || core.write_document(id, &data).unwrap())
+            .await
+            .unwrap();
+
+        self.sizes.lock().unwrap().insert(id, new_size);
+    }
+
     pub async fn get_file_by_id(&self, id: fileid3) -> File {
         let core = self.c();
-        let id = *self.f3_uid.get(&id).unwrap();
+        let id = *self.f3_uid.lock().unwrap().get(&id).unwrap();
 
         spawn_blocking(move || core.get_file_by_id(id).unwrap())
             .await
             .unwrap()
+    }
+
+    pub async fn create_file(
+        &self, parent: fileid3, file_type: FileType, name: &filename3,
+    ) -> File {
+        let core = self.c();
+        let filename = String::from_utf8(name.0.clone()).unwrap();
+        let parent = *self.f3_uid.lock().unwrap().get(&parent).unwrap();
+
+        let file = spawn_blocking(move || core.create_file(&filename, parent, file_type).unwrap())
+            .await
+            .unwrap();
+
+        if file_type == FileType::Document {
+            self.sizes.lock().unwrap().insert(file.id, 0);
+        }
+
+        self.f3_uid
+            .lock()
+            .unwrap()
+            .insert(file.id.as_u64_pair().0, file.id);
+
+        file
+    }
+
+    pub async fn rename_file(&self, id: Uuid, name: String) {
+        let core = self.c();
+
+        spawn_blocking(move || core.rename_file(id, &name).unwrap())
+            .await
+            .unwrap();
     }
 
     pub async fn sync(&self) {
@@ -94,28 +152,35 @@ impl AsyncCore {
         .unwrap();
     }
 
+    pub fn file_to_fattr(&self, f: &File) -> fattr3 {
+        let ftype = if f.is_folder() { ftype3::NF3DIR } else { ftype3::NF3REG };
+
+        // todo this deserves some scrutiny and cross platform testing
+        let mode = if f.is_folder() { 0o755 } else { 0o644 };
+
+        let fileid = f.id.as_u64_pair().0;
+        // intereREADDIR3resstingly a number of key read operations rely on this being correct
+        let size =
+            if f.is_folder() { 0 } else { *self.sizes.lock().unwrap().get(&f.id).unwrap() as u64 };
+
+        fattr3 {
+            ftype,
+            mode,
+            nlink: Default::default(), // hard links to this file
+            uid: 501,                  // owner field? not resolved by this lib
+            gid: 20,                   // group id
+            size,
+            used: size,               // ?
+            rdev: Default::default(), // ?
+            fsid: Default::default(), // file system id
+            fileid,
+            atime: Default::default(), // access time
+            mtime: Default::default(), // todo modify time could populate with last_updated
+            ctime: Default::default(), // create time
+        }
+    }
+
     fn c(&self) -> Core {
         self.core.clone()
-    }
-}
-
-pub fn file_to_attr(f: File) -> fattr3 {
-    let ftype = if f.is_folder() { ftype3::NF3DIR } else { ftype3::NF3REG };
-    let fileid = f.id.as_u64_pair().0;
-
-    fattr3 {
-        ftype,
-        mode: Default::default(), // file protection bit (could be used for locking)
-        nlink: Default::default(), // hard links to this file
-        uid: Default::default(),  // owner field? not resolved by this lib
-        gid: Default::default(),  // group id
-        size: Default::default(), // todo: compute via cache
-        used: Default::default(), // ?
-        rdev: Default::default(), // ?
-        fsid: Default::default(), // file system id
-        fileid,
-        atime: Default::default(), // access time
-        mtime: Default::default(), // todo modify time could populate with last_updated
-        ctime: Default::default(), // create time
     }
 }
--- clients/drive/src/fs_impl.rs
+++ clients/drive/src/fs_impl.rs
@@ -1,62 +1,76 @@
 use async_trait::async_trait;
+use lb_rs::FileType;
 use nfsserve::{
-    nfs::{fattr3, fileid3, filename3, ftype3, nfspath3, nfsstat3, sattr3},
+    nfs::{fattr3, fileid3, filename3, ftype3, nfspath3, nfsstat3, nfsstring, sattr3},
     tcp::{NFSTcp, NFSTcpListener},
-    vfs::{NFSFileSystem, ReadDirResult, VFSCapabilities},
+    vfs::{DirEntry, NFSFileSystem, ReadDirResult, VFSCapabilities},
 };
 
-use crate::{core::file_to_attr, Drive};
+use crate::Drive;
 
 #[async_trait]
 impl NFSFileSystem for Drive {
     fn root_dir(&self) -> fileid3 {
+        println!("ROOT_DIR");
         let root = self.ac.get_root().id;
         root.as_u64_pair().0
     }
 
     fn capabilities(&self) -> VFSCapabilities {
+        println!("CAPABILITIES");
         VFSCapabilities::ReadWrite
     }
 
     // todo replace actual todos with Err(nfsstat3::NFS3ERR_NOTSUPP)
     async fn write(&self, id: fileid3, offset: u64, data: &[u8]) -> Result<fattr3, nfsstat3> {
-        todo!()
+        let offset = offset as usize;
+
+        let mut doc = self.ac.read_document(id).await;
+        if offset + data.len() > doc.len() {
+            doc.resize(offset + data.len(), 0);
+            doc[offset..].copy_from_slice(data);
+        } else {
+            for (idx, datum) in data.into_iter().enumerate() {
+                doc[offset + idx] = *datum;
+            }
+        }
+
+        self.ac.write_document(id, doc).await;
+        let file = self.ac.get_file_by_id(id).await;
+        let file = self.ac.file_to_fattr(&file);
+
+        Ok(file)
     }
 
+    // todo sattr would be what chmod +x requires, but we don't deal with that for now
+    // that's going to require us to hold on to fattr3s inmem and make modifications that
+    // core doesn't know or care about. Could be annoying for people trying to set executables
+    // those flags won't be sticky without adding fields to metadata
     async fn create(
         &self, dirid: fileid3, filename: &filename3, _attr: sattr3,
     ) -> Result<(fileid3, fattr3), nfsstat3> {
-        todo!()
-        // let name = String::from_utf8(filename.0.clone()).unwrap();
-        // let files = self.core.list_metadatas().unwrap();
-        // let mut uuid = Uuid::nil();
-        // for file in files {
-        //     let id = file.id;
-        //     if u_to_f3(id) == dirid {
-        //         uuid = id;
-        //     }
-        // }
-
-        // if uuid == Uuid::nil() {
-        //     panic!("UUID was not found");
-        // }
-
-        // let newfile = self
-        //     .core
-        //     .create_file(&name, uuid, FileType::Document)
-        //     .unwrap();
-        // let mut attr = fattr3::default();
-        // let retid = u_to_f3(newfile.id);
-        // attr.fileid = retid;
-        // attr.ftype = ftype3::NF3REG;
-        // //attr.mtime = newfile.last_modified;
-        // Ok((retid, attr))
+        println!(
+            "create(: {}, filename: {}",
+            dirid,
+            String::from_utf8(filename.0.clone()).unwrap()
+        );
+        let file = self
+            .ac
+            .create_file(dirid, FileType::Document, filename)
+            .await;
+        let file = self.ac.file_to_fattr(&file);
+        Ok((file.fileid, file))
     }
 
     async fn create_exclusive(
         &self, _dirid: fileid3, _filename: &filename3,
     ) -> Result<fileid3, nfsstat3> {
-        todo!()
+        println!(
+            "create_exclusive(dirid: {}, filename: {}",
+            _dirid,
+            String::from_utf8(_filename.0.clone()).unwrap()
+        );
+        return Err(nfsstat3::NFS3ERR_NOTSUPP);
     }
 
     async fn lookup(&self, dirid: fileid3, filename: &filename3) -> Result<fileid3, nfsstat3> {
@@ -87,26 +101,91 @@ impl NFSFileSystem for Drive {
 
         Err(nfsstat3::NFS3ERR_NOENT)
     }
+
     async fn getattr(&self, id: fileid3) -> Result<fattr3, nfsstat3> {
+        println!("GETATTR");
         let file = self.ac.get_file_by_id(id).await;
-        let file = file_to_attr(file);
+        let file = self.ac.file_to_fattr(&file);
         // todo: handle error
         Ok(file)
     }
+
+    // todo this may be how the os communicates truncation to us
     async fn setattr(&self, id: fileid3, setattr: sattr3) -> Result<fattr3, nfsstat3> {
-        todo!()
+        println!("SETATTR UNSUPPORTED");
+        Err(nfsstat3::NFS3ERR_NOTSUPP)
     }
 
     async fn read(
         &self, id: fileid3, offset: u64, count: u32,
     ) -> Result<(Vec<u8>, bool), nfsstat3> {
-        todo!()
+        println!("READ: id: {}, offset: {}, count: {}", id, offset, count);
+        let offset = offset as usize;
+        let count = count as usize;
+
+        let doc = self.ac.read_document(id).await;
+
+        if offset >= doc.len() {
+            println!("EOF == TRUE");
+            return Ok((vec![], true));
+        }
+
+        if offset + count >= doc.len() {
+            println!("returning {} bytes", doc[offset..].len());
+            return Ok((doc[offset..].to_vec(), true));
+        }
+
+        return Ok((doc[offset..offset + count].to_vec(), false));
     }
 
+    // todo it's unclear to me whether they would be as bold to provide a start_after of 0 if
+    // they don't have a particular start_after in mind yet
+    // possibly fileid3 is just the wrong type and they mean a usize offset
     async fn readdir(
         &self, dirid: fileid3, start_after: fileid3, max_entries: usize,
     ) -> Result<ReadDirResult, nfsstat3> {
-        todo!()
+        println!(
+            "readdir(dirid: {}, start_after: {}, max_entries: {})",
+            dirid, start_after, max_entries
+        );
+
+        let mut children = self.ac.get_children(dirid).await;
+
+        children.sort_by(|a, b| a.id.cmp(&b.id));
+
+        // this is how the example does it, we'd never return a fileid3 of 0
+        let mut start_index = 0;
+        if start_after > 0 {
+            for (idx, child) in children.iter().enumerate() {
+                if child.id.as_u64_pair().0 == start_after {
+                    start_index = idx + 1;
+                }
+            }
+        }
+
+        let mut ret = ReadDirResult::default();
+
+        if start_index == children.len() {
+            ret.end = true;
+            return Ok(ret);
+        }
+
+        let end_index = if start_index + max_entries >= children.len() {
+            ret.end = true;
+            children.len()
+        } else {
+            start_index + max_entries
+        };
+
+        for child in &children[start_index..end_index] {
+            ret.entries.push(DirEntry {
+                fileid: child.id.as_u64_pair().0,
+                name: nfsstring(child.name.clone().into_bytes()),
+                attr: self.ac.file_to_fattr(&child), // todo could probably eliminate this alloc
+            });
+        }
+
+        Ok(ret)
     }
 
     /// Removes a file.
@@ -114,33 +193,80 @@ impl NFSFileSystem for Drive {
     /// this should return Err(nfsstat3::NFS3ERR_ROFS)
     #[allow(unused)]
     async fn remove(&self, dirid: fileid3, filename: &filename3) -> Result<(), nfsstat3> {
-        todo!()
+        let name = String::from_utf8(filename.0.clone()).unwrap();
+        println!("remove filename({name} UNSUPPORTED");
+        let children = self.ac.get_children(dirid).await;
+        return Ok(());
     }
 
     /// Removes a file.
     /// If not supported dur to readonly file system
     /// this should return Err(nfsstat3::NFS3ERR_ROFS)
+    /// either an overwrite rename or move
     #[allow(unused)]
     async fn rename(
         &self, from_dirid: fileid3, from_filename: &filename3, to_dirid: fileid3,
         to_filename: &filename3,
     ) -> Result<(), nfsstat3> {
-        todo!()
+        let from_filename = String::from_utf8(from_filename.0.clone()).unwrap();
+        let to_filename = String::from_utf8(to_filename.0.clone()).unwrap();
+        println!(
+            "RENAME UNSUPPORTED from {} {} to {} {}",
+            from_filename, from_dirid, to_filename, to_dirid
+        );
+
+        if from_dirid != to_dirid {
+            panic!("moves not yet supported");
+        }
+
+        let children = self.ac.get_children(from_dirid).await;
+
+        let mut from_id = None;
+        let mut to_id = None;
+
+        for child in children {
+            if child.name == from_filename {
+                from_id = Some(child.id);
+            }
+
+            if child.name == to_filename {
+                to_id = Some(child.id);
+            }
+        }
+
+        let from_id = from_id.unwrap();
+        if to_id.is_none() {
+            self.ac.rename_file(from_id, to_filename).await;
+            return Ok(());
+        }
+        let to_id = to_id.unwrap();
+
+        let from_doc = self.ac.read_document(from_id.as_u64_pair().0).await;
+        self.ac
+            .write_document(to_id.as_u64_pair().0, from_doc)
+            .await;
+
+        return Ok(());
     }
 
     #[allow(unused)]
     async fn mkdir(
-        &self, _dirid: fileid3, _dirname: &filename3,
+        &self, dirid: fileid3, dirname: &filename3,
     ) -> Result<(fileid3, fattr3), nfsstat3> {
-        todo!()
+        println!("mkdir(: {}, filename: {}", dirid, String::from_utf8(dirname.0.clone()).unwrap());
+        let file = self.ac.create_file(dirid, FileType::Folder, dirname).await;
+        let file = self.ac.file_to_fattr(&file);
+        Ok((file.fileid, file))
     }
 
     async fn symlink(
         &self, _dirid: fileid3, _linkname: &filename3, _symlink: &nfspath3, _attr: &sattr3,
     ) -> Result<(fileid3, fattr3), nfsstat3> {
-        todo!()
+        println!("symlink UNSUPPORTED");
+        return Err(nfsstat3::NFS3ERR_NOTSUPP);
     }
     async fn readlink(&self, _id: fileid3) -> Result<nfspath3, nfsstat3> {
-        todo!()
+        println!("readklink UNSUPPORTED");
+        return Err(nfsstat3::NFS3ERR_NOTSUPP);
     }
 }
