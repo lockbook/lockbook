b10ab832f045d854f9a056b11ea89e7c3dc6a45d
Corey Cole <coreyleoc@gmail.com>
2024-02-25 02:05:11 -0800

    macos: simplify window manager

--- clients/apple/macOS/WindowManager.swift
+++ clients/apple/macOS/WindowManager.swift
@@ -7,20 +7,21 @@ let macOSLogoutWindowSize = 1024.0
 let macOSLogoutH1 = 24.0
 let macOSLogoutH2 = 20.0
 let macOSButtonWidth = 512.0
-let xyOffset = 20.0
+let xyOffset = 0.0
 
 class WindowManager: NSObject, NSWindowDelegate {
     static let shared = WindowManager()
     private var windowRef: NSWindow?
 
     func openLogoutConfirmationWindow() {
+        let contentView = NSHostingView(rootView: LogoutConfirmationView(
+            h1: macOSLogoutH1,
+            h2: macOSLogoutH2,
+            buttonWidth: macOSButtonWidth))
         // Check if the window already exists and bring it to the front
         if let curWindow = windowRef {
             // replaces the contentView so that the @State variables reset
-            curWindow.contentView = NSHostingView(rootView: LogoutConfirmationView(
-                h1: macOSLogoutH1,
-                h2: macOSLogoutH2,
-                buttonWidth: macOSButtonWidth))
+            curWindow.contentView = contentView
         } else {
             // Create a new window if it does not exist
             let window = NSWindow(
@@ -29,10 +30,7 @@ class WindowManager: NSObject, NSWindowDelegate {
                 backing: .buffered, defer: false)
             window.center()
             window.title = "Logout Confirmation"
-            window.contentView = NSHostingView(rootView: LogoutConfirmationView(
-                h1: macOSLogoutH1,
-                h2: macOSLogoutH2,
-                buttonWidth: macOSButtonWidth))
+            window.contentView = contentView
             window.isReleasedWhenClosed = false // Prevents the window from being deallocated when closed
             window.delegate = self
             
