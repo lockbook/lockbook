49b1f97afd7831b36002f6b0afb223d2d4a39388
“Travis <t.vanderstad@gmail.com>
2024-03-23 13:05:47 -0400

    fix image viewer

--- libs/content/workspace/src/tab/image_viewer.rs
+++ libs/content/workspace/src/tab/image_viewer.rs
@@ -1,4 +1,4 @@
-use egui::{pos2, Image, Rect};
+use egui::Image;
 
 pub struct ImageViewer {
     img: Image<'static>,
@@ -7,27 +7,14 @@ pub struct ImageViewer {
 impl ImageViewer {
     pub fn new(bytes: &[u8]) -> Self {
         let bytes = Vec::from(bytes);
-        let img = Image::from_bytes("bytes://qr.png", bytes);
+        let img = Image::from_bytes("bytes://image.png", bytes);
 
         Self { img }
     }
 
     pub fn show(&mut self, ui: &mut egui::Ui) {
         egui::ScrollArea::both().show(ui, |ui| {
-            let img_size = self.img.calc_size(ui.available_size(), None);
-            let ui_size = ui.available_size();
-            if img_size.x < ui_size.x || img_size.y < ui_size.y {
-                ui.with_layout(
-                    egui::Layout::left_to_right(egui::Align::Center).with_cross_justify(true),
-                    |ui| {
-                        self.img
-                            .paint_at(ui, Rect::from_min_size(pos2(0.0, 0.0), img_size));
-                    },
-                );
-            } else {
-                self.img
-                    .paint_at(ui, Rect::from_min_size(pos2(0.0, 0.0), img_size));
-            }
+            ui.add(self.img.clone()); // nb: doesn't clone the image data
         });
     }
 }
