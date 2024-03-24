040eddb40da9d5f7b4a24ca8eddba9709b9da322
Adam Trabelsi <aew31gcmuy@gmail.com>
2024-03-24 15:23:09 -0400

    wip

--- libs/content/workspace/src/tab/svg_editor/selection/rect.rs
+++ libs/content/workspace/src/tab/svg_editor/selection/rect.rs
@@ -7,6 +7,105 @@ use crate::tab::svg_editor::{history::ManipulatorGroupId, util::pointer_interest
 // todo: consider making this value dynamic depending on the scale of the element
 pub const SCALE_BRUSH_SIZE: f64 = 10.0;
 
+pub struct SelectionRectContainer {
+    rect: (egui::Rect, SelectionRect),
+    child_rects: Vec<SelectionRect>,
+}
+impl SelectionRectContainer {
+    pub fn new(
+        els: &[SelectedElement], working_rect: egui::Rect, buffer: &mut Buffer,
+    ) -> Option<Self> {
+        let mut container_bb = [DVec2::new(f64::MAX, f64::MAX), DVec2::new(f64::MIN, f64::MIN)];
+        let mut child_rects = vec![];
+        for el in els.iter() {
+            let bb = match buffer.paths.get(&el.id) {
+                Some(path) => path.bounding_box().unwrap_or_default(),
+                None => continue,
+            };
+
+            let rect = egui::Rect {
+                min: egui::pos2(bb[0].x as f32, bb[0].y as f32),
+                max: egui::pos2(bb[1].x as f32, bb[1].y as f32),
+            };
+
+            container_bb[0].x = container_bb[0].x.min(bb[0].x);
+            container_bb[0].y = container_bb[0].y.min(bb[0].y);
+
+            container_bb[1].x = container_bb[1].x.max(bb[1].x);
+            container_bb[1].y = container_bb[1].y.max(bb[1].y);
+        }
+
+        // clip the container bb to not overflow the canvas region
+        let mut clipped_bb = container_bb;
+        clipped_bb[0].x = clipped_bb[0].x.max(working_rect.left() as f64);
+        clipped_bb[0].y = clipped_bb[0].y.max(working_rect.top() as f64);
+
+        clipped_bb[1].x = clipped_bb[1].x.min(working_rect.right() as f64);
+        clipped_bb[1].y = clipped_bb[1].y.min(working_rect.bottom() as f64);
+
+        let is_clipped_bb_outside_of_working_rect =
+            clipped_bb[0].x > clipped_bb[1].x || clipped_bb[0].y > clipped_bb[1].y;
+
+        if is_clipped_bb_outside_of_working_rect {
+            return None;
+        }
+
+        let rect = egui::Rect {
+            min: egui::pos2(container_bb[0].x as f32, container_bb[0].y as f32),
+            max: egui::pos2(container_bb[1].x as f32, container_bb[1].y as f32),
+        };
+
+        let mut selection_rect = SelectionRect {
+            left: Some(Subpath::from_anchors(
+                [
+                    DVec2 { x: clipped_bb[0].x, y: clipped_bb[0].y },
+                    DVec2 { x: clipped_bb[0].x, y: clipped_bb[1].y },
+                ],
+                false,
+            )),
+            right: Some(Subpath::from_anchors(
+                [
+                    DVec2 { x: clipped_bb[1].x, y: clipped_bb[0].y },
+                    DVec2 { x: clipped_bb[1].x, y: clipped_bb[1].y },
+                ],
+                false,
+            )),
+            top: Some(Subpath::from_anchors(
+                [
+                    DVec2 { x: clipped_bb[0].x, y: clipped_bb[0].y },
+                    DVec2 { x: clipped_bb[1].x, y: clipped_bb[0].y },
+                ],
+                false,
+            )),
+            bottom: Some(Subpath::from_anchors(
+                [
+                    DVec2 { x: clipped_bb[0].x, y: clipped_bb[1].y },
+                    DVec2 { x: clipped_bb[1].x, y: clipped_bb[1].y },
+                ],
+                false,
+            )),
+            rect,
+            child_rects,
+        };
+
+        // when a bb is clipped, don't show the edge that's being clipeed
+        if clipped_bb[1].y != container_bb[1].y {
+            selection_rect.bottom = None;
+        }
+        if clipped_bb[0].y != container_bb[0].y {
+            selection_rect.top = None;
+        }
+        if clipped_bb[1].x != container_bb[1].x {
+            selection_rect.right = None;
+        }
+        if clipped_bb[0].x != container_bb[0].x {
+            selection_rect.left = None;
+        }
+
+        Some(selection_rect)
+    }
+}
+
 pub struct SelectionRect {
     left: Option<Subpath<ManipulatorGroupId>>,
     right: Option<Subpath<ManipulatorGroupId>>,
