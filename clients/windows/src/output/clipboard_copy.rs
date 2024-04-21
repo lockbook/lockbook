180a0c16e4270b18643c7305801f3a9420585a03
Travis Vanderstad <t.vanderstad@gmail.com>
2024-04-21 13:24:00 -0400

    windows/linux: clipboard fixes (#2636)
    * fixes an egui client issue where clipboard would be written each frame
    * gracefully handles clipboard write errors on windows, which were
    crashing the app

--- clients/windows/src/output/clipboard_copy.rs
+++ clients/windows/src/output/clipboard_copy.rs
@@ -1,6 +1,9 @@
-pub fn handle(copied_text: String) {
+use clipboard_win::SysResult;
+
+pub fn handle(copied_text: String) -> SysResult<()> {
     if !copied_text.is_empty() {
         clipboard_win::set_clipboard(clipboard_win::formats::Unicode, copied_text)
-            .expect("set clipboard");
+    } else {
+        Ok(())
     }
 }
