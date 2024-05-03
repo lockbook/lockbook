2b6e08e337c42daf9473d9e55c651dff0f023211
Smail Barkouch <smailbarkouch1@gmail.com>
2024-05-03 12:54:02 -0400

    releaser: fix android

--- utils/releaser/src/android/ws.rs
+++ utils/releaser/src/android/ws.rs
@@ -4,6 +4,6 @@ use std::process::Command;
 pub fn build() {
     Command::new("bash")
         .args(["create_android_libs.sh"])
-        .current_dir("libs/content/editor/egui_editor")
+        .current_dir("libs/content/workspace-ffi")
         .assert_success();
 }
