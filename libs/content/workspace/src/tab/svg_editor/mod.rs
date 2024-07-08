31938cb5c1f9a4c1567821abd51c7a9212ad3644
Adam Trabelsi <aew31gcmuy@gmail.com>
2024-07-08 18:06:54 -0400

    canvas: fix phantom strokes

--- libs/content/workspace/src/tab/svg_editor/mod.rs
+++ libs/content/workspace/src/tab/svg_editor/mod.rs
@@ -11,6 +11,7 @@ mod zoom;
 use std::time::Instant;
 
 use crate::tab::svg_editor::toolbar::Toolbar;
+use egui::EventFilter;
 pub use eraser::Eraser;
 pub use history::DeleteElement;
 pub use history::Event;
@@ -109,8 +110,19 @@ impl SVGEditor {
         });
 
         handle_zoom_input(ui, self.inner_rect, &mut self.buffer);
-
-        if ui.input(|r| r.multi_touch().is_some()) || self.skip_frame {
+        
+        
+        let start_of_touch = ui.input(|i|{
+            i.events.iter().any(|e| {
+                if let egui::Event::Touch  { device_id: _, id: _ , phase, pos: _, force: _ } = e{
+                    phase.eq(&egui::TouchPhase::Start)
+                }else{
+                    false
+                }
+            })
+        });
+        
+        if ui.input(|r| r.multi_touch().is_some()) || self.skip_frame || start_of_touch {
             self.skip_frame = false;
             return;
         }
