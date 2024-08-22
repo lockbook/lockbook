6333c984fa008fecac7b450ff9042741428c56e4
Adam Trabelsi <66345861+ad-tra@users.noreply.github.com>
2024-08-22 17:25:46 -0400

    Update autorun-apple.sh

--- utils/dev/autorun-apple.sh
+++ utils/dev/autorun-apple.sh
@@ -4,6 +4,9 @@ set -ea
 
 projRoot=`git rev-parse --show-toplevel`
 
+# get the device id
+deviceName=$(xcrun devicectl list devices --hide-default-columns --columns "Name" --hide-headers | sed -n '2p')
+
 # build workspace 
 cd "$projRoot"/libs/content/workspace-ffi/SwiftWorkspace 
 ./create_libs.sh 
@@ -11,8 +14,8 @@ cd "$projRoot"/libs/content/workspace-ffi/SwiftWorkspace
 
 # build ios app 
 cd "$projRoot"/clients/apple
-xcodebuild -workspace ./lockbook.xcworkspace -scheme "Lockbook (iOS)" -sdk iphoneos17.5 -configuration Debug -archivePath ./build/Lockbook-iOS.xcarchive archive 
-appBundlePath=$(xcrun devicectl device install app --device "$1" ./build/Lockbook-iOS.xcarchive/Products/Applications/Lockbook.app/ | grep "installationURL:" | sed 's/.*installationURL: //')
+# xcodebuild -workspace ./lockbook.xcworkspace -scheme "Lockbook (iOS)" -sdk iphoneos17.5 -configuration Debug -archivePath ./build/Lockbook-iOS.xcarchive archive 
+appBundlePath=$(xcrun devicectl device install app --device "$deviceName" ./build/Lockbook-iOS.xcarchive/Products/Applications/Lockbook.app/ | grep "installationURL:" | sed 's/.*installationURL: //')
 
 # run the app 
-xcrun devicectl device process launch --console --device "$1" $appBundlePath
+xcrun devicectl device process launch --console --device "$deviceName" $appBundlePath
