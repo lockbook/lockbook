a8999059587b6b73b5602ae9573f7636e34ebd05
Adam Trabelsi <66345861+ad-tra@users.noreply.github.com>
2024-08-22 17:31:35 -0400

    Update autorun-apple.sh

--- utils/dev/autorun-apple.sh
+++ utils/dev/autorun-apple.sh
@@ -14,7 +14,7 @@ cd "$projRoot"/libs/content/workspace-ffi/SwiftWorkspace
 
 # build ios app 
 cd "$projRoot"/clients/apple
-# xcodebuild -workspace ./lockbook.xcworkspace -scheme "Lockbook (iOS)" -sdk iphoneos17.5 -configuration Debug -archivePath ./build/Lockbook-iOS.xcarchive archive 
+xcodebuild -workspace ./lockbook.xcworkspace -scheme "Lockbook (iOS)" -sdk iphoneos17.5 -configuration Debug -archivePath ./build/Lockbook-iOS.xcarchive archive 
 appBundlePath=$(xcrun devicectl device install app --device "$deviceName" ./build/Lockbook-iOS.xcarchive/Products/Applications/Lockbook.app/ | grep "installationURL:" | sed 's/.*installationURL: //')
 
 # run the app 
