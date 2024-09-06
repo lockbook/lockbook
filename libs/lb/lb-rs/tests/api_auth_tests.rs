591e961c0dfef3842e7257b1f7c0cff7ebcca0da
“Travis <t.vanderstad@gmail.com>
2024-09-06 16:46:20 -0400

    api_auth_tests fixes

--- libs/lb/lb-rs/tests/api_auth_tests.rs
+++ libs/lb/lb-rs/tests/api_auth_tests.rs
@@ -2,154 +2,136 @@ use lb_rs::logic::api::*;
 use lb_rs::logic::crypto::AESEncrypted;
 use lb_rs::logic::file_like::FileLike;
 use lb_rs::logic::file_metadata::FileDiff;
-use lb_rs::service::api_service::{ApiError, Requester};
+use lb_rs::service::network::ApiError;
 use test_utils::*;
 
-#[test]
-fn upsert_id_takeover() {
-    let core1 = test_core_with_account();
-    let core2 = test_core_with_account();
+#[tokio::test]
+async fn upsert_id_takeover() {
+    let core1 = test_core_with_account().await;
+    let core2 = test_core_with_account().await;
 
     let acc1 = &core1.get_account().unwrap();
     let acc2 = &core2.get_account().unwrap();
 
     let mut file1 = {
-        let id = core1.create_at_path("/test.md").unwrap().id;
-        core1.sync(None).unwrap();
+        let id = core1.create_at_path("/test.md").await.unwrap().id;
+        core1.sync(None).await.unwrap();
+
         core1
-            .in_tx(|s| {
-                Ok(s.client
-                    .request(acc1, GetUpdatesRequest { since_metadata_version: 0 })
-                    .unwrap()
-                    .file_metadata
-                    .iter()
-                    .find(|&f| f.id() == &id)
-                    .unwrap()
-                    .clone())
-            })
+            .client
+            .request(acc1, GetUpdatesRequest { since_metadata_version: 0 })
+            .await
             .unwrap()
+            .file_metadata
+            .iter()
+            .find(|&f| f.id() == &id)
+            .unwrap()
+            .clone()
     };
 
-    file1.timestamped_value.value.parent = core2.get_root().unwrap().id;
+    file1.timestamped_value.value.parent = core2.root().await.unwrap().id;
 
     // If this succeeded account2 would be able to control file1
-    core2
-        .in_tx(|s| {
-            let result = s
-                .client
-                .request(acc2, UpsertRequest { updates: vec![FileDiff::new(&file1)] });
-            assert_matches!(
-                result,
-                Err(ApiError::<UpsertError>::Endpoint(UpsertError::OldVersionRequired))
-            );
-            Ok(())
-        })
-        .unwrap();
+    let result = core2
+        .client
+        .request(acc2, UpsertRequest { updates: vec![FileDiff::new(&file1)] })
+        .await;
+    assert_matches!(
+        result,
+        Err(ApiError::<UpsertError>::Endpoint(UpsertError::OldVersionRequired))
+    );
 }
 
-#[test]
-fn upsert_id_takeover_change_parent() {
-    let core1 = test_core_with_account();
-    let core2 = test_core_with_account();
+#[tokio::test]
+async fn upsert_id_takeover_change_parent() {
+    let core1 = test_core_with_account().await;
+    let core2 = test_core_with_account().await;
     let account1 = core1.get_account().unwrap();
     let account2 = core2.get_account().unwrap();
 
     let file1 = {
-        let id = core1.create_at_path("/test.md").unwrap().id;
-        core1.sync(None).unwrap();
+        let id = core1.create_at_path("/test.md").await.unwrap().id;
+        core1.sync(None).await.unwrap();
         core1
-            .in_tx(|s| {
-                Ok(s.client
-                    .request(&account1, GetUpdatesRequest { since_metadata_version: 0 })
-                    .unwrap()
-                    .file_metadata
-                    .iter()
-                    .find(|&f| f.id() == &id)
-                    .unwrap()
-                    .clone())
-            })
+            .client
+            .request(&account1, GetUpdatesRequest { since_metadata_version: 0 })
+            .await
             .unwrap()
+            .file_metadata
+            .iter()
+            .find(|&f| f.id() == &id)
+            .unwrap()
+            .clone()
     };
 
     // If this succeeded account2 would be able to control file1
-    core2
-        .in_tx(|s| {
-            let result = s
-                .client
-                .request(&account2, UpsertRequest { updates: vec![FileDiff::new(&file1)] });
-            assert_matches!(
-                result,
-                Err(ApiError::<UpsertError>::Endpoint(UpsertError::OldVersionRequired))
-            );
-            Ok(())
-        })
-        .unwrap();
+    let result = core2
+        .client
+        .request(&account2, UpsertRequest { updates: vec![FileDiff::new(&file1)] })
+        .await;
+    assert_matches!(
+        result,
+        Err(ApiError::<UpsertError>::Endpoint(UpsertError::OldVersionRequired))
+    );
 }
 
-#[test]
-fn change_document_content() {
-    let core1 = test_core_with_account();
-    let core2 = test_core_with_account();
+#[tokio::test]
+async fn change_document_content() {
+    let core1 = test_core_with_account().await;
+    let core2 = test_core_with_account().await;
 
     let file1 = {
-        let id = core1.create_at_path("/test.md").unwrap().id;
-        core1.sync(None).unwrap();
-        core1
-            .in_tx(|s| Ok(s.db.base_metadata.get().get(&id).unwrap().clone()))
-            .unwrap()
+        let id = core1.create_at_path("/test.md").await.unwrap().id;
+        core1.sync(None).await.unwrap();
+
+        let mut tx = core1.begin_tx().await;
+        tx.db().base_metadata.get().get(&id).unwrap().clone()
     };
 
     let mut file2 = file1.clone();
     file2.timestamped_value.value.document_hmac = Some([0; 32]);
 
     let acc2 = &core2.get_account().unwrap();
-    core2
-        .in_tx(|s| {
-            let result = s.client.request(
-                acc2,
-                ChangeDocRequest {
-                    diff: FileDiff::edit(&file1, &file2),
-                    new_content: AESEncrypted {
-                        value: vec![69],
-                        nonce: vec![69],
-                        _t: Default::default(),
-                    },
+    let result = core2
+        .client
+        .request(
+            acc2,
+            ChangeDocRequest {
+                diff: FileDiff::edit(&file1, &file2),
+                new_content: AESEncrypted {
+                    value: vec![69],
+                    nonce: vec![69],
+                    _t: Default::default(),
                 },
-            );
-            assert_matches!(
-                result,
-                Err(ApiError::<ChangeDocError>::Endpoint(ChangeDocError::NotPermissioned))
-            );
-            Ok(())
-        })
-        .unwrap();
+            },
+        )
+        .await;
+    assert_matches!(
+        result,
+        Err(ApiError::<ChangeDocError>::Endpoint(ChangeDocError::NotPermissioned))
+    );
 }
 
-#[test]
-fn get_someone_else_document() {
-    let core1 = test_core_with_account();
-    let core2 = test_core_with_account();
+#[tokio::test]
+async fn get_someone_else_document() {
+    let core1 = test_core_with_account().await;
+    let core2 = test_core_with_account().await;
 
     let file = {
-        let id = core1.create_at_path("/test.md").unwrap().id;
-        core1.write_document(id, &[1, 2, 3]).unwrap();
-        core1.sync(None).unwrap();
-        core1
-            .in_tx(|s| Ok(s.db.base_metadata.get().get(&id).unwrap().clone()))
-            .unwrap()
+        let id = core1.create_at_path("/test.md").await.unwrap().id;
+        core1.write_document(id, &[1, 2, 3]).await.unwrap();
+        core1.sync(None).await.unwrap();
+
+        let mut tx = core1.begin_tx().await;
+        tx.db().base_metadata.get().get(&id).unwrap().clone()
     };
 
     let req = GetDocRequest { id: *file.id(), hmac: *file.document_hmac().unwrap() };
 
     let acc2 = &core2.get_account().unwrap();
-    core2
-        .in_tx(|s| {
-            let result = s.client.request(acc2, req);
-            assert_matches!(
-                result,
-                Err(ApiError::<GetDocumentError>::Endpoint(GetDocumentError::NotPermissioned))
-            );
-            Ok(())
-        })
-        .unwrap();
+    let result = core2.client.request(acc2, req).await;
+    assert_matches!(
+        result,
+        Err(ApiError::<GetDocumentError>::Endpoint(GetDocumentError::NotPermissioned))
+    );
 }
