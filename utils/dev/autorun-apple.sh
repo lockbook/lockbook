dbae9c140ee9b2bad7e79051560eff3129f4bf95
Adam Trabelsi <66345861+ad-tra@users.noreply.github.com>
2024-10-04 11:40:57 -0400

    infra: autorun with macos support (#2893)

--- utils/dev/autorun-apple.sh
+++ utils/dev/autorun-apple.sh
@@ -2,20 +2,72 @@
 
 set -ea
 
+if [ -z "$1" ];
+then
+echo "Apple autorun: utility script to build and launch apps on macOS or iOS without xcode \n\n<target-platform> required (iOS|macOS)\n<build-deps> optional. the deps you'd like to build and can be (all|workspace|lb)\n"
+fi
+
 projRoot=`git rev-parse --show-toplevel`
 
-# get the device id
-deviceName=$(xcrun devicectl list devices --hide-default-columns --columns "Name" --hide-headers | sed -n '2p')
-
-# build workspace 
-cd "$projRoot"/libs/content/workspace-ffi/SwiftWorkspace 
-./create_libs.sh 
 
 
-# build ios app 
+build_workspace() {
+	echo "building workspace"
+	cd "$projRoot"/libs/content/workspace-ffi/SwiftWorkspace
+	./create_libs.sh
+}
+
+build_lb_rs() {
+	echo "building lb-rs"
+	cd "$projRoot"/libs/lb/lb_external_interface
+	make swift_libs
+}
+
+# build workspace
+case "$2" in
+	"workspace")
+	build_workspace
+	;;
+  "lb-rs")
+	build_lb_rs
+	;;
+	"all")
+	build_workspace
+	build_lb_rs
+	;;
+	*)
+	;;
+	esac
+
+
+
+
+
+# build and run app
 cd "$projRoot"/clients/apple
-xcodebuild -workspace ./lockbook.xcworkspace -scheme "Lockbook (iOS)" -sdk iphoneos17.5 -configuration Debug -archivePath ./build/Lockbook-iOS.xcarchive archive 
-appBundlePath=$(xcrun devicectl device install app --device "$deviceName" ./build/Lockbook-iOS.xcarchive/Products/Applications/Lockbook.app/ | grep "installationURL:" | sed 's/.*installationURL: //')
 
-# run the app 
-xcrun devicectl device process launch --console --device "$deviceName" $appBundlePath
+case "$1" in
+		"iOS")
+# get the device id
+		deviceName=$(xcrun devicectl list devices --hide-default-columns --columns "Name" --hide-headers | sed -n '2p' | xargs)
+		           echo running on $deviceName
+
+		if [ -z "$deviceName" ];
+		then
+		echo "No target device was found, make sure your ipad/iphone are on the same network"
+		exit 1
+		fi
+		xcodebuild -workspace ./lockbook.xcworkspace -scheme "Lockbook (iOS)" -sdk iphoneos17.5 -configuration Debug -archivePath ./build/Lockbook-iOS.xcarchive archive
+		appBundlePath=$(xcrun devicectl device install app --device "$deviceName" ./build/Lockbook-iOS.xcarchive/Products/Applications/Lockbook.app/ | grep "installationURL:" | sed 's/.*installationURL: //')
+		              xcrun devicectl device process launch --console --device "$deviceName" $appBundlePath
+
+		              ;;
+		"macOS")
+		xcodebuild -workspace ./lockbook.xcworkspace -scheme "Lockbook (macOS)" -sdk macosx -configuration Debug -archivePath ./build/Lockbook-macOS.xcarchive archive
+		./build/Lockbook-macOS.xcarchive/Products/Applications/Lockbook.app/Contents/MacOS/Lockbook
+		;;
+		esac
+
+
+
+
