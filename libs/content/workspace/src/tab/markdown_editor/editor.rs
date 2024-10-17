b06cee36a35f881eaf525e447828363f40a8feab
“Travis <t.vanderstad@gmail.com>
2024-10-17 16:25:09 -0400

    fix

--- libs/content/workspace/src/tab/markdown_editor/editor.rs
+++ libs/content/workspace/src/tab/markdown_editor/editor.rs
@@ -232,7 +232,6 @@ impl Editor {
 
         // remember state for change detection
         let prior_suggested_title = self.get_suggested_title();
-        let prior_selection = self.buffer.current.selection;
 
         // process events
         let (text_updated, selection_updated) = if self.initialized {
