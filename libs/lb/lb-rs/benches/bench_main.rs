7494d45abfe10ae74d17fe7e8a86aa8e7064bb61
Parth <parth@mehrotra.me>
2024-10-25 13:50:38 -0400

    clippy

--- libs/lb/lb-rs/benches/bench_main.rs
+++ libs/lb/lb-rs/benches/bench_main.rs
@@ -1,4 +1,6 @@
 use criterion::criterion_main;
+use lb_rs::blocking::Lb;
+use test_utils::{random_name, test_config, url};
 
 mod benchmarks;
 
@@ -17,4 +19,10 @@ criterion_main! {
     benchmarks::sync_benchmark::benches,
     benchmarks::test_repo_integrity_benchmark::benches,
     benchmarks::write_file_benchmark::benches,
+}
+
+pub fn blocking_core() -> Lb {
+    let lb = Lb::init(test_config()).unwrap();
+    lb.create_account(&random_name(), &url(), false).unwrap();
+    lb
 }
