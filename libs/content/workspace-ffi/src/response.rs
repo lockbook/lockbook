4e285033fd24ba7d465bd6ce3c2879e8be806522
“Travis <t.vanderstad@gmail.com>
2024-10-28 17:27:50 -0400

    editor: context menu with clipboard ops

--- libs/content/workspace-ffi/src/response.rs
+++ libs/content/workspace-ffi/src/response.rs
@@ -16,6 +16,7 @@ pub struct Response {
     pub cursor: CursorIcon,
     pub virtual_keyboard_shown: Option<bool>,
     pub window_title: Option<String>,
+    pub request_paste: bool,
     pub context_menu: Option<egui::Pos2>,
 }
 
@@ -24,18 +25,21 @@ impl Response {
         context: &Context, platform: PlatformOutput, viewport: ViewportIdMap<ViewportOutput>,
         workspace: workspace_rs::Response,
     ) -> Self {
-        let maybe_viewport_output = viewport.values().next();
-        if maybe_viewport_output.is_none() {
+        let mut redraw_in = None;
+        let mut window_title = None;
+        let mut request_paste = false;
+        if let Some(viewport) = viewport.into_values().next() {
+            redraw_in = Some(viewport.repaint_delay.as_millis() as _);
+            for cmd in viewport.commands.into_iter() {
+                match cmd {
+                    ViewportCommand::Title(title) => window_title = Some(title),
+                    ViewportCommand::RequestPaste => request_paste = true,
+                    _ => {} // remaining viewport commands ignored (many such cases!)
+                }
+            }
+        } else {
             eprintln!("viewport missing: not redrawing or setting window title");
         }
-
-        let redraw_in = maybe_viewport_output.map(|v| v.repaint_delay.as_millis() as u64);
-        let window_title = maybe_viewport_output.and_then(|v| {
-            v.commands.iter().find_map(|c| match c {
-                ViewportCommand::Title(title) => Some(title.clone()),
-                _ => None,
-            })
-        });
 
         Self {
             workspace,
@@ -45,6 +49,7 @@ impl Response {
             cursor: platform.cursor_icon,
             virtual_keyboard_shown: context.pop_virtual_keyboard_shown(),
             window_title,
+            request_paste,
             context_menu: context.pop_context_menu(),
         }
     }
