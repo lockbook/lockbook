75be9174c462c909e0b16f965efa661435e81a1e
“Travis <t.vanderstad@gmail.com>
2024-10-31 16:20:32 -0400

    workspace: prevent a tab from saving and loading concurrently

--- libs/content/workspace/src/tab/mod.rs
+++ libs/content/workspace/src/tab/mod.rs
@@ -24,6 +24,10 @@ pub struct Tab {
     pub is_new_file: bool,
     pub last_changed: Instant,
     pub last_saved: Instant,
+
+    /// ...with lb-rs. Different from lb-rs's syncing, which is with the server.
+    /// This is used to prevent saves and loads of this tab from happening concurrently.
+    pub is_syncing: bool,
 }
 
 #[derive(Debug, Default)]
