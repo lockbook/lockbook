594d1ad8b08f021a2a142c35e0084f2b4580e086
ad-tra <aew31gcmuy@gmail.com>
2024-11-09 13:45:49 -0500

    done

--- libs/content/workspace/src/tab/svg_editor/eraser.rs
+++ libs/content/workspace/src/tab/svg_editor/eraser.rs
@@ -33,11 +33,9 @@ impl Eraser {
         }
     }
 
-    pub fn handle_input(
-        &mut self, ui: &mut egui::Ui, eraser_ctx: &mut ToolContext, is_locked_vw: bool,
-    ) {
-        if is_multi_touch(ui, eraser_ctx)
-            || (is_locked_vw && eraser_ctx.settings.pencil_only_drawing)
+    pub fn handle_input(&mut self, ui: &mut egui::Ui, eraser_ctx: &mut ToolContext) {
+        if is_multi_touch(ui)
+            || (eraser_ctx.is_locked_vw_pen_only && eraser_ctx.settings.pencil_only_drawing)
         {
             return;
         }
