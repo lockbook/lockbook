1ac0e9cb349a96d5dfc1a6d8e865de561310f756
Smail Barkouch <smailbarkouch1@gmail.com>
2024-11-18 11:40:45 -0500

    got rid of whitespace

--- libs/content/workspace-ffi/SwiftWorkspace/Sources/Workspace/macMTK.swift
+++ libs/content/workspace-ffi/SwiftWorkspace/Sources/Workspace/macMTK.swift
@@ -75,7 +75,6 @@ public class MacMTK: MTKView, MTKViewDelegate {
                                       owner: self, userInfo: nil)
         self.addTrackingArea(trackingArea!)
     }
-    
 
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
