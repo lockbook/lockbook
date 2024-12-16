a99a4f235332f13978247fdb3ce6e9580b36629b
Smail Barkouch <smailbarkouch1@gmail.com>
2024-12-16 18:05:29 -0500

    android: debug info (#3191)

---
+++ clients/android/app/src/main/java/app/lockbook/screen/DebugInfoActivity.kt
@@ -0,0 +1,61 @@
+package app.lockbook.screen
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import app.lockbook.R
+import app.lockbook.databinding.ActivityDebugInfoBinding
+import app.lockbook.model.AlertModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import net.lockbook.Lb
+import net.lockbook.LbError
+import java.lang.ref.WeakReference
+
+class DebugInfoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDebugInfoBinding
+    private val alertModel by lazy {
+        AlertModel(WeakReference(this))
+    }
+
+    var debugInfo: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDebugInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.debugInfoToolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        binding.debugInfoToolbar.setOnMenuItemClickListener {
+            if (it.itemId == R.id.menu_debug_info_copy) {
+                val clipBoard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                clipBoard.setPrimaryClip(ClipData.newPlainText("lockbook debug info", debugInfo ?: ""))
+            }
+
+            true
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                val osInfo = "${Build.VERSION.RELEASE}.${Build.VERSION.SDK_INT}"
+                debugInfo = Lb.getDebugInfo(osInfo)
+                withContext(Dispatchers.Main) {
+                    binding.debugInfoText.text = debugInfo
+                    binding.debugInfoProgressBar.visibility = View.GONE
+                    binding.debugInfoText.visibility = View.VISIBLE
+                }
+            } catch (err: LbError) {
+                alertModel.notifyError(err)
+            }
+        }
+    }
+}
