e19fce12c0c4bb7e92b78fddcb9318902a1ea5f3
Parth <parth@mehrotra.me>
2024-12-23 16:48:50 -0500

    Update libs/lb/lb-rs/tests/cli_data_tests.rs

--- libs/lb/lb-rs/tests/cli_data_tests.rs
+++ libs/lb/lb-rs/tests/cli_data_tests.rs
@@ -1,7 +1,7 @@
 use lb_rs::{model::core_config::Config, Lb};
 
 #[tokio::test]
-// #[ignore]
+#[ignore]
 async fn pending_shares_perf() {
     let lb = Lb::init(Config::cli_config()).await.unwrap();
     for _ in 0..1000 {
