1897a38cc6411a92126d2030ee43a022180714c3
ad-tra <aew31gcmuy@gmail.com>
2025-01-03 12:18:11 -0500

    navbar and header

---
+++ public-site/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['./*.html'],
+    theme: {
+      colors: {
+        abyss: '#060E15',
+        egshell: '#F0EBE7',
+        surfline: '#28CCDF',
+        sundown: '#DF5F28',
+        dusk: '#BA28DF',
+      },
+      fontFamily: {
+        'sans': ['"Martian Mono"', 'serif'],
+      }
+    }
+  }
