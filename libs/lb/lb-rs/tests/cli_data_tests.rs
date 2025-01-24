108a758e05f03bc6951f943ec5e3aea6bebf6e4a
Parth <parth@mehrotra.me>
2025-01-23 23:10:00 -0500

    lb-rs: 50x time debug_info takes

--- libs/lb/lb-rs/tests/cli_data_tests.rs
+++ libs/lb/lb-rs/tests/cli_data_tests.rs
@@ -3,8 +3,16 @@ use lb_rs::{model::core_config::Config, Lb};
 #[tokio::test]
 #[ignore]
 async fn pending_shares_perf() {
-    let lb = Lb::init(Config::cli_config("test")).await.unwrap();
+    let lb = Lb::init(Config::cli_config("cli")).await.unwrap();
     for _ in 0..1000 {
         lb.get_pending_shares().await.unwrap();
+    }
+}
+
+#[tokio::test]
+async fn debug_info_test() {
+    let lb = Lb::init(Config::cli_config("cli")).await.unwrap();
+    for _ in 0..2 {
+        lb.debug_info("none".to_string()).await.unwrap();
     }
 }
