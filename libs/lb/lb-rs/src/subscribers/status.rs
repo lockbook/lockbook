5493e712a658e085100033d73bc64ad58b533a72
parth <parth@mehrotra.me>
2025-02-23 19:40:18 -0500

    sit and stare at the screen and think about things

--- libs/lb/lb-rs/src/subscribers/status.rs
+++ libs/lb/lb-rs/src/subscribers/status.rs
@@ -1,11 +1,30 @@
 use crate::Lb;
 
-pub enum Status {
-    Syncing,
-    Offline,
+pub struct Status {
+    pub offline: bool,
+    pub pending_shares: bool,
+    
+    pub local_status: Option<LocalStatus>,
+    pub sync_status: Option<SyncStatus>,
+
+    pub space_used: Option<UsageInfo>,
 }
 
+pub struct LocalStatus {}
+
+pub struct SyncStatus {}
+
+pub struct UsageInfo {}
+
 impl Lb {
-    fn status(&self) -> Vec<Status> {
+    fn status(&self) -> Status {
     }
 }
+
+// this is going to be cheap to ask for
+// we will eagerly compute this and have it ready
+// we will broadcast changes to these fields
+// we will consume other status updates and keep these fields up to date
+// some of these fields can invalidate one another
+// offline for example can invalidate the other statuses, and it's nice to
+// centrally manage that data dependency here
