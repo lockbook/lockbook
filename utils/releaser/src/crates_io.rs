100dbc1bff0608e0d8f6f46964ddbd63be6b1237
Kousay Jebir <144362888+Kousay-Jebir@users.noreply.github.com>
2025-04-06 20:01:47 +0100

    Update crates_io.rs

--- utils/releaser/src/crates_io.rs
+++ utils/releaser/src/crates_io.rs
@@ -6,11 +6,8 @@ use std::process::Command;
 
 pub fn release_crate(package: String) -> CliResult<()> {
     let api_token = CratesIO::env();
-    let root_path = fs::canonicalize("../../../")?;
-
     Command::new("cargo")
         .args(["publish", &format!("--token={}", api_token.0), "-p", &package])
-        .current_dir(root_path)
         .assert_success();
 
     Ok(())
