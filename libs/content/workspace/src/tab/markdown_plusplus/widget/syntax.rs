266215605c977cbb9038248c708b26ae6af490d3
Travis Vanderstad <t.vanderstad@gmail.com>
2025-04-13 02:49:22 -0400

    file I forgot to track

---
+++ libs/content/workspace/src/tab/markdown_plusplus/widget/syntax.rs
@@ -0,0 +1,12 @@
+use comrak::nodes::AstNode;
+use egui::TextFormat;
+
+use crate::tab::markdown_plusplus::MarkdownPlusPlus;
+
+impl<'ast> MarkdownPlusPlus {
+    pub fn text_format_syntax(&self, node: &'ast AstNode<'ast>) -> TextFormat {
+        let mut text_format = self.text_format(node);
+        text_format.color = self.theme.fg().accent_tertiary;
+        text_format
+    }
+}
