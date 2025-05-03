79ff16ba79a28eada5a5a09172deb2426ccc329f
Parth <parth@mehrotra.me>
2025-05-03 15:08:45 -0400

    linux: fix dark & light mode detection (#3528)
    fixes #3527

--- clients/egui/src/theme.rs
+++ clients/egui/src/theme.rs
@@ -9,7 +9,7 @@ use crate::settings::{Settings, ThemeMode};
 
 pub fn init(s: &Arc<RwLock<Settings>>, ctx: &egui::Context) {
     let initial_mode = match s.read().unwrap().theme_mode {
-        ThemeMode::System => dark_light::detect(),
+        ThemeMode::System => detect_theme_wrapper(),
         ThemeMode::Dark => dark_light::Mode::Dark,
         ThemeMode::Light => dark_light::Mode::Light,
     };
@@ -25,7 +25,7 @@ pub fn init(s: &Arc<RwLock<Settings>>, ctx: &egui::Context) {
 
 pub fn apply_settings(s: &Settings, ctx: &egui::Context) {
     let mode = match s.theme_mode {
-        ThemeMode::System => dark_light::detect(),
+        ThemeMode::System => detect_theme_wrapper(),
         ThemeMode::Dark => dark_light::Mode::Dark,
         ThemeMode::Light => dark_light::Mode::Light,
     };
@@ -43,21 +43,37 @@ fn poll_system_theme(
 
     thread::spawn(move || loop {
         if s.read().unwrap().theme_mode == ThemeMode::System {
-            let m = dark_light::detect();
-            if mode != m {
-                mode = m;
-                ctx.set_visuals(egui_visuals(m, s.read().unwrap().theme_color));
-                ctx.request_repaint();
+            match dark_light::detect() {
+                Ok(m) => {
+                    if mode != m {
+                        mode = m;
+                        ctx.set_visuals(egui_visuals(m, s.read().unwrap().theme_color));
+                        ctx.request_repaint();
+                    }
+                }
+                Err(e) => {
+                    eprintln!("Failed to detect current dark/light mode: {e:?}")
+                }
             }
         }
         thread::sleep(Duration::from_secs(1));
     });
 }
 
+fn detect_theme_wrapper() -> dark_light::Mode {
+    dark_light::detect().unwrap_or_else(|err| {
+        eprintln!("Failed to detect current dark/light mode: {err:?} (2)");
+        dark_light::Mode::Unspecified
+    })
+}
+
 pub fn egui_visuals(m: dark_light::Mode, primary: ColorAlias) -> egui::Visuals {
     match m {
-        dark_light::Mode::Default | dark_light::Mode::Dark => visuals::dark(primary),
-        dark_light::Mode::Light => visuals::light(primary),
+        // the default mode of operation is light because on gnome, by default, there is no
+        // light mode, it is either "Default" (which is presented to us as Unspecified) or
+        // dark. This "Default" mode is also illustrated as a mix of light and dark windows
+        dark_light::Mode::Unspecified | dark_light::Mode::Light => visuals::light(primary),
+        dark_light::Mode::Dark => visuals::dark(primary),
     }
 }
 
