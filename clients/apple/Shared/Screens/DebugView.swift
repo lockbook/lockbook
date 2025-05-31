0ce4b1c6a041ec0d06e912311af6c9e4645c18ed
Parth <parth@mehrotra.me>
2025-05-31 18:18:37 -0400

    apple: add a recalculate button for debug information (#3569)
    in service of #3463

--- clients/apple/Shared/Screens/DebugView.swift
+++ clients/apple/Shared/Screens/DebugView.swift
@@ -8,7 +8,7 @@ struct DebugView: View {
     
     var body: some View {
         VStack {
-            #if os(macOS)
+#if os(macOS)
             if let debugInfo {
                 HStack {
                     Text("Debug Info")
@@ -18,19 +18,21 @@ struct DebugView: View {
                     Button("Copy To Clipboard", action: {
                         ClipboardHelper.copyToClipboard(debugInfo)
                     })
+                    
+                    Button("Recalculate", action: calculateDebugInfo)
                 }
             }
-            #endif
+#endif
             
             if let debugInfo {
                 ScrollView {
                     Spacer()
-                    
+                    Button("Recalculate", action: calculateDebugInfo)
                     Text(debugInfo)
                         .monospaced()
                         .padding()
                         .textSelection(.enabled)
-                
+                    
                     Spacer()
                 }
             } else {
@@ -38,18 +40,22 @@ struct DebugView: View {
                 
                 ProgressView()
                     .onAppear {
-                        DispatchQueue.global(qos: .userInitiated).async {
-                            let debug = AppState.lb.debugInfo()
-                            DispatchQueue.main.async {
-                                debugInfo = debug
-                            }
-                        }
+                        calculateDebugInfo()
                     }
                 
                 Spacer()
             }
         }
         .navigationTitle("Debug Info")
+    }
+    
+    func calculateDebugInfo() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let debug = AppState.lb.debugInfo()
+            DispatchQueue.main.async {
+                self.debugInfo = debug
+            }
+        }
     }
 }
 
