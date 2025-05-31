a6a920b589bc444f0fa280d9f2bc71efe5ee8110
Parth <parth@mehrotra.me>
2025-05-31 18:04:23 -0400

    apple: add a recalculate button for debug information

--- clients/apple/Shared/Screens/DebugView.swift
+++ clients/apple/Shared/Screens/DebugView.swift
@@ -8,7 +8,7 @@ struct DebugView: View {
     
     var body: some View {
         VStack {
-            #if os(macOS)
+#if os(macOS)
             if let debugInfo {
                 HStack {
                     Text("Debug Info")
@@ -18,19 +18,21 @@ struct DebugView: View {
                     Button("Copy To Clipboard", action: {
                         ClipboardHelper.copyToClipboard(debugInfo)
                     })
+                    
+                    Button("Recalculate", action: calculateDebugInfo)
                 }
             }
-            #endif
+#endif
             
             if let debugInfo {
                 ScrollView {
                     Spacer()
-                    
+                    Button("Recalculate", action: calculateDebugInfo)
                     Text(debugInfo)
                         .monospaced()
                         .padding()
                         .textSelection(.enabled)
-                
+                    
                     Spacer()
                 }
             } else {
@@ -50,6 +52,15 @@ struct DebugView: View {
             }
         }
         .navigationTitle("Debug Info")
+    }
+    
+    func calculateDebugInfo() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let debug = AppState.lb.debugInfo()
+            DispatchQueue.main.async {
+                self.debugInfo = debug
+            }
+        }
     }
 }
 
