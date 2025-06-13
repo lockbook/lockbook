e5db93b0e22e69eecea26cdfd2d83725554c7951
parth <parth@mehrotra.me>
2025-06-13 15:11:33 -0400

    clippy

--- clients/egui/src/account/modals/mod.rs
+++ clients/egui/src/account/modals/mod.rs
@@ -17,7 +17,7 @@ pub use file_picker::FilePickerAction;
 pub use help::HelpModal;
 pub use new_file::{NewFileParams, NewFolderModal};
 pub use search::SearchModal;
-pub use settings::{SettingsModal, SettingsResponse};
+pub use settings::SettingsModal;
 
 use super::OpenModal;
 
