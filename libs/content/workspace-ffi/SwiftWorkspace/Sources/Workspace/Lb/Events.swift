b38d0cb7d61586180d04f550bdc699b424a0b940
Smail Barkouch <smailbarkouch1@gmail.com>
2025-06-28 14:47:18 -0400

    Started using status api

--- libs/content/workspace-ffi/SwiftWorkspace/Sources/Workspace/Lb/Events.swift
+++ libs/content/workspace-ffi/SwiftWorkspace/Sources/Workspace/Lb/Events.swift
@@ -1,7 +1,14 @@
 import Bridge
 import Foundation
 
+public class Events: ObservableObject {
+    @Published public var status: Status = Status()
+    @Published public var pendingShares: [UUID] = []
+    @Published public var metadataUpdated: Bool = false
+}
+
 public struct Status {
+    public var offline: Bool
     public var syncing: Bool
     public var outOfSpace: Bool
     public var pendingShares: Bool
@@ -13,18 +20,20 @@ public struct Status {
     public var syncStatus: String
     
     init(_ status: LbStatus) {
+        self.offline = status.offline
         self.syncing = status.syncing
         self.outOfSpace = status.out_of_space
         self.pendingShares = status.pending_shares
         self.updateRequired = status.update_required
-        self.dirtyLocally = Array(UnsafeBufferPointer(start: status.dirty_locally.ids, count: Int(status.pushing_files.len))).toUUIDs()
+        self.dirtyLocally = Array(UnsafeBufferPointer(start: status.dirty_locally.ids, count: Int(status.dirty_locally.len))).toUUIDs()
         self.pushingFiles = Array(UnsafeBufferPointer(start: status.pushing_files.ids, count: Int(status.pushing_files.len))).toUUIDs()
-        self.pullingFiles = Array(UnsafeBufferPointer(start: status.pulling_files.ids, count: Int(status.pushing_files.len))).toUUIDs()
+        self.pullingFiles = Array(UnsafeBufferPointer(start: status.pulling_files.ids, count: Int(status.pulling_files.len))).toUUIDs()
         self.spaceUsed = status.space_used != nil ? UsageMetrics(status.space_used.move()) : nil
         self.syncStatus = status.sync_status != nil ? String(cString: status.sync_status) : ""
     }
 
     init() {
+        self.offline = false
         self.syncing = false
         self.outOfSpace = false
         self.pendingShares = false
--- libs/lb/lb-c/src/lib.rs
+++ libs/lb/lb-c/src/lib.rs
@@ -963,6 +963,7 @@ pub struct LbIds {
 
 #[repr(C)]
 pub struct LbStatus {
+    pub offline: bool,
     pub syncing: bool,
     pub out_of_space: bool,
     pub pending_shares: bool,
@@ -1004,6 +1005,7 @@ pub extern "C" fn lb_get_status(lb: *mut Lb) -> LbStatus {
     };
 
     LbStatus {
+        offline: status.offline,
         syncing: status.syncing,
         out_of_space: status.out_of_space,
         pending_shares: status.pending_shares,
@@ -1020,6 +1022,8 @@ pub extern "C" fn lb_get_status(lb: *mut Lb) -> LbStatus {
 #[repr(C)]
 pub struct LbEvent {
     pub status_updated: bool,
+    pub metadata_updated: bool,
+    pub pending_shares_changed: bool
 }
 
 pub type LbNotify = extern "C" fn(*const c_void, LbEvent);
@@ -1033,10 +1037,17 @@ pub unsafe extern "C" fn lb_subscribe(lb: *mut Lb, notify_obj: *const c_void, no
     let notify_obj = Arc::new(AtomicPtr::new(notify_obj as *mut c_void));
 
     std::thread::spawn(move || loop {
-        if let Ok(Event::StatusUpdated) = rx.try_recv() {
+        if let Ok(event) = rx.try_recv() {
+            let event = match event {
+                Event::StatusUpdated => LbEvent { status_updated: true, ..Default::default() },
+                Event::MetadataChanged => LbEvent { metadata_updated: true, ..Default::default() },
+                Event::PendingSharesChanged => LbEvent { pending_shares_changed: true, ..Default::default() },
+                _ => continue
+            };
+
             notify(
                 notify_obj.load(std::sync::atomic::Ordering::Relaxed) as *const c_void,
-                LbEvent { status_updated: true },
+                event,
             );
         }
     });
