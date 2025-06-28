281ef1383821c0a9783045e488442337359b3d5a
Smail Barkouch <smailbarkouch1@gmail.com>
2025-06-28 15:40:32 -0400

    apple: mobile workspace landing page (#3595)

--- libs/content/workspace/src/show.rs
+++ libs/content/workspace/src/show.rs
@@ -34,7 +34,11 @@ impl Workspace {
         self.status.message = self.status_message();
 
         if self.is_empty() {
-            self.show_landing_page(ui);
+            if self.show_tabs {
+                self.show_landing_page(ui);
+            } else {
+                self.show_mobile_landing_page(ui);
+            }
         } else {
             ui.centered_and_justified(|ui| self.show_tabs(ui));
         }
@@ -67,7 +71,6 @@ impl Workspace {
         }
     }
 
-    #[cfg(target_vendor = "apple")]
     fn show_mobile_landing_page(&mut self, ui: &mut egui::Ui) {
         let punchout = if ui.visuals().dark_mode {
             include_image!("../punchout-dark.png")
