11cdee6c7dd5e633b1b615f0d13d197be64f9319
Arav <145138457+AravDesai@users.noreply.github.com>
2025-07-23 03:38:14 +0530

    Update to 1.88 + tokio things
    App wont crash when attempting to transcribe. Transcription does not seem like its working though

--- libs/content/workspace/src/audio/show.rs
+++ libs/content/workspace/src/audio/show.rs
@@ -1,6 +1,7 @@
 use egui::{Area, Frame, Id, Order, Pos2, Ui};
 use egui_player::{player::Player, InputMode};
 use lb_rs::Uuid;
+use tokio::runtime::Runtime;
 
 pub struct Audio {
     pub id: Uuid,
@@ -15,12 +16,15 @@ impl Audio {
         Audio { id, player }
     }
     pub fn show(&mut self, ui: &mut Ui) {
+        let rt = Runtime::new().unwrap();
         Area::new(Id::new(1))
             .order(Order::Background)
             .fixed_pos(Pos2 { x: ui.max_rect().min.x, y: 50.0 })
             .show(ui.ctx(), |ui| {
                 Frame::none().show(ui, |ui| {
-                    let _ = self.player.ui(ui);
+                    let _ = rt.block_on(async {
+                        let _ = self.player.ui(ui);
+                    });
                 });
             });
     }
