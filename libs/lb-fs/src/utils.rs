63678c77cad58f095bac83bb07cb46734634b8c3
Vaiz <vaizerd@gmail.com>
2025-08-03 19:10:32 +0200

    lb-fs: preload fattr3 in readdirplus (#3708)

--- libs/lb-fs/src/utils.rs
+++ libs/lb-fs/src/utils.rs
@@ -1,5 +1,11 @@
+use lb_rs::model::file::File;
 use nfs3_server::nfs3_types::nfs3::filename3;
 
 pub fn get_string(f: &filename3) -> String {
     String::from_utf8(f.as_ref().to_vec()).expect("Invalid UTF-8")
+}
+
+/// Should be the same as `UuidFileHandle::fileid`.
+pub fn file_id(f: &File) -> u64 {
+    f.id.as_u64_pair().0
 }
