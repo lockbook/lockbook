336ed07b647db5bdb8414e988e6e8e6678e21829
parth <parth@mehrotra.me>
2025-08-12 14:13:03 -0400

    infra: fix android releaser script

--- utils/lbdev/src/releaser/android/ws.rs
+++ utils/lbdev/src/releaser/android/ws.rs
@@ -1,11 +1,11 @@
 use cli_rs::cli_error::CliResult;
 
-use crate::utils::CommandRunner;
+use crate::{places::workspace_ffi, utils::CommandRunner};
 use std::process::Command;
 
 pub fn build() -> CliResult<()> {
     Command::new("bash")
         .args(["create_android_libs.sh"])
-        .current_dir("libs/content/workspace-ffi")
+        .current_dir(workspace_ffi())
         .assert_success()
 }
