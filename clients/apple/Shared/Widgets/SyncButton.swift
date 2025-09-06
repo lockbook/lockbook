7868a949b75c525e6737d29a1f0966d5bd4a5929
Smail Barkouch <smailbarkouch1@gmail.com>
2025-09-06 14:31:54 -0400

    apple: fix macOS progress spinner color (#3785)

--- clients/apple/Shared/Widgets/SyncButton.swift
+++ clients/apple/Shared/Widgets/SyncButton.swift
@@ -26,6 +26,8 @@ struct SyncButton: View {
                         .padding(.trailing, 1)
                         .modifier(SyncButtonProgressBarSize())
                         .tint(.white)
+                        // hack to get the dark mode version of progress spinner on macOS
+                        .colorScheme(.dark)
                         
                 }).padding(.vertical, 5)
             } else {
