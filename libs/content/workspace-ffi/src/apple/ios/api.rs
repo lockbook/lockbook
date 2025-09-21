173d6d650d1fea62f1a8458b23444fee0177d8c9
Arav <145138457+AravDesai@users.noreply.github.com>
2025-09-21 04:29:47 -0400

    Fixed typo

--- libs/content/workspace-ffi/src/apple/ios/api.rs
+++ libs/content/workspace-ffi/src/apple/ios/api.rs
@@ -831,7 +831,7 @@ pub unsafe extern "C" fn current_tab(obj: *mut c_void) -> i64 {
             ContentState::Open(tab) => match tab {
                 TabContent::Image(_) => 2,
                 TabContent::Markdown(_) => 3,
-                // TabContent::PlanText(_) => 4,
+                // TabContent::PlainText(_) => 4,
                 TabContent::Pdf(_) => 5,
                 TabContent::Svg(_) => 6,
                 TabContent::MindMap(_) => 7,
