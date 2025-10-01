b968f1446b6027cce43c8036f47931e892f4c5ce
parth <parth@mehrotra.me>
2025-10-01 16:48:09 -0400

    better debug info

--- libs/lb/lb-rs/src/service/debug.rs
+++ libs/lb/lb-rs/src/service/debug.rs
@@ -1,16 +1,14 @@
 use crate::model::clock;
 use crate::model::errors::LbResult;
-use crate::service::logging::LOG_FILE;
 use crate::{Lb, get_code_version};
 use basic_human_duration::ChronoHumanDuration;
 use chrono::NaiveDateTime;
 use serde::Serialize;
 use std::env;
-use std::io::SeekFrom;
-use std::path::{Path, PathBuf};
+use std::path::Path;
+use std::sync::atomic::Ordering;
 use time::Duration;
-use tokio::fs::{self, File};
-use tokio::io::{AsyncReadExt, AsyncSeekExt};
+use tokio::fs::{self};
 
 #[derive(Serialize)]
 pub struct DebugInfo {
@@ -23,33 +21,12 @@ pub struct DebugInfo {
     pub lb_dir: String,
     pub server_url: String,
     pub integrity: String,
-    pub log_tail: String,
-    pub last_panic: String,
+    pub is_syncing: bool,
+    pub status: String,
+    pub panics: Vec<String>,
 }
 
 impl Lb {
-    async fn tail_log(&self) -> LbResult<String> {
-        let mut path = PathBuf::from(&self.config.writeable_path);
-        if path.exists() {
-            path.push(LOG_FILE);
-            let mut file = File::open(path).await?;
-            let size = file.metadata().await?.len();
-            let read_amount = 5 * 1024;
-            let pos = size.saturating_sub(read_amount);
-
-            let mut buffer = Vec::with_capacity(read_amount as usize);
-            file.seek(SeekFrom::Start(pos)).await?;
-            file.read_to_end(&mut buffer).await?;
-            if self.config.colored_logs {
-                // strip colors
-                buffer = strip_ansi_escapes::strip(buffer);
-            }
-            Ok(String::from_utf8_lossy(&buffer).to_string())
-        } else {
-            Ok("NO LOGS FOUND".to_string())
-        }
-    }
-
     async fn human_last_synced(&self) -> String {
         let tx = self.ro_tx().await;
         let db = tx.db();
@@ -70,16 +47,15 @@ impl Lb {
         now.format("%Y-%m-%d %H:%M:%S %Z").to_string()
     }
 
-    async fn find_most_recent_panic_log(&self) -> LbResult<String> {
+    async fn collect_panics(&self) -> LbResult<Vec<String>> {
+        let mut panics = vec![];
+
         let dir_path = &self.config.writeable_path;
         let path = Path::new(dir_path);
 
         let prefix = "panic---";
         let suffix = ".log";
         let timestamp_format = "%Y-%m-%d---%H-%M-%S";
-
-        let mut most_recent_file: Option<String> = None;
-        let mut most_recent_time: Option<NaiveDateTime> = None;
 
         let mut entries = fs::read_dir(path).await?;
         while let Some(entry) = entries.next_entry().await? {
@@ -94,31 +70,14 @@ impl Lb {
                 if let Ok(timestamp) =
                     NaiveDateTime::parse_from_str(timestamp_str, timestamp_format)
                 {
-                    // Compare to find the most recent timestamp
-                    match most_recent_time {
-                        Some(ref current_most_recent) => {
-                            if timestamp > *current_most_recent {
-                                most_recent_time = Some(timestamp);
-                                most_recent_file = Some(file_name.clone());
-                            }
-                        }
-                        None => {
-                            most_recent_time = Some(timestamp);
-                            most_recent_file = Some(file_name.clone());
-                        }
-                    }
+                    let file_path = path.join(file_name);
+                    let contents = fs::read_to_string(file_path).await?;
+                    let contents = format!("time: {timestamp}: contents: {contents}");
+                    panics.push(contents);
                 }
             }
         }
-
-        // If we found the most recent file, read its contents
-        if let Some(file_name) = most_recent_file {
-            let file_path = path.join(file_name);
-            let contents = fs::read_to_string(file_path).await?;
-            Ok(contents)
-        } else {
-            Ok(String::default())
-        }
+        Ok(panics)
     }
 
     #[instrument(level = "debug", skip(self), err(Debug))]
@@ -129,12 +88,14 @@ impl Lb {
         let os = env::consts::OS;
         let family = env::consts::FAMILY;
 
-        let (integrity, log_tail, last_synced, last_panic) = tokio::join!(
+        let (integrity, last_synced, panics) = tokio::join!(
             self.test_repo_integrity(),
-            self.tail_log(),
             self.human_last_synced(),
-            self.find_most_recent_panic_log()
+            self.collect_panics()
         );
+
+        let status = format!("{:?}", self.status().await);
+        let is_syncing = self.syncing.load(Ordering::Relaxed);
 
         Ok(serde_json::to_string_pretty(&DebugInfo {
             time: self.now(),
@@ -143,11 +104,12 @@ impl Lb {
             rust_triple: format!("{arch}.{family}.{os}"),
             server_url: account.api_url.clone(),
             integrity: format!("{integrity:?}"),
-            log_tail: log_tail?,
             lb_dir: self.config.writeable_path.clone(),
             last_synced,
             os_info,
-            last_panic: last_panic?,
+            status,
+            is_syncing,
+            panics: panics?,
         })?)
     }
 }
