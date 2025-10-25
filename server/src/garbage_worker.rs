ea1a322403dc8eecc68dca019a44d68fcb154571
parth <parth@mehrotra.me>
2025-10-25 15:26:33 -0400

    tweak and finalize

--- server/src/garbage_worker.rs
+++ server/src/garbage_worker.rs
@@ -1,5 +1,5 @@
 use std::time::Duration;
-use tracing::{error, info};
+use tracing::{debug, error, info};
 
 use lb_rs::model::clock::get_time;
 
@@ -24,25 +24,41 @@ where
 
         tokio::spawn(async move {
             loop {
+                tokio::time::sleep(Duration::from_secs(60)).await;
                 info!("garbage collecting");
                 bg_self.garbage_collect().await;
-                tokio::time::sleep(Duration::from_secs(60)).await;
+                info!("garbage collected");
             }
         });
     }
 
     pub async fn garbage_collect(&self) {
-        let db = self.index_db.lock().await;
+        let mut db = self.index_db.lock().await;
         let files = db.scheduled_file_cleanups.get();
+        let mut cleaned = 0;
+        let mut skipped = 0;
+        let mut remove = vec![];
         for ((id, hmac), time) in files {
-            if get_time().0 - time > 1000 * 60 * 10 {
+            if get_time().0 - time > 1000 * 60 * 5 {
+                cleaned += 1;
                 if let Err(e) = self.document_service.delete::<()>(id, hmac).await {
                     error!(
                         "failed to garbage collect {:?} {e:?}",
                         self.document_service.get_path(id, hmac)
                     );
+                } else {
+                    remove.push((*id, *hmac));
                 }
+                debug!("garbage collected: {:?}", self.document_service.get_path(id, hmac));
+            } else {
+                skipped += 1;
             }
         }
+
+        for (id, hmac) in remove {
+            db.scheduled_file_cleanups.remove(&(id, hmac)).unwrap();
+        }
+
+        info!("cleaned {cleaned}, skipped {skipped}");
     }
 }
