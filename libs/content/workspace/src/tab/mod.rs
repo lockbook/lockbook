c92989362baa403ebecb00f6514d69ea3e837fc1
parth <parth@mehrotra.me>
2025-11-04 15:40:01 -0500

    ws: handle spaces in paths

--- libs/content/workspace/src/tab/mod.rs
+++ libs/content/workspace/src/tab/mod.rs
@@ -17,6 +17,7 @@ use lb_rs::model::errors::{LbErr, LbErrKind};
 use lb_rs::model::file::File;
 use lb_rs::model::file_metadata::{DocumentHmac, FileType};
 use lb_rs::model::svg;
+use urlencoding::decode;
 use std::ops::IndexMut;
 use std::path::{Component, Path, PathBuf};
 use std::time::{Instant, SystemTime, UNIX_EPOCH};
@@ -570,6 +571,8 @@ pub fn core_get_by_relative_path<P: AsRef<Path>>(
 
     #[cfg(windows)]
     let target_path = target_path.replace('\\', "/");
+
+    let target_path = decode(&target_path).map(|cow| cow.to_string()).unwrap_or(target_path);
 
     core.get_by_path(&target_path).map_err(|e| e.to_string())
 }
