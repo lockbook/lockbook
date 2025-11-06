712f3029d58cd03f30ba26c13a0b8e1c63a805b5
Parth <parth@mehrotra.me>
2025-11-06 17:29:45 -0500

    egui: prettier icons (#3932)

--- clients/egui/src/theme.rs
+++ clients/egui/src/theme.rs
@@ -78,16 +78,3 @@ pub fn egui_visuals(m: dark_light::Mode, primary: ColorAlias) -> egui::Visuals {
         dark_light::Mode::Dark => visuals::dark(primary),
     }
 }
-
-pub fn register_fonts(fonts: &mut egui::FontDefinitions) {
-    let mut font = egui::FontData::from_static(lb_fonts::MATERIAL_SYMBOLS_OUTLINED);
-    font.tweak.y_offset_factor = -0.1;
-
-    fonts.font_data.insert("material_icons".to_owned(), font);
-
-    fonts
-        .families
-        .get_mut(&egui::FontFamily::Monospace)
-        .unwrap()
-        .push("material_icons".to_owned());
-}
