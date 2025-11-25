6a14f5902b599624d5712cea73dbca06a611ba4e
Travis Vanderstad <t.vanderstad@gmail.com>
2025-11-25 13:58:26 -0500

    ios: fix broken pencil interactions (#3976)

--- libs/content/workspace-ffi/SwiftWorkspace/Sources/Workspace/iOSMTK.swift
+++ libs/content/workspace-ffi/SwiftWorkspace/Sources/Workspace/iOSMTK.swift
@@ -826,7 +826,7 @@ public class SvgView: UIView {
         let pencilDelegate = SvgPencilDelegate(mtkView: mtkView)
         let pencilInteraction = UIPencilInteraction()
 
-        pencilInteraction.delegate = self.pencilDelegate
+        pencilInteraction.delegate = pencilDelegate
         addInteraction(pencilInteraction)
 
         self.pencilDelegate = pencilDelegate
