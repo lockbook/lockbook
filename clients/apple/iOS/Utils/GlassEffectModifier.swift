2c6df3543e7b847b572c368ad356eea751171a78
Smail Barkouch <smailbarkouch1@gmail.com>
2026-01-02 18:59:12 -0500

    cleanup

--- clients/apple/iOS/Utils/GlassEffectModifier.swift
+++ clients/apple/iOS/Utils/GlassEffectModifier.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct GlassEffectModifier: ViewModifier {
     let radius: CGFloat = 20
-    
+
     func body(content: Content) -> some View {
         if #available(iOS 26.0, *) {
             content
