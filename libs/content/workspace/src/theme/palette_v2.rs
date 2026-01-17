9271a94314c7c953280276947e334a2697ca31cd
parth <parth@mehrotra.me>
2026-01-17 14:43:51 -0500

    progress

--- libs/content/workspace/src/theme/palette_v2.rs
+++ libs/content/workspace/src/theme/palette_v2.rs
@@ -1,12 +1,14 @@
-use egui::Color32;
+use egui::{Color32, Id, style};
 use epaint::hex_color;
 
+#[derive(Clone, Copy)]
 pub struct Theme {
     current: Mode,
     light: ThemeVariant,
     dark: ThemeVariant,
 }
 
+#[derive(Clone, Copy)]
 pub struct ThemeVariant {
     pub black: Color32,
     pub red: Color32,
@@ -58,6 +60,7 @@ impl ThemeVariant {
     }
 }
 
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub enum Mode {
     Light,
     Dark,
@@ -118,5 +121,108 @@ impl Theme {
                 quaternary: Palette::Yellow,
             },
         }
+    }
+}
+
+pub trait ThemeExt {
+    fn set_theme(&self, t: Theme);
+    fn get_theme(&self) -> Theme;
+}
+
+impl ThemeExt for egui::Context {
+    fn set_theme(&self, t: Theme) {
+        self.memory_mut(|m| m.data.insert_temp(Id::new("theme"), t));
+        self.set_visuals(t.base_visuals());
+    }
+
+    fn get_theme(&self) -> Theme {
+        self.memory_mut(|m| m.data.get_temp(Id::new("theme")))
+            .unwrap()
+    }
+}
+
+impl Theme {
+    fn base_visuals(&self) -> egui::Visuals {
+        egui::Visuals {
+            dark_mode: self.current == Mode::Dark,
+            override_text_color: None,
+            window_fill: self.bg().black,
+            extreme_bg_color: self.bg().black,
+            ..Default::default()
+            // widgets: style::Widgets {
+            //     noninteractive: style::WidgetVisuals {
+            //         bg_fill: (),
+            //         weak_bg_fill: (),
+            //         bg_stroke: (),
+            //         rounding: (),
+            //         fg_stroke: (),
+            //         expansion: (),
+            //     },
+            //     inactive: (),
+            //     hovered: (),
+            //     active: (),
+            //     open: (),
+            // },
+            // selection: (),
+            // hyperlink_color: (),
+            // faint_bg_color: (),
+            // extreme_bg_color: (),
+            // code_bg_color: (),
+            // warn_fg_color: (),
+            // error_fg_color: (),
+            // window_rounding: (),
+            // window_shadow: (),
+            // window_fill: (),
+            // window_stroke: (),
+            // window_highlight_topmost: (),
+            // menu_rounding: (),
+            // panel_fill: (),
+            // popup_shadow: (),
+            // resize_corner_size: (),
+            // text_cursor: (),
+            // clip_rect_margin: (),
+            // button_frame: (),
+            // collapsing_header_frame: (),
+            // indent_has_left_vline: (),
+            // striped: (),
+            // slider_trailing_fill: (),
+            // handle_shape: (),
+            // interact_cursor: (),
+            // image_loading_spinners: (),
+            // numeric_color_space: (),
+        }
+    }
+
+    pub fn dark(&self) -> egui::Visuals {
+        let mut v = egui::Visuals::dark();
+        let is_mobile = cfg!(target_os = "ios") || cfg!(target_os = "android");
+
+        if is_mobile {
+            v.window_fill = Color32::from_rgb(0, 0, 0);
+            v.extreme_bg_color = Color32::from_rgb(0, 0, 0);
+        } else {
+            v.window_fill = Color32::from_rgb(20, 20, 20);
+            v.extreme_bg_color = Color32::from_rgb(20, 20, 20);
+        }
+
+        v.faint_bg_color = Color32::from_rgb(35, 35, 37);
+        v.widgets.noninteractive.bg_fill = Color32::from_rgb(25, 25, 27);
+        v.widgets.noninteractive.fg_stroke.color = Color32::from_rgb(242, 242, 247);
+        v.widgets.inactive.fg_stroke.color = Color32::from_rgb(242, 242, 247);
+        // v.widgets.active.bg_fill = ThemePalette::DARK[primary];
+
+        v.widgets.hovered.bg_fill = v.code_bg_color.linear_multiply(0.1);
+
+        v
+    }
+
+    pub fn light(&self) -> egui::Visuals {
+        let mut v = egui::Visuals::light();
+        v.window_fill = Color32::from_rgb(255, 255, 255);
+        v.extreme_bg_color = Color32::from_rgb(255, 255, 255);
+        // v.widgets.active.bg_fill = ThemePalette::LIGHT[primary];
+        v.widgets.hovered.bg_fill = v.code_bg_color.linear_multiply(0.9);
+
+        v
     }
 }
