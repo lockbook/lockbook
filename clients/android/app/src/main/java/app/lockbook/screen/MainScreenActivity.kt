95c54218a3183066f2e2511fea0f951bc6efa73e
Adam Trabelsi <66345861+ad-tra@users.noreply.github.com>
2026-01-24 21:24:43 +0100

    android: fix bttom view visibility state on soft starts (#4125)

--- clients/android/app/src/main/java/app/lockbook/screen/MainScreenActivity.kt
+++ clients/android/app/src/main/java/app/lockbook/screen/MainScreenActivity.kt
@@ -233,6 +233,17 @@ class MainScreenActivity : AppCompatActivity(), BottomNavProvider {
             }
         )
 
+        // let's set the default state of the bottom navigation. there are cases where the
+        // sliding pane will be open on startup for example if the activity was killed
+        // due to memory pressure. toggle "don't keep activities" to reproduce
+        slidingPaneLayout.post {
+            if (slidingPaneLayout.isOpen) {
+                binding.bottomNavigation.visibility = View.GONE
+            } else {
+                binding.bottomNavigation.visibility = View.VISIBLE
+            }
+        }
+
         slidingPaneLayout.addPanelSlideListener(object : SlidingPaneLayout.SimplePanelSlideListener() {
             override fun onPanelOpened(panel: View) {
                 window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
