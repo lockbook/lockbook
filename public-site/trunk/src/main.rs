fc814ff3d96e6a3968451847d34491cc5b5f3bb0
Adam Trabelsi <66345861+ad-tra@users.noreply.github.com>
2026-02-20 05:01:11 +0100

    public-site: unbreak build caused by egui version upgrade (#4235)

--- public-site/trunk/src/main.rs
+++ public-site/trunk/src/main.rs
@@ -5,11 +5,13 @@ use public_site::InitialScreen;
 #[cfg(target_arch = "wasm32")]
 use public_site::LbWebApp;
 
+#[cfg(target_arch = "wasm32")]
+use eframe::wasm_bindgen::JsCast;
+
 // When compiling to web using trunk:
 #[cfg(target_arch = "wasm32")]
 fn main() {
     // Redirect `log` message to `console.log` and friends:
-
     eframe::WebLogger::init(log::LevelFilter::Debug).ok();
 
     let layer = tracing_wasm::WASMLayerConfigBuilder::new()
@@ -18,9 +20,11 @@ fn main() {
     tracing_wasm::set_as_global_default_with_config(layer);
 
     wasm_bindgen_futures::spawn_local(async {
+        let editor_demo_el = get_canvas_element("editor-demo");
+
         let start_result = eframe::WebRunner::new()
             .start(
-                "editor-demo",
+                editor_demo_el,
                 Default::default(),
                 Box::new(|cc| Ok(Box::new(LbWebApp::new(cc, InitialScreen::Editor)))),
             )
@@ -43,9 +47,11 @@ fn main() {
             }
         }
 
+        let canvas_demo_el = get_canvas_element("canvas-demo");
+
         let start_result = eframe::WebRunner::new()
             .start(
-                "canvas_demo",
+                canvas_demo_el,
                 Default::default(),
                 Box::new(|cc| Ok(Box::new(LbWebApp::new(cc, InitialScreen::Canvas)))),
             )
@@ -69,6 +75,16 @@ fn main() {
             }
         }
     });
+}
+
+#[cfg(target_arch = "wasm32")]
+fn get_canvas_element(id: &str) -> web_sys::HtmlCanvasElement {
+    web_sys::window()
+        .and_then(|w| w.document())
+        .and_then(|d| d.get_element_by_id(id))
+        .expect(&format!("{} element not found", id))
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .expect(&format!("{} element is not a HtmlCanvasElement", id))
 }
 
 #[cfg(not(target_arch = "wasm32"))]
