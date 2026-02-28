7a757d8471d17a4b0b8b0694253f5e9c669ee08d
Amumug <fabriali4@gmail.com>
2026-02-28 18:22:50 +0000

    add new lbdev command

--- utils/lbdev/src/main.rs
+++ utils/lbdev/src/main.rs
@@ -85,7 +85,8 @@ fn main() {
                                 .subcommand(Command::name("gh").handler(releaser::linux::desktop::upload_gh))
                                 .subcommand(Command::name("deb").handler(releaser::linux::desktop::upload_deb_gh))
                                 .subcommand(Command::name("snap").handler(releaser::linux::desktop::update_snap))
-                                .subcommand(Command::name("aur").handler(releaser::linux::desktop::update_aur)),
+                                .subcommand(Command::name("aur").handler(releaser::linux::desktop::update_aur))
+                                .subcommand(Command::name("flatpak")),
                         )
                 )
                 .subcommand(
