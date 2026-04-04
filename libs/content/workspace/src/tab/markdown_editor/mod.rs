7508e65ea28d65783fa8e8cd518c2401be3df598
Travis Vanderstad <t.vanderstad@gmail.com>
2026-04-04 00:26:28 -0400

    incremental height cache

--- libs/content/workspace/src/tab/markdown_editor/mod.rs
+++ libs/content/workspace/src/tab/markdown_editor/mod.rs
@@ -479,7 +479,8 @@ impl Editor {
             root = comrak::parse_document(&arena, &text_with_newline, &options);
 
             self.bounds.inline_paragraphs.clear();
-            self.layout_cache.invalidate_text_change();
+            self.layout_cache
+                .invalidate_text_change(&self.buffer, buffer_resp.seq_before);
 
             self.calc_source_lines();
             self.compute_bounds(root);
