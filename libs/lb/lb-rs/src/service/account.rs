1d18127e2abcb649768ab7b42a67f3bd88a9d17c
Travis Vanderstad <t.vanderstad@gmail.com>
2026-04-07 19:43:32 -0400

    ws: welcome doc fix

--- libs/lb/lb-rs/src/service/account.rs
+++ libs/lb/lb-rs/src/service/account.rs
@@ -235,9 +235,9 @@ To make text into a link, wrap it with `[` `]`, add a link destination to the en
 ## Images
 To embed an image, add a `!` to the beginning of the link syntax.
 ```md
-![Lockbook's logo](raw.githubusercontent.com/lockbook/lockbook/master/docs/graphics/logo.svg)
+![Lockbook's logo](https://raw.githubusercontent.com/lockbook/lockbook/master/docs/graphics/logo.svg)
 ```
-> ![Lockbook's logo](raw.githubusercontent.com/lockbook/lockbook/master/docs/graphics/logo.svg)
+> ![Lockbook's logo](https://raw.githubusercontent.com/lockbook/lockbook/master/docs/graphics/logo.svg)
 
 ## Headings
 To create a heading, add up to six `#`'s plus a space before your text. More `#`'s create a smaller heading.
