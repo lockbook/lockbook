e9a20a19f8a917f799fc73ce9bead16442d435f2
Travis Vanderstad <t.vanderstad@gmail.com>
2026-05-20 20:34:46 -0400

    md: reinstate mobile link buttons (#4593)

--- libs/content/workspace/src/tab/markdown_editor/widget/inline/wiki_link.rs
+++ libs/content/workspace/src/tab/markdown_editor/widget/inline/wiki_link.rs
@@ -1,9 +1,10 @@
 use comrak::nodes::AstNode;
 use lb_rs::Uuid;
-use lb_rs::model::text::offset_types::Grapheme;
+use lb_rs::model::text::offset_types::{Grapheme, RangeExt as _};
 
 use crate::tab::markdown_editor::MdRender;
 use crate::tab::markdown_editor::widget::utils::wrap_layout::Layout;
+use crate::theme::icons::Icon;
 
 impl<'ast> MdRender {
     pub fn resolve_wikilink(&self, url: &str) -> Option<Uuid> {
@@ -17,14 +18,29 @@ impl<'ast> MdRender {
             comrak::nodes::NodeValue::WikiLink(nwl) => nwl.url.clone(),
             _ => String::new(),
         };
-        let fmt = self.text_format_link(node.parent().unwrap(), self.link_state_for_wikilink(&url));
+        let parent = node.parent().unwrap();
+        let node_range = self.node_range(node);
+        let fmt = self.text_format_link(parent, self.link_state_for_wikilink(&url));
         let cmd = self.ctx.input(|i| i.modifiers.command);
+        let salt = Self::link_interaction_id_salt(node_range);
         if cmd {
-            let salt = Self::link_interaction_id_salt(self.node_range(node));
             layout.interaction_open(salt, egui::Sense::click());
         }
         self.layout_circumfix(layout, node, range, fmt);
         if cmd {
+            layout.interaction_close();
+        }
+
+        if self.touch_mode && range.contains_inclusive(node_range.end()) {
+            let anchor = (node_range.end(), node_range.end());
+            let parent_fmt = self.text_format(parent);
+            layout.push_override(anchor, " ", parent_fmt);
+            layout.interaction_open(salt, egui::Sense::click());
+            layout.push_override(
+                anchor,
+                Icon::OPEN_IN_NEW.icon,
+                self.text_format_link_button(parent),
+            );
             layout.interaction_close();
         }
     }
