a57aeb22bfc42951a76ea20a06147c4da61d6552
ad-tra <aew31gcmuy@gmail.com>
2026-06-10 17:13:15 -0400

    egui: don't exapnd sharer section on a new file creation

--- libs/content/workspace/src/workspace.rs
+++ libs/content/workspace/src/workspace.rs
@@ -1019,6 +1019,13 @@ impl Workspace {
             .create_file(new_file.to_name().as_str(), &parent, FileType::Document)
             .map_err(|err| format!("{err:?}"));
 
+        if let Ok(file) = &result {
+            self.files
+                .write()
+                .unwrap()
+                .insert_created_file(file.clone());
+            self.out.file_cache_updated = true;
+        }
         self.out.file_created = Some(result);
         self.ctx.request_repaint();
     }
@@ -1034,6 +1041,13 @@ impl Workspace {
             .create_file(new_file.to_name().as_str(), &parent, FileType::Folder)
             .map_err(|err| format!("{err:?}"));
 
+        if let Ok(file) = &result {
+            self.files
+                .write()
+                .unwrap()
+                .insert_created_file(file.clone());
+            self.out.file_cache_updated = true;
+        }
         self.out.file_created = Some(result);
         self.ctx.request_repaint();
     }
