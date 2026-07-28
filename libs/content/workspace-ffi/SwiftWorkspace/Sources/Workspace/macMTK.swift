e7d3b4c3a895772d6ae271f2ec47c7ffe4bf6c36
Travis Vanderstad <t.vanderstad@gmail.com>
2026-07-28 14:08:42 -0400

    macos: scroll without focus

--- libs/content/workspace-ffi/SwiftWorkspace/Sources/Workspace/macMTK.swift
+++ libs/content/workspace-ffi/SwiftWorkspace/Sources/Workspace/macMTK.swift
@@ -52,7 +52,7 @@
                 removeTrackingArea(trackingArea!)
             }
             let options: NSTrackingArea.Options =
-                [.mouseEnteredAndExited, .mouseMoved, .enabledDuringMouseDrag, .activeInKeyWindow]
+                [.mouseEnteredAndExited, .mouseMoved, .enabledDuringMouseDrag, .activeAlways]
             trackingArea = NSTrackingArea(rect: bounds, options: options,
                                           owner: self, userInfo: nil)
             addTrackingArea(trackingArea!)
@@ -181,20 +181,12 @@
         }
 
         override public func mouseDragged(with event: NSEvent) {
-            if window?.firstResponder != self {
-                return
-            }
-
             let local = viewCoordinates(event)
             mouse_moved(wsHandle, Float(local.x), Float(local.y))
             setNeedsDisplay(frame)
         }
 
         override public func mouseMoved(with event: NSEvent) {
-            if window?.firstResponder != self {
-                return
-            }
-
             let local = viewCoordinates(event)
             mouse_moved(wsHandle, Float(local.x), Float(local.y))
             setNeedsDisplay(frame)
